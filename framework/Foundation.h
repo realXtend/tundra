8f9c962d902ee5c5274ce03421ea1cb5b355570f
cmayhem <cmayhem@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-02-27 12:59:47 +0000

    Added a bit of documentation.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@13 5b2332b8-efa3-11de-8684-7d64432d61a3

--- framework/Foundation.h
+++ framework/Foundation.h
@@ -13,6 +13,30 @@
 #include "Entity.h"
 #include "ServiceManager.h"
 
+//! Foundation is the main helper in your quest to create new or improved functionality for the realXtend viewer.
+/*! To use all the services offered by the foundation, simply include "Foundation.h" in your own module.
+
+
+    - ModuleManager is there to help you create new pluggable modules for the
+    viewer. It works mostly transparently so when you create a new module you
+    don't need to worry about it, it does most of the worrying for you!
+
+    For creating new modules, see ModuleManager.
+
+
+    - ComponentManager handles extending scene objects with new functionality.
+    When you create a new component for an entity, ComponentManager makes sure
+    the component is usable by any module in the viewer, including the ones
+    you create yourself.
+
+    For creating new components, see ComponentManager.
+
+
+    - Framework class is the entry point for the viewer. It contains the main
+    loop and is responsible for all the other parts of the foundation.
+*/
+namespace Foundation {}
+
 #endif
 
 
