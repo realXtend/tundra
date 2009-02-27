3240808197fa33a8f36893ec086fbdb89f8d7262
cmayhem <cmayhem@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-02-27 14:47:40 +0000

    Added unit testing framework (boost).
    Add unit tests to UnitTests project using boost UnitTestingFramework conventions.
    Use UnitTests.exe testrunner to run the unit tests.
    
    Added Entity::getComponent() for testing purposes.
    
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@19 5b2332b8-efa3-11de-8684-7d64432d61a3

--- TestSystem/TestSystem.cpp
+++ TestSystem/TestSystem.cpp
@@ -1,12 +1,17 @@
 // For conditions of distribution and use, see copyright notice in license.txt
 
 #include "StableHeaders.h"
+
+
+
 #include "TestSystem.h"
 #include "EC_Dummy.h"
 #include "EC_Geometry.h"
 #include "EC_OgreEntity.h"
 #include <Poco/ClassLibrary.h>
 #include "Foundation.h"
+
+
 
 namespace Test
 {
@@ -54,53 +59,18 @@ namespace Test
     void TestSystem::update()
     {
         // create new entity
-        LOG(Test::EC_Dummy::name());
+        LOG("Constructing entity with component: " + Test::EC_Dummy::name() + ".");
 
-        //// One way to construct an entity
-        ////
-        //Core::StringVector components;
-        //components.push_back(Geometry::EC_Dummy::name());
-        //components.push_back(Geometry::EC_Geometry::name());
-        //components.push_back(OgreRenderer::EC_OgreEntity::name());
-        //Foundation::EntityPtr entity = mFramework->getEntityManager()->createEntity(components);
-
-        // alternative way to construct an entity
-        //
         Foundation::EntityPtr entity = mFramework->getEntityManager()->createEntity();
         entity->addEntityComponent(mFramework->getComponentManager()->createComponent(Test::EC_Dummy::name()));
-        entity->addEntityComponent(mFramework->getComponentManager()->createComponent(Geometry::EC_Geometry::name()));
-
-        entity = mFramework->getEntityManager()->createEntity();
-        entity->addEntityComponent(mFramework->getComponentManager()->createComponent(Test::EC_Dummy::name()));
-        entity->addEntityComponent(mFramework->getComponentManager()->createComponent(Geometry::EC_Geometry::name()));
-        entity->addEntityComponent(mFramework->getComponentManager()->createComponent(OgreRenderer::EC_OgreEntity::name()));
-
-
-        //// move pre-existing entities
-        ////
-        //// Case where packed data is predecoded
-        ////
-        //Core::entity_id_t id = entity->getId();
-        //Foundation::Change change = mFramework->getChangeManager()->getGlobalChange(Geometry::EC_Geometry::name(), Geometry::EC_Geometry::Position);
-        //Core::Vector3 newPos(1001);
-
-        //mFramework->getChangeManager()->change(id, change, boost::any(newPos));
-        //LOG("Testing sending new position: " + boost::lexical_cast<std::string>(newPos) + " for entity id: " + boost::lexical_cast<std::string>(id));
-
-        //// Case where component does it's own decoding on the packet data
-        ////
-        //Core::entity_id_t id = entity->getId();
-        //Foundation::ComponentInterface *component = mFramework->getComponentManager()->createComponent(packedData); // returns new component based on packed data
-        //component->parsePackedData(packedData);
-        //Foundation::Change change = mFramework->getChangeManager()->getGlobalChange(Geometry::EC_Geometry::name(), Geometry::EC_Geometry::All); 
-        //mFramework->getChangeManager()->change(id, change, boost::any(component));
     }
 }
 
 using namespace Test;
-    POCO_BEGIN_MANIFEST(Foundation::ModuleInterface)
-       POCO_EXPORT_CLASS(TestSystem)
-    POCO_END_MANIFEST
 
-//}
+POCO_BEGIN_MANIFEST(Foundation::ModuleInterface)
+   POCO_EXPORT_CLASS(TestSystem)
+POCO_END_MANIFEST
+
+
 
