e961bba921d067f191333872da917eade8dada91
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-03-04 10:06:59 +0000

    Removed implementation from pure virtual functions.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@47 5b2332b8-efa3-11de-8684-7d64432d61a3

--- Interfaces/ComponentRegistrarInterface.h
+++ Interfaces/ComponentRegistrarInterface.h
@@ -13,9 +13,9 @@ namespace Foundation
         ComponentRegistrarInterface()  {}
         virtual ~ComponentRegistrarInterface() {}
         
-        virtual void _register(Framework *framework) = 0 {}
+        virtual void _register(Framework *framework) = 0;
 
-        virtual void _unregister(Framework *framework) = 0 {}
+        virtual void _unregister(Framework *framework) = 0;
     };
 
     typedef boost::shared_ptr<ComponentRegistrarInterface> ComponentRegistrarInterfacePtr;
