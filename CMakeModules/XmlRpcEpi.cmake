a8ade695309051f4d8d174a1153abb731240ee87
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-03-09 13:48:28 +0000

    Added a directory for cmake modules, used currently for finding/setting external library include & link directories. Modified CMakeLists in each subproject to use the modules.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@92 5b2332b8-efa3-11de-8684-7d64432d61a3

---
+++ CMakeModules/XmlRpcEpi.cmake
@@ -0,0 +1,24 @@
+# Find, include and link xmlrpc-epi
+# Find is already called in the top-level CMakeLists
+
+macro (FIND_XMLRPCEPI)
+endmacro (FIND_XMLRPCEPI)
+
+macro (INCLUDE_XMLRPCEPI)
+	if (MSVC)
+		include_directories (${PROJECT_SOURCE_DIR}/external_libs/xmlrpc-epi/src)
+
+		link_directories (
+			${PROJECT_SOURCE_DIR}/external_libs/xmlrpc-epi/
+		)
+	endif (MSVC)
+endmacro (INCLUDE_XMLRPCEPI)
+
+macro (LINK_XMLRPCEPI)
+	if (MSVC)
+		target_link_libraries (${TARGET_NAME}
+			debug xmlrpcepid
+			optimized xmlrpcepi
+		)
+	endif (MSVC)
+endmacro (LINK_XMLRPCEPI)
