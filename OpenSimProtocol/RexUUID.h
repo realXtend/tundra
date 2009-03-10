18da1659e7d56a694910c956d1173cafc9f54668
jaakkoallander <jaakkoallander@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-03-10 14:40:19 +0000

    UUID.h & .cpp renamed to RexUUID.h & .cpp.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@111 5b2332b8-efa3-11de-8684-7d64432d61a3

---
+++ OpenSimProtocol/RexUUID.h
@@ -0,0 +1,33 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+#ifndef incl_RexUUID_h
+#define incl_RexUUID_h
+
+#include "RexTypes.h"
+
+/// RexUUID is a 16-byte identifier for resources in a virtual world.
+class RexUUID
+{
+public:
+	/// Constructs an RexUUID from a string in form "1c1bbda2-304b-4cbf-ba3f-75324b044c73" or "1c1bbda2304b4cbfba3f75324b044c73".
+	explicit RexUUID(const char *str);
+	explicit RexUUID(const std::string &str);
+	/// Constructs a null RexUUID.
+	RexUUID();
+
+	/// Sets all 16 bytes of the ID to '00'.
+	void SetNull();
+
+	void FromString(const char *str);
+	void FromString(const std::string &str) { FromString(str.c_str()); }
+
+	std::string ToString() const;
+
+	bool operator ==(const RexUUID &rhs) const;
+	bool operator <(const RexUUID &rhs) const;
+
+	static const uint8_t cSizeBytes = 16;
+
+	uint8_t data[cSizeBytes];
+};
+
+#endif
