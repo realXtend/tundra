2cb19264b987f4f039a427320a14f3751938f52d
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-03-12 10:45:46 +0000

    Ogre message pump & reacting to window events.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@134 5b2332b8-efa3-11de-8684-7d64432d61a3

--- OgreRenderingModule/Renderer.h
+++ OgreRenderingModule/Renderer.h
@@ -1,10 +1,11 @@
 // For conditions of distribution and use, see copyright notice in license.txt
 
-#ifndef incl_OgreRenderingSystem_Renderer_h
-#define incl_OgreRenderingSystem_Renderer_h
+#ifndef incl_OgreRenderingModule_Renderer_h
+#define incl_OgreRenderingModule_Renderer_h
 
 #include "RenderServiceInterface.h"
 #include <boost/shared_ptr.hpp>
+#include "OgreWindowEventUtilities.h"
 
 namespace Foundation
 {
@@ -22,9 +23,11 @@ namespace Ogre
 namespace OgreRenderer
 {
     class OgreRenderingModule;
+
+    typedef boost::shared_ptr<Ogre::Root> OgreRootPtr;
     
     //! Ogre renderer
-    class Renderer : public Foundation::RenderServiceInterface
+    class Renderer : public Foundation::RenderServiceInterface, public Ogre::WindowEventListener
     {
     public:
         Renderer(OgreRenderingModule* module, Foundation::Framework* framework);
@@ -33,12 +36,12 @@ namespace OgreRenderer
         //! initializes renderer
         bool Initialize();
         
+        //! returns Ogre root
+        OgreRootPtr GetRoot() const { return root_; }
+
         //! returns Ogre scenemanager
         Ogre::SceneManager* GetSceneManager() const { return scenemanager_; }
-
-        //! returns Ogre root
-        Ogre::Root* GetRoot() const { return root_; }
-
+        
         //! returns active camera
         Ogre::Camera* GetCurrentCamera() const { return camera_; }
 
@@ -61,6 +64,12 @@ namespace OgreRenderer
         //! renders one frame
         void Update();
 
+        //! handles Ogre window close event
+        void windowClosed(Ogre::RenderWindow* rw);
+        
+        //! handles Ogre window resize event
+        void windowResized(Ogre::RenderWindow* rw);
+        
     private:
         //! sets up Ogre resources based on resources.cfg
         void Renderer::SetupResources();
@@ -71,7 +80,7 @@ namespace OgreRenderer
         boost::mutex renderer_;
         
         //! Ogre root object
-        Ogre::Root* root_;
+        OgreRootPtr root_;
         
         //! scene manager
         Ogre::SceneManager* scenemanager_;
