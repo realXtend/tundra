a7916ccc3c5f42ea90e72f92cb2a4faf9dca80c7
jaakkoallander <jaakkoallander@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-03-12 11:10:57 +0000

    Initial implementation of NetTestLogicModule, missing files.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@136 5b2332b8-efa3-11de-8684-7d64432d61a3

---
+++ NetTestLogicModule/NetTestLogicModule.h
@@ -0,0 +1,55 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
+#ifndef incl_NetTestLogicModule_h
+#define incl_NetTestLogicModule_h
+
+#include "ModuleInterface.h"
+
+#include "OpenSimProtocolModule.h"
+
+#include "NetInMessage.h"
+#include "INetMessageListener.h"
+#include "NetMessage.h"
+
+namespace Foundation
+{
+   class Framework;
+}
+
+namespace OpenSimProtocol
+{
+	class OpenSimProtocolModule;
+}
+
+//! Contains unit tests
+/*! All Core and Foundation classes should be unit tested.
+*/
+namespace NetTest
+{
+    //! interface for modules
+    class NetTestLogicModule: public Foundation::ModuleInterface_Impl, public INetMessageListener
+    {
+    public:
+        NetTestLogicModule();
+        virtual ~NetTestLogicModule();
+
+        virtual void Load();
+        virtual void Unload();
+        virtual void Initialize(Foundation::Framework *framework);
+        virtual void Uninitialize(Foundation::Framework *framework);
+        virtual void Update();
+
+        virtual void OnNetworkMessageReceived(NetMsgID msgID, NetInMessage *msg);
+        
+    private:
+        void PerformXMLRPCLogin(const char *first_name, const char *last_name, const char *password, const char *address, int port);
+        
+        Foundation::Framework *framework_;
+		OpenSimProtocol::OpenSimProtocolModule *netInterface_;
+		/// Temporary counter.
+		u32 updateCounter;
+		
+		
+    };
+}
+#endif
