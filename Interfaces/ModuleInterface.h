492ce0852755df7049b01c2d21f23ffdba6af471
cmayhem <cmayhem@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-03-13 11:57:06 +0000

    Fix for dll export in Windows.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@158 5b2332b8-efa3-11de-8684-7d64432d61a3

--- Interfaces/ModuleInterface.h
+++ Interfaces/ModuleInterface.h
@@ -60,7 +60,7 @@ namespace Foundation
       
         \note Every module should have a name. Only internal modules have types.
     */
-    class ModuleInterface
+    class REX_API ModuleInterface
     {
     public:
         ModuleInterface()  {}
@@ -98,7 +98,7 @@ namespace Foundation
     };
 
     //! interface for modules, implementation
-    class ModuleInterface_Impl : public ModuleInterface
+    class REX_API ModuleInterface_Impl : public ModuleInterface
     {
     public:
     
