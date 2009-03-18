f3f33f5f3617ec8d7b8492cee894dedb1bab1973
sempuki <sempuki@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-03-18 08:34:40 +0000

    minor renaming cleanups
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@186 5b2332b8-efa3-11de-8684-7d64432d61a3

--- rexlogic/StableHeaders.h
+++
@@ -1,9 +0,0 @@
-// For conditions of distribution and use, see copyright notice in license.txt
-
-#ifndef incl_rexlogic_StableHeaders_h
-#define incl_rexlogic_StableHeaders_h
-
-#include "CoreStdIncludes.h"
-#include "Core.h"
-
-#endif // incl_rexlogic_StableHeaders_h
