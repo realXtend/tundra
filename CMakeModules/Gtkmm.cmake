6c5aae60d11709a79a1e95745e7be2dfba22e49b
sempuki <sempuki@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-03-18 11:22:40 +0000

    dont need GTKMM_BASEPATH in linux
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@199 5b2332b8-efa3-11de-8684-7d64432d61a3

--- CMakeModules/Gtkmm.cmake
+++ CMakeModules/Gtkmm.cmake
@@ -2,41 +2,39 @@
 # Find is already called in the top-level CMakeLists
 
 macro (FIND_GTKMM)
-	if (NOT DEFINED ENV{GTKMM_BASEPATH})
+	if (MSVC AND NOT DEFINED ENV{GTKMM_BASEPATH})
 		message (FATAL_ERROR "Environment variable GTKMM_BASEPATH not set! Please set it to point to where you installed gtkmm, http://www.gtkmm.org/download.shtml e.g. \"C:\\Program Files\\gtkmm\"")
 	endif ()
 endmacro ()
 
 macro (INCLUDE_GTKMM)
-	include_directories (
-		$ENV{GTKMM_BASEPATH}/include/gtk-2.0/gdk
-		$ENV{GTKMM_BASEPATH}/include/atk-1.0
-		$ENV{GTKMM_BASEPATH}/lib/gtk-2.0/include
-		$ENV{GTKMM_BASEPATH}/include/gdkmm-2.4
-		$ENV{GTKMM_BASEPATH}/include/atkmm-1.6
-		$ENV{GTKMM_BASEPATH}/lib/gdkmm-2.4/include
-		$ENV{GTKMM_BASEPATH}/lib/gtkmm-2.4/include
-		$ENV{GTKMM_BASEPATH}/include/cairomm-1.0
-		$ENV{GTKMM_BASEPATH}/include/cairo
-		$ENV{GTKMM_BASEPATH}/include/pango-1.0
-		$ENV{GTKMM_BASEPATH}/include/gtk-2.0
-		$ENV{GTKMM_BASEPATH}/include/pangomm-1.4
-		$ENV{GTKMM_BASEPATH}/include/giomm-2.4
-		$ENV{GTKMM_BASEPATH}/include/sigc++-2.0
-		$ENV{GTKMM_BASEPATH}/lib/glib-2.0/include
-		$ENV{GTKMM_BASEPATH}/include/glib-2.0
-		$ENV{GTKMM_BASEPATH}/lib/glibmm-2.4/include
-		$ENV{GTKMM_BASEPATH}/include/glibmm-2.4
-		$ENV{GTKMM_BASEPATH}/include/gtkmm-2.4
-		$ENV{GTKMM_BASEPATH}/lib/sigc++-2.0/include
-		$ENV{GTKMM_BASEPATH}/include/libglade-2.0
-		$ENV{GTKMM_BASEPATH}/include/libglademm-2.4
-	)
-	
-	link_directories (
-		$ENV{GTKMM_BASEPATH}/lib
-	)
-	
+    if (MSVC)
+        include_directories 
+            ($ENV{GTKMM_BASEPATH}/include/gtk-2.0/gdk
+            $ENV{GTKMM_BASEPATH}/include/atk-1.0
+            $ENV{GTKMM_BASEPATH}/lib/gtk-2.0/include
+            $ENV{GTKMM_BASEPATH}/include/gdkmm-2.4
+            $ENV{GTKMM_BASEPATH}/include/atkmm-1.6
+            $ENV{GTKMM_BASEPATH}/lib/gdkmm-2.4/include
+            $ENV{GTKMM_BASEPATH}/lib/gtkmm-2.4/include
+            $ENV{GTKMM_BASEPATH}/include/cairomm-1.0
+            $ENV{GTKMM_BASEPATH}/include/cairo
+            $ENV{GTKMM_BASEPATH}/include/pango-1.0
+            $ENV{GTKMM_BASEPATH}/include/gtk-2.0
+            $ENV{GTKMM_BASEPATH}/include/pangomm-1.4
+            $ENV{GTKMM_BASEPATH}/include/giomm-2.4
+            $ENV{GTKMM_BASEPATH}/include/sigc++-2.0
+            $ENV{GTKMM_BASEPATH}/lib/glib-2.0/include
+            $ENV{GTKMM_BASEPATH}/include/glib-2.0
+            $ENV{GTKMM_BASEPATH}/lib/glibmm-2.4/include
+            $ENV{GTKMM_BASEPATH}/include/glibmm-2.4
+            $ENV{GTKMM_BASEPATH}/include/gtkmm-2.4
+            $ENV{GTKMM_BASEPATH}/lib/sigc++-2.0/include
+            $ENV{GTKMM_BASEPATH}/include/libglade-2.0
+            $ENV{GTKMM_BASEPATH}/include/libglademm-2.4)
+        
+        link_directories ($ENV{GTKMM_BASEPATH}/lib)
+    endif (MSVC)
 endmacro ()
 
 macro (LINK_GTKMM)
@@ -62,7 +60,6 @@ macro (LINK_GTKMM)
 			optimized gtkmm-vc90-2_4.lib
 			optimized pangomm-vc90-1_4.lib
 			optimized sigc-vc90-2_0.lib
-			optimized glademm-vc90-2_4.lib
-			)
+			optimized glademm-vc90-2_4.lib)
 	endif ()
 endmacro ()
