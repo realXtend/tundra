11b50240d7442d660785f13c84b37f0dee8043b6
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-03-23 09:59:01 +0000

    Functionality added to OgreMesh.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@247 5b2332b8-efa3-11de-8684-7d64432d61a3

--- OgreRenderingModule/EC_OgreMesh.cpp
+++ OgreRenderingModule/EC_OgreMesh.cpp
@@ -24,7 +24,12 @@ namespace OgreRenderer
     
     bool EC_OgreMesh::SetMesh(const std::string& mesh_name)
     {
-        assert (placeable_);
+        if (!placeable_)
+        {
+            OgreRenderingModule::LogError("Could not set mesh: placeable not set");
+            return false;
+        }
+        
         EC_OgrePlaceable* placeable = static_cast<EC_OgrePlaceable*>(placeable_.get());
         Ogre::SceneNode* node = placeable->GetSceneNode();
         
@@ -39,7 +44,7 @@ namespace OgreRenderer
         }
         catch (Ogre::Exception& e)
         {
-            OgreRenderingModule::LogError("Could not set mesh " + mesh_name + ":" + e.what());
+            OgreRenderingModule::LogError("Could not set mesh " + mesh_name + ": " + e.what());
             return false;
         }
         
@@ -48,17 +53,51 @@ namespace OgreRenderer
     
     void EC_OgreMesh::RemoveMesh()
     {
-        if (ogre_entity_)
-        {
-            assert (placeable_);
-            EC_OgrePlaceable* placeable = static_cast<EC_OgrePlaceable*>(placeable_.get());
-            Ogre::SceneNode* node = placeable->GetSceneNode();
+        if ((!placeable_) || (!ogre_entity_))
+            return;
 
-            Ogre::SceneManager* scene_mgr = module_->GetRenderer()->GetSceneManager();
+        EC_OgrePlaceable* placeable = static_cast<EC_OgrePlaceable*>(placeable_.get());
+        Ogre::SceneNode* node = placeable->GetSceneNode();
+
+        Ogre::SceneManager* scene_mgr = module_->GetRenderer()->GetSceneManager();
+        
+        node->detachObject(ogre_entity_);
+        scene_mgr->destroyEntity(ogre_entity_);
+        ogre_entity_ = NULL;
+    }
+    
+    unsigned EC_OgreMesh::GetNumMaterials()
+    {
+        if (!ogre_entity_)
+            return 0;
             
-            node->detachObject(ogre_entity_);
-            scene_mgr->destroyEntity(ogre_entity_);
-            ogre_entity_ = NULL;
+        return ogre_entity_->getNumSubEntities();
+    }
+    
+    bool EC_OgreMesh::SetMaterial(unsigned index, const std::string& material_name)
+    {
+        if (!ogre_entity_)
+        {
+            OgreRenderingModule::LogError("Could not set material " + material_name + ": no mesh");
+            return false;
         }
+        
+        if (index >= ogre_entity_->getNumSubEntities())
+        {
+            OgreRenderingModule::LogError("Could not set material " + material_name + ": illegal submesh index " + boost::lexical_cast<std::string>(index));
+            return false;
+        }
+        
+        try
+        {
+            ogre_entity_->getSubEntity(index)->setMaterialName(material_name);
+        }
+        catch (Ogre::Exception& e)
+        {
+            OgreRenderingModule::LogError("Could not set material " + material_name + ": " + e.what());
+            return false;
+        }
+        
+        return true;
     }
 }
