f17714c2256dbf488d2d7b5222ad66c79958d09c
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-04-03 13:47:57 +0000

    Store/retrieve assets to disk-based cache. Fixed doxygen comment in EC_OgreSky.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@334 5b2332b8-efa3-11de-8684-7d64432d61a3

---
+++ AssetModule/AssetTransfer.cpp
@@ -0,0 +1,88 @@
+#include "StableHeaders.h"
+#include "AssetDefines.h"
+#include "AssetTransfer.h"
+#include "AssetModule.h"
+
+using namespace RexTypes;
+
+namespace Asset
+{
+    AssetTransfer::AssetTransfer() :
+        size_(0),
+        received_(0)
+    {
+    }
+    
+    AssetTransfer::~AssetTransfer()
+    {
+    }
+    
+    bool AssetTransfer::Ready() const
+    {
+        if (!size_) 
+            return false; // No header received, size not known yet
+        
+        return received_ >= size_;
+    }
+    
+    Core::uint AssetTransfer::GetReceivedContinuous() const
+    {
+        Core::uint size = 0;
+        
+        DataPacketMap::const_iterator i = data_packets_.begin();
+        
+        Core::uint expected_index = 0;
+      
+        while (i != data_packets_.end())
+        {
+            if (i->first != expected_index)
+                break;
+            
+            size += i->second.size();
+            
+            ++expected_index;
+            ++i;
+        }
+        
+        return size;
+    }
+    
+    void AssetTransfer::ReceiveData(Core::uint packet_index, const Core::u8* data, Core::uint size)
+    {
+        if (!size)
+        {
+            AssetModule::LogWarning("Trying to store zero bytes of data");
+            return;
+        }
+        
+        if (!data_packets_[packet_index].size())
+        {
+            data_packets_[packet_index].resize(size);
+            memcpy(&data_packets_[packet_index][0], data, size);
+            received_ += size;
+        }
+        else
+        {
+            AssetModule::LogWarning("Already received asset data packet index " + Core::ToString<Core::uint>(packet_index));
+        }
+    }
+    
+    void AssetTransfer::AssembleData(Core::u8* buffer) const
+    {
+        DataPacketMap::const_iterator i = data_packets_.begin();
+        
+        Core::uint expected_index = 0;
+      
+        while (i != data_packets_.end())
+        {
+            if (i->first != expected_index)
+                break;
+            
+            memcpy(buffer, &i->second[0], i->second.size());
+            buffer += i->second.size();
+            
+            ++expected_index;
+            ++i;
+        }
+    }
+}
