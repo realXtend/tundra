11dd2da26a29b2155e7603a6aa00126851f615ed
tuco <tuco@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-04-08 07:50:01 +0000

    input handler skeleton
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@366 5b2332b8-efa3-11de-8684-7d64432d61a3

--- ReXLogic/InputEventHandler.cpp
+++ ReXLogic/InputEventHandler.cpp
@@ -5,11 +5,19 @@
 
 namespace RexLogic
 {
-    InputEventHandler::InputEventHandler()
+    InputEventHandler::InputEventHandler(Foundation::Framework *framework, RexLogicModule *rexlogicmodule)
     {
+        framework_ = framework;
+        rexlogicmodule_ = rexlogicmodule;    
     }
 
     InputEventHandler::~InputEventHandler()
     {
+    }
+    
+    bool InputEventHandler::HandleInputEvent(Core::event_id_t event_id, Foundation::EventDataInterface* data)
+    {
+        // todo tucofixme, handle input events
+        return false;
     }
 }
