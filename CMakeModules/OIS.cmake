adf3a0966be00a7fa2199adf299ead4683108d3b
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-04-08 10:10:23 +0000

    Find OIS cmake script fixed for non-MSVC builds.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@371 5b2332b8-efa3-11de-8684-7d64432d61a3

--- CMakeModules/OIS.cmake
+++ CMakeModules/OIS.cmake
@@ -2,12 +2,24 @@
 # Find is already called in the top-level CMakeLists
 
 macro (FIND_OIS)
+	if (NOT MSVC)
+		find_package (PkgConfig)
+		pkg_search_module (OIS OIS)
+		if (NOT OIS_INCLUDE_DIRS AND NOT OIS_LIBRARIES)
+			message (FATAL_ERROR "OIS not found by pkg-config")
+		endif (NOT OIS_INCLUDE_DIRS AND NOT OIS_LIBRARIES)
+		separate_arguments (OIS_INCLUDE_DIRS)
+		separate_arguments (OIS_LIBRARIES)
+	endif (NOT MSVC)
 endmacro (FIND_OIS)
 
 macro (INCLUDE_OIS)
 	if (MSVC)
 		include_directories (${REX_DEP_PATH}/ois/includes)
 		link_directories (${REX_DEP_PATH}/ois/dll)
+	else (MSVC)
+		include_directories (${OIS_INCLUDE_DIRS})
+		link_directories (${OIS_LIBDIR})
 	endif (MSVC)
 endmacro (INCLUDE_OIS)
 
@@ -16,5 +28,7 @@ macro (LINK_OIS)
 		target_link_libraries (${TARGET_NAME}
 			debug OIS_d
 			optimized OIS)
+	else (MSVC)
+		target_link_libraries (${TARGET_NAME} ${OIS_LIBRARIES})
 	endif (MSVC)
 endmacro (LINK_OIS)
