b15677d3c236b004e434775a9bc6d579eb7fffeb
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-04-09 08:22:03 +0000

    Added function to query asset download status.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@386 5b2332b8-efa3-11de-8684-7d64432d61a3

--- AssetModule/AssetManager.h
+++ AssetModule/AssetManager.h
@@ -34,25 +34,40 @@ namespace Asset
         virtual ~AssetManager();
         
         //! gets asset
-        /*! \param asset_id asset UUID
+        /*! if asset not in cache, will return empty pointer and queue the asset request. An event will
+            be sent when the asset has been downloaded.
+            
+            \param asset_id asset UUID
             \param asset_type asset type
             \return pointer to asset
-            if asset not in cache, will return empty pointer and queue the asset request. An event will
-            be sent when the asset has been downloaded.
+
          */
         virtual Foundation::AssetPtr GetAsset(const std::string& asset_id, Core::asset_type_t asset_type);
 
         //! gets incomplete asset
-        /*! note: a new incomplete asset object (with copy of the data) will be created for each call. Please
+        /*! if asset not yet requested, will request it and return empty pointer
+            if not enough bytes received, will return empty pointer
+            note: a new incomplete asset object (with copy of the data) will be created for each call. Please
             do not store the shared pointer for longer than necessary.
+            
             \param asset_id asset UUID
             \param asset_type asset type
             \param received minimum continuous bytes received from the start
             \return pointer to asset
-            if asset not yet requested, will request it and return empty pointer
-            if not enough bytes received, will return empty pointer
+           
          */
         virtual Foundation::AssetPtr GetIncompleteAsset(const std::string& asset_id, Core::asset_type_t asset_type, Core::uint received);
+        
+        //! queries status of asset download
+        /*! if asset has been already fully received, size, received & received_continuous will be the same
+        
+            \param asset_id asset ID, UUID for legacy UDP assets
+            \param size variable to receive asset size (if known, 0 if unknown)
+            \param received variable to receive amount of bytes received
+            \param received_continuous variable to receive amount of continuous bytes received from the start
+            \return true if asset was found either in cache or as a transfer in progress, and variables have been filled, false if not found
+         */
+        virtual bool QueryAssetStatus(const std::string& asset_id, Core::uint& size, Core::uint& received, Core::uint& received_continuous);
         
         //! performs time-based update
         /*! uses time to handle timeouts
