71197408c6336a0c2983d7bc60d4156b881de77a
matti_re <matti_re@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-04-16 09:42:16 +0000

    update interface
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@423 5b2332b8-efa3-11de-8684-7d64432d61a3

--- Interfaces/ScriptServiceInterface.h
+++ Interfaces/ScriptServiceInterface.h
@@ -7,14 +7,49 @@
 
 namespace Foundation
 {
+
+
+	class ScriptObject
+	{
+	public:
+        ScriptObject() {}
+        virtual ~ScriptObject() {}
+
+		virtual bool PassFunctionPointerToScript(void(*f)(char*), const std::string& methodname, std::string key) = 0;
+		//virtual bool PassFunctionPointerToScript(void* methodptr, const std::string& methodname) = 0;
+
+		// for syntax parameter see http://docs.python.org/c-api/arg.html
+		virtual ScriptObject* CallMethod(std::string& methodname, std::string& syntax, char* argv[]) = 0;
+		virtual ScriptObject* CallMethod(std::string& methodname, const std::string& syntax, const ScriptObject* args) = 0;
+		virtual ScriptObject* GetObject(const std::string& objectname) = 0;
+
+		//virtual std::string ConvertToStdString() = 0;
+		virtual char* ConvertToChar() = 0;
+	};
+
+	class ScriptEventInterface
+	{
+	public:
+        ScriptEventInterface() {}
+        virtual ~ScriptEventInterface() {}
+		virtual void SetCallback(void(*f)(char*), std::string key) = 0;
+		virtual void NotifyScriptEvent(const std::string& key, const std::string& message) = 0;		
+	};
+
     class ScriptServiceInterface : public ServiceInterface
     {
     public:
         ScriptServiceInterface() {}
         virtual ~ScriptServiceInterface() {}
 
+
         virtual void RunScript(const std::string& scriptname) = 0;
 		virtual void RunString(const std::string& codestr) = 0;
+		//virtual ScriptClass LoadScript(const std::string& codestr) = 0;
+
+		virtual ScriptObject* LoadScript(const std::string& scriptname, std::string& error) = 0;
+		virtual ScriptObject* GetObject(const ScriptObject& script, const std::string& objectname, std::string& error) = 0;
+
     };
 }
 
