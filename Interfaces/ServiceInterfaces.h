2204c96060a64963c9f0f6dd091f36cbcb3df3c2
rvampire <rvampire@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-04-20 11:43:30 +0000

    Added ST_Sound to ServiceInterfaces.h Added OpenALAudioModule to CMakeLists.txt
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@456 5b2332b8-efa3-11de-8684-7d64432d61a3

--- Interfaces/ServiceInterfaces.h
+++ Interfaces/ServiceInterfaces.h
@@ -21,7 +21,7 @@ namespace Foundation
 {
     namespace Service
     {
-        enum Type {ST_SceneManager = 0, ST_Renderer, ST_Physics, ST_Gui, ST_Scripting, ST_Test, ST_Console, ST_ConsoleCommand, ST_Asset, ST_CommunicationManager, ST_Texture, ST_Unknown };
+        enum Type {ST_SceneManager = 0, ST_Renderer, ST_Physics, ST_Gui, ST_Scripting, ST_Test, ST_Console, ST_ConsoleCommand, ST_Asset, ST_CommunicationManager, ST_Texture, ST_Sound, ST_Unknown };
     }
 }
 
