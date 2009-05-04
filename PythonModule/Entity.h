bed9697bac8109bf90ae7b4b641f1d86efc268c1
antont <antont@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-05-04 13:24:49 +0000

    boilerplate and a bit of specific impl for the Entity wrapper class for Python API. the wrapper (struct) holds a EntityPtr to the wrapped entity, and will have the means to get component data either by components being there as members, or via a getitem impl (prototypes of both approeaches in py are in proto/entity.py)
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@581 5b2332b8-efa3-11de-8684-7d64432d61a3

---
+++ PythonModule/Entity.h
@@ -0,0 +1,46 @@
+//#ifndef incl_PythonEntityType_h
+//#define incl_PythonEntityType_h
+
+#include "Foundation.h"
+#include <Python.h>
+
+namespace PythonScript
+{
+	typedef struct {
+		PyObject_HEAD
+		/* Type-specific fields go here. */
+		Foundation::EntityPtr entity;
+	} rexviewer_EntityObject;
+
+	static PyTypeObject rexviewer_EntityType = {
+		PyObject_HEAD_INIT(NULL)
+		0,                         /*ob_size*/
+		"rexviewer.Entity",             /*tp_name*/
+		sizeof(rexviewer_EntityObject), /*tp_basicsize*/
+		0,                         /*tp_itemsize*/
+		0,                         /*tp_dealloc*/
+		0,                         /*tp_print*/
+		0,                         /*tp_getattr*/
+		0,                         /*tp_setattr*/
+		0,                         /*tp_compare*/
+		0,                         /*tp_repr*/
+		0,                         /*tp_as_number*/
+		0,                         /*tp_as_sequence*/
+		0,                         /*tp_as_mapping*/
+		0,                         /*tp_hash */
+		0,                         /*tp_call*/
+		0,                         /*tp_str*/
+		0,                         /*tp_getattro*/
+		0,                         /*tp_setattro*/
+		0,                         /*tp_as_buffer*/
+		Py_TPFLAGS_DEFAULT,        /*tp_flags*/
+		"Entity object",           /* tp_doc */
+	};
+
+	static PyMethodDef noddy_methods[] = {
+		{NULL}  /* Sentinel */
+	};
+
+	void entity_init(PyObject* m);
+	PyObject* entity_create(Foundation::EntityPtr entity);
+}
