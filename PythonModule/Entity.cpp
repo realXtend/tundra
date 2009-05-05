5f25d92edadcfb65083f0cada2a99329b1b1c26a
antont <antont@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-05-05 12:59:12 +0000

    entity py wrapper w.i.p - finding a component matching the attr name given works, but storing the EntityPtr crashes now for some reason, am committing so can collab on debugging
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@601 5b2332b8-efa3-11de-8684-7d64432d61a3

--- PythonModule/Entity.cpp
+++ PythonModule/Entity.cpp
@@ -1,14 +1,19 @@
 #include "StableHeaders.h"
 
-#include "StableHeaders.h"
-
 #include "Entity.h"
+
+#include "RexLogicModule.h" //for getting prim component data
+#include "EC_OpenSimPrim.h"
 
 /* this is not a module but defines a type for the module created in PythonScriptModule */
 //initentity(void) 
 
 namespace PythonScript
 {
+	static PyMethodDef entity_methods[] = {
+		{NULL}  /* Sentinel */
+	};
+
 	using namespace PythonScript;
 
 	void PythonScript::entity_init(PyObject* m)
@@ -28,10 +33,43 @@ namespace PythonScript
 	{
 		//PyObject* pye;
 		rexviewer_EntityObject* eob;
+		std::cout << "Entity: creating a wrapper pyobject ..";
 		eob = PyObject_New(rexviewer_EntityObject, &rexviewer_EntityType); //sets refcount to 1
-		eob->entity = entity; //doesn't have a constructor, just this factory
 
+		std::cout << "setting the pointer to the entity in the wrapper: " << entity << std::endl;
+		eob->entity = entity; //doesn't have a constructor, just this factory
 		return (PyObject*) eob;
+	}
+
+	PyObject* PythonScript::entity_getattro(rexviewer_EntityObject* self, PyObject* name)
+	{
+		PyObject* tmp;
+
+		if (!(tmp = PyObject_GenericGetAttr((PyObject*)self, name))) {
+			if (!PyErr_ExceptionMatches(PyExc_AttributeError))
+				return NULL;
+			PyErr_Clear();
+		}
+		else
+			return tmp;
+
+		char* s_name = PyString_AsString(name);
+
+		std::cout << "Entity: getting unknown attribute: " << s_name;
 		//Py_RETURN_NONE;
+
+		if (s_name == "prim")
+		{
+			std::cout << ".. getting prim" << std::endl;
+			const Foundation::ComponentInterfacePtr &prim_component = self->entity->GetComponent("EC_OpenSimPrim");
+			RexLogic::EC_OpenSimPrim *prim = checked_static_cast<RexLogic::EC_OpenSimPrim *>(prim_component.get());
+		        
+			//m->AddU32(prim->LocalId);
+			std::string retstr = "local id:" + prim->FullId.ToString() + "- prim name: " + prim->ObjectName;
+			return PyString_FromString(retstr.c_str());
+		}
+
+		std::cout << "unknown component type."  << std::endl;
+		return NULL;
 	}
 }
