56c22f429d8449f3f1f389d61b410104b0632a44
cmayhem <cmayhem@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-05-06 12:35:40 +0000

    Added debug console todo items.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@629 5b2332b8-efa3-11de-8684-7d64432d61a3

--- SupportModules/OgreOverlay.h
+++ SupportModules/OgreOverlay.h
@@ -18,6 +18,9 @@ namespace Console
     typedef boost::shared_ptr<LogListener> LogListenerPtr;
 
     //! Ogre debug console
+    /*! \todo Set max line length for command line and printed lines
+        \todo Set proper maximum buffer size for commands, printed text and command history.
+    */
     class OgreOverlay : public Console::ConsoleServiceInterface, public boost::noncopyable
     {
         OgreOverlay();
