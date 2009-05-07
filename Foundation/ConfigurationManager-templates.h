d6259f6fc1a570883f230ffe3d7096bf8f65583e
tuoki <tuoki@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-05-07 15:17:51 +0000

    [Add] new configuration manager interface, requires new PoCo version.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@640 5b2332b8-efa3-11de-8684-7d64432d61a3

--- Foundation/ConfigurationManager-templates.h
+++ Foundation/ConfigurationManager-templates.h
@@ -4,47 +4,27 @@
 # error "Never use <ConfigurationManager-templates.h> directly; include <ConfigurationManager.h> instead."
 #endif
 
-#include <Poco/Exception.h>
 #include <limits>
 #include <string>
+
 namespace Foundation
 {
    
     template <typename T > T ConfigurationManager::DeclareSetting(const std::string& group, const std::string& key, 
-	    					    							      const T& defaultValue, bool save
-		    					    						      ) const
+	    					    							      const T& defaultValue) const
      {	
-	    T value = defaultValue;
-	    if (!configuration_.isNull())
-            {
-			    // Generate search key 
-                std::string groupKey = group + "." + key;
-		        try     
-			    {
-				    value = boost::lexical_cast<T>(configuration_->getString(groupKey));     
-			    }
-		        catch ( Poco::NotFoundException& /*ob*/)
-			    {
-				    // If key does not exist, NotFoundException is thrown. 
+	    if ( HasKey(group, key))
+            return GetSetting<T>(group,key);
+        else
+            values_[std::make_pair(group, key)] = boost::lexical_cast<std::string>(defaultValue);
+           
+        return defaultValue;
 
-                    // Sanity check, is key allready saved into values map?
-                    std::map<string_pair_t, std::string>::iterator iter = values_.find(std::make_pair(group, key));
-                    if ( iter != values_.end() )
-                    {
-                        /// @note PENDING What to do if sanity is loss ?
-                    }
-			    }
-            }
-         if ( save ) 
-            values_[std::make_pair(group, key)] = boost::lexical_cast<std::string>(value);
-        
-	    return value;
-
-    }
+    } 
+         
 
     template <typename T> void ConfigurationManager::SetSetting(const std::string& group, const std::string& key, const T& value)
     {
-        std::string groupKey = group + "." + key;
         std::map<string_pair_t, std::string>::iterator iter = values_.find(std::make_pair(group, key));
         if ( iter != values_.end() )  
             iter->second = boost::lexical_cast<std::string>(value);
@@ -52,9 +32,22 @@ namespace Foundation
             values_[std::make_pair(group, key)] = boost::lexical_cast<std::string>(value);
     }
 
-    template <typename T> T ConfigurationManager::GetSetting(const std::string& group, const std::string& key) const 
+    
+    template <> inline std::string ConfigurationManager::GetSetting<std::string>(const std::string& group, const std::string& key) const
     {
-        std::string groupKey = group + "." + key;
+        std::string value = "";
+        std::map<string_pair_t, std::string>::iterator iter = values_.find(std::make_pair(group, key));
+        if ( iter != values_.end() )    
+            value = boost::lexical_cast<std::string>(iter->second);
+        else
+            value ="";
+
+        return value;
+    }
+   
+    
+    template <typename T> T ConfigurationManager::GetSetting(const std::string& group, const std::string& key) const
+    {
         T value;
         std::map<string_pair_t, std::string>::iterator iter = values_.find(std::make_pair(group, key));
         if ( iter != values_.end() )    
@@ -65,23 +58,6 @@ namespace Foundation
         return value;
     }
 
-    template <typename T> T ConfigurationManager::GetSettingFromFile(const std::string& group, const std::string& key) const
-    {
-        std::string groupKey = group + "." + key;
-        
-        T value;
-        
-        try
-        {
-            if (configuration_)
-                value =  boost::lexical_cast<T>(configuration_->getString(groupKey));
-        }
-        catch ( Poco::NotFoundException& /*ob*/)
-        {
-            // Setting was not found, return NAN value
-            value = boost::lexical_cast<T>(std::numeric_limits<double>::quiet_NaN());
-        }
-        return value;
-    }
-
+    
+   
 }
