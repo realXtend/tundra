a0904ce284e7be7dba755709468c383a570dfb38
antont <antont@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-05-13 12:40:16 +0000

    py event manager work
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@727 5b2332b8-efa3-11de-8684-7d64432d61a3

--- PythonModule/proto/circuits_manager.py
+++ PythonModule/proto/circuits_manager.py
@@ -9,43 +9,66 @@ currently here is only test code for modules and handelers that would
 use the manager, nothing of the Manager itself yet.
 """
 
-import circuits
+from circuits import handler, Event, Component, Manager
 
-class RealxtendViewer(pyglet.event.EventDispatcher):
+# Create a new Manager
+m = Manager()
+
+class Update(Event):
+    pass #to have the deltatime as event data
+    
+class Chat(Event):
+    
+
+class ComponentRunner(Component):
     def run(self, deltatime):
         print ".",
-        self.dispatch_event('update', deltatime)
-
-RealxtendViewer.register_event_type('update')
+        m.push(Update(), "update")
+        m.Push(Chat(), "on_chat")
     
-viewer = RealxtendViewer()
+runner = ComponentRunner()
 
-class TestModule:
+class TestModule(Component):
     """A wish of how a py written module might like to have the system.
     This could for example be the CommunicationsModule, right?
     Or something that implements as a custom control type..
     Now attempting a generic test thing."""
     
     def __init__(self):
+        Component.__init__(self)
         self.data = 1
         
-    @viewer.event
     def update(self):
         if self.data == 1:
-            self.data == 0
+            self.data = 0
         else:
-            self.data == 1
+            self.data = 1
         print self.data,
-            
+        
+    def on_chat(self, m=None):
+        print "Test Module received chat message:", m
+        
+tm = TestModule()
+m += tm # Equivalent to: tm.register(m)
+
 """
 why not allow plain functions as event handlers too,
 the python module of it, i.e. the file, can be considered as the module,
 so no classes are required.
 """
 
+"""apparently not possible with circuits, see circuits_test for the failed attempt.
+hm reportedly is possible:
+14:29 <@prologic> <antont> is it possible to register an arbitary method or
+                  function as an event handler, one that is not in a subclass
+                  of Component? am reading the docs and going to try that now..
+                  <-- yes it's possible
+14:58 <+antont> i wonder how, as m._add is not supposed to be used (i know what
+                that _ means :) and didn't work either
 @viewer.event
 def dosomething():
     pass #e.g. move an object w.r.t to something (like time?)
+"""
     
 """
 update may be nice as a method, but the real thing are the events.
@@ -60,6 +83,10 @@ update may be nice as a method, but the real thing are the events.
     #~ create_sparkle((0,2, 0,2), 0.5) #in upper right corner, for half a sec
     
 if __name__ == '__main__':
-    pyglet.clock.schedule_interval(viewer.run, 0.1)
+    import time
+    m.start()
+    while True:
+        runner.run(0.1)
+        time.sleep(0.1)
 
 
