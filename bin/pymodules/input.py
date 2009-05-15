bddc185a61f9989d2aca924e922d9a13096fb39c
antont <antont@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-05-15 11:02:05 +0000

    loading the pymod manager from the actual viewer, and passing one event to it. a stub ModuleManager impl that works. next will bring the PygletManager from proto/ to be actually used. then we just add all the relevant events..
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@769 5b2332b8-efa3-11de-8684-7d64432d61a3

--- bin/pymodules/input.py
+++ bin/pymodules/input.py
@@ -1,9 +1,24 @@
 import rexviewer as r
 
+#some prim
 idnum = 720001
 #idnum = 0
+
+#av ent
+av_entid = 8880005
+
+idnum = av_entid
 
 print "Getting entity id", idnum,
 e = r.getEntity(idnum)
 print "got:", e
 #print dir(r)
+
+if 1: #test avatar tracking, works :)
+    a = r.getEntity(av_entid)
+    print "Avatar pos:", a.place
+    """
+    perhaps some local script could track movement?
+    make a sound of a nearby object of interest, 
+    like when a pet or a friend moves?
+    """
