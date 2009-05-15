629c98179adeccc879eeb219b73e0da5b10a2a2b
mattiku <mattiku@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-05-15 05:03:07 +0000

    code refactoring
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@759 5b2332b8-efa3-11de-8684-7d64432d61a3

--- bin/pymodules/dbusmanager.py
+++ bin/pymodules/dbusmanager.py
@@ -1,24 +1,29 @@
 import sys
 import dbus
-#from dbus.mainloop.glib import DBusGMainLoop
 import dbus.service
 import threading
 from threading import Thread
 import time
 import subprocess        
 
-bus_name = 'org.realxtend.test'
-interface_name = 'org.realxtend.test'
-object_path = "/org/realxtend/test/dbus_service_test"    
+'''
+Simple manager for starting dbus-daemon (windbus) in Windows platform.
+- Can check if dbus service is running
+- Can start dbus service (windbus)
+- Can stop started dbus service (windbus) <-- NOT WORKING WITH PYTHON 2.5
+'''
 
-class TestDBusObjectObject(dbus.service.Object):
+# fixed properties for test object
+bus_name = 'org.realxtend.test'                        # dbus name for test object
+interface_name = 'org.realxtend.test'                  # interface for test object
+object_path = "/org/realxtend/test/dbus_service_test"  # test object path    
+
+class TestObject(dbus.service.Object):
+
     ''' A test dbus object with one dbus callable method '''
+    
     def __init__(self, object_path):    
         dbus.service.Object.__init__(self, dbus.SessionBus(), object_path)    
-    # '''    def __init__(self, object_path):
-        # #dbus.service.Object.__init__(self, bus, object_path)
-        # dbus.service.Object.__init__(self, bus_name, object_path)
-        # print "Initialized a TestObject object: " + str(object_path)'''    
         
     @dbus.service.method(dbus_interface=interface_name, in_signature='s', out_signature='s') 
     def testFunction(self, data=None):
@@ -35,98 +40,139 @@ class ServiceThread(threading.Thread):
         self.arguments_ = arguments
         self.running_ = False
         self.process_ = None
+        self.print_debug_info_ = True
     
     def run(self):
         self.running_ = True
         try:
-#            self.process_ = subprocess.Popen([self.application_path_, self.arguments_])
-#            std_out, std_err = self.process_.communicate()
-            import os
-            os.environ['dbusdir'] = os.getcwd()+str('\pymodules') # dbus daemon needs this to fond service files
-            print ">>>"+str(os.environ['dbusdir'])
-            print ">>>"
-            print("BEGIN subprocess: ["+str(self.application_path_)+"] with ["+str(self.arguments_)+"]")
+            #self.set_dbusdir_environment_variable(os.getcwd())
+            #print ">>>"+str(os.environ['dbusdir'])
+            if self.print_debug_info_:
+                print("BEGIN subprocess: ["+str(self.application_path_)+"] with ["+str(self.arguments_)+"]")
             subprocess.call([self.application_path_, self.arguments_]) # block until process is terminated
-            print("END subprocess")
+            if self.print_debug_info_:
+                print("END subprocess")
         except WindowsError, (error):
-            print("END subprocess (Exception)")
-            print str(error) 
+            if self.print_debug_info_:
+                print("END subprocess (Exception)")
+                print str(error) 
         self.running_ = False
         
     def stop(self):
         if self.process_ == None:
             return
         try:
-            print("Try to kill") #debuf message
+            if self.print_debug_info_:        
+                print("Try to kill") #debuf message
             self.process_.kill() # <--- PROBLEM: THIS IS NOT IMPLEMENTED IN PYTHON 2.5 !!!
-            print("Killed");
+            if self.print_debug_info_:
+                print("Killed");
         except:
-            print("Cannot kill...");
+            if self.print_debug_info_:
+                print("Cannot kill...");
             
-    def is_running():
+    def is_running(self):
       return self.running_    
+      
+    def set_dbusdir_environment_variable(self, value):
+        import os
+        os.environ['dbusdir'] = str(value)
         
         
 class DBusManager:
     ''' Offer functionality to a) check if dbus service is running
                                b) start dbus-daemon (windbus)
-                               c) stop started dbus-daemon (windbus) <--- Need python 2.6 ->
+                               c) stop started dbus-daemon (windbus) <--- Need python at least 2.6 
     '''
     
     def __init__(self):
+        ''' Initialize member variables '''
         self.service_thread_ = None
-#        DBusGMainLoop(set_as_default=True)
-#        self.dbus_loop_ = DBusGMainLoop()
+        self.create_test_object_ = False
+        self.bus_ = None
+        self.test_object_ = None
+        
+        if self.create_test_object_:
+            from dbus.mainloop.glib import DBusGMainLoop
+            DBusGMainLoop(set_as_default=True)
+            self.dbus_loop_ = DBusGMainLoop()
     
     def is_dbus_service_running(self):
-        ''' return True if running otherwise False
+        ''' return True if running otherwise False '''        
         
+        if self.create_test_object_:
+            return self.test_dbus_with_object()
+        else:    
+            return self.test_dbus_simple()
+                
+    def test_dbus_with_object(self):
+        '''
             try to create a bus object. If that success then dbus service is running
             @todo: more sophisticatd test: expert dbus object and call a test function of that object
         '''
-        # TEST: * Try to export object to dbus -> success or fail
-        #       * In future more clean solution would be try to also use that object by calling one test function
         try:
-            # create dbus object
-            bus = dbus.SessionBus()
-#            bus = dbus.SessionBus(mainloop = self.dbus_loop_)
-            #name = dbus.service.BusName(bus_name, bus)
-            #object = TestDBusObjectObject(object_path)
-            
-            # call function of dbus object
-#            obj = bus.get_object(bus_name, object_path)
-#            obj_interface = dbus.Interface(obj, dbus_interface=interface_name)
-#            ret = obj_interface.testFunction("test")
-#            ret = False
-#            if ret == "ok":
+            self.init_bus()
+            self.export_dbus_object()
+            self.call_test_object_method()
             return True
-                
         except:
-            # dbus service test failed: Cannot export object to dbus
             pass
-            
         return False
         
+    def init_bus(self):
+        if self.bus_ == None:
+            self.bus_ = dbus.SessionBus()
+            # bus = dbus.SessionBus(mainloop = self.dbus_loop_) # with main loop
+        
+    def export_dbus_object():
+        if self.test_object_ == None:
+            name = dbus.service.BusName(bus_name, bus)
+            self.test_object_ = TestObject(object_path)
+        
+    # NOTE: This is not working yet, DO NOT USE    
+    def call_test_object_method():
+        ''' Get reference to exported dbus object and call "test" methos 
+            return value must be "ok". If this success return True otherwise return False
+        '''
+        obj = bus.get_object(bus_name, object_path)
+        obj_interface = dbus.Interface(obj, dbus_interface=interface_name)
+        ret = obj_interface.testFunction("test")
+        if ret == "ok":
+            return True
+        else:
+            return False        
+        
+    def test_dbus_simple(self):
+        ''' Just test if it's possible to get bus object or not '''
+        try:
+            self.init_bus()
+            return True
+        except:
+            pass
+        return False    
+
     def start_dbus_service(self, app_path, app_args):
         ''' Start dbus-daemon from given path with given arguments
             Does nothing if dbus service is already running
+            
+            This starts a thread wich blocks untila dbus-daemon quits
         
             @param app_path path to dbus daemon application eg.  "../dbus-daemon.exe"
             @param app_args arguments for dbus daemon application eg. "--config-file=../data/session.conf"
         '''
         
         if self.is_dbus_service_running():
-            return # service already running, no need to start
+            return # service already running, no need to start antother one
             
         try:    
-            print("START thread for dbus")
             self.service_thread_ = ServiceThread(app_path, app_args)
             self.service_thread_.setDaemon(True) 
             self.service_thread_.start()
         except str,(reason):
             print("Cannot start dbus daemon: " + str(reason) )
         
-        # wait a couple of seconds so dbus service can really start up
+        # Check that service exist
+        # * waits a couple of seconds so dbus service can really start up
         wait_time = 5
         wait_interval = 1
         start_time = time.time()
@@ -142,9 +188,10 @@ class DBusManager:
         
         
 if __name__ == "__main__":
-    ''' Prints dbus service status
+    ''' Testing module functionality: Prints dbus service status
         If service is not running then:
             try start and stop the service
+        else do nothing    
     '''
     
     print "DBusManager:"
