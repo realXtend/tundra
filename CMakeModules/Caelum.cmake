81b17bb3f6c20aed19b3f0d48ee12614a6474ef0
jaakkoallander <jaakkoallander@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-06-04 12:41:19 +0000

    Missing cmake file
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@892 5b2332b8-efa3-11de-8684-7d64432d61a3

---
+++ CMakeModules/Caelum.cmake
@@ -0,0 +1,20 @@
+# Find, include and link Caelum
+# Find is already called in the top-level CMakeLists
+
+macro (FIND_CAELUM)
+endmacro (FIND_CAELUM)
+
+macro (INCLUDE_CAELUM)
+	if (MSVC)
+		include_directories (${REX_DEP_PATH}/Caelum/include)
+		link_directories (${REX_DEP_PATH}/Caelum/lib)
+	endif (MSVC)
+endmacro (INCLUDE_CAELUM)
+
+macro (LINK_CAELUM)
+	if (NOT MSVC)
+		target_link_libraries (${TARGET_NAME}
+			optimized caelum
+			debug caelum_d)
+	endif (NOT MSVC)
+endmacro (LINK_CAELUM)
