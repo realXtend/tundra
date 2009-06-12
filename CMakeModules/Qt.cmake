e72e6952eb0af3e4919d8c46b5491146e9f27dbf
tuoki <tuoki@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-06-12 14:52:18 +0000

    Added working version CMakeLists for QtModule (in Linux) Note: Currently QtModule is comment out from main CMakeFile
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@949 5b2332b8-efa3-11de-8684-7d64432d61a3

--- CMakeModules/Qt.cmake
+++ CMakeModules/Qt.cmake
@@ -4,11 +4,14 @@
 macro (FIND_QT4)
 
 if (MSVC)
+
 FIND_PACKAGE(Qt4 REQUIRED)
+
 elseif (UNIX)
 
 include(FindPkgMacros)
 findpkg_begin(Qt4)
+
 
 # Construct search paths from enviromental hits and OS spesific guesses
 
@@ -33,36 +36,25 @@ set(Qt4_PREFIX_PATH
 create_search_paths(Qt4)
 
 # try to locate Qt4 via pkg-config
-use_pkgconfig(Qt4 "Qt4")
+find_package( Qt4 )
 
-# try to find framework on OSX
-findpkg_framework(Qt4)
-
-if (Qt4_FOUND)
+if (QT4_FOUND )
   message(STATUS "Qt4 found through pkg-config")
-  # Not needed
-  set(Qt4_LIBRARY_DIR "${QT4_LIBRARY_DIRS}")
-  set(Qt4_INCLUDE_DIR "${QT4_INCLUDE_DIRS}")
 else ()
   message(STATUS "try to find Qt4 from guessed paths")
   find_path(Qt4_INCLUDE_DIR QtCore/qstack.h HINTS ${Qt4_INC_SEARCH_PATH} ${Qt4_PKGC_INCLUDE_DIRS})
-  if (MSVC)
-    find_path(Qt4_LIBRARY_DIR  QtCore4.lib HINTS ${Qt4_LIB_SEARCH_PATH})
-  else ()
-    find_path(Qt4_LIBRARY_DIR libQtCore4.so HINTS ${Qt4_LIB_SEARCH_PATH})
-  endif()  
+  find_path(Qt4_LIBRARY_DIR libQtCore.so HINTS ${Qt4_LIB_SEARCH_PATH})
 endif()
 
-if (NOT MSVC AND NOT Qt4_FOUND AND NOT Qt4_INCLUDE_DIR OR NOT Qt4_LIBRARY_DIR)
+if (NOT MSVC AND NOT QT4_FOUND AND NOT Qt4_INCLUDE_DIR OR NOT Qt4_LIBRARY_DIR)
   message(STATUS "Qt4 was not found either guessed paths or pkg-config, please add enviroment variable QT4_HOME")
 endif()
 
 endif()
+
 endmacro (FIND_QT4)
 
 macro (INCLUDE_QT4)
-
-include(${QT_USE_FILE})
   
   if (MSVC)
     if (DEFINED ENV{QTDIR})
@@ -72,9 +64,15 @@ include(${QT_USE_FILE})
       include_directories (${REX_DEP_PATH}/Qt/include)
       link_directories (${REX_DEP_PATH}/Qt/lib)
     endif()
-  elseif (NOT MSVC AND Qt4_FOUND)
-    include_directories(${Qt4_INCLUDE_DIRS})
-    link_directories(${Qt4_LIBRARY_DIRS})
+  elseif (NOT MSVC AND QT4_FOUND)
+    include_directories(${QT_INCLUDE_DIR} ${QT_QTWEBKIT_INCLUDE_DIR} 
+      ${QT_QTCORE_INCLUDE_DIR} 
+      ${QT_QTDBUS_INCLUDE_DIR}
+      ${QT_QTGUI_INCLUDE_DIR}
+      ${QT_QTNETWORK_INCLUDE_DIR}
+      ${QT_QTSCRIPT_INCLUDE_DIR}
+      ${QT_QTSQL_DIR})
+    link_directories(${QT_LIBRARY_DIRS})
   else()
     include_directories(${Qt4_INCLUDE_DIR})
     link_directories(${Qt4_LIBRARY_DIR})
@@ -84,23 +82,18 @@ include(${QT_USE_FILE})
 endmacro (INCLUDE_QT4)
 
 macro (LINK_QT4)
-
-  #PENDING is some of these needed: 
-  #TARGET_LINK_LIBRARIES(
-  # ${QT_LIBRARIES}
-  # ${QT_QTNETWORK_LIBRARIES}
-  # ${QT_QTTEST_LIBRARIES}
-  #)
-
-
-	if (MSVC)
-	  target_link_libraries (${TARGET_NAME}
-	    QtCore4  QtGui4  qtmain  QtNetwork4  QtWebKit4
-	    )
-	elseif (NOT MSVC AND Qt4_FOUND)
-	  target_link_libraries (${TARGET_NAME} ${Qt4_LIBRARIES})
-	else()	
-	  target_link_libraries(${TARGET_NAME} QtCore4 QtGui4  qtmain  QtNetwork4  QtWebKit4)
-	endif (MSVC)
-
+  if (MSVC)
+    target_link_libraries (${TARGET_NAME}
+      QtCore4  QtGui4  qtmain  QtNetwork4  QtWebKit4
+      )
+  elseif (NOT MSVC AND QT4_FOUND)
+    target_link_libraries (${TARGET_NAME} ${QT_LIBRARIES} ${QT_QTGUI_LIBRARY} 
+      ${QT_QTWEBKIT_LIBRARY} ${QT_QTCORE_LIBRARY} ${QT_QTDBUS_LIBRARY}
+      ${QT_QTNETWORK_LIBRARY}
+      ${QT_QTSCRIPT_LIBRARY}
+      ${QT_QTSQL_LIBRARY})
+  else()	
+    target_link_libraries(${TARGET_NAME} QtCore QtGui  qtmain  QtNetwork  QtWebKit)
+  endif (MSVC)
+  
 endmacro (LINK_QT4)
