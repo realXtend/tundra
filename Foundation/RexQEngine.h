584e5b746299d8c578f78552aad75af940bdabea
tuoki <tuoki@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-06-18 13:27:40 +0000

    [Fix] removed extra header "Ogre3DWidget" and also fixed some GCC problems.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@959 5b2332b8-efa3-11de-8684-7d64432d61a3

--- Foundation/RexQEngine.h
+++ Foundation/RexQEngine.h
@@ -4,16 +4,15 @@
 #include <QObject>
 #include <QTimer>
 
-#include "Ogre3DWidget.h"
-
 class QApplication;
-
 namespace Foundation
 {
 
 class Framework;
+
 /// Bridges QtApplication and Rex Framework objects together and helps drive the continuous
 /// application update ticks using a timer object.
+
 class RexQEngine : public QObject
 {
     Q_OBJECT
@@ -23,25 +22,31 @@ public:
     virtual ~RexQEngine();
 
     /// Transitions into the QApplication main loop.
+
     void Go();
 
     std::string GetMainWindowHandle() const;
 
 public slots:
+
     void OnQuitQApp();
+
     /// Responds to the frameupdate timer ticks by doing a logic and render step.
+
     void UpdateFrame();
 
 private:
     QApplication *app_;
+
     Framework *owner_;
     QTimer frame_update_timer_;
 
-//    Ogre3DWidget *ogre_widget_;
+
     QWidget *ogre_host_widget_;
 
     /// Transitions to true when the QApplication is being torn down and the application
     /// is about to quit.
+
     bool exit_signalled_;
 };
 
