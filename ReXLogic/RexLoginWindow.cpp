17c6e6bf6ce663933a37d396bfa4c62f714cabcb
tuoki <tuoki@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-06-22 12:51:43 +0000

    Refactor of login window so that is made as Qt-widget. Also fixed some (GCC) compiler issues in EventDataInterface. Added QT_QTUITOOLS linking in Qt.cmake. Added data/ui -folder which contains login.ui which defines login window geometry etc. Because current viewer code crashes when login is done in linux i cannot assure that this code works:(
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@965 5b2332b8-efa3-11de-8684-7d64432d61a3

--- ReXLogic/RexLoginWindow.cpp
+++ ReXLogic/RexLoginWindow.cpp
@@ -4,137 +4,206 @@
 
 #include "RexLoginWindow.h"
 #include "RexLogicModule.h"
-#include "GtkmmUI.h"
+//#include "GtkmmUI.h"
+#include <QDebug>
+#include <QFile>
+#include <QPushButton>
+#include <QLabel>
+#include <QString>
+#include <QtUiTools>
+#include <QTabWidget>
+#include <QLineEdit>
 
 namespace RexLogic
 {
-    RexLoginWindow::RexLoginWindow(Foundation::Framework *framework, RexLogicModule *module) :
-    framework_(framework), rexLogic_(module), loginWindow(0), entryServer(0), entryUsername(0)
+    RexLoginWindow::RexLoginWindow(Foundation::Framework* framework, RexLogicModule *module) :
+    framework_(framework), rexLogic_(module), pLogin_widget_(0)
     {
         InitLoginWindow();
     }
     
     RexLoginWindow::~RexLoginWindow()
     {
-        SAFE_DELETE(loginWindow);
+        SAFE_DELETE(pLogin_widget_);
     }
 
     void RexLoginWindow::InitLoginWindow()
     {
-        Foundation::ModuleWeakPtr gtkmmui_module = framework_->GetModuleManager()->GetModule("GtkmmUI");
-        if (gtkmmui_module.expired() == false)
-        {
-            // Create the login window from glade (xml) file.
-            loginControls = Gnome::Glade::Xml::create("data/loginWindow.glade");
-        }
-
-        if (!loginControls)
-            return;
-                 
-        // Get the widgets.
-        loginControls->get_widget("dialog_login", loginWindow);
-        entryUsername = loginControls->get_widget("entry_username",entryUsername);
-        entryServer = loginControls->get_widget("entry_server", entryServer);
-        
-        // Bind window to main window if available                   
-        GtkmmUI* ui = dynamic_cast<GtkmmUI*>(gtkmmui_module.lock().get());
-        Gtk::Window* mainWindow = ui->GetMainWindow();
-        if (mainWindow)
-        {
-            loginWindow->set_transient_for(*mainWindow);
-        }        
-        
-        // Bind callbacks.
-        loginControls->connect_clicked("button_connect", sigc::mem_fun(*this, &RexLoginWindow::OnClickConnect));
-        loginControls->connect_clicked("button_logout", sigc::mem_fun(*this, &RexLoginWindow::OnClickLogout));
-        loginControls->connect_clicked("button_quit", sigc::mem_fun(*this, &RexLoginWindow::OnClickQuit));
-        entryServer->signal_activate().connect(sigc::mem_fun(*this, &RexLoginWindow::OnClickConnect));
-        
-        // Read old connection settings from xml configuration file.
-        std::string strText = "";
-        std::string strGroup = "Login";
-        std::string strKey = "server";
-        
+      ///todo Set check that qt_module exist 
+      //Foundation::ModuleWeakPtr qt_module = framework_->GetModuleManager()->GetModule("QtModule");
+       
+      //if (!qt_module.expired())
+      //	 {
+       QUiLoader loader;
+       QFile file("../data/ui/login.ui");
+       pLogin_widget_ = loader.load(&file); 
       
-        strText = framework_->GetDefaultConfigPtr()->GetSetting<std::string>(strGroup, strKey);
-        entryServer->set_text(Glib::ustring(strText));
+      //	 }
+      //else
+      // return;
+
+       // Create connections.
+
+       QPushButton *pButton = pLogin_widget_->findChild<QPushButton *>("but_connect");
+       QObject::connect(pButton, SIGNAL(clicked()), this, SLOT(Connect()));
+
+       pButton = pLogin_widget_->findChild<QPushButton *>("but_log_out");
+       QObject::connect(pButton, SIGNAL(clicked()), this, SLOT(Disconnect()));
+       
+       pButton = pLogin_widget_->findChild<QPushButton *>("but_quit");
+       QObject::connect(pButton, SIGNAL(clicked()), this, SLOT(Quit()));
+       
+
+       // Read old connection settings from xml configuration file.
+
+       std::string strText = "";
+       std::string strGroup = "Login";
+       std::string strKey = "username";
+       
+      
+       strText = framework_->GetDefaultConfigPtr()->GetSetting<std::string>(strGroup, strKey);
+      
+       // Open sim:
+
+       QLineEdit *pLine = pLogin_widget_->findChild<QLineEdit* >("line_user_name");
+       pLine->setText(QString(strText.c_str()));
+       
+       // Rex Auth: 
+
+       pLine = pLogin_widget_->findChild<QLineEdit* >("line_user_name_au");
+       pLine->setText(QString(strText.c_str()));
+         
+       strKey = "server";
+       strText = "";
+       
+       strText = framework_->GetDefaultConfigPtr()->GetSetting<std::string>(strGroup, strKey);
+      
+       // Open sim: 
+
+       pLine = pLogin_widget_->findChild<QLineEdit* >("line_server");
+       pLine->setText(QString(strText.c_str()));
+ 
+       // Rex Auth:
+
+       pLine = pLogin_widget_->findChild<QLineEdit* >("line_server_au");
+       pLine->setText(QString(strText.c_str()));
+ 
+       // Show :
+       pLogin_widget_->show();
+
+
+    
+    }
+
+    void RexLoginWindow::Connect()
+    {
+      // Check which tab is active
+      qDebug("RexLoginWindow::Connect() START");
+      QTabWidget *pWidget = pLogin_widget_->findChild<QTabWidget* >("tabWidget");
+      int index = pWidget->currentIndex();
+      
+      bool succesful = false;
+      std::string user_name = "";
+      std::string server_address = "";
+
+      switch (index)
+	{
+	case 0:
+	  {
+	    // Open sim
+	    qDebug("RexLoginWindow::Connect() Open Sim connection");
+	    QLineEdit *pLine = pLogin_widget_->findChild<QLineEdit* >("line_user_name");
+	    user_name = pLine->text().toStdString();
+	    pLine =  pLogin_widget_->findChild<QLineEdit* >("line_server");
+	    server_address = pLine->text().toStdString();
+
+	    // password
+
+	    pLine = pLogin_widget_->findChild<QLineEdit* >("line_password");
+
+	    std::string password = pLine->text().toStdString();
+
+	    succesful = rexLogic_->GetServerConnection()->ConnectToServer(user_name,
+									  password, server_address);
+	    break;
+	  }
+	case 1:
+	  {
+	    // Rex authentication 
+	    qDebug("RexLoginWindow::Connect() Rex authentication connection");
+	    QLineEdit *pLine = pLogin_widget_->findChild<QLineEdit* >("line_user_name_au");
+	    user_name = pLine->text().toStdString();
+	    pLine =  pLogin_widget_->findChild<QLineEdit* >("line_server_au");
+	    server_address = pLine->text().toStdString();
+
+	    // password
+
+	    pLine = pLogin_widget_->findChild<QLineEdit* >("line_password_au");
+
+	    std::string password = pLine->text().toStdString();
+	    std::cout<<"Password: "<<password<<"user_name: "<<user_name<<" server_address: "<<server_address<<std::endl; 
+	    pLine = pLogin_widget_->findChild<QLineEdit* >("line_auth_login");
+	    std::string auth_login = pLine->text().toStdString();
+
+	    pLine = pLogin_widget_->findChild<QLineEdit* >("line_auth_server");
+	    std::string auth_server = pLine->text().toStdString();
+	    std::cout<<"auth_server: "<<auth_server<<" auth_login: "<<auth_login<<std::endl;
+
+	    succesful = rexLogic_->GetServerConnection()->ConnectToServer(user_name, password,
+									  server_address, auth_server, auth_login);
+	    break;
+	  }
+	case 2:
+	  {
+	    // OpenID
+	    break;
+	  }
+	default:
+	  break;
+	}
+      
+      if (succesful)
+      	{
+	  // Save login and server settings for future use. 
+	  framework_->GetConfigManager()->SetSetting<std::string>(
+								  std::string("Login"),
+								  std::string("server"),
+								  server_address);
+	  framework_->GetConfigManager()->SetSetting<std::string>(
+								  std::string("Login"),
+								  std::string("username"),
+								  user_name);
+        }
+
+     
+    }
+
+    void RexLoginWindow::Disconnect()
+    {
+      // Disconnect from server
+      rexLogic_->GetServerConnection()->RequestLogout();
+    }
+  
+    void RexLoginWindow::Quit()
+    {
    
-        strKey = "username";
-        strText = "";
+      if (rexLogic_->GetServerConnection()->IsConnected())
+	rexLogic_->GetServerConnection()->RequestLogout();
         
-        strText = framework_->GetDefaultConfigPtr()->GetSetting<std::string>(strGroup, strKey);
-        entryUsername->set_text(Glib::ustring(strText));
-        
-        ///@note Pending : Currently password is not loaded and saved.
-        
-        // Set the window title and position.
-        loginWindow->set_title("Login");
-        loginWindow->set_position(Gtk::WIN_POS_CENTER);
-        loginWindow->show();        
+      framework_->Exit();
+      
     }
-    
-    void RexLoginWindow::OnClickConnect()
-    {
-        // Initialize UI widgets.
-        Gtk::Entry *entry_password = 0;
-		Gtk::Entry *entry_authentication = 0;
-		loginControls->get_widget("entry_password", entry_password);
-		loginControls->get_widget("entry_authentication", entry_authentication);
+  
+  
+   void RexLoginWindow::UpdateConnectionStateToUI(OpenSimProtocol::Connection::State state)
+    { 
+      if ( pLogin_widget_ != 0)
+	{
+	  QLabel *pLabel = pLogin_widget_->findChild<QLabel* >("lab_state");
+	  pLabel->setText(QString(OpenSimProtocol::Connection::NetworkStateToString(state).c_str()));
 
-		bool succesful = false;
-		if (entry_authentication != 0 && std::string(entry_authentication->get_text()) != std::string(""))
-		{
-			// Connect to Authentication server.
-		    // entry_authentication contains authentication server address and port
-			// entry_auth_login contains login username. 
-			
-			Gtk::Entry *entry_auth_login = 0;
-			loginControls->get_widget("entry_auth_login", entry_auth_login);
-			
-			succesful = rexLogic_->GetServerConnection()->ConnectToServer(entryUsername->get_text(), entry_password->get_text(),
-			    entryServer->get_text(), entry_authentication->get_text(), entry_auth_login->get_text());
-		}
-		else 
-		{
-		    succesful = rexLogic_->GetServerConnection()->ConnectToServer(entryUsername->get_text(),
-		        entry_password->get_text(), entryServer->get_text());
-        }
-        
-		if (succesful)
-		{
-            // Save login and server settings for future use. 
-            framework_->GetConfigManager()->SetSetting<std::string>(
-                std::string("Login"),
-                std::string("server"),
-                std::string(entryServer->get_text()));
-            framework_->GetConfigManager()->SetSetting<std::string>(
-                std::string("Login"),
-                std::string("username"),
-                std::string(entryUsername->get_text()));
-        }
-    }
-    
-    void RexLoginWindow::OnClickLogout()
-    {
-        rexLogic_->GetServerConnection()->RequestLogout();
-        ///\todo Handle server timeouts.
-    }
-    
-    void RexLoginWindow::OnClickQuit()
-    {
-        if (rexLogic_->GetServerConnection()->IsConnected())
-            rexLogic_->GetServerConnection()->RequestLogout();
-        
-        framework_->Exit();
-    }
-
-    void RexLoginWindow::UpdateConnectionStateToUI(OpenSimProtocol::Connection::State state)
-    {
-        Gtk::Label *label_state = 0;
-        if (loginControls)
-            loginControls->get_widget("label_state", label_state);
-
-        if (label_state)
-            label_state->set_text(OpenSimProtocol::Connection::NetworkStateToString(state));
+	}
+      
+      
     }
 }
