819a74651ac1f151f1bd01c2e129d68b4b8824b8
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-06-26 10:57:56 +0000

    Modified build script to take everything GTK-related out
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1022 5b2332b8-efa3-11de-8684-7d64432d61a3

--- makebuild.bat
+++ makebuild.bat
@@ -1,7 +1,7 @@
 @rem Makes a build directory of the viewer, so that you can run the NSIS install script
 @rem You should have a directory ..\viewerbuilddlls which has release versions of
-@rem all dependency dlls, including gtkmm & Qt core/ui/webkit, and also VS2008 
-@rem redistributable 
+@rem all dependency dlls, including Qt core/ui/webkit dll's, and also VS2008 
+@rem redistributable (vcredist_x86.exe) 
 @echo off
 rmdir build /S /Q
 md build
@@ -13,6 +13,8 @@ del build\viewerd.exe
 del build\modules\core\*d.dll
 del build\modules\core\OpenAL*.*
 del build\modules\core\DebugStats*.*
+del build\modules\core\Gtkmm*.*
+del build\modules\core\CommunicationUI*.*
 del build\modules\test\*.xml
 del build\modules\test\*.dll
 xcopy ..\viewerbuilddlls\*.* build /S /C
