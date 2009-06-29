b1f76d0e81d14425c3fbcaaec363e7d3dcb0e209
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-06-29 10:36:41 +0000

    Cleaned up makebuild.bat
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1070 5b2332b8-efa3-11de-8684-7d64432d61a3

--- makebuild.bat
+++ makebuild.bat
@@ -1,6 +1,6 @@
 @rem Makes a build directory of the viewer, so that you can run the NSIS install script
 @rem You should have a directory ..\viewerbuilddlls which has release versions of
-@rem all dependency dlls, including Qt core/ui/webkit dll's, and also VS2008 
+@rem all dependency dlls, including Qt core/ui/network/webkit/phonon dll's, and also VS2008 
 @rem redistributable (vcredist_x86.exe) 
 @echo off
 rmdir build /S /Q
@@ -12,12 +12,6 @@ rmdir build\testing /S /Q
 del build\*.dll
 del build\viewerd.exe
 del build\modules\core\*d.dll
-del build\modules\core\OpenAL*.*
-del build\modules\core\DebugStats*.*
-del build\modules\core\Gtkmm*.*
-del build\modules\core\CommunicationUI*.*
-del build\modules\test\*.xml
-del build\modules\test\*.dll
 xcopy ..\viewerbuilddlls\*.* build /S /C
 del build\pymodules\*.pyc
 del build\pymodules\apitest\*.pyc
