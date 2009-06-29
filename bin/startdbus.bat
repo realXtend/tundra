650943384a9fbdac48af6a9f4fc52041e06be5d8
mattiku <mattiku@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-06-29 14:00:50 +0000

    add environment variables for dbus daemon
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1079 5b2332b8-efa3-11de-8684-7d64432d61a3

--- bin/startdbus.bat
+++ bin/startdbus.bat
@@ -1,1 +1,8 @@
+:: session bus address
+set DBUS_SESSION_BUS_ADDRESS=tcp:host=localhost,port=12434
+
+:: system bus address
+set DBUS_SYSTEM_BUS_DEFAULT_ADDRESS=tcp:host=localhost,port=12434 
+
+:: Launch dbus daemon application
 telepathy\dbus-daemon.exe --config-file=data\session.conf
