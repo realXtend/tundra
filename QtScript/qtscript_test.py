16c4c5f114e10e0316013505394c1f477aad9c08
antont <antont@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-08-05 13:44:51 +0000

    tested connecting a qt signal to a slot. planning to test overriding default handlers with ones from js. now using old style pyqt signal support on 4.4, must upgrade to 4.5 to get the new style.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1115 5b2332b8-efa3-11de-8684-7d64432d61a3

--- QtScript/qtscript_test.py
+++ QtScript/qtscript_test.py
@@ -2,22 +2,47 @@
 defines qt properties and slots in py, and exposes them to js"""
 
 import sys
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4 import QtGui, QtCore
 from PyQt4.QtScript import QScriptEngine, QScriptValue
+from PyQt4.QtCore import QObject, pyqtSignature, pyqtProperty, QVariant, QTimer
 
 
 ps1 = ">>> "
 ps2 = "... "
 
-app = QApplication(sys.argv)
+def printhello():
+    print "Hello."
+
+app = QtGui.QApplication(sys.argv)
+window = QtGui.QWidget()
+# Create our main window using a plain QWidget.
+window.setWindowTitle("Signals")
+# Set our window's title as "Signals".
+button = QtGui.QPushButton("Press", window)
+# Create, with "Press" as its caption,
+# a child button in the window.
+# By specifying a parent object,
+# this new widget is automatically added to the same.
+button.resize(200, 40)
+# Resize our button to (200, 40) -> (X, Y)
+
+#old style signals, works in pyqt 4.4
+button.connect(button, QtCore.SIGNAL("clicked()"),\
+    printhello) #QtCore.SLOT("quit()"))
+
+#new style signals in 4.5, must upgdate to get these
+#button.clicked.connect(printhello)
+
+# Connect the button's click signal to the QApplication's quit() slot.
+window.show()
+# Show our window.
 
 class Python(QObject):
     def __init__(self):
         QObject.__init__(self)
         self.setObjectName("python")
         # Does not work as expected :(
-        self.setProperty("app", QVariant(qApp))
+        self.setProperty("app", QVariant(self))
         self.t = QTimer(self)
         self.t.setObjectName("timer")
 
@@ -37,10 +62,13 @@ py = Python()
 spy = engine.newQObject(py)
 engine.globalObject().setProperty("python", spy)
 
+#app.exec_()
+
 print "Ctrl+D to quit"
 prompt = ps1
 code = ""
 while True:
+    app.processEvents()
     line = raw_input(prompt)
     if not line.strip():
         continue
