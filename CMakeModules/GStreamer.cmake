68d15af4df9c74ab13b2fcbba54effde494d6c8b
mattiku <mattiku@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-09-04 12:43:57 +0000

    Added cmake files for glib, gstreamer, telepathy-farsight2 and telepathy-qt4 libraries. Note: only MSVC support implemented.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1192 5b2332b8-efa3-11de-8684-7d64432d61a3

---
+++ CMakeModules/GStreamer.cmake
@@ -0,0 +1,50 @@
+# NOTE: Only MSVC environment setup done
+
+include (glib)
+
+macro (FIND_GSTREAMER)
+  FIND_GLIB()
+endmacro(FIND_GSTREAMER)
+
+
+macro (INCLUDE_GSTREAMER)
+	INCLUDE_GLIB()
+    if (MSVC)
+	  include_directories (${REX_DEP_PATH}/gstreamer/include)
+	  link_directories (${REX_DEP_PATH}/gstreamer/lib)
+    else(NOT MSVC AND POCO_FOUND)
+	  include_directories(${GSTREAMER_INCLUDE_DIRS})
+	  link_directories(${GSTREAMER_LIBRARY_DIRS})
+	else()
+	  include_directories(${GSTREAMER_INCLUDE_DIR})
+	  link_directories(${GSTREAMER_LIBRARY_DIR})
+	endif (MSVC)
+	
+endmacro (INCLUDE_GSTREAMER)
+
+macro (LINK_GSTREAMER)
+  LINK_GLIB()
+  target_link_libraries (${TARGET_NAME} libavcodec.lib
+        libavformat.lib
+        libavutil.lib
+        libgstapp-0.10.lib
+        libgstaudio-0.10.lib
+        libgstbase-0.10.lib
+        libgstcontroller-0.10.lib
+        libgstdataprotocol-0.10.lib
+        libgstdshow-0.10.lib
+        libgstinterfaces-0.10.lib
+        libgstnet-0.10.lib
+        libgstnetbuffer-0.10.lib
+        libgstpbutils-0.10.lib
+        libgstreamer-0.10.lib
+        libgstriff-0.10.lib
+        libgstrtp-0.10.lib
+        libgstrtsp-0.10.lib
+        libgstsdp-0.10.lib
+        libgsttag-0.10.lib
+        libgstvideo-0.10.lib
+        libjpeg.lib
+        libjpeg-static.lib)
+
+endmacro (LINK_GSTREAMER)
---
+++ CMakeModules/TelepathyFarsight2.cmake
@@ -0,0 +1,27 @@
+
+macro (FIND_TELEPATHY_FARSIGHT2)
+endmacro (FIND_TELEPATHY_FARSIGHT2)
+
+
+macro (INCLUDE_TELEPATHY_FARSIGHT2)
+	
+    if (MSVC)
+	  include_directories (${REX_DEP_PATH}telepathy-farsight2/include)
+	  link_directories (${REX_DEP_PATH}/telepathy-farsight2/lib)
+    else(NOT MSVC AND TELEPATHY_FARSIGHT2_FOUND)
+	  include_directories(${TELEPATHY_FARSIGHT2_INCLUDE_DIRS})
+	  link_directories(${TELEPATHY_FARSIGHT2_LIBRARY_DIRS})
+	else()
+	  include_directories(${TELEPATHY_FARSIGHT2_INCLUDE_DIR})
+	  link_directories(${TELEPATHY_FARSIGHT2_LIBRARY_DIR})
+	endif (MSVC)
+	
+endmacro (INCLUDE_TELEPATHY_FARSIGHT2)
+
+macro (LINK_TELEPATHY_FARSIGHT2)
+  
+  if (MSVC)
+    target_link_libraries (${TARGET_NAME} gstfarsight.lib)
+  endif()
+
+endmacro (LINK_TELEPATHY_FARSIGHT2)
