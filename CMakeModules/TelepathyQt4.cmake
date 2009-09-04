b212b36e1fc57df6450dfa1305dd4d0f3769cb5b
mattiku <mattiku@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-09-04 12:43:57 +0000

    Added cmake files for glib, gstreamer, telepathy-farsight2 and telepathy-qt4 libraries. Note: only MSVC support implemented.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1192 5b2332b8-efa3-11de-8684-7d64432d61a3

---
+++ CMakeModules/TelepathyQt4.cmake
@@ -0,0 +1,28 @@
+# NOTE: Only MSVC environment setup done
+
+macro (FIND_TELEPATHY_QT4)
+endmacro (FIND_TELEPATHY_QT4)
+
+
+macro (INCLUDE_TELEPATHY_QT4)
+	
+    if (MSVC)
+	  include_directories (${REX_DEP_PATH}/telepathy-qt4/include)
+	  link_directories (${REX_DEP_PATH}/telepathy-qt4/lib)
+    else(NOT MSVC AND TELEPATHY_QT4_FOUND)
+	  include_directories(${TELEPATHY_QT4_INCLUDE_DIRS})
+	  link_directories(${TELEPATHY_QT4_LIBRARY_DIRS})
+	else()
+	  include_directories(${TELEPATHY_QT4_INCLUDE_DIR})
+	  link_directories(${TELEPATHY_QT4_LIBRARY_DIR})
+	endif (MSVC)
+	
+endmacro (INCLUDE_TELEPATHY_QT4)
+
+macro (LINK_TELEPATHY_QT4)
+  
+  if (MSVC)
+    target_link_libraries (${TARGET_NAME} telepathy-qt4.lib)
+  endif()
+
+endmacro (LINK_TELEPATHY_QT4)
---
+++ CMakeModules/glib.cmake
@@ -0,0 +1,27 @@
+# NOTE: Only MSVC environment setup done
+
+macro (FIND_GLIB)
+endmacro (FIND_GLIB)
+
+macro (INCLUDE_GLIB)
+	
+    if (MSVC)
+	  include_directories (${REX_DEP_PATH}/glib/include)
+	  link_directories (${REX_DEP_PATH}/glib/lib)
+    else(NOT MSVC AND GLIB_FOUND)
+	  include_directories(${GLIB_INCLUDE_DIRS})
+	  link_directories(${GLIB_LIBRARY_DIRS})
+	else()
+	  include_directories(${GLIB_INCLUDE_DIR})
+	  link_directories(${GLIB_LIBRARY_DIR})
+	endif (MSVC)
+	
+endmacro (INCLUDE_GLIB)
+
+macro (LINK_GLIB)
+  
+  if (MSVC)
+    target_link_libraries (${TARGET_NAME} glib-2.0.lib gobject-2.0.lib)
+  endif()
+
+endmacro (LINK_GLIB)
