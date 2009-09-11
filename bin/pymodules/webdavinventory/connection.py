ac7ba2330a18fb8d039f9292cf07fcaeb1106ce1
jonnenau <jonnenau@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-09-11 00:34:46 +0000

    * Created HTTPClient class that will handle (with pythons httplib) getting users identity and users webdav url via CBs SimpleServicesGetter * Implemented deleteResource and deleteDirectory * Added a lot of exception handling to every where its needed * Renaming attributes and methods to make more sense to people reading the code * Implemented new GUI class UserDefinesSettings to get manual input if automatic retrieving fails * Implemented HTTPClient and WebDavClient initialisations and handling to MainFunction
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1213 5b2332b8-efa3-11de-8684-7d64432d61a3

--- bin/pymodules/webdavinventory/connection.py
+++ bin/pymodules/webdavinventory/connection.py
@@ -4,46 +4,105 @@ Created on 8.9.2009
 @author: jonnena
 '''
 
+import httplib
+
+from httplib import HTTPConnection, HTTPException
 from webdav import WebdavClient
 from webdav.Connection import WebdavError, AuthorizationError
+from PyQt4.QtGui import QInputDialog, QLineEdit
 
-class WebDav(object):
+CABLEBEACH_IDENTITY_HEADER = "CableBeach-Identity"
+CABLEBEACH_WEBDAV_HEADER = "CableBeach-WebDavInventory"
+
+class HTTPClient(object):
     
+    httpclient = None
+    identityQueryString= None
+    
+    def __init__(self):
+        pass
+    
+    def setupConnection(self, host, identityType, identity = None, firstName = None, lastName = None):
+        strict = None
+        port = None
+        timeout = 10
+        if (self.createRequestURL(identityType, identity, firstName, lastName) == False):
+            return False
+        """ host = 'www.example.com:port' NO http:// or / at end """
+        self.httpclient = HTTPConnection(host, port, strict, timeout)
+    
+    def createRequestURL(self, identityType, identity, firstName, lastName):
+        self.identityQueryString = "/services/webdav?type=" + identityType + "&"
+        if (identityType == "openid" and identity != None):
+            self.identityQueryString += "identity=" + identity
+        elif (identityType == "normal" and firstName != None and lastName != None):
+            self.identityQueryString += "firstname=" + firstName + "&lastname=" + lastName
+        else:
+            return False
+        
+    def requestIdentityAndWebDavURL(self):
+        identityurl = None
+        webdavurl = None
+        if (self.httpclient != None):
+            try:
+                self.httpclient.request("GET", self.identityQueryString)
+            except httplib.socket.error:
+                raise httplib.error
+            response = self.httpclient.getresponse()
+            if (response.status == httplib.OK):
+                identityurl = response.getheader(CABLEBEACH_IDENTITY_HEADER)
+                webdavurl = response.getheader(CABLEBEACH_WEBDAV_HEADER)
+            elif (response.status == httplib.NOT_FOUND):
+                identityurl = None
+                webdavurl = None
+            self.httpclient.close()
+            return identityurl, webdavurl
+
+class WebDavClient(object):
+        
     connection = None
     resource = None
     url = None
     user = None
     password = None
     
-    def __init__(self):
-        print "[Connection] Initilizing webdav connection to %s with user %s" % (self.url, self.user)
-    
-    def setConnection(self, url, user, password):
-        self.url = url
+    def __init__(self, identityurl, webdavurl):
+        self.user = identityurl
+        self.url = webdavurl
+        
+    def setHostAndUser(self, user, url):
         self.user = user
-        self.password = ""# = password # change to None when really  using passwords
+        self.url = url
+    
+    def setupConnection(self):
         if (self.connection != None):
             self.connection = None
         try:
             self.resource = WebdavClient.CollectionStorer(self.url, self.connection)
-            self.tryDigestAuth()
+            self.determineAuthentication()
             return True
         except WebdavError:
-            return False
+            raise HTTPException
             
-    def tryDigestAuth(self):
+    def determineAuthentication(self):
         authFailures = 0
         while authFailures < 2:
             try:
                 self.resource.getCollectionContents()
             except AuthorizationError as e:                           
                 if e.authType == "Basic":
-                    self.resource.connection.addBasicAuthorization(self.user, self.password)
+                    #self.password = QInputDialog.getText(None, "WebDav Inventory Password", "WebDav Inventory is asking for Basic authentication\n\nPlease give your password:", QLineEdit.Normal, "")
+                    self.password = ""
+                    self.resource.connection.addBasicAuthorization(self.user, str(self.password))
                 elif e.authType == "Digest":
+                    #self.password = QInputDialog.getText(None, "WebDav Inventory Password", "WebDav Inventory is asking for Digest authentication\n\nPlease give your password:", QLineEdit.Normal, "")
+                    self.password = ""
                     info = WebdavClient.parseDigestAuthInfo(e.authInfo)
                     self.resource.connection.addDigestAuthorization(self.user, self.password, realm=info["realm"], qop=info["qop"], nonce=info["nonce"])
                 else:
-                    raise           
+                    raise WebdavError
+            except HTTPException:
+                raise HTTPException  
             authFailures += 1
         
     def listResources(self, path):
@@ -70,17 +129,12 @@ class WebDav(object):
             return True
         except WebdavError:
             return False
-
-    def setResourceStorerToPath(self, path):
-        self.resource = WebdavClient.ResourceStorer(self.url + path, None)
-        self.tryDigestAuth()
-        return True
     
     def downloadFile(self, filePath, collectionWebDavPath, resourceWebDavName):
         if ( self.setCollectionStorerToPath(collectionWebDavPath) ):
             try:
-                child = self.resource.getResourceStorer(resourceWebDavName)
-                child.downloadFile(filePath + "/" + resourceWebDavName)
+                webdavResource = self.resource.getResourceStorer(resourceWebDavName)
+                webdavResource.downloadFile(filePath + "/" + resourceWebDavName)
                 return True
             except Exception:
                 return False
@@ -92,10 +146,13 @@ class WebDav(object):
             dataFile= open(filePath, 'rb')
         except IOError:
             return False
-        if ( self.setCollectionStorerToPath(collectionWebDavPath) ):         
-            self.resource.addResource(resourceWebDavName, dataFile.read())
-            dataFile.close()
-            return True
+        if ( self.setCollectionStorerToPath(collectionWebDavPath) ):
+            try:     
+                self.resource.addResource(resourceWebDavName, dataFile.read())
+                dataFile.close()
+                return True
+            except WebdavError, IOError:
+                return False
         else:
             return False
         
@@ -107,3 +164,21 @@ class WebDav(object):
             except WebdavError:
                 return False
             
+    def deleteResource(self, collectionWebDavPath, resourceWebDavName):
+        if (self.setCollectionStorerToPath(collectionWebDavPath)):
+            try:
+                webdavResource = self.resource.getResourceStorer(resourceWebDavName)
+                webdavResource.delete()
+                return True
+            except WebdavError:
+                return False
+        else:
+            return False
+        
+    def deleteDirectory(self, collectionWebDavPath, collectionName):
+        if (self.setCollectionStorerToPath(collectionWebDavPath)):
+            try:
+                self.resource.deleteResource(collectionName)
+                return True
+            except WebdavError:
+                return False
