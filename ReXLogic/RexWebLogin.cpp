4e0ae3c9379020aa6fac4c579cd6eca4f263269a
jonnenau <jonnenau@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-09-16 19:18:55 +0000

    * RexWebLogin: now working browser with weview, addressbar, "gobutton", satusbar with progress bar. Is not shown anywhere, will come later when doing weblogin via openid or facebook etc. * NOTE: Run prebuild before building to make moc file of RexWebLogin.h, otherwise will get linker error. RexLogicModule CMakeLists modified accordingly.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1246 5b2332b8-efa3-11de-8684-7d64432d61a3

--- ReXLogic/RexWebLogin.cpp
+++ ReXLogic/RexWebLogin.cpp
@@ -10,10 +10,11 @@
 
 namespace RexLogic
 {
-	RexWebLogin::RexWebLogin(QWidget *parent)
-		: QWidget(parent)
+	RexWebLogin::RexWebLogin(QWidget *parent, QString address)
+		: QWidget(parent), address_(address)
 	{
 		initWidget();
+		connectSignals();
 	}
 
 	RexWebLogin::~RexWebLogin()
@@ -21,28 +22,42 @@ namespace RexLogic
 
 	}
 
+	void RexWebLogin::showEvent(QShowEvent *showEvent)
+	{
+		this->webView_->setUrl(QUrl(address_));
+		QWidget::showEvent(showEvent);
+	}
+
 	void RexWebLogin::initWidget()
 	{
 		// Load ui to widget from file
 		QUiLoader loader;
         QFile uiFile("./data/ui/weblogin.ui");
-		this->widget_ = loader.load(&uiFile, this);
+		this->widget_ = loader.load(&uiFile);
 		this->widget_->setMinimumSize(750, 550);
 		uiFile.close();
 
+		// Get ui elements we want to update in signal processing
+		progressBar = this->widget_->findChild<QProgressBar *>("progressBar_Status");
+		progressBar->hide();
+		statusLabel = this->widget_->findChild<QLabel *>("label_Status");
+
 		// Add webview by hand, the QFormBuilder cannot do this (custom widget)
 		QVBoxLayout *centerLayout = this->widget_->layout()->findChild<QVBoxLayout *>("verticalLayout_Container");
-		this->webView_ = new QWebView(this);
-		this->webView_->setUrl(QUrl("http://www.google.com"));
+		this->webView_ = new QWebView(this->widget_);
 		centerLayout->insertWidget(2, this->webView_);
 
+		// Get comboBox and set url to it
+		comboBoxAddress = this->widget_->findChild<QComboBox *>("comboBox_Address");
+		comboBoxAddress->setEditText(address_);
+
 		// Add image to button
-		QPushButton *goButton = this->widget_->findChild<QPushButton *>("pushButton_Go");
+		goButton = this->widget_->findChild<QPushButton *>("pushButton_Go");
 		goButton->setIcon(QIcon("./data/ui/images/arrow_right_green_48.png"));
 		goButton->setIconSize(QSize(20, 20));
 		
 		// But widget to layout, set layout to this
-		this->layout_ = new QVBoxLayout(this);
+		this->layout_ = new QVBoxLayout(0);
 		this->layout_->setSpacing(0);
 		this->layout_->setMargin(0);
 		this->layout_->addWidget(this->widget_);
@@ -51,7 +66,45 @@ namespace RexLogic
 
 	void RexWebLogin::connectSignals()
 	{
-		// webview finished to progressbar and statuslabel
-		// webview to parsehandler after load is done, see RexCBLogin for handling/parsing
+		QObject::connect(this->comboBoxAddress->lineEdit(), SIGNAL( returnPressed() ), this, SLOT( goToUrl() ));
+		QObject::connect(this->goButton, SIGNAL( clicked(bool) ), this, SLOT( goToUrl(bool) ));
+		QObject::connect(this->webView_, SIGNAL( loadStarted() ), this, SLOT( loadStarted() ));
+		QObject::connect(this->webView_, SIGNAL( loadProgress(int) ), this, SLOT( updateUi(int) ));
+		QObject::connect(this->webView_, SIGNAL( loadFinished(bool) ), this, SLOT( processPage(bool) ));
 	}
+
+	void RexWebLogin::goToUrl()
+	{
+		goToUrl(true);
+	}
+
+	void RexWebLogin::goToUrl(bool checked)
+	{
+		this->webView_->setUrl(QUrl(this->comboBoxAddress->lineEdit()->text()));
+	}
+
+	void RexWebLogin::loadStarted()
+	{
+		this->statusLabel->setText("Loading page...");
+		this->progressBar->show();
+	}
+
+	void RexWebLogin::updateUi(int progress)
+	{
+		this->progressBar->setValue(progress);
+	}
+
+	void RexWebLogin::processPage(bool success)
+	{
+		address_ = this->webView_->url().toString();
+		this->comboBoxAddress->lineEdit()->setText(address_);
+		if ( this->comboBoxAddress->findText(address_, Qt::MatchFixedString) == -1 )
+			this->comboBoxAddress->addItem(address_);
+		this->statusLabel->setText("Done");
+		this->progressBar->hide();
+
+		// Do actual HTML page processing...
+	}
+	
+
 }
