eaa961f703ae0833ca50aa46770dd620077bcc80
mattiku <mattiku@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-09-17 13:54:40 +0000

    fix memory leaks CommunicationManger can now terminate dbus daemon process which it start. added more IM class skeletons
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1250 5b2332b8-efa3-11de-8684-7d64432d61a3

--- CommunicationModule/CommunicationModule.h
+++ CommunicationModule/CommunicationModule.h
@@ -2,10 +2,9 @@
 #define incl_CommunicationModule_h
 
 #include "Foundation.h"
-//#include "ModuleInterface.h"
+#include "ModuleInterface.h"
 
 #include "CommunicationModuleApi.h"
-//#include "CommunicationManager.h"
 #include "TpCommunicationManager.h"
 #include "ConsoleUI.h"
 #include "QtUI.h"
@@ -25,9 +24,6 @@
  */
 namespace Communication
 {
-  //  class CommunicationManager;
-//	typedef boost::shared_ptr<CommunicationManager> CommunicationManagerPtr;    
-
 //	class TpQt4CommunicationManager;
 //	typedef boost::shared_ptr<TpQt4CommunicationManager> TpQt4CommunicationManagerPtr;    
     
@@ -52,14 +48,9 @@ namespace Communication
 		static const Foundation::Module::Type type_static_ = Foundation::Module::MT_Communication;
 
 	private:
-		//CommunicationManagerPtr communication_manager_;
 		TpQt4Communication::CommunicationManager* communication_manager_;
-//		TpQt4Communication::Connection* my_connection_;
-//		TpQt4Communication::TextChatSessionPtr my_chat_session_;
 		CommunicationUI::ConsoleUI* console_ui_;
 		CommunicationUI::QtUI* qt_ui_;
-		//TpQt4CommunicationManagerPtr comm_;
-		//TpQt4CommunicationManager* comm_;
 	};
 }
 
