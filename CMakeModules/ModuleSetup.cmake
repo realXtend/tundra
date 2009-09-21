fb751349b705ff5aff906c9ce5e4bfb2c604cd2d
sempuki <sempuki@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-09-21 10:07:25 +0000

    convert cmake build to init_target macro
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1261 5b2332b8-efa3-11de-8684-7d64432d61a3

---
+++ CMakeModules/ModuleSetup.cmake
@@ -0,0 +1,147 @@
+# Naali build macros
+
+# =============================================================================
+# reusable macros
+
+# define target name, and directory, if it should be output
+# ARGV1 is directive to output, and ARGV2 is where to
+macro (init_target NAME)
+
+    # Define target name and output directory
+    set (TARGET_NAME ${NAME})
+    set (${TARGET_NAME}_OUTPUT ${ARGV1})
+
+    message (STATUS "Found build target: " ${TARGET_NAME})
+
+    # if OUTPUT is defined
+    if (${TARGET_NAME}_OUTPUT)# AND ${TARGET_NAME}_OUTPUT STREQUAL "OUTPUT")
+        
+        #message (STATUS "target dir: " ${ARGV2})
+        set (TARGET_DIR ${PROJECT_BINARY_DIR}/bin/${ARGV2})
+
+        if (MSVC)
+            # export symbols
+            add_definitions (-DMODULE_EXPORTS)
+
+            # copy to target directory
+            add_custom_command (TARGET ${TARGET_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} ARGS -E make_directory ${TARGET_DIR})
+            add_custom_command (TARGET ${TARGET_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} ARGS -E copy_if_different \"$(TargetPath)\" ${TARGET_DIR})
+        else ()
+            # copy to target directory
+            set (LIBRARY_OUTPUT_PATH ${TARGET_DIR})
+        endif ()
+    endif ()
+
+    # Disable warnings
+    if (MSVC)
+        add_definitions (-D_CRT_SECURE_NO_WARNINGS)
+    endif ()
+endmacro (init_target)
+
+# include and lib directories, and definitions
+macro (use_package PREFIX)
+    include_directories (${${PREFIX}_INCLUDE_DIRS})
+    link_directories (${${PREFIX}_LIBRARY_DIRS})
+    add_definitions (${${PREFIX}_DEFINITIONS})
+endmacro (use_package)
+
+# include and link local modules
+macro (use_internal_module MODULES TARGET_NAME)
+    foreach (module_ ${MODULES})
+        include_directories (../${module_})
+        target_link_libraries (${TARGET_NAME} ${module_})
+    endforeach ()
+endmacro (use_internal_module)
+
+# link directories
+macro (link_package PREFIX TARGET_NAME)
+    target_link_libraries (${TARGET_NAME} ${${PREFIX}_LIBRARIES})
+endmacro (link_package)
+
+# =============================================================================
+# per-project macros
+
+macro (configure_boost)
+    set (Boost_USE_STATIC_LIBS ON)
+    sagase_configure_package (BOOST 
+        NAMES Boost boost
+        COMPONENTS date_time filesystem system thread program_options unit_test_framework
+        PREFIXES $ENV{NAALI_DEP_PATH})
+endmacro (configure_boost)
+
+macro (configure_poco)
+    sagase_configure_package (POCO 
+        NAMES Poco PoCo poco
+        COMPONENTS Poco PocoFoundation PocoNet PocoUtil PocoXML
+        PREFIXES $ENV{NAALI_DEP_PATH})
+endmacro (configure_poco)
+
+macro (configure_qt4)
+    sagase_configure_package (QT4 
+        NAMES Qt4 qt4 Qt qt
+        COMPONENTS QtCore QtGui QtWebkit QtScript QtXml QtNetwork QtUiTools
+        PREFIXES $ENV{NAALI_DEP_PATH} $ENV{QTDIR})
+
+    # FindQt4.cmake does it's own thing...
+    if (QT4_FOUND)
+
+        include (${QT_USE_FILE})
+
+        set (QT4_INCLUDE_DIRS 
+            ${QT_INCLUDE_DIR}
+            ${QT_QTCORE_INCLUDE_DIR}
+            ${QT_QTDBUS_INCLUDE_DIR}
+            ${QT_QTGUI_INCLUDE_DIR}
+            ${QT_QTUITOOLS_INCLUDE_DIR}
+            ${QT_QTNETWORK_INCLUDE_DIR}
+            ${QT_QTXML_INCLUDE_DIR}
+            ${QT_QTSCRIPT_INCLUDE_DIR}
+            ${QT_QTWEBKIT_INCLUDE_DIR}
+            ${QT_PHONON_INCLUDE_DIR})
+
+        set (QT4_LIBRARY_DIR  
+            ${QT_LIBRARY_DIR})
+
+        set (QT4_LIBRARIES 
+            ${QT_LIBRARIES}
+            ${QT_QTCORE_LIBRARY}
+            ${QT_QTDBUS_LIBRARY}
+            ${QT_QTGUI_LIBRARY}
+            ${QT_QTUITOOLS_LIBRARY}
+            ${QT_QTNETWORK_LIBRARY}
+            ${QT_QTXML_LIBRARY}
+            ${QT_QTSCRIPT_LIBRARY}
+            ${QT_QTWEBKIT_LIBRARY}
+            ${QT_PHONON_LIBRARY})
+    endif ()
+endmacro (configure_qt4)
+
+macro (configure_ois)
+    sagase_configure_package (OIS 
+        NAMES Ois ois OIS 
+        COMPONENTS OIS
+        PREFIXES $ENV{NAALI_DEP_PATH})
+endmacro (configure_ois)
+
+macro (link_OIS ${TARGET_NAME})
+    if (MSVC)
+        target_link_libraries (${TARGET_NAME} debug OIS_d optimized OIS)
+    else ()
+        target_link_libraries (${TARGET_NAME} ${OIS_LIBRARIES})
+    endif ()
+endmacro (link_OIS)
+
+macro (configure_ogre)
+    sagase_configure_package (OGRE 
+        NAMES Ogre OgreSDK ogre OGRE
+        COMPONENTS Ogre ogre OGRE OgreMain 
+        PREFIXES $ENV{NAALI_DEP_PATH} $ENV{OGRE_HOME})
+endmacro (configure_ogre)
+
+macro (link_ogre ${TARGET_NAME})
+    if (MSVC)
+        target_link_libraries (${TARGET_NAME} debug OgreMain_d optimized OgreMain)
+    else ()
+        target_link_libraries (${TARGET_NAME} ${OGRE_LIBRARIES})
+    endif ()
+endmacro (link_ogre)
---
+++ CMakeModules/Sagase.cmake
@@ -0,0 +1,247 @@
+# -- Ryan McDougall, realXtend -- 2009
+
+# parse a keyword delimited list of arguments into single list
+# results are in OUT
+function (sagase_parse_arguments BEGIN END ARGS)
+    set (take FALSE)
+    foreach (var ${ARGN})
+        if (${var} STREQUAL ${BEGIN})
+            set (take TRUE)
+        elseif (${var} STREQUAL ${END})
+            set (take FALSE)
+        else ()
+            if (take)
+                set (out ${out} ${var})
+            endif ()
+        endif ()
+    endforeach ()
+
+    set (${ARGS} ${out} PARENT_SCOPE)
+endfunction (sagase_parse_arguments)
+
+
+# generates a set of likely paths for a files (includes or libraries)
+# "NAMES" is a list of names by which the package is known
+# "PREFIXES" is a list of path prefixes where the components might be found
+# results are in INCLUDE_PATHS, LIBRARY_PATHS  
+function (sagase_generate_paths INCLUDE_PATHS LIBRARY_PATHS)
+    sagase_parse_arguments ("NAMES" "PREFIXES" path_names ${ARGN})
+    sagase_parse_arguments ("PREFIXES" none path_prefixes ${ARGN})
+
+    if (MSVC)
+        set (path_prefixes ${path_prefixes} "C:/")
+    elseif (APPLE)
+        message (FATAL_ERROR "sagase: MacOS Support incomplete")
+    elseif (UNIX)
+        set (path_prefixes ${path_prefixes} "/usr" "/usr/local" "/opt")
+    else ()
+        message (FATAL_ERROR "sagase: Unable to detect OS type")
+    endif ()
+
+    # add prefix paths
+    foreach (prefix ${path_prefixes})
+
+        set (includes ${includes} 
+            ${prefix}/includes)
+
+        set (libraries ${libraries} 
+            ${prefix}/lib 
+            ${prefix}/bin 
+            ${prefix}/dll)
+
+    endforeach ()
+
+    # add prefix+name paths
+    foreach (prefix ${path_prefixes})
+        foreach (pkgname ${path_names})
+
+            set (includes ${includes} 
+                ${prefix}/${pkgname}/includes 
+                ${prefix}/includes/${pkgname})
+
+            set (libraries ${libraries} 
+                ${prefix}/${pkgname}/lib ${prefix}/lib/${pkgname}
+                ${prefix}/${pkgname}/bin ${prefix}/bin/${pkgname}
+                ${prefix}/${pkgname}/dll ${prefix}/dll/${pkgname})
+
+        endforeach ()
+    endforeach ()
+
+    set (${INCLUDE_PATHS} ${includes} PARENT_SCOPE)
+    set (${LIBRARY_PATHS} ${libraries} PARENT_SCOPE)
+
+endfunction (sagase_generate_paths)
+
+# tries a series of methods to find correct compile and link info.
+# "NAMES" is a list of names by which the package is known. used by
+# find_package and in constructing search path names.
+# "COMPONENTS" is a list of sub-components that are required. used by
+# find_package and by pkg-config.
+# "PREFIXES" is a list of path prefixes where the components might be found.
+# "SKIP_FIND_PACKAGE" causes it to bypass find_package
+# results are in ${PREFIX}_INCLUDE_DIRS, ${PREFIX}_LIBRARY_DIRS, 
+# ${PREFIX}_LIBRARIES, ${PREFIX}_DEFINITIONS, or fatal error.
+
+# Tryies the following methods to find a package, in order of decreasing
+# specialization or detail:
+# 1. find_package
+# 2. OS-dependent (pkg-config, framework, etc.)
+# 3. find_path 
+
+# "NAMES" and "COMPONENTS" mean slightly different things depending on the
+# seach methond. For find_package, consult the cmake documentation. For
+# pkg-config, only the "NAMES" are used. For find_path/brute force, "NAMES"
+# are possible directory names, and "COMPONENTS" are the possible header or
+# library filenames.
+
+# example usages: 
+# sagase_configure_package (BOOST 
+#     NAMES BOOST Boost 
+#     COMPONENTS date_time filesystem system thread
+#     PREFIXES "C:")
+#
+# sagase_configure_package (GLIB
+#     NAMES glib-2.0
+#     COMPONENTS glib glib-2.0)
+#
+# sagase_configure_package (CAELUM
+#     NAMES Caelum
+#     COMPONENTS Caelum
+#     PREFIXES $ENV{NAALI_DEP_PATH})
+
+
+macro (sagase_configure_package PREFIX)
+    sagase_parse_arguments ("NAMES" "COMPONENTS" PKG_NAMES ${ARGN})
+    sagase_parse_arguments ("COMPONENTS" "PREFIXES" PKG_COMPONENTS ${ARGN})
+    sagase_parse_arguments ("PREFIXES" none PKG_PREFIXES ${ARGN})
+
+    set (found_ FALSE)
+
+    foreach (name_ ${PKG_NAMES})
+
+        string (TOUPPER ${name_} name_upper_)
+
+        # find_package can't handle packages in all caps.
+        # cmake is a macro language, which means it defines global variables,
+        # which it tries to namespace by choosing all caps variable names,
+        # when searching for a package who's name is all caps, this appears to
+        # cause a name collision, and is thus bypassed.
+        if (NOT name_ STREQUAL name_upper_)
+            message (STATUS "trying find_package: " ${name_})
+
+            # try built-in CMake modules first
+            find_package (${name_} QUIET COMPONENTS ${PKG_COMPONENTS})
+        endif ()
+
+        if (${name_}_FOUND OR ${name_upper_}_FOUND)
+            message (STATUS "sagase: configured " ${PREFIX})
+            set (${PREFIX}_INCLUDE_DIRS ${${name_}_INCLUDE_DIRS})
+            set (${PREFIX}_LIBRARIES ${${name_}_LIBRARIES})
+            set (${PREFIX}_LIBRARY_DIRS ${${name_}_LIBRARY_DIRS})
+            set (${PREFIX}_DEFINITIONS ${${name_}_CFLAGS_OTHER})
+            set (found_ TRUE)
+            break ()
+
+        else ()
+            # try system module manager
+
+            if (MSVC)
+                # MS has no automatic module management
+
+            elseif (APPLE)
+                # I don't know how to use Apple "Framework"
+                message (FATAL_ERROR "sagase: " ${PREFIX} ": MacOS Support incomplete")
+
+            elseif (UNIX)
+                # what else is there besides pkg-config?
+                # non-linux OSes may be a problem
+                message (STATUS "tring pkg_check_modules: " ${name_})
+
+                include (FindPkgConfig)
+                if (PKG_CONFIG_FOUND)
+                    pkg_check_modules(${PREFIX} ${name_})
+                else ()
+                    message (STATUS "pkg-config cannot be found.")
+                endif ()
+
+                if (${PREFIX}_FOUND)
+                    message (STATUS "sagase: configured " ${PREFIX})
+                    # already set: ${PREFIX}_INCLUDE_DIRS, 
+                    # ${PREFIX}_LIBRARY_DIRS, ${PREFIX}_LIBRARIES
+                    set (${PREFIX}_DEFINITIONS ${${PREFIX}_CFLAGS_OTHER})
+                    set (found_ TRUE)
+                    break ()
+                endif ()
+
+            else ()
+                message (FATAL_ERROR "sagase: " ${PREFIX} ": Unable to detect OS type")
+            endif ()
+
+        endif ()
+    endforeach ()
+
+    if (NOT found_)
+        message (STATUS "trying brute-force search ")
+
+        # generate a combination of possible search paths
+        sagase_generate_paths (include_paths library_paths NAMES ${PKG_NAMES} PREFIXES ${PKG_PREFIXES})
+
+        # follow platform library naming
+        if (MSVC)
+            set (LIB_PREFIX "")
+            set (LIB_POSTFIXES ".lib")
+        elseif (UNIX)
+            set (LIB_PREFIX "lib")
+            set (LIB_POSTFIXES ".so" ".a")
+        else ()
+            message (FATAL_ERROR "sagase: " ${PREFIX} ": Unable to detect OS type")
+        endif ()
+
+        # try using "COMPONENTS" as possible file names (without prefix or extension)
+        foreach (component_ ${PKG_COMPONENTS})
+            
+            # get header path
+            find_path (${PREFIX}_${component_}_INCLUDE_DIR ${component_}.h ${include_paths})
+            
+            if (${PREFIX}_${component_}_INCLUDE_DIR)
+                set (${PREFIX}_INCLUDE_DIRS ${${PREFIX}_INCLUDE_DIRS} ${${PREFIX}_${component_}_INCLUDE_DIR})
+            endif ()
+
+            # get library path
+            foreach (extension_ ${LIB_POSTFIXES})
+                find_path (${PREFIX}_${component_}_LIBRARY_DIR ${LIB_PREFIX}${component_}${extension_} ${library_paths})
+
+                if (${PREFIX}_${component_}_LIBRARY_DIR)
+                    set (${PREFIX}_LIBRARIES ${${PREFIX}_LIBRARIES} ${component_})
+                    set (${PREFIX}_LIBRARY_DIRS ${${PREFIX}_LIBRARY_DIRS} ${${PREFIX}_${component_}_LIBRARY_DIR})
+                endif ()
+            endforeach ()
+        endforeach ()
+    endif ()
+
+    # stop process if nothing is found through any means
+    if (NOT found_ AND NOT ${PREFIX}_INCLUDE_DIRS AND NOT ${PREFIX}_LIBRARY_DIRS AND NOT ${PREFIX}_LIBRARIES)
+        message (FATAL_ERROR "!! sagase: unable to configure " ${PREFIX}) 
+    endif ()
+    
+    # remove duplicate entires from return variables
+    if (${PREFIX}_INCLUDE_DIRS)
+        list (REMOVE_DUPLICATES ${PREFIX}_INCLUDE_DIRS)
+    endif ()
+
+    if (${PREFIX}_LIBRARY_DIRS)
+        list (REMOVE_DUPLICATES ${PREFIX}_LIBRARY_DIRS)
+    endif ()
+
+    if (${PREFIX}_LIBRARIES)
+        list (REMOVE_DUPLICATES ${PREFIX}_LIBRARIES)
+    endif ()
+
+    # report to screen what was found
+    message (STATUS "sagase: " ${PREFIX} " Configure Results.")
+    message (STATUS "-- Include Directories: " ${${PREFIX}_INCLUDE_DIRS})
+    message (STATUS "-- Libarary Directories: " ${${PREFIX}_LIBRARY_DIRS})
+    message (STATUS "-- Libraries: " ${${PREFIX}_LIBRARIES})
+    message (STATUS "-- Defines: " ${${PREFIX}_DEFINITIONS})
+
+endmacro (sagase_configure_package)
