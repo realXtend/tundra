9a4375558125dd665cd1606317e1dd685627342f
jonnenau <jonnenau@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-09-21 11:45:42 +0000

    * Initial work for communications UI work. Will continue when getting actual data from CommunicationManager.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1267 5b2332b8-efa3-11de-8684-7d64432d61a3

--- CommunicationModule/QtUI.h
+++ CommunicationModule/QtUI.h
@@ -4,13 +4,55 @@
 #include "StableHeaders.h"
 #include "Foundation.h"
 
+#include <QtGui>
+
 namespace CommunicationUI
 {
-	class QtUI
+	class QtUI : public QWidget
 	{
+	
+	Q_OBJECT
+
 	public:
-		QtUI(Foundation::Framework* framework);
+		QtUI(QWidget *parent, Foundation::Framework* framework);
 		~QtUI(void);
+
+	private:
+		void initWidget();
+		void setAllEnabled(bool enabled);
+
+		QLayout *layout_;
+		QWidget *widget_;
+		QTabWidget *tabWidgetCoversations_;
+		QPushButton *buttonSendMessage_;
+		QLineEdit *lineEditMessage_;
+		QLabel *labelUsername_;
+		QLineEdit *lineEditStatus_;
+		QComboBox *comboBoxStatus_;
+		QLabel *connectionStatus_;
+
+	};
+
+	// CONVERSATION CLASS
+
+	class Conversation : public QWidget
+	{
+	
+	Q_OBJECT
+
+	friend class QtUI;
+
+	public:
+		Conversation(QWidget *parent); // Add as inparam also the "conversation object" from mattiku
+		~Conversation(void);
+
+	private:
+		void initWidget();
+
+		QLayout *layout_;
+		QWidget *widget_;
+		QPlainTextEdit *textEditChat_;
+		
 	};
 
 } //end if namespace: CommunicationUI
