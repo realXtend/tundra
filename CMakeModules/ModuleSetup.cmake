e63520a3446653bc3e4489d2b334ff1cb52bc238
sempuki <sempuki@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-09-22 14:44:23 +0000

    split ModuleSetup in 2, move output commands into finalize_target macro
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1307 5b2332b8-efa3-11de-8684-7d64432d61a3

--- CMakeModules/ModuleSetup.cmake
+++ CMakeModules/ModuleSetup.cmake
@@ -1,15 +1,17 @@
 # Naali build macros
 #
 # Generally used as follows:
-# 1. call configure_${PACKAGE}() once
-# 2. call init_target (${NAME}) once per build target
-# 3. call build_library/executable() on the source files
-# 4. call use_framework_modules() with a list of framework module names
-# 4. call use_module_headers() with a list of local module names for includes
-# 4. call use_module_libraries() with a list of local module names libraries
-# 4. call use_package (${PACKAGE}) once per build target
-# 5. call copy_target () at the end of build target's cmakelists.txt
+# 1. call configure_${PACKAGE}() once from the main CMakeLists.txt
+# 2. call init_target (${NAME}) once per build target in the module CMakeLists.txt
+# 3. call use_package (${PACKAGE}) once per build target
+# 3. call use_modules() with a list of local module names for includes
+# 4. call build_library/executable() on the source files
+# 5. call use_framework_modules() with a list of framework module names
+# 5. call link_package (${PACKAGE}) once per build target
+# 5. call link_modules() with a list of local module names libraries
+# 6. call final_target () at the end of build target's cmakelists.txt
 #    (not needed for lib targets, only exe's & modules)
+
 # =============================================================================
 # reusable macros
 
@@ -27,35 +29,31 @@ macro (init_target NAME)
     include_directories (${ENV_NAALI_DEP_PATH}/include)
     link_directories (${ENV_NAALI_DEP_PATH}/lib)
 
-    # if OUTPUT is defined
-    if (${TARGET_NAME}_OUTPUT)# AND ${TARGET_NAME}_OUTPUT STREQUAL "OUTPUT")
-        
-        #message (STATUS "target dir: " ${ARGV2})
+    # set TARGET_DIR
+    if (${TARGET_NAME}_OUTPUT)
         set (TARGET_DIR ${PROJECT_BINARY_DIR}/bin/${ARGV2})
 
         if (MSVC)
             # export symbols, copy needs to be added via copy_target
             add_definitions (-DMODULE_EXPORTS)
+        endif ()
+    endif ()
+endmacro (init_target)
+
+macro (final_target)
+    # set TARGET_DIR
+    if (TARGET_DIR)
+        if (MSVC)
+            # copy to target directory
+            add_custom_command (TARGET ${TARGET_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} ARGS -E make_directory ${TARGET_DIR})
+            add_custom_command (TARGET ${TARGET_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} ARGS -E copy_if_different \"$(TargetPath)\" ${TARGET_DIR})
         else ()
             # set target directory
             set (LIBRARY_OUTPUT_PATH ${TARGET_DIR})
             set (EXECUTABLE_OUTPUT_PATH ${TARGET_DIR})
         endif ()
     endif ()
-
-    # Disable warnings
-    if (MSVC)
-        add_definitions (-D_CRT_SECURE_NO_WARNINGS)
-    endif ()
-endmacro (init_target)
-
-macro (copy_target)
-    if (MSVC)
-        # copy to target directory
-        add_custom_command (TARGET ${TARGET_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} ARGS -E make_directory ${TARGET_DIR})
-        add_custom_command (TARGET ${TARGET_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} ARGS -E copy_if_different \"$(TargetPath)\" ${TARGET_DIR})
-    endif ()
-endmacro (copy_target)
+endmacro (final_target)
 
 # build a library from internal sources
 macro (build_library TARGET_NAME LIB_TYPE)
@@ -144,177 +142,3 @@ macro (find_debug_libraries PREFIX DEBUG_POSTFIX)
             ${lib_}${DEBUG_POSTFIX})
     endforeach ()
 endmacro ()
-
-# =============================================================================
-# per-project configuration macros
-
-macro (configure_boost)
-    set (Boost_USE_STATIC_LIBS ON)
-    sagase_configure_package (BOOST 
-        NAMES Boost boost
-        COMPONENTS date_time filesystem system thread program_options unit_test_framework
-        PREFIXES ${ENV_NAALI_DEP_PATH})
-
-    if (MSVC)
-        set (BOOST_INCLUDE_DIRS ${BOOST_INCLUDE_DIRS} ${ENV_NAALI_DEP_PATH}/Boost/include)
-        set (BOOST_LIBRARY_DIRS ${BOOST_LIBRARY_DIRS} ${ENV_NAALI_DEP_PATH}/Boost/lib)
-    endif ()
-
-endmacro (configure_boost)
-
-macro (configure_poco)
-    sagase_configure_package (POCO 
-        NAMES Poco PoCo poco
-        COMPONENTS Poco PocoFoundation PocoNet PocoUtil PocoXML
-        PREFIXES ${ENV_NAALI_DEP_PATH})
-endmacro (configure_poco)
-
-macro (configure_qt4)
-    sagase_configure_package (QT4 
-        NAMES Qt4 qt4 Qt qt
-        COMPONENTS QtCore QtGui QtWebkit QtScript QtXml QtNetwork QtUiTools
-        PREFIXES ${ENV_NAALI_DEP_PATH} ${ENV_QT_DIR})
-
-    # FindQt4.cmake
-    if (QT4_FOUND AND QT_USE_FILE)
-
-        include (${QT_USE_FILE})
-
-        set (QT4_INCLUDE_DIRS 
-            ${QT_INCLUDE_DIR}
-            ${QT_QTCORE_INCLUDE_DIR}
-            ${QT_QTDBUS_INCLUDE_DIR}
-            ${QT_QTGUI_INCLUDE_DIR}
-            ${QT_QTUITOOLS_INCLUDE_DIR}
-            ${QT_QTNETWORK_INCLUDE_DIR}
-            ${QT_QTXML_INCLUDE_DIR}
-            ${QT_QTSCRIPT_INCLUDE_DIR}
-            ${QT_QTWEBKIT_INCLUDE_DIR}
-            ${QT_PHONON_INCLUDE_DIR})
-
-        set (QT4_LIBRARY_DIR  
-            ${QT_LIBRARY_DIR})
-
-        set (QT4_LIBRARIES 
-            ${QT_LIBRARIES}
-            ${QT_QTCORE_LIBRARY}
-            ${QT_QTDBUS_LIBRARY}
-            ${QT_QTGUI_LIBRARY}
-            ${QT_QTUITOOLS_LIBRARY}
-            ${QT_QTNETWORK_LIBRARY}
-            ${QT_QTXML_LIBRARY}
-            ${QT_QTSCRIPT_LIBRARY}
-            ${QT_QTWEBKIT_LIBRARY}
-            ${QT_PHONON_LIBRARY})
-    endif ()
-endmacro (configure_qt4)
-
-macro (configure_python)
-    sagase_configure_package (PYTHON
-        NAMES PythonLibs Python python Python26 python26 Python2.6 python2.6
-        COMPONENTS Python python Python26 python Python2.6 python2.6
-        PREFIXES ${ENV_NAALI_DEP_PATH})
-
-    # FindPythonLibs.cmake
-    if (PYTHONLIBS_FOUND)
-        set (PYTHON_LIBRARIES ${PYTHON_LIBRARY})
-        set (PYTHON_INCLUDE_DIRS ${PYTHON_INCLUDE_PATH})
-		#if (PYTHON_DEBUG_LIBRARY)
-        #    set (PYTHON_DEBUG_LIBRARIES ${PYTHON_DEBUG_LIBRARY})
-        #endif ()
-    endif ()
-    
-endmacro (configure_python)
-
-macro (configure_python_qt)
-    sagase_configure_package (PYTHON_QT
-        NAMES PythonQt
-        COMPONENTS PythonQt PythonQt_QtAll
-        PREFIXES ${ENV_NAALI_DEP_PATH})
-
-endmacro (configure_python_qt)
-
-macro (configure_ois)
-    sagase_configure_package (OIS 
-        NAMES Ois ois OIS 
-        COMPONENTS OIS
-        PREFIXES ${ENV_NAALI_DEP_PATH})
-    
-    find_debug_libraries (OIS "_d")
-
-endmacro (configure_ois)
-
-macro (configure_ogre)
-    sagase_configure_package (OGRE 
-        NAMES Ogre OgreSDK ogre OGRE
-        COMPONENTS Ogre ogre OGRE OgreMain 
-        PREFIXES ${ENV_NAALI_DEP_PATH} ${ENV_OGRE_HOME})
-
-    find_debug_libraries (OGRE "_d")
-
-endmacro (configure_ogre)
-
-macro (configure_caelum)
-    sagase_configure_package (CAELUM 
-        NAMES Caelum caelum CAELUM
-        COMPONENTS Caelum caelum CAELUM
-        PREFIXES ${ENV_NAALI_DEP_PATH})
-    
-    find_debug_libraries (CAELUM "_d")
-
-endmacro (configure_caelum)
-
-macro (configure_hydrax)
-    sagase_configure_package (HYDRAX 
-        NAMES Hydrax
-        COMPONENTS Hydrax
-        PREFIXES ${ENV_NAALI_DEP_PATH})
-    
-    find_debug_libraries (HYDRAX "_d")
-
-endmacro (configure_hydrax)
-
-macro (configure_xmlrpc)
-    sagase_configure_package (XMLRPC 
-        NAMES xmlrpc xmlrpcepi xmlrpc-epi
-        COMPONENTS xmlrpc xmlrpcepi xmlrpc-epi xmlrpcepid 
-        PREFIXES ${ENV_NAALI_DEP_PATH}
-        ${ENV_NAALI_DEP_PATH}/xmlrpc-epi/src
-        ${ENV_NAALI_DEP_PATH}/xmlrpc-epi/Debug
-		${ENV_NAALI_DEP_PATH}/xmlrpc-epi/Release)
-   
-	if (MSVC)
-		set (XMLRPC_LIBRARIES xmlrpcepi)
-		set (XMLRPC_DEBUG_LIBRARIES xmlrpcepid)
-	endif()
-	
-endmacro (configure_xmlrpc)
-
-macro (configure_curl)
-    sagase_configure_package (CURL 
-        NAMES Curl curl libcurl
-        COMPONENTS curl libcurl_imp libcurld_imp
-        PREFIXES ${ENV_NAALI_DEP_PATH}
-        ${ENV_NAALI_DEP_PATH}/libcurl/lib/DLL-Debug 
-        ${ENV_NAALI_DEP_PATH}/libcurl/lib/DLL-Release)		
-    
-	if (MSVC)
-		set (CURL_LIBRARIES libcurl_imp)
-		set (CURL_DEBUG_LIBRARIES libcurld_imp)
-	endif()
-	
-endmacro (configure_curl)
-
-macro (configure_openjpeg)
-    sagase_configure_package (OPENJPEG 
-        NAMES OpenJpeg OpenJPEG openjpeg
-        COMPONENTS OpenJpeg OpenJPEG openjpeg
-        PREFIXES ${ENV_NAALI_DEP_PATH}
-        ${ENV_NAALI_DEP_PATH}/OpenJpeg/libopenjpeg
-        ${ENV_NAALI_DEP_PATH}/OpenJpeg/Debug
-        ${ENV_NAALI_DEP_PATH}/OpenJpeg/Release)
-    
-    find_debug_libraries (OPENJPEG "d")
-
-endmacro (configure_openjpeg)
-
