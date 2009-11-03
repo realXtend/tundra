b71ecb0eb4d6fea77216de5984247761abc1fc03
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-11-03 08:11:22 +0000

    Delete .pyc files when making build. Make sure that data/configuration directory exists in the build.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1617 5b2332b8-efa3-11de-8684-7d64432d61a3

--- makebuild.bat
+++ makebuild.bat
@@ -1,21 +1,33 @@
 @rem Makes a build directory of the viewer, so that you can run the NSIS install script
 @rem You should have a directory ..\viewerbuilddlls which has release versions of
-@rem all dependency dlls, including Qt core/ui/network/webkit/phonon dll's, and also VS2008 
-@rem redistributable (vcredist_x86.exe) 
+@rem all dependency dlls, and also VS2008 redistributable (vcredist_x86.exe) 
 @echo off
 rmdir build /S /Q
 md build
 copy readme.txt build
-xcopy bin\*.* build /S /C
-del build\data\configuration\*.xml
-rmdir build\testing /S /Q
+xcopy bin\*.* build /S /C /Y
 del build\*.dll
 del build\viewerd.exe
 del build\modules\core\*d.dll
-xcopy ..\viewerbuilddlls\*.* build /S /C
+xcopy ..\viewerbuilddlls\*.* build /S /C /Y
 del build\pymodules\*.pyc
 del build\pymodules\apitest\*.pyc
 del build\pymodules\circuits\*.pyc
-del build\pymodules\communication\*.pyc
+del build\pymodules\circuits\core\*.pyc
+del build\pymodules\circuits\net\*.pyc
+del build\pymodules\circuits\tools\*.pyc
+del build\pymodules\circuits\web\*.pyc
 del build\pymodules\core\*.pyc
+del build\pymodules\editgui\*.pyc
+del build\pymodules\lib\*.pyc
+del build\pymodules\lib\webdav\*.pyc
+del build\pymodules\lib\webdav\acp\*.pyc
 del build\pymodules\usr\*.pyc
+del build\pymodules\webdavinventory\*.pyc
+del build\pymodules\webserver\*.pyc
+cd build\data
+rmdir configuration /S /Q
+mkdir configuration
+cd ..\..
+
+
