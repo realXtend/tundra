b6927b5b58fc5edb2da8e69b0279ae705ebf30b8
sempuki <sempuki@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-11-25 12:52:43 +0000

    static member variables must be initialized within the compilation unit, not header
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1830 5b2332b8-efa3-11de-8684-7d64432d61a3

--- ProtocolUtilities/BitStream.cpp
+++ ProtocolUtilities/BitStream.cpp
@@ -5,8 +5,8 @@
 
 namespace ProtocolUtilities
 {
-    //! REMOVE
-    //const int BitStream::num_bits_in_elem_;
+    //! static variables must be initialized within the compilation unit (not header)
+    const int BitStream::num_bits_in_elem_ = 8;
 
     BitStream::BitStream(const void *data, size_t num_bytes)
         :data_(reinterpret_cast<const Core::u8*>(data)), num_elems_((num_bytes*num_bits_in_elem_ + num_bits_in_elem_ - 1) / num_bits_in_elem_), elem_ofs_(0), bit_ofs_(0)
