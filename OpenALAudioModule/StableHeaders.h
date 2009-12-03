93cd2117fc0a761267fc2ed8e30b8f6295efc756
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-12-03 12:03:01 +0000

    Made Foundation's StableHeaders include Foundation.h, to prevent build errors in the future. Fixed include guards.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1879 5b2332b8-efa3-11de-8684-7d64432d61a3

--- OpenALAudioModule/StableHeaders.h
+++ OpenALAudioModule/StableHeaders.h
@@ -1,7 +1,7 @@
 // For conditions of distribution and use, see copyright notice in license.txt
 
-#ifndef incl_OgreRenderer_StableHeaders_h__
-#define incl_OgreRenderer_StableHeaders_h__
+#ifndef incl_OpenALAudioModule_StableHeaders_h__
+#define incl_OpenALAudioModule_StableHeaders_h__
 
 #include "CoreStdIncludes.h"
 #include "Core.h"
