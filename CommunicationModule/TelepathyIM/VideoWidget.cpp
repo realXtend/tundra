5d26777a109de00d5118e68260f6e00829cb0d7b
mattiku <mattiku@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-12-09 13:28:08 +0000

    Added VideoWidget class.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@1951 5b2332b8-efa3-11de-8684-7d64432d61a3

--- CommunicationModule/TelepathyIM/VideoWidget.cpp
+++ CommunicationModule/TelepathyIM/VideoWidget.cpp
@@ -1,3 +1,115 @@
 // For conditions of distribution and use, see copyright notice in license.txt
 
 #include "StableHeaders.h"
+#include "VideoWidget.h"
+#include <QApplication>
+#include <QDebug>
+#include <QPainter>
+#include <QThread>
+#include <QVariant>
+#include <gst/interfaces/xoverlay.h>
+
+namespace TelepathyIM
+{
+
+    //QWidget(bus,parent)
+    VideoWidget::VideoWidget(GstBus *bus,  QWidget *parent) : QWidget(parent), bus_((GstBus *) gst_object_ref(bus)), overlay_(0), sink_(0)
+    {
+        notifier_ = fs_element_added_notifier_new();
+        g_signal_connect(notifier_, "element-added", G_CALLBACK(&VideoWidget::OnElementAdded), this);
+
+
+        sink_ = gst_element_factory_make("glimagesink", NULL);   //TODO: Get element name as argument
+        gst_object_ref(sink_);
+        gst_object_sink(sink_);
+
+        fs_element_added_notifier_add(notifier_, GST_BIN(sink_));
+        gst_bus_enable_sync_message_emission(bus_);
+
+        g_signal_connect(bus_, "sync-message", G_CALLBACK(&VideoWidget::OnSyncMessage), this);
+
+
+        QPalette palette;
+        palette.setColor(QPalette::Background, Qt::black);
+        setPalette(palette);
+        setAutoFillBackground(true);
+    }
+
+    VideoWidget::~VideoWidget()
+    {
+        g_object_unref(bus_);
+        g_object_unref(sink_);
+    }
+
+    void VideoWidget::OnElementAdded(FsElementAddedNotifier *notifier, GstBin *bin, GstElement *element, VideoWidget *self)
+    {
+        if (!self->overlay_ && GST_IS_X_OVERLAY(element))
+        {
+            self->overlay_ = element;
+            QMetaObject::invokeMethod(self, "WindowExposed", Qt::QueuedConnection);
+        }
+
+        if (g_object_class_find_property(G_OBJECT_GET_CLASS(element), "force-aspect-ratio"))
+        {
+            g_object_set(G_OBJECT(element), "force-aspect-ratio", TRUE, NULL);
+        }
+    }
+
+    void VideoWidget::OnSyncMessage(GstBus *bus, GstMessage *message, VideoWidget *self)
+    {
+        if (GST_MESSAGE_TYPE(message) != GST_MESSAGE_ELEMENT)
+        {
+            return;
+        }
+
+        if (GST_MESSAGE_SRC(message) != (GstObject *) self->overlay_)
+        {
+            return;
+        }
+
+        const GstStructure *s = gst_message_get_structure (message);
+
+        if (gst_structure_has_name(s, "prepare-xwindow-id") && self->overlay_)
+        {
+            QMetaObject::invokeMethod(self, "SetOverlay", Qt::QueuedConnection);
+        }
+    }
+
+    GstElement* VideoWidget::GetVideoSink() const
+    {
+        return sink_;
+    }
+
+    bool VideoWidget::EventFilter(QEvent *ev)
+    {
+        if (ev->type() == QEvent::Show)
+        {
+            setAttribute(Qt::WA_NoSystemBackground, true);
+            setAttribute(Qt::WA_PaintOnScreen, true);
+            SetOverlay();
+        }
+        return false;
+    }
+
+    void VideoWidget::SetOverlay()
+    {
+        if (overlay_ && GST_IS_X_OVERLAY(overlay_))
+        {
+            WId windowId = winId();
+            //gulong guWindowId = (gulong) windowId;
+            QApplication::syncX();
+            gst_x_overlay_set_xwindow_id(GST_X_OVERLAY(overlay_),
+                    (gulong) windowId);
+        }
+        WindowExposed();
+    }
+
+    void VideoWidget::WindowExposed()
+    {
+        QApplication::syncX();
+        if (overlay_ && GST_IS_X_OVERLAY(overlay_))
+        {
+            gst_x_overlay_expose(GST_X_OVERLAY(overlay_));
+        }
+    }
+}
---
+++ CommunicationModule/TelepathyIM/VideoWidget.h
@@ -0,0 +1,44 @@
+#ifndef incl_Communication_TelepathyIM_VideoWidget_h
+#define incl_Communication_TelepathyIM_VideoWidget_h
+
+#include <QWidget>
+#include <gst/gst.h>
+#include <gst/farsight/fs-element-added-notifier.h>
+
+#include "../interface.h"
+              
+namespace TelepathyIM
+{
+    /**
+     *  Based on call example of telepathy-qt4 library
+     *  QWidget for video playback. Created by FarsightChannel object and used by communication UI widgets.
+     */
+    class VideoWidget : public QWidget
+    {
+        Q_OBJECT
+    public: 
+        VideoWidget(GstBus *bus,QWidget *parent = 0);
+        virtual ~VideoWidget();
+
+        GstElement *GetVideoSink() const;
+        bool EventFilter(QEvent *ev);
+
+
+    private Q_SLOTS:
+        void SetOverlay();
+        void WindowExposed();
+
+    static void OnElementAdded(FsElementAddedNotifier *notifier, GstBin *bin, GstElement *element, VideoWidget *self);
+
+    static void OnSyncMessage(GstBus *bus, GstMessage *message, VideoWidget *self);
+
+    private:
+//        VideoWidget *parent_;
+        GstBus *bus_;
+        FsElementAddedNotifier *notifier_;
+        GstElement *sink_;
+        GstElement *overlay_;
+    };
+} // End of namespace: TelepathyIM
+
+#endif // incl_Communication_TelepathyIM_VideoWidget_h
