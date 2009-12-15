7ba8c612e1fa6ca0f25cb2b36be569873cfba7fd
antont <antont@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-12-15 07:46:48 +0000

    Creating ec_uicanvases to all submeshes that use the given texture uuid. Functionality not verified yet, had other probs with uicanvases, continuing..
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@2027 5b2332b8-efa3-11de-8684-7d64432d61a3

--- bin/pymodules/mediaurlhandler/mediaurlhandler.py
+++ bin/pymodules/mediaurlhandler/mediaurlhandler.py
@@ -5,53 +5,42 @@ from circuits import Component
 
 INTERNAL = 1
 
-networkeventcount = 0
-
 class MediaURLHandler(Component):
     def __init__(self):
         Component.__init__(self)
 
         mode = INTERNAL
-        self.canvas = r.createCanvas(mode)
+        #self.canvas = r.createCanvas(mode)
 
         self.wv = PythonQt.QtWebKit.QWebView()
         
         #self.canvas.AddWidget(self.wv)
     
-        #qtmodule = r.getQtModule()
-        #qtmodule.AddCanvasToControlBar(self.canvas, "WebView")
-        #self.canvas.connect('Hidden()', self.on_hide)
-
         uism = r.getUiSceneManager()
         uiprops = r.createUiWidgetProperty()
         uiprops.widget_name_ = "MediaURL"
-        #uiprops.my_size_ = QSize(width, height)
-        #self.proxywidget = uism.AddWidgetToCurrentScene(ui, uiprops)
+        ##uiprops.my_size_ = QSize(width, height)
+        ##self.proxywidget = uism.AddWidgetToCurrentScene(ui, uiprops)
         self.proxywidget = r.createUiProxyWidget(self.wv, uiprops)
-        #print widget, dir(widget)
+        ##print widget, dir(widget)
         uism.AddProxyWidget(self.proxywidget)
 
     #def on_scene(self, evid, entid):
     #    pass
     #    print "Scene event in mediaurlhandler:", evid, entid
 
-    #def on_inboundnetwork(self, evid, name, callback):
-    #    pass
-        #global networkeventcount
-        #networkeventcount += 1
-        #print "editgui got an inbound network event:", evid, name, networkeventcount
-
     def on_genericmessage(self, name, data):
         #print "MediaURLHandler got Generic Message:", name, data
         if name == "RexMediaUrl":
             print "MediaURLHandler got data:", data
-            objuuid, url, refreshrate = data
-            ent = r.getEntityByUUID(objuuid)
-            self.loadurl(ent, url)
+            textureuuid, url, refreshrate = data
+            
+            self.loadurl(url)
+            
+            #r.applyUICanvasToSubmeshesWithTexture(self.canvas, textureuuid)
               
-    #def on_hide(self):
 
-    def loadurl(self, ent, urlstring):
-        print "Showing URL for entity:", ent, urlstring
+    def loadurl(self, urlstring):
+        print "Showing URL", urlstring
         url = PythonQt.QtCore.QUrl(urlstring)
         self.wv.load(url)
