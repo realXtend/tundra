059ca893545ab79275c54fbf075e569a12429fef
jaakkoallander <jaakkoallander@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-12-16 21:22:19 +0000

    Trying to fix linux build.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@2077 5b2332b8-efa3-11de-8684-7d64432d61a3

--- OgreRenderingModule/CompositionHandler.h
+++ OgreRenderingModule/CompositionHandler.h
@@ -8,108 +8,106 @@
 #include "OgreModuleApi.h"
 
 namespace OgreRenderer
-{   
-    /////
-    //This class and it's implementation is taken from the Ogre samples
-    /////
+{
+    /**
+     *  This class and it's implementation is taken from the Ogre samples
+     */
     class HDRListener: public Ogre::CompositorInstance::Listener
-	{
-	protected:
-		int mVpWidth, mVpHeight;
-		int mBloomSize;
-		// Array params - have to pack in groups of 4 since this is how Cg generates them
-		// also prevents dependent texture read problems if ops don't require swizzle
-		float mBloomTexWeights[15][4];
-		float mBloomTexOffsetsHorz[15][4];
-		float mBloomTexOffsetsVert[15][4];
-	public:
-		HDRListener();
-		virtual ~HDRListener();
-		void notifyViewportSize(int width, int height);
-		void notifyCompositor(Ogre::CompositorInstance* instance);
-		virtual void notifyMaterialSetup(Ogre::uint32 pass_id, Ogre::MaterialPtr &mat);
-		virtual void notifyMaterialRender(Ogre::uint32 pass_id, Ogre::MaterialPtr &mat);
-	};
-	//---------------------------------------------------------------------------
-      /////
-    //This class and it's implementation is taken from the Ogre samples
-    /////
-	class GaussianListener: public Ogre::CompositorInstance::Listener
-	{
-	protected:
-		int mVpWidth, mVpHeight;
-		// Array params - have to pack in groups of 4 since this is how Cg generates them
-		// also prevents dependent texture read problems if ops don't require swizzle
-		float mBloomTexWeights[15][4];
-		float mBloomTexOffsetsHorz[15][4];
-		float mBloomTexOffsetsVert[15][4];
-	public:
-		GaussianListener();
-		virtual ~GaussianListener();
-		void notifyViewportSize(int width, int height);
-		virtual void notifyMaterialSetup(Ogre::uint32 pass_id, Ogre::MaterialPtr &mat);
-		virtual void notifyMaterialRender(Ogre::uint32 pass_id, Ogre::MaterialPtr &mat);
-	};
-
-
-
+    {
+    protected:
+        int mVpWidth, mVpHeight;
+        int mBloomSize;
+        // Array params - have to pack in groups of 4 since this is how Cg generates them
+        // also prevents dependent texture read problems if ops don't require swizzle
+        float mBloomTexWeights[15][4];
+        float mBloomTexOffsetsHorz[15][4];
+        float mBloomTexOffsetsVert[15][4];
+    public:
+        HDRListener();
+        virtual ~HDRListener();
+        void notifyViewportSize(int width, int height);
+        void notifyCompositor(Ogre::CompositorInstance* instance);
+        virtual void notifyMaterialSetup(Ogre::uint32 pass_id, Ogre::MaterialPtr &mat);
+        virtual void notifyMaterialRender(Ogre::uint32 pass_id, Ogre::MaterialPtr &mat);
+    };
+    //---------------------------------------------------------------------------
+    /**
+     *This class and it's implementation is taken from the Ogre samples
+     */
+    class GaussianListener: public Ogre::CompositorInstance::Listener
+    {
+    protected:
+        int mVpWidth, mVpHeight;
+        // Array params - have to pack in groups of 4 since this is how Cg generates them
+        // also prevents dependent texture read problems if ops don't require swizzle
+        float mBloomTexWeights[15][4];
+        float mBloomTexOffsetsHorz[15][4];
+        float mBloomTexOffsetsVert[15][4];
+    public:
+        GaussianListener();
+        virtual ~GaussianListener();
+        void notifyViewportSize(int width, int height);
+        virtual void notifyMaterialSetup(Ogre::uint32 pass_id, Ogre::MaterialPtr &mat);
+        virtual void notifyMaterialRender(Ogre::uint32 pass_id, Ogre::MaterialPtr &mat);
+    };
 
     //! This class handles the post-processing effects
-	class OGRE_MODULE_API CompositionHandler
-	{
-	private:
+    class OGRE_MODULE_API CompositionHandler
+    {
+    private:
         //! effects available
         typedef std::vector<std::string> Postprocesses;
 
+        //Used to specify postprocessing effects currently available. Number is needed to map server requests to the actual effect name.
+        Postprocesses postprocess_effects;
 
-		//Used to specify postprocessing effects currently available. Number is needed to map server requests to the actual effect name.
-		Postprocesses postprocess_effects;
         //! Ogres compositormanager
-		Ogre::CompositorManager* c_manager_;
+        Ogre::CompositorManager* c_manager_;
+
         //! Default viewport. This will be used if no other viewport is specified when enabling an effect
         Ogre::Viewport* default_vp;
+
         //! Framelistener for HDR effect
         HDRListener hdr_listener_;
+
         //! Framelistener for gaussian blur
         GaussianListener gaussian_listener_;
+
         //! handle to framework
-		Foundation::Framework* framework_;
-
-
+        Foundation::Framework* framework_;
 
         //! map number to effect name (used to interpret server request since server requests composite activation with a number)
         std::string MapNumberToEffectName( std::string const &number);
 
+    public:
         
-
-
-	public:
-		
         //! Everything Initialized to 0.
-		CompositionHandler();
-
+        CompositionHandler();
 
         //! Destroys the ui_
-		virtual ~CompositionHandler();
+        virtual ~CompositionHandler();
         
         //! Initialize the composition handler. This has to be called before trying to enable/disable effects
-		bool Initialize(Foundation::Framework* framework, Ogre::Viewport *vp);
+        bool Initialize(Foundation::Framework* framework, Ogre::Viewport *vp);
 
-        
         //! Adds specified compositor for the viewport if it's found. Effect is appended last in the chain if position is not specified (Except HDR)
-		bool AddCompositorForViewport( std::string  const &compositor, Ogre::Viewport *vp, int position = -1);
+        bool AddCompositorForViewport( std::string  const &compositor, Ogre::Viewport *vp, int position = -1);
+
         //! Remove specified compositor effect from viewport
-		void RemoveCompositorFromViewport( std::string  const &compositor, Ogre::Viewport *vp);
+        void RemoveCompositorFromViewport( std::string  const &compositor, Ogre::Viewport *vp);
+
         //! translates the message (RexPostP) 
         void ExecuteServersShaderRequest( Core::StringVector const &parameters);
+
         //! Returns list of available post-processing effects
-        std::vector<std::string> GetAvailableCompositors();
+        std::vector<std::string> &GetAvailableCompositors();
 
         //! Convenience function that will add specified compositor for the default viewport given in initialization
-		bool AddCompositorForViewport( std::string  const &compositor, int position = -1);
+        bool AddCompositorForViewport( std::string  const &compositor, int position = -1);
+
         //! Convenience funtion to remove specified  compositor from the default viewport
-		void RemoveCompositorFromViewport( std::string  const &compositor);
-	};
+        void RemoveCompositorFromViewport( std::string  const &compositor);
+    };
 
     
 }
