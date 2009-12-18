abc6f8a75117b97707db1a841a7e4ec772364a5b
cadaver <cadaver@5b2332b8-efa3-11de-8684-7d64432d61a3>
2009-12-18 08:52:31 +0000

    Tweaked makebuild.bat
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@2113 5b2332b8-efa3-11de-8684-7d64432d61a3

--- makebuild.bat
+++ makebuild.bat
@@ -8,6 +8,7 @@ copy readme.txt build
 copy vcredist_x86.exe build
 copy oalinst.exe build
 xcopy bin\*.* build /S /C /Y
+del build\fullmemoryleaklog.txt
 del build\openal32.dll
 del build\wrap_oal.dll
 del build\*d4.dll
@@ -17,6 +18,9 @@ del build\Poco*d.dll
 del build\libcurld.dll
 del build\OpenJPEGd.dll
 del build\xmlrpcepid.dll
+del build\liboggd.dll
+del build\libvorbisd.dll
+del build\libvorbisfiled.dll
 del build\viewerd.exe
 del build\modules\core\*d.dll
 del build\pymodules\*.pyc
@@ -36,3 +40,4 @@ del build\pymodules\webdavinventory\*.pyc
 del build\pymodules\webserver\*.pyc
 del build\pymodules\mediaurlhandler\*.pyc
 del build\pymodules\DLLs\*_d.pyd
+del build\plugins\*_d.dll
