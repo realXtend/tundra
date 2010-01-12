55e59f45d1435fce8f4937dc8944b05ea72933d9
loorni <loorni@5b2332b8-efa3-11de-8684-7d64432d61a3>
2010-01-12 08:58:40 +0000

    Updated version number.
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@2217 5b2332b8-efa3-11de-8684-7d64432d61a3

--- Foundation/Framework.cpp
+++ Foundation/Framework.cpp
@@ -106,7 +106,7 @@ namespace Foundation
 
             // Set config values we explicitly always want to override
             config_manager_->SetSetting(Framework::ConfigurationGroup(), std::string("version_major"), std::string("0"));
-            config_manager_->SetSetting(Framework::ConfigurationGroup(), std::string("version_minor"), std::string("0.2"));
+            config_manager_->SetSetting(Framework::ConfigurationGroup(), std::string("version_minor"), std::string("1alpha"));
                         
             CreateLoggingSystem(); // depends on config and platform
 
