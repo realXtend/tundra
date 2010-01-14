0413837cef7ba5f35864a73132a2fe4244a707b9
jonne.nauha@evocativi.com <jonne.nauha@evocativi.com@5b2332b8-efa3-11de-8684-7d64432d61a3>
2010-01-14 18:05:11 +0000

    * Small shadow effect for inworld scene dialog widgets * Fade in on show event * Should not drop FPS as the animations did. If people have problems in debug we can put these only for debug. Try them out!
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@2241 5b2332b8-efa3-11de-8684-7d64432d61a3

--- UiModule/UiProxyWidget.cpp
+++ UiModule/UiProxyWidget.cpp
@@ -8,7 +8,7 @@ namespace UiServices
     UiProxyWidget::UiProxyWidget(QWidget *widget, const UiWidgetProperties &in_widget_properties)
         : QGraphicsProxyWidget(0, in_widget_properties.getWindowStyle()),
           widget_properties_(in_widget_properties),
-          show_timeline_(new QTimeLine(1300, this)),
+          show_timeline_(new QTimeLine(300, this)),
           show_animation_(new QGraphicsItemAnimation(this))
     {
         widget->setWindowFlags(widget_properties_.getWindowStyle());
@@ -18,7 +18,7 @@ namespace UiServices
         setPos(widget_properties_.getPosition().x(), widget_properties_.getPosition().y());
         setGeometry(QRectF(widget_properties_.getPosition(), QSizeF(widget_properties_.getSize())));
 
-        //InitAnimations();
+        InitAnimations();
     }
 
     UiProxyWidget::~UiProxyWidget()
@@ -29,39 +29,40 @@ namespace UiServices
 
     void UiProxyWidget::InitAnimations()
     {
-        setTransform(QTransform().scale(0,0));
-        show_timeline_->setFrameRange(0, 100);
-        show_timeline_->setUpdateInterval(40);
-        show_timeline_->setCurveShape(QTimeLine::EaseOutCurve);
-        QObject::connect(show_timeline_, SIGNAL( frameChanged(int) ), SLOT( AnimationStep(int) ));
+        if (!widget_properties_.isFullscreen())
+        {
+            QGraphicsDropShadowEffect *shadow_effect = new QGraphicsDropShadowEffect(this);
+            shadow_effect->setBlurRadius(3);
+            shadow_effect->setOffset(3.0, 3.0);
+            setGraphicsEffect(shadow_effect);
+        }
+
+        if (widget_properties_.getWidgetName() != "Login loader") // fix
+            QObject::connect(show_timeline_, SIGNAL( valueChanged(qreal) ), SLOT( AnimationStep(qreal) ));
     }
 
-    void UiProxyWidget::AnimationStep(int step)
+    void UiProxyWidget::AnimationStep(qreal step)
     {
-        QTransform transformation;
-        if (widget_properties_.getAnimationType() == UiServices::SlideFromTop)
-            transformation = QTransform().translate(0.0, size().width()*(-0.01*(100-step)));
-        else if (widget_properties_.getAnimationType() == UiServices::SlideFromBottom)
-            transformation = QTransform().translate(0.0, size().width()*(0.01*(100-step)));
-        setTransform(transformation);
+        setOpacity(step);
     }
 
     void UiProxyWidget::showEvent(QShowEvent *show_event)
     {
         emit Visible(true);
         QGraphicsProxyWidget::showEvent(show_event);
-	    //show_timeline_->start();
-    }  
+	    show_timeline_->start();
+    }
 
     void UiProxyWidget::hideEvent(QHideEvent *hide_event)
     {
         emit Visible(false);
         QGraphicsProxyWidget::hideEvent(hide_event);
+        if (widget_properties_.getWidgetName() != "Login loader") // fix
+            setOpacity(0.0);
     }
 
     UiWidgetProperties UiProxyWidget::getWidgetProperties()
     {
         return widget_properties_;
     }
-
 }
