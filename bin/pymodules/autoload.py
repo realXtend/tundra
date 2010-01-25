3094e1a01730e246cd34d6d97e08af98ceb3d6f4
petri.aura <petri.aura@5b2332b8-efa3-11de-8684-7d64432d61a3>
2010-01-25 13:29:26 +0000

    rerenaming the folder aswell
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@2385 5b2332b8-efa3-11de-8684-7d64432d61a3

--- bin/pymodules/autoload.py
+++ bin/pymodules/autoload.py
@@ -48,15 +48,15 @@ else:
 #~ usr.keycommands = load_module("usr.keycommands")
 
 try:
-    editgui.editgui #only_layout
+    editgui.objectedit #only_layout
 except: #first run
     try:
-        import editgui.editgui #only_layout
+        import editgui.objectedit #only_layout
     except ImportError, e:
         print "couldn't load edigui:", e
 else:
-    r.logDebug("   reloading editgui")
-    editgui.editgui = reload(editgui.editgui) #only_layout)
+    r.logDebug("   reloading objectedit")
+    editgui.objectedit = reload(editgui.objectedit) #only_layout)
 
 #~ editgui = load_module("editgui")
 
@@ -110,7 +110,7 @@ modules = [
     #usr.chathandler.ChatHandler,
     usr.keycommands.KeyCommander,
     #usr.sleeper.Sleeper,
-    editgui.editgui.EditGUI, #only_layout.OnlyLayout,
+    editgui.objectedit.ObjectEdit, #only_layout.OnlyLayout,
     #mediaurlhandler.mediaurlhandler.MediaURLHandler,
     #apitest.pythonqt_gui.TestGui,
     #WebServer,
