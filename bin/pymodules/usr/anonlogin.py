4d9afc1168dc9e6c10467b873d20b015e93361f5
toni.alatalo <toni.alatalo@5b2332b8-efa3-11de-8684-7d64432d61a3>
2010-02-02 14:37:35 +0000

    little code style tweaking (doesn't keep a ref to a uiloader instance forever in vain etc)
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@2463 5b2332b8-efa3-11de-8684-7d64432d61a3

--- bin/pymodules/usr/anonlogin.py
+++ bin/pymodules/usr/anonlogin.py
@@ -2,28 +2,34 @@ from circuits import Component
 import rexviewer as r
 import urllib2
 
-from PythonQt.QtGui import QGroupBox, QPushButton
+from PythonQt.QtUiTools import QUiLoader
+from PythonQt.QtCore import QFile, QSize
+#from PythonQt.QtGui import QGroupBox, QPushButton
 from PythonQt.QtUiTools import QUiLoader
 from PythonQt.QtCore import QFile
 
 ACCOUNTURI = "http://www.playsign.fi/engine/rex/anonuser"
 DEMOLOGIN = "http://world.realxtend.org:9000"
-
-UIFILELOCATION = "data/ui/demoworld/DemoLoginWidget.ui"
+UIFILELOC = "data/ui/demoworld/DemoLoginWidget.ui"
 
 class AnonLogin(Component):
     def __init__(self):
         Component.__init__(self)
 
-        self.loader = QUiLoader()
-        self.uifile = QFile(UIFILELOCATION)
-        self.demologinwidget = self.loader.load(self.uifile)
-        self.demologinwidget.fishWorldButton.connect('clicked()', self.do_fistworld_login)
+        loader = QUiLoader()
+        uifile = QFile(UIFILELOC)
+        self.widget = loader.load(uifile)
+        self.widget.fishWorldButton.connect('clicked()', self.do_login)
         
-        uism = r.getUiSceneManager()
-        uism.SetDemoLoginWidget(self.demologinwidget)      
+        #self.group = QGroupBox()
+        #self.pushbut = QPushButton(self.group)
+        #self.pushbut.text = "Anon login"
+        #self.pushbut.connect('clicked()', self.dologin)
 
-    def do_fistworld_login(self):
+        uism = r.getUiSceneManager()
+        uism.SetDemoLoginWidget(self.widget)      
+
+    def do_login(self):
         urlfile = urllib2.urlopen(ACCOUNTURI)
         s = urlfile.read()
         s = s.strip()
@@ -37,7 +43,7 @@ class AnonLogin(Component):
             pwd = "anonpwd"
             r.startLoginOpensim(user, pwd, DEMOLOGIN)
 
-    def on_exit(self):
-        self.demologinwidget = None
-        self.uifile = None
-        self.loader = None
+    def on_exit(self): 
+        #for live reloading
+        self.widget.hide()
+        self.widget = None
