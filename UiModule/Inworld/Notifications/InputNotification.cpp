30124c05a5803a19562565b08660574b6d675a22
jonne.nauha@evocativi.com <jonne.nauha@evocativi.com@5b2332b8-efa3-11de-8684-7d64432d61a3>
2010-03-18 22:06:41 +0000

    * For some reason visual studio didn't complain about an 'a' character being the first thing in the .cpp, go wonder!
    git-svn-id: https://realxtend-naali.googlecode.com/svn/trunk@2710 5b2332b8-efa3-11de-8684-7d64432d61a3

--- UiModule/Inworld/Notifications/InputNotification.cpp
+++ UiModule/Inworld/Notifications/InputNotification.cpp
@@ -1,4 +1,4 @@
-a// For conditions of distribution and use, see copyright notice in license.txt
+// For conditions of distribution and use, see copyright notice in license.txt
 
 #include "StableHeaders.h"
 #include "InputNotification.h"
