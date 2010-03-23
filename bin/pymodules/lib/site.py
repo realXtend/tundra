94f5a470d879217dbf02914d7eff00c7b697d1a5
Toni Alatalo <toni@playsign.net>
2010-03-23 13:29:16 +0200

    moved py Windows stdlib zip path adding out of site.py back to naali code, 'cause setting the PYTHONHOME env var doesn't work within visual studio at least. the only function of site.py now is to stop incompatible sys py installs from interfering with the python naali ships on Windows.

--- bin/pymodules/lib/site.py
+++ bin/pymodules/lib/site.py
@@ -6,6 +6,10 @@ and this file not loaded.
 PythonEngine.cpp PythonEngine::Initialize() adds the additional paths Naali needs for all plats.
 """
 
-import sys
-sys.path.append('pymodules/python26_Lib.zip') #python modules from folder python26/Lib
+#import sys
+"""
+setting env vars from code doesn't work when running in Visual Studio so
+this file is not loaded then. hence the win specific lib adding code is back on the cpp side.
+"""
+#sys.path.append('pymodules/python26_Lib.zip') #python modules from folder python26/Lib
 #apparently setting PYTHONHOME puts bin\python26.zip already in path - probably we should switch to that.
