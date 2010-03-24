59230e91639f9b821c9c42aac3bde0c4129dfae5
Toni Alatalo <toni@playsign.net>
2010-03-25 00:28:24 +0200

    made a loader executable which mocks/facades ogre scene classes and runs the dotscene importer against those implementations. modified the dotscene loader to not use ogre. next step is to make the facade things be/call Naali things.

--- bin/pymodules/localscene/dotscene.py
+++ bin/pymodules/localscene/dotscene.py
@@ -10,7 +10,8 @@ http://www.ogre3d.org/wiki/index.php/DotScene_Loader_with_User_Data_Class
 """
 from xml.dom import minidom, Node
 import string
-import ogre.renderer.OGRE as ogre
+#import ogre.renderer.OGRE as ogre
+from quat import Quaternion
 
 """
 self.dotscene = DotScene(self.fileName, self.sceneManager, rootnode)
@@ -42,6 +43,8 @@ class DotScene:
         # TODO: Use the userData for sound/physics
         for node in self.root:
             if node.nodeType == Node.ELEMENT_NODE and node.nodeName == 'node':
+                attachMe = None
+                
                 realName =  node.attributes['name'].nodeValue
                 # create new scene node
                 newNode = self.rootNode.createChildSceneNode() # self.prefix + realName)
@@ -53,12 +56,12 @@ class DotScene:
                 # rotate it
                 try:
                     rot = self.findNodes(node, 'rotation')[0].attributes
-                    newNode.orientation = ogre.Quaternion(float(rot['qw'].nodeValue), float(rot['qx'].nodeValue),
+                    newNode.orientation = Quaternion(float(rot['qw'].nodeValue), float(rot['qx'].nodeValue),
                                                             float(rot['qy'].nodeValue), float(rot['qz'].nodeValue))
 #                     print float(rot['qw'].nodeValue), float(rot['qx'].nodeValue), float(rot['qy'].nodeValue),float(rot['qz'].nodeValue)
                 except IndexError: # probably doesn't have rotation attribute
                     rot = self.findNodes(node, 'quaternion')[0].attributes
-                    newNode.orientation = ogre.Quaternion(float(rot['w'].nodeValue), float(rot['x'].nodeValue), 
+                    newNode.orientation = Quaternion(float(rot['w'].nodeValue), float(rot['x'].nodeValue), 
                                                           float(rot['y'].nodeValue), float(rot['z'].nodeValue))
 #                     print float(rot['w'].nodeValue), float(rot['x'].nodeValue), float(rot['y'].nodeValue), float(rot['z'].nodeValue)
                 
@@ -67,40 +70,40 @@ class DotScene:
                 newNode.scale = (float(scale['x'].nodeValue), float(scale['y'].nodeValue), float(scale['z'].nodeValue))
                 
                 # is it a light?
-                try:
-                    thingy = self.findNodes(node, 'light')[0].attributes
-                    name = str(thingy['name'].nodeValue)
-                    attachMe = self.sceneManager.createLight(name)
-                    ltypes={'point':ogre.Light.LT_POINT,'directional':ogre.Light.LT_DIRECTIONAL,'spot':ogre.Light.LT_SPOTLIGHT,'radPoint':ogre.Light.LT_POINT}
-                    try:
-                        attachMe.type = ltypes[thingy['type'].nodeValue]
-                    except IndexError:
-                        pass
+                #try:
+                #    thingy = self.findNodes(node, 'light')[0].attributes
+                #    name = str(thingy['name'].nodeValue)
+                    #attachMe = self.sceneManager.createLight(name)
+                    #ltypes={'point':ogre.Light.LT_POINT,'directional':ogre.Light.LT_DIRECTIONAL,'spot':ogre.Light.LT_SPOTLIGHT,'radPoint':ogre.Light.LT_POINT}
+                    #try:
+                    #    attachMe.type = ltypes[thingy['type'].nodeValue]
+                    #except IndexError:
+                    #    pass
                     
-                    lightNode = self.findNodes(node, 'light')[0]
+                #    lightNode = self.findNodes(node, 'light')[0]
 
-                    try:
-                        tempnode = self.findNodes(lightNode, 'colourSpecular')[0]
-                        attachMe.specularColour = (float(tempnode.attributes['r'].nodeValue), float(tempnode.attributes['g'].nodeValue), float(tempnode.attributes['b'].nodeValue), 1.0)
-                    except IndexError:
-                        pass
+                #    try:
+                #        tempnode = self.findNodes(lightNode, 'colourSpecular')[0]
+                #        attachMe.specularColour = (float(tempnode.attributes['r'].nodeValue), float(tempnode.attributes['g'].nodeValue), float(tempnode.attributes['b'].nodeValue), 1.0)
+                #    except IndexError:
+                #        pass
                     
-                    try:
-                        tempnode = self.findNodes(lightNode, 'colourDiffuse')[0]
-                        attachMe.diffuseColour = (float(tempnode.attributes['r'].nodeValue), float(tempnode.attributes['g'].nodeValue), float(tempnode.attributes['b'].nodeValue), 1.0)
-                    except IndexError:
-                        pass
+                #    try:
+                #        tempnode = self.findNodes(lightNode, 'colourDiffuse')[0]
+                #        attachMe.diffuseColour = (float(tempnode.attributes['r'].nodeValue), float(tempnode.attributes['g'].nodeValue), float(tempnode.attributes['b'].nodeValue), 1.0)
+                #    except IndexError:
+                #        pass
                     
-                    try:
-                        tempnode = self.findNodes(lightNode, 'colourDiffuse')[0]
-                        attachMe.diffuseColour = (float(tempnode.attributes['r'].nodeValue), float(tempnode.attributes['g'].nodeValue), float(tempnode.attributes['b'].nodeValue), 1.0)
-                    except IndexError:
-                        pass
-                    self.lights.append( attachMe )
+                #    try:
+                #        tempnode = self.findNodes(lightNode, 'colourDiffuse')[0]
+                #        attachMe.diffuseColour = (float(tempnode.attributes['r'].nodeValue), float(tempnode.attributes['g'].nodeValue), float(tempnode.attributes['b'].nodeValue), 1.0)
+                #    except IndexError:
+                #        pass
+                #    self.lights.append( attachMe )
                     
-                    print 'added light: "%s"' % name
-                except IndexError:
-                    pass
+                #    print 'added light: "%s"' % name
+                #except IndexError:
+                #    pass
                 
                 # is it an entity?
                 try:
@@ -108,12 +111,12 @@ class DotScene:
                     name = str(thingy['name'].nodeValue)
                     mesh = str(thingy['meshFile'].nodeValue)
                     attachMe = self.sceneManager.createEntity(name,mesh)
-#                     print 'added entity: "%s" %s' % (name, mesh)
+                    print 'added entity: "%s" %s' % (name, mesh)
                 except IndexError:
                     pass
-                except ogre.OgreFileNotFoundException: # mesh is missing
+                #except: #ogre.OgreFileNotFoundException: # mesh is missing
 #                     print "Missing Mesh:",mesh                     
-                    pass
+                #    pass
 
                 # is it a camera?
                 # TODO: there are other attributes I need in here
@@ -129,7 +132,7 @@ class DotScene:
                         attachMe.farClipDistance = float(tempnode.attributes['far'].nodeValue)
                     except IndexError:
                         pass
-                    attachMe.setFOVy ( ogre.Radian( fov ) )  #fOVy = fov
+                    ##attachMe.setFOVy ( ogre.Radian( fov ) )  #fOVy = fov
                     
                     self.cameras.append ( attachMe )
                     print 'added camera: "%s" fov: %f type: %s clipping: %f,%f' % (name, fov, projectionType,attachMe.nearClipDistance,attachMe.farClipDistance)
@@ -137,10 +140,11 @@ class DotScene:
                     pass
                 
                 # attach it to the scene
-                try:
+                #try:
+                if attachMe is not None:
                     newNode.attachObject(attachMe)
-                except:
-                    print "could not attach:",realName
+                #except:
+                    #print "could not attach:",realName
                 
 
         
---
+++ bin/pymodules/localscene/quat.py
@@ -0,0 +1,6 @@
+class Quaternion:
+    def __init__(self, x, y, z, w):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
