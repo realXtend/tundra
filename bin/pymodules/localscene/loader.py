872b2a932118ace141ee860e5d999fa155152226
Toni Alatalo <toni@playsign.net>
2010-03-26 13:42:16 +0200

    coordsys conversions w.i.p.

--- bin/pymodules/localscene/loader.py
+++ bin/pymodules/localscene/loader.py
@@ -9,6 +9,21 @@ else:
     Vec = PythonQt.QtGui.QVector3D
     Quat = PythonQt.QtGui.QQuaternion
 
+def rotquat(x, y, z):
+    rot = Quat()
+    rot.setVector(x, y, z)
+        
+    xyzsum = 1 - rot.x()**2 - rot.y()**2 - rot.z**2float xyzsum = 1 - X * X - Y * Y - Z * Z;
+    if xyzsum > 0:
+        w = math.sqrt(xyzsum)
+    else:
+        w = 0
+    rot.setScalar(w)        
+    return rot
+    #in libomv Quat(x,y,z)
+    #float xyzsum = 1 - X * X - Y * Y - Z * Z;
+    #W = (xyzsum > 0) ? (float)Math.Sqrt(xyzsum) : 0;
+
 class OgreNode:
     """the equivalent of Ogre Node in Naali for this import now.
     perhaps this will wrap or just be the Naali .. placeable E-C?"""
@@ -43,9 +58,46 @@ class OgreNode:
         p.Position = Vec(*self.position)
             
         #print p.Orientation.toString(), self.orientation
-        p.Orientation = self.orientation
+        o = self.orientation
+        o.__imul__(rotquat(1, 0, 0))
+        o.__imul__(rotquat(0, 1, 0))
+        p.Orientation = o
 
         p.Scale = Vec(*self.scale)
+
+"""
+the server side importer in modrex does conversions like this:
+                        Quaternion rot = new Quaternion(node.DerivedOrientation.X, node.DerivedOrientation.Y, node.DerivedOrientation.Z, node.DerivedOrientation.W);
+                        if (m_swapAxes == true)
+                        {
+                            Vector3 temp = new Vector3(rot.X, rot.Y, rot.Z);
+                            rot.X = -temp.X;
+                            rot.Y = temp.Z;
+                            rot.Z = temp.Y;
+                        }
+                        else
+                        {
+                            // Do the rotation adjust as in original importer
+                            rot *= new Quaternion(1, 0, 0);
+                            rot *= new Quaternion(0, 1, 0);
+                        }
+                        rot = sceneRotQuat * rot;
+                        
+                        SceneObjectGroup sceneObject = m_scene.AddNewPrim(m_scene.RegionInfo.EstateSettings.EstateOwner,
+                            m_scene.RegionInfo.EstateSettings.EstateOwner, objPos, rot, PrimitiveBaseShape.CreateBox());
+                        Vector3 newScale = new Vector3();
+                        newScale.X = node.DerivedScale.X * m_objectScale;
+                        newScale.Y = node.DerivedScale.Y * m_objectScale;
+                        newScale.Z = node.DerivedScale.Z * m_objectScale;
+                        if (m_swapAxes == true)
+                        {
+                            Vector3 temp = new Vector3(newScale);
+                            newScale.X = temp.X;
+                            newScale.Y = temp.Z;
+                            newScale.Z = temp.Y;
+                        } 
+                        sceneObject.RootPart.Scale = newScale;
+"""
 
 class OgreEntity:
     def __init__(self, name, mesh):
