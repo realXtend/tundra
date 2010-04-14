c267695bc96ceef33d7f3e8e802e3696d7dff04b
Jonne Vaisanen <jonnevaisanen@ludocraft.com>
2010-04-14 15:43:11 +0300

    Added sound preview window to OgreAssetEditorModule. You can play audio files and get information about it  like frequency, bit depth, duration etc.

---
+++ OgreAssetEditorModule/AudioPreviewEditor.cpp
@@ -0,0 +1,244 @@
+#include "StableHeaders.h"
+#include "Framework.h"
+#include "DebugOperatorNew.h"
+#include "AudioPreviewEditor.h"
+#include "AudioSignalLabel.h"
+#include "OgreAssetEditorModule.h"
+
+#include <UiModule.h>
+#include "Inworld/View/UiProxyWidget.h"
+#include "Inworld/View/UiWidgetProperties.h"
+#include "Inworld/InworldSceneController.h"
+#include "ModuleManager.h"
+#include "SoundServiceInterface.h"
+#include "AssetInterface.h"
+
+#include <QUiLoader>
+#include <QFile>
+#include <QLayout>
+#include <QPushButton>
+#include <QImage>
+#include <QTimer>
+
+#include "MemoryLeakCheck.h"
+
+namespace Naali
+{
+    AudioPreviewEditor::AudioPreviewEditor(Foundation::Framework *framework,
+                                           const QString &inventory_id,
+                                           const asset_type_t &asset_type,
+                                           const QString &name,
+                                           QWidget *parent):
+        QWidget(parent),
+        framework_(framework),
+        assetType_(asset_type),
+        inventoryId_(inventory_id),
+        soundId_(0),
+        request_tag_(0),
+        okButton_(0),
+        playButton_(0),
+        playTimer_(0)
+    {
+        setObjectName(name);
+        InitializeEditorWidget();
+    }
+
+    AudioPreviewEditor::~AudioPreviewEditor()
+    {
+
+    }
+
+    void AudioPreviewEditor::HandleAssetReady(Foundation::AssetPtr asset)
+    {
+        Foundation::ServiceManagerPtr service_manager = framework_->GetServiceManager();
+        if(service_manager)
+        {
+            if(service_manager->IsRegistered(Foundation::Service::ST_Sound))
+            {
+                boost::shared_ptr<Foundation::SoundServiceInterface> sound_service = 
+                    service_manager->GetService<Foundation::SoundServiceInterface>(Foundation::Service::ST_Sound).lock();
+                if(!sound_service)
+                    return;
+
+                request_tag_ = sound_service->RequestSoundResource(asset->GetId());
+            }
+        }
+    }
+
+    void AudioPreviewEditor::HandleResouceReady(Resource::Events::ResourceReady *res)
+    {
+        if(request_tag_ == res->tag_)
+        {
+            Foundation::SoundResource *sound = dynamic_cast<Foundation::SoundResource *>(res->resource_.get());
+            if(sound)
+            {
+                QLabel *audioInfoLabel = findChild<QLabel*>("descriptionLabel");
+                Foundation::SoundServiceInterface::SoundBuffer buffer = sound->GetBuffer();
+                audioInfoLabel->setText(QString("Frequency: %1Hz").arg(buffer.frequency_));
+                
+                int bits = 16;
+                if(!buffer.sixteenbit_)
+                    bits = 8;
+                audioInfoLabel->setText(audioInfoLabel->text() + QString(" Bits: %1 bit").arg(bits));
+
+                QString stereo = "Stereo";
+                if(!buffer.stereo_)
+                    stereo = "Mono";
+                audioInfoLabel->setText(audioInfoLabel->text() + " Format: " + stereo);
+
+                float duration;
+                if(!buffer.stereo_)
+                    duration = float(buffer.data_.size() / ((bits / 8))) / float(buffer.frequency_);
+                else
+                    duration = float(buffer.data_.size() / ((bits / 8) * 2)) / float(buffer.frequency_);
+                audioInfoLabel->setText(audioInfoLabel->text() + QString("\nTime: %1 sec").arg(duration));
+
+                QVBoxLayout *layout = qobject_cast<QVBoxLayout*>(mainWidget_->layout());
+                if(layout)
+                {
+                    AudioSignalLabel *audioSignalLabel = mainWidget_->findChild<AudioSignalLabel *>("audioSignalLabel");
+                    if(!audioSignalLabel)
+                    {
+                        audioSignalLabel = new AudioSignalLabel();
+                        audioSignalLabel->setObjectName("audioSignalLabel");
+                        layout->insertWidget(2, audioSignalLabel);
+                    }
+                    audioSignalLabel->setMaximumSize(QSize(mainWidget_->width(), mainWidget_->height() * 0.66f));
+                    audioSignalLabel->SetAudioData(buffer.data_, buffer.frequency_, bits, buffer.stereo_);
+                    QObject::connect(this, SIGNAL(WidgetResized(QSize)), audioSignalLabel, SLOT(ResizeImage(QSize)));
+                }
+                assetId_ = QString(sound->GetId().c_str());
+            }
+        }
+    }
+
+    void AudioPreviewEditor::Closed()
+    {
+        boost::shared_ptr<UiServices::UiModule> ui_module =
+            framework_->GetModuleManager()->GetModule<UiServices::UiModule>(Foundation::Module::MT_UiServices).lock();
+        if (!ui_module.get())
+            return;
+
+        ui_module->GetInworldSceneController()->RemoveProxyWidgetFromScene(this);
+
+        emit Closed(inventoryId_, assetType_);
+    }
+
+    void AudioPreviewEditor::PlaySound()
+    {
+        //If sound asset is not ready yet no need to play it.
+        if(assetId_.size() <= 0)
+            return;
+
+        Foundation::ServiceManagerPtr service_manager = framework_->GetServiceManager();
+        if(service_manager)
+        {
+            if(service_manager->IsRegistered(Foundation::Service::ST_Sound))
+            {
+                boost::shared_ptr<Foundation::SoundServiceInterface> sound_service = 
+                    service_manager->GetService<Foundation::SoundServiceInterface>(Foundation::Service::ST_Sound).lock();
+                if(!sound_service)
+                    return;
+
+                if(soundId_ == 0)
+                {
+                    soundId_ = sound_service->PlaySound(assetId_.toStdString(), Foundation::SoundServiceInterface::Ambient);
+                    playButton_->setText("Stop");
+                    
+                    AudioSignalLabel *audioSignalLabel = mainWidget_->findChild<AudioSignalLabel *>("audioSignalLabel");
+                    if(audioSignalLabel)
+                    {
+                        float duration = audioSignalLabel->GetAudioDuration();
+                        if(!playTimer_)
+                        {
+                            playTimer_ = new QTimer(this);
+                            playTimer_->setSingleShot(true);
+                            QObject::connect(playTimer_, SIGNAL(timeout()), this, SLOT(TimerTimeout()));
+                        }
+                        playTimer_->start(duration * 1000);
+                    }
+                }
+                else
+                {
+                    //User pressed stop audio before audio clip was finnished.
+                    sound_service->StopSound(soundId_);
+                    soundId_ = 0;
+                    playButton_->setText("Play");
+                    if(playTimer_)
+                    {
+                        if(playTimer_->isActive())
+                            playTimer_->stop();
+                    }
+                }
+            }
+        }
+    }
+
+    void AudioPreviewEditor::TimerTimeout()
+    {
+        //If sound asset is not ready yet no need to play it.
+        if(assetId_.size() <= 0 || soundId_ == 0)
+            return;
+
+        Foundation::ServiceManagerPtr service_manager = framework_->GetServiceManager();
+        if(service_manager)
+        {
+            if(service_manager->IsRegistered(Foundation::Service::ST_Sound))
+            {
+                boost::shared_ptr<Foundation::SoundServiceInterface> sound_service = 
+                    service_manager->GetService<Foundation::SoundServiceInterface>(Foundation::Service::ST_Sound).lock();
+                if(!sound_service)
+                    return;
+
+                sound_service->StopSound(soundId_);
+                soundId_ = 0;
+                playButton_->setText("Play");
+            }
+        }
+    }
+
+    void AudioPreviewEditor::resizeEvent(QResizeEvent *ev)
+    {
+        QWidget::resizeEvent(ev);
+        emit WidgetResized(ev->size());
+    }
+
+    void AudioPreviewEditor::InitializeEditorWidget()
+    {
+        // Get QtModule and create canvas
+        boost::shared_ptr<UiServices::UiModule> ui_module = 
+            framework_->GetModuleManager()->GetModule<UiServices::UiModule>(Foundation::Module::MT_UiServices).lock();
+        if (!ui_module.get())
+            return;
+
+        // Create widget from ui file
+        QUiLoader loader;
+        QFile file("./data/ui/audio_preview.ui");
+        if (!file.exists())
+        {
+            OgreAssetEditorModule::LogError("Cannot find OGRE Script Editor .ui file.");
+            return;
+        }
+        mainWidget_ = loader.load(&file);
+        file.close();
+
+        resize(mainWidget_->size());
+
+        QVBoxLayout *layout = new QVBoxLayout;
+        setLayout(layout);
+        layout->addWidget(mainWidget_);
+        layout->setContentsMargins(0, 0, 0, 0);
+
+        okButton_ = mainWidget_->findChild<QPushButton *>("okButton");
+        QObject::connect(okButton_, SIGNAL(clicked()), this, SLOT(Closed()));
+
+        playButton_ = mainWidget_->findChild<QPushButton *>("playButton");
+        QObject::connect(playButton_, SIGNAL(clicked()), this, SLOT(PlaySound()));
+
+        // Add widget to UI via ui services module
+        UiServices::UiProxyWidget *proxy = ui_module->GetInworldSceneController()->AddWidgetToScene(this, UiServices::UiWidgetProperties(tr("Audio: ") + objectName(), UiServices::SceneWidget));
+        QObject::connect(proxy, SIGNAL(Closed()), this, SLOT(Closed()));
+        proxy->show();
+        ui_module->GetInworldSceneController()->BringProxyToFront(proxy);
+    }
+}
---
+++ OgreAssetEditorModule/AudioPreviewEditor.h
@@ -0,0 +1,90 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
+/**
+ *  @file   AudioPreviewEditor.h
+ *  @brief  Preview window for uploaded audio clips.
+ *          Window is used to play audio assets and display the infomation about that file, including the visual presentation of audio signal.
+ */
+
+#ifndef incl_OgreAssetEditorModule_AudioPreviewEditor_h
+#define incl_OgreAssetEditorModule_AudioPreviewEditor_h
+
+#include <RexTypes.h>
+#include <QWidget>
+
+#include <boost/shared_ptr.hpp>
+
+QT_BEGIN_NAMESPACE
+class QPushButton;
+class QTimer;
+QT_END_NAMESPACE
+
+namespace Foundation
+{
+    class Framework;
+    class AssetInterface;
+    typedef boost::shared_ptr<AssetInterface> AssetPtr;
+}
+
+namespace Resource
+{
+    namespace Events
+    {
+        class ResourceReady;
+    }
+}
+
+namespace Naali
+{
+    //! AudioPreviewEditor is used to play different audioclips from the inventory and show audio info.
+    class AudioPreviewEditor: public QWidget
+    {
+        Q_OBJECT
+    public:
+        AudioPreviewEditor(Foundation::Framework *framework,
+                           const QString &inventory_id,
+                           const asset_type_t &asset_type,
+                           const QString &name,
+                           QWidget *parent = 0);
+        virtual ~AudioPreviewEditor();
+
+        void HandleAssetReady(Foundation::AssetPtr asset);
+
+        void HandleResouceReady(Resource::Events::ResourceReady *res);
+
+    public slots:
+        /// Close the window.
+        void Closed();
+
+        //! Toggle between sound play and stop states.
+        void PlaySound();
+        void TimerTimeout();
+
+    signals:
+        //! This signal is emitted when the editor is closed.
+        void Closed(const QString &inventory_id, asset_type_t asset_type);
+
+        //! Signal for widget resize.
+        void WidgetResized(QSize size);
+
+    protected:
+        virtual void resizeEvent(QResizeEvent *ev);
+
+    private:
+        void InitializeEditorWidget();
+
+        Foundation::Framework *framework_;
+        asset_type_t assetType_;
+        request_tag_t request_tag_;
+        QString inventoryId_;
+        QString assetId_;
+        sound_id_t soundId_;
+
+        QWidget     *mainWidget_;
+        QPushButton *okButton_;
+        QPushButton *playButton_;
+        QTimer      *playTimer_;
+    };
+}
+
+#endif
