07ecfefc11be19a3905ba77291de567e50023312
mattiku <matti.kuonanoja@evocativi.com>
2010-04-21 18:18:41 +0300

    Added a very simple voice indicator.

---
+++ MumbleVoipModule/VoiceIndicator.h
@@ -0,0 +1,51 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
+#ifndef incl_MumbleVoipModule_VoiceIndicator_h
+#define incl_MumbleVoipModule_VoiceIndicator_h
+
+#include <QObject>
+
+namespace MumbleVoip
+{
+    class PCMAudioFrame;
+
+    class VoiceIndicatorInterface : public QObject
+    {
+        Q_OBJECT
+    public:
+        virtual ~VoiceIndicatorInterface() {};
+        virtual bool IsSpeaking() = 0;
+        virtual double VoiceLevel() = 0;
+        virtual void AnalyzeAudioFrame(PCMAudioFrame* frame) = 0;
+
+        // For user settings..
+        //virtual void SetTresholdLevel(double);
+        //virtual double GetTresholdLevel();
+
+    signals:
+        void StartSpeaking();
+        void StopSpeaking();
+    };
+
+    //! A very simple voice indicator 
+    //! 
+    //! Analyzes every audio packet for average absolute sample value.
+    //! Positive audio packet sets speaking on for a certain time and if
+    //! no voice is detect during that time then speaking is set to false.
+    class SimpleVoiceIndicator : public VoiceIndicatorInterface
+    {
+        Q_OBJECT
+    public:
+        SimpleVoiceIndicator();
+        virtual ~SimpleVoiceIndicator();
+        virtual bool IsSpeaking();
+        virtual double VoiceLevel();
+        virtual void AnalyzeAudioFrame(PCMAudioFrame* frame);
+    private:
+        bool speaking_;
+        int last_voice_ms_;
+    };
+
+} // MumbleVoip
+
+#endif // incl_MumbleVoipModule_VoiceIndicator_h
