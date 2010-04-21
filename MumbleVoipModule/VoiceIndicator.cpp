0d6cd7545fee62d26f2891eaf6a8898523ce5193
mattiku <matti.kuonanoja@evocativi.com>
2010-04-21 18:18:41 +0300

    Added a very simple voice indicator.

---
+++ MumbleVoipModule/VoiceIndicator.cpp
@@ -0,0 +1,72 @@
+#include "StableHeaders.h"
+#include "VoiceIndicator.h"
+#include "MumbleVoipModule.h"
+#include "PCMAudioFrame.h"
+
+namespace MumbleVoip
+{
+
+    SimpleVoiceIndicator::SimpleVoiceIndicator() : speaking_(false), last_voice_ms_(0)
+    {
+
+    }
+
+    SimpleVoiceIndicator::~SimpleVoiceIndicator()
+    {
+
+    }
+    
+    bool SimpleVoiceIndicator::IsSpeaking()
+    {
+        return speaking_;
+    }
+
+    double SimpleVoiceIndicator::VoiceLevel()
+    {
+        return 0; // \todo Implement
+    }
+
+    void SimpleVoiceIndicator::AnalyzeAudioFrame(PCMAudioFrame* frame)
+    {
+        int average_treshold = 150;
+        bool was_speaking = speaking_;
+        int voice_timeout_ms = 150;
+        int average = 0;
+
+        if (frame->SampleWidth() == 16)
+        {
+            short* data = (short*)frame->DataPtr();
+            int samples = frame->Samples();
+            for(int i = 0; i < samples; ++i)
+            {
+                short sample = data[i];
+                average += abs(sample);
+            }
+            average /= samples;
+            if (average > average_treshold)
+            {
+                // Did notice voice activity
+                speaking_ = true;
+                last_voice_ms_ = 0;
+                if (!was_speaking)
+                {
+                    emit StartSpeaking();
+                }
+            }
+            else
+            {
+                // Didn't notice any voice activity 
+                last_voice_ms_ += frame->GetLengthMs();
+                if (last_voice_ms_ > voice_timeout_ms)
+                {
+                    speaking_ = false;
+                    if (was_speaking)
+                    {
+                        emit StopSpeaking();
+                    }
+                }
+            }
+        }
+    }
+
+} // MumbleVoip
