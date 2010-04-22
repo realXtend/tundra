32d5ea7c75ef19690fbd582439a2a2c61757879c
Toni Alatalo <toni@playsign.net>
2010-04-22 15:19:59 +0300

    a tool to copy the ogre stuff from how it is for server side import, to naali bin/media so that works for local loading currently. run this in your Naali bin/media/ dir.

---
+++ bin/pymodules/localscene/copystuff.sh
@@ -0,0 +1,5 @@
+cp /host/k2/oukatk-svn/trunk/ogrescene/*.mesh models/
+cp /host/k2/oukatk-svn/trunk/ogrescene/*.png textures/
+cp /host/k2/oukatk-svn/trunk/ogrescene/*.jpg textures/
+cp /host/k2/oukatk-svn/trunk/ogrescene/*.material materials/scripts/
+cp /host/k2/oukatk-svn/trunk/ogrescene/*.scene .
