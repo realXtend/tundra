5414e1a179fc23a62f980a6467447ec2ed304d12
Stinkfist0 <ali.kamarainen@ludocraft.com>
2010-04-23 14:57:18 +0300

    * Added DisableMemoryLeakCheck & EnableMemoryLeakCheck headers. For example use case see Renderer.cpp.

---
+++ Core/DisableMemoryLeakCheck.h
@@ -0,0 +1,18 @@
+/**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
+ *  @file   DisableMemoryLeakCheck.h
+ *  @brief  Clears the #define new macro that is used for debug memory leak checking.
+ *  
+ *  #include this to overcome situations in code where placement new or other problematic construction is
+ *  used that interferes with the #define new in MemoryLeakCheck.h.
+ *  
+ *  #include EnableMemoryLeakCheck.h afterwards to re-enable memory leak checking for the remainder of
+ *  the compilation unit.
+ *  
+ *  @sa     EnableMemoryLeakCheck.h
+*/
+
+#if defined(_MSC_VER) && defined(_DEBUG) && defined(MEMORY_LEAK_CHECK)
+#undef new
+#endif
