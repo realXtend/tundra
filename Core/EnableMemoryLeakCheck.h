3487cfe76fb2d2d69e721c97e655e9f956055a9d
Stinkfist0 <ali.kamarainen@ludocraft.com>
2010-04-23 14:57:18 +0300

    * Added DisableMemoryLeakCheck & EnableMemoryLeakCheck headers. For example use case see Renderer.cpp.

---
+++ Core/EnableMemoryLeakCheck.h
@@ -0,0 +1,14 @@
+/**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
+ *  @file   EnableMemoryLeakCheck.h
+ *  @brief  (Re)enables a preprocessor macro that routes operator new to its debug counterpart. Win32 only.
+ *  @sa     DisableMemoryLeakCheck.h
+*/
+
+#if defined(_MSC_VER) && defined(_DEBUG) && defined(MEMORY_LEAK_CHECK)
+#ifdef new
+#undef new
+#endif
+#define new new (__FILE__, __LINE__)
+#endif
