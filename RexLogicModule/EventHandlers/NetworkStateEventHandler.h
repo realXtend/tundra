1a5385b34efa619a32cd54a237f0a098577e58eb
Stinkfist0 <ali.kamarainen@ludocraft.com>
2010-05-04 12:45:28 +0300

    *Replace the hack used to prevent duplicate avatar joined notifications with a better implementation *Some cleanup

--- RexLogicModule/EventHandlers/NetworkStateEventHandler.h
+++ RexLogicModule/EventHandlers/NetworkStateEventHandler.h
@@ -1,12 +1,14 @@
-// For conditions of distribution and use, see copyright notice in license.txt
+/**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
+ *  @file   NetworkStateEventHandler.h
+ *  @brief  Handles events related to changes in server connectivity in a RexLogic-specific way.
+ */
 
 #ifndef incl_NetworkStateEventHandler_h
 #define incl_NetworkStateEventHandler_h
 
 #include "CoreTypes.h"
-#include "RexUUID.h"
-
-#include <QList>
 
 namespace Foundation
 {
@@ -17,7 +19,6 @@ namespace RexLogic
 {
     class RexLogicModule;
 
-    /// Handles events related to changes in server connectivity in a RexLogic-specific way.
     class NetworkStateEventHandler
     {
     public:
@@ -29,17 +30,13 @@ namespace RexLogic
         virtual ~NetworkStateEventHandler();
 
         /// Handles
-        /// @param event_id
-        /// @param data
+        /// @param event_id Event ID.
+        /// @param data Event data.
         bool HandleNetworkStateEvent(event_id_t event_id, Foundation::EventDataInterface* data);
 
     private:
         /// Owner module.
         RexLogicModule *owner_;
-
-        /// Trying to avoid double notifications, somehow for every avatar we go twice to 
-        /// HandleOSNE_ObjectUpdate() when there are already avatars inworld
-        QList<RexUUID> sent_avatar_notifications_;
     };
 }
 
--- RexLogicModule/EventHandlers/NetworkStateHandler.cpp
+++ RexLogicModule/EventHandlers/NetworkStateHandler.cpp
@@ -1,4 +1,9 @@
-// For conditions of distribution and use, see copyright notice in license.txt
+/**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
+ *  @file   NetworkStateEventHandler.h
+ *  @brief  Handles events related to changes in server connectivity in a RexLogic-specific way.
+ */
 
 #include "StableHeaders.h"
 #include "EventHandlers/NetworkStateEventHandler.h"
@@ -27,64 +32,58 @@ bool NetworkStateEventHandler::HandleNetworkStateEvent(event_id_t event_id, Foun
 {
     switch(event_id)
     {
-        case ProtocolUtilities::Events::EVENT_SERVER_CONNECTED:
-        {   
-            // The client has connected to the server. Create a new scene for that.
-            owner_->CreateNewActiveScene("World");
-            // Send WorldStream as internal event
-            event_category_id_t framework_category_id = owner_->GetFramework()->GetEventManager()->QueryEventCategory("Framework");
-            ProtocolUtilities::WorldStreamReadyEvent event_data(owner_->GetServerConnection());
-            owner_->GetFramework()->GetEventManager()->SendEvent(framework_category_id, Foundation::WORLD_STREAM_READY, &event_data);
-            break;
-        }
-        case ProtocolUtilities::Events::EVENT_SERVER_DISCONNECTED:
-        {
-            // Might be user quitting or server dropping connection.
-            // This event occurs when OpenSimProtocolModule has already closed connection. 
-            // Make sure the rexlogic also thinks connection is closed.
-            if (owner_->GetServerConnection()->IsConnected())
-                owner_->GetServerConnection()->ForceServerDisconnect();
-            if (owner_->GetFramework()->HasScene("World"))
-                owner_->DeleteScene("World");
-            break;
-        }
-        case ProtocolUtilities::Events::EVENT_USER_CONNECTED:
-        {
-            /// Show notification about new user
-            ProtocolUtilities::UserConnectivityEvent *event_data = checked_static_cast<ProtocolUtilities::UserConnectivityEvent *>(data);
-            assert(event_data);
-            if (!event_data)
-                return false;
+    case ProtocolUtilities::Events::EVENT_SERVER_CONNECTED:
+    {
+        // The client has connected to the server. Create a new scene for that.
+        owner_->CreateNewActiveScene("World");
+        // Send WorldStream as internal event
+        event_category_id_t framework_category_id = owner_->GetFramework()->GetEventManager()->QueryEventCategory("Framework");
+        ProtocolUtilities::WorldStreamReadyEvent event_data(owner_->GetServerConnection());
+        owner_->GetFramework()->GetEventManager()->SendEvent(framework_category_id, Foundation::WORLD_STREAM_READY, &event_data);
+        break;
+    }
+    case ProtocolUtilities::Events::EVENT_SERVER_DISCONNECTED:
+    {
+        // Might be user quitting or server dropping connection.
+        // This event occurs when OpenSimProtocolModule has already closed connection. 
+        // Make sure the rexlogic also thinks connection is closed.
+        if (owner_->GetServerConnection()->IsConnected())
+            owner_->GetServerConnection()->ForceServerDisconnect();
+        if (owner_->GetFramework()->HasScene("World"))
+            owner_->DeleteScene("World");
+        break;
+    }
+    case ProtocolUtilities::Events::EVENT_USER_CONNECTED:
+    {
+        /// Show notification about new user
+        ProtocolUtilities::UserConnectivityEvent *event_data = checked_static_cast<ProtocolUtilities::UserConnectivityEvent *>(data);
+        assert(event_data);
+        if (!event_data)
+            return false;
 
-            UiModulePtr ui_module = owner_->GetFramework()->GetModuleManager()->GetModule
-                <UiServices::UiModule>(Foundation::Module::MT_UiServices).lock();
-            if (ui_module.get() && !sent_avatar_notifications_.contains(event_data->agentId))
-            {
-                ui_module->GetNotificationManager()->ShowNotification(new UiServices::MessageNotification(
-                    QString("%1 joined the world").arg(event_data->fullName.c_str())));
-                sent_avatar_notifications_.append(event_data->agentId);
-            }
-            break;
-        }
-        case ProtocolUtilities::Events::EVENT_USER_DISCONNECTED:
-        {
-            ProtocolUtilities::UserConnectivityEvent *event_data = checked_static_cast<ProtocolUtilities::UserConnectivityEvent *>(data);
-            assert(event_data);
-            if (!event_data)
-                return false;
+        UiModulePtr ui_module = owner_->GetFramework()->GetModuleManager()->GetModule<UiServices::UiModule>(
+            Foundation::Module::MT_UiServices).lock();
+        if (ui_module.get())
+            ui_module->GetNotificationManager()->ShowNotification(new UiServices::MessageNotification(
+                QString("%1 joined the world").arg(event_data->fullName.c_str())));
+        break;
+    }
+    case ProtocolUtilities::Events::EVENT_USER_DISCONNECTED:
+    {
+        ProtocolUtilities::UserConnectivityEvent *event_data = checked_static_cast<ProtocolUtilities::UserConnectivityEvent *>(data);
+        assert(event_data);
+        if (!event_data)
+            return false;
 
-            UiModulePtr ui_module = owner_->GetFramework()->GetModuleManager()->GetModule
-                <UiServices::UiModule>(Foundation::Module::MT_UiServices).lock();
-            if (ui_module.get())
-            {
-                ui_module->GetNotificationManager()->ShowNotification(new UiServices::MessageNotification(
-                    QString("%1 logged out").arg(event_data->fullName.c_str())));
-                sent_avatar_notifications_.removeOne(event_data->agentId);
-            }
-            break;
-        }
-        default:
-            break;
+        UiModulePtr ui_module = owner_->GetFramework()->GetModuleManager()->GetModule<UiServices::UiModule>(
+            Foundation::Module::MT_UiServices).lock();
+        if (ui_module.get())
+            ui_module->GetNotificationManager()->ShowNotification(new UiServices::MessageNotification(
+                QString("%1 logged out").arg(event_data->fullName.c_str())));
+        break;
+    }
+    default:
+        break;
     }
 
     return false;
