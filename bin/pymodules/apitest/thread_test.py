0e463423f88f9969f3a4279558b640e728d11d3e
Toni Alatalo <toni@playsign.net>
2010-05-10 14:40:21 +0300

    An actually working threaded circuits component, a python plugin for Naali that runs a worker thread with which event receivers triggered from the main thread can communicate. There's no magic here, just using a thread safe queue etc.

--- bin/pymodules/apitest/thread_test.py
+++ bin/pymodules/apitest/thread_test.py
@@ -1,4 +1,5 @@
 import time
+from collections import deque
 
 try:
     import rexviewer as r
@@ -13,18 +14,37 @@ class TestThread(circuits.Thread):
     A threaded circuits component inside Naali,
     now testing how this behaves, as can be needed for e.g. webdav inv
     """
+    def __init__(self):
+        circuits.Thread.__init__(self)
+        self.commands = deque()
+        self.start()
 
-    #def update(self, deltatime):
-    #    if self.data == 1:
-    #        self.data = 2 #change this to any non-1 number to test live reloading
-    #    else:
-    #        self.data = 1
-    #    #r.logInfo(str(self.data))
-        
+    #gets events from the manager in the main thread
     @handler("on_input") #why is this needed for Thread, but not for Component?
     def on_input(self, evid):
         print "Test Thread received input event:", evid
-        #time.sleep(1)
+
+    #this is executed in a separate thread
+    def run(self):
+        while 1:
+            time.sleep(1)
+            print "I'm a lazy sleeper. And even a blocking one."
+            if len(self.commands) > 0:
+                print self.commands
+                c = self.commands.popleft()
+                if c == "stop":
+                    self.stop()
+                    return
+
+    #when this worker thread stops itself
+    def stop(self):
+        circuits.Thread.stop(self)
+        print "stopped"
+
+    #when the manager is stopped
+    @handler("stopped")
+    def stopped(self, caller):
+        self.commands.append("stop")
 
 if __name__ == '__main__':
     from circuits import Event, Manager, Debugger
@@ -32,12 +52,25 @@ if __name__ == '__main__':
 
     m = Manager()
     c = TestThread()
-    #c.start()
     m += c + Debugger()
 
-    m.push(Input(1), "on_input")
-    #c.join()
+    now = time.time()
+    interval = 0.5
+    prev_time = now
 
-    while m:
-        m.flush()
+    try:
+        while 1:
+            now = time.time()
+            if prev_time + interval < now:
+                m.push(Input(1), "on_input")
+                prev_time = now
+            while m:
+                m.flush()
 
+    except KeyboardInterrupt:
+        print "stopping"
+        m.stop()
+    
+        while m:
+            m.flush()
+        c.join()
