111544144a88bb536d68619e81d557677da832fa
Toni Alatalo <toni@playsign.net>
2010-05-26 14:23:55 +0300

    Working slider control for animation time position of scene objects, synched to clients. Made with a Python-written DynamicComponent handler that uses qt signals to hook to the component.

--- bin/pymodules/apitest/attrsync.py
+++ bin/pymodules/apitest/attrsync.py
@@ -1,19 +1,35 @@
 """
 A test / draft for defining Naali Entity-Components with Naali Attribute data.
 """
+from __future__ import division
 
 import circuits
 import rexviewer as r
 import naali
 
+import PythonQt
+from PythonQt import QtGui, QtCore
+
 class DynamiccomponentHandler(circuits.Component):
     def __init__(self):
         circuits.Component.__init__(self)
-        obid = None #2514504734 #1220426476
+        obid = None 
+        #obid = 1136641184
+        #obid = 1239367367
         self.d = None
         if obid is not None:
             ent = r.getEntity(obid)
             self.add_component(ent)
+
+        self.widget = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.widget.connect('valueChanged(int)', self.sliderChanged)
+
+        uism = r.getUiSceneManager()
+        uiprops = r.createUiWidgetProperty(1)
+        uiprops.widget_name_ = "DynamicComponent Widget"
+        self.proxywidget = r.createUiProxyWidget(self.widget, uiprops)
+        if not uism.AddProxyWidget(self.proxywidget):
+            print "Adding the ProxyWidget to the bar failed."
 
     def on_sceneadded(self, name):
         print "Scene added:", name#,
@@ -33,6 +49,7 @@ class DynamiccomponentHandler(circuits.Component):
         if comp.className() == "EC_DynamicComponent":
             comp.connect("OnChanged()", self.onChanged)
             self.d = comp
+            print "DYNAMIC COMPONENT FOUND", self.d
 
     def add_component(self, ent):
         try:
@@ -41,20 +58,36 @@ class DynamiccomponentHandler(circuits.Component):
             ent.createComponent("EC_DynamicComponent")
         print ent.dynamic
             
-        #d = ent.dynamic
-        #print dir(d)
-        #d.AddAttribute()
-        #print d.GetAttribute()
+        if 1:
+            d = ent.dynamic
+            #print dir(d)
+            #d.AddAttribute()
+            print d.GetAttribute()
 
-        #d.connect("OnChanged()", self.onChanged)
-        
-        #self.d = d
+            d.connect("OnChanged()", self.onChanged)
+            self.d = d
+
+    def sliderChanged(self, val):
+        print val
+        d = self.d
+        if d is not None:
+            d.SetAttribute(val / 100)
 
     def onChanged(self):
         print "onChanged",
         d = self.d
         if d is not None:
             print d.GetAttribute()
+            ent = r.getEntity(d.GetParentEntityId())
+            try:
+                a = ent.animationcontroller
+            except AttributeError:
+                print "ent with DynamicComponent doesn't have animation"
+                return
+
+            print a
+            a.SetAnimationTimePosition("Wave", d.GetAttribute())
+            
         else:
             print "- don't know what :o"
 
@@ -69,3 +102,8 @@ class DynamiccomponentHandler(circuits.Component):
                 d.SetAttribute(pos.x())
                #d.ComponentChanged(1) #Foundation::ChangeType::Local - not a qenum yet
             #print d.GetAttribute()
+
+    def on_exit(self):
+        uism = r.getUiSceneManager()
+        uism.RemoveProxyWidgetFromScene(self.proxywidget)
+    
