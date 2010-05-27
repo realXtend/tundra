b251391e685e8608351c9705d298966bc308b595
Toni Alatalo <toni@playsign.net>
2010-05-27 16:56:17 +0300

    First version of a door made with DynamicComponent + a py handler. A GUI button toggles open/closed state, and handler moves an object accordingly. The position in the scene is not updated in this version, first by accident, but is good to verify the attr sync.
    Refactored the common parts of current comphandlers (animsync&door) to a base.

--- bin/pymodules/apitest/attrsync.py
+++ bin/pymodules/apitest/attrsync.py
@@ -1,93 +1,45 @@
 """
 A test / draft for defining Naali Entity-Components with Naali Attribute data.
+This one synchs animation state with a GUI slider.
+There is now also another test, door.py, so moved common parts to componenthandler.py
 """
+
 from __future__ import division
 import time
-
-import circuits
 import rexviewer as r
-import naali
 
 import PythonQt
 from PythonQt import QtGui, QtCore
 
+from componenthandler import DynamiccomponentHandler
+
 INTERVAL = 0.2
 
-class DynamiccomponentHandler(circuits.Component):
-    def __init__(self):
-        circuits.Component.__init__(self)
-        obid = None 
-        #obid = 1136641184
-        #obid = 1239367367
-        self.d = None
-        if obid is not None:
-            ent = r.getEntity(obid)
-            self.add_component(ent)
+class AnimationSync(DynamiccomponentHandler):
+    GUINAME = "Animation Sync"
 
+    def initgui(self):
         self.widget = QtGui.QSlider(QtCore.Qt.Horizontal)
         self.widget.connect('valueChanged(int)', self.sliderChanged)
-
-        uism = r.getUiSceneManager()
-        uiprops = r.createUiWidgetProperty(1)
-        uiprops.widget_name_ = "DynamicComponent Widget"
-        self.proxywidget = r.createUiProxyWidget(self.widget, uiprops)
-        if not uism.AddProxyWidget(self.proxywidget):
-            print "Adding the ProxyWidget to the bar failed."
 
         #to not flood the network
         self.prev_sync = 0
 
-    def on_sceneadded(self, name):
-        print "Scene added:", name#,
-        s = naali.getScene(name)
-        print s
-        print dir(s)
-
-        #s.connect("ComponentInitialized(Foundation::ComponentInterface*)", self.onComponentInitialized)
-        s.connect("ComponentAdded(Scene::Entity*, Foundation::ComponentInterface*, Foundation::ChangeType)", self.onComponentAdded)
-
-    #def onComponentInitialized(self, comp):
-    #    print "Comp inited:", comp
-
-    def onComponentAdded(self, entity, comp, changetype):
-        print "Comp added:", entity, comp, changetype
-        #print comp.className()
-        if comp.className() == "EC_DynamicComponent":
-            comp.connect("OnChanged()", self.onChanged)
-            self.d = comp
-            print "DYNAMIC COMPONENT FOUND", self.d
-
-    def add_component(self, ent):
-        try:
-            ent.dynamic
-        except AttributeError:
-            ent.createComponent("EC_DynamicComponent")
-        print ent.dynamic
-            
-        if 1:
-            d = ent.dynamic
-            #print dir(d)
-            #d.AddAttribute()
-            print d.GetAttribute()
-
-            d.connect("OnChanged()", self.onChanged)
-            self.d = d
-
     def sliderChanged(self, val):
         print val
-        d = self.d
-        if d is not None:
+        comp = self.comp
+        if comp is not None:
             now = time.time()
             if self.prev_sync + INTERVAL < now:
-                d.SetAttribute(val / 100)
+                comp.SetAttribute(val / 100)
                 self.prev_sync = now
 
     def onChanged(self):
         print "onChanged",
-        d = self.d
-        if d is not None:
-            print d.GetAttribute()
-            ent = r.getEntity(d.GetParentEntityId())
+        comp = self.comp
+        if comp is not None:
+            print comp.GetAttribute()
+            ent = r.getEntity(comp.GetParentEntityId())
             try:
                 a = ent.animationcontroller
             except AttributeError:
@@ -95,26 +47,21 @@ class DynamiccomponentHandler(circuits.Component):
                 return
 
             #print a
-            v = d.GetAttribute()
+            v = comp.GetAttribute()
             a.SetAnimationTimePosition("Wave", v)
             #self.widget.value = v * 100 #needs changetypes to work well, i guess
             
         else:
             print "- don't know what :o"
 
-    def update(self, frametime):
-        d = self.d
-        if d is not None:
-            if 0:
-                avid = r.getUserAvatarId()
-                e = r.getEntity(avid)
-                pos = e.placeable.Position
-                print pos
-                d.SetAttribute(pos.x())
+#    def update(self, frametime):
+#        comp = self.comp
+#        if comp is not None:
+#            if 0:
+#                avid = r.getUserAvatarId()
+#                e = r.getEntity(avid)
+#                pos = e.placeable.Position
+#                print pos
+#                comp.SetAttribute(pos.x())
                #d.ComponentChanged(1) #Foundation::ChangeType::Local - not a qenum yet
             #print d.GetAttribute()
-
-    def on_exit(self):
-        uism = r.getUiSceneManager()
-        uism.RemoveProxyWidgetFromScene(self.proxywidget)
-    
