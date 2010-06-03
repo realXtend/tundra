cbb439565c89fdf9c506775bcc463a84dbf50ccd
Stinkfist0 <ali.kamarainen@ludocraft.com>
2010-06-03 19:32:03 +0300

    Log out, delete world and show modal message box when KickUser message is received.

--- RexLogicModule/EventHandlers/NetworkEventHandler.h
+++ RexLogicModule/EventHandlers/NetworkEventHandler.h
@@ -1,19 +1,21 @@
-// For conditions of distribution and use, see copyright notice in license.txt
+/**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
+ *  @file   NetworkEventEventHandler.h
+ *  @brief  Handles incoming SLUDP network events (messages) in a reX-specific way.
+ */
 
-#ifndef incl_NetworkEventHandler_h
-#define incl_NetworkEventHandler_h
-
-#include "ComponentInterface.h"
-#include "NetworkEvents.h"
+#ifndef incl_RexLogicModule_NetworkEventHandler_h
+#define incl_RexLogicModule_NetworkEventHandler_h
 
 namespace ProtocolUtilities
 {
     class ProtocolModuleInterface;
+    class NetworkEventInboundData;
 }
 
 namespace RexLogic
 {
-    struct DecodedTerrainPatch;
     class RexLogicModule;
     class ScriptDialogHandler;
     typedef boost::shared_ptr<ScriptDialogHandler> ScriptDialogHandlerPtr;
@@ -23,49 +25,111 @@ namespace RexLogic
         class Provider;
     }
     typedef boost::shared_ptr<InWorldChat::Provider> InWorldChatProviderPtr;
- 
-    /// Handles incoming SLUDP network events in a reX-specific way. \todo Break down into more logical functions.
+
+    /// Handles incoming SLUDP network events (messages) in a reX-specific way.
     class NetworkEventHandler
     {
     public:
-        explicit NetworkEventHandler(RexLogicModule *rexlogicmodule);
+        //! Constructor
+        //! \param owner Owner module.
+        explicit NetworkEventHandler(RexLogicModule *owner);
+
+        //! Destructor.
         virtual ~NetworkEventHandler();
 
         // !Handle network events coming from OpenSimProtocolModule
         bool HandleOpenSimNetworkEvent(event_id_t event_id, Foundation::EventDataInterface *data);
+
     private:
-        // !Handler functions for Opensim network events
+        //! Handles AgentMovementComplete network message.
+        //! \param data Network event data.
         bool HandleOSNE_AgentMovementComplete(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles ImprovedTerseObjectUpdate network message.
+        //! \param data Network event data.
         bool HandleOSNE_ImprovedTerseObjectUpdate(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles KillObject network message.
+        //! \param data Network event data.
         bool HandleOSNE_KillObject(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles LogoutReply network message.
+        //! \param data Network event data.
         bool HandleOSNE_LogoutReply(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles ObjectUpdate network message.
+        //! \param data Network event data.
         bool HandleOSNE_ObjectUpdate(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles RegionHandshake network message.
+        //! \param data Network event data.
         bool HandleOSNE_RegionHandshake(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles SoundTrigger network message.
+        //! \param data Network event data.
         bool HandleOSNE_SoundTrigger(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles PreloadSound network message.
+        //! \param data Network event data.
         bool HandleOSNE_PreloadSound(ProtocolUtilities::NetworkEventInboundData *data);
-        bool HandleOSNE_ScriptDialog(ProtocolUtilities::NetworkEventInboundData *data);       
+
+        //! Handles ScriptDialog network message.
+        //! \param data Network event data.
+        bool HandleOSNE_ScriptDialog(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles LoadURL network message.
+        //! \param data Network event data.
         bool HandleOSNE_LoadURL(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles MapBlock network message.
+        //! \param data Network event data.
         bool HandleOSNE_MapBlock(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles ScriptTeleport network message.
+        //! \param data Network event data.
         bool HandleOSNE_ScriptTeleport(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles ChatFromSimulator network message.
+        //! \param data Network event data.
         bool HandleOSNE_ChatFromSimulator(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles ImprovedInstantMessage network message.
+        //! \param data Network event data.
         bool HandleOSNE_ImprovedInstantMessage(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles OnlineNotification network message.
+        //! \param data Network event data.
         bool HandleOSNE_OnlineNotification(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles OfflineNotification network message.
+        //! \param data Network event data.
         bool HandleOSNE_OfflineNotification(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles TerminateFriendship network message.
+        //! \param data Network event data.
         bool HandleOSNE_TerminateFriendship(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles DeclineFriendship network message.
+        //! \param data Network event data.
         bool HandleOSNE_DeclineFriendship(ProtocolUtilities::NetworkEventInboundData *data);
-        
-        //! Handler functions for GenericMessages
+
+        //! Handles KickUser network message.
+        //! \param data Network event data.
+        bool HandleOSNE_KickUser(ProtocolUtilities::NetworkEventInboundData *data);
+
+        //! Handles GenericMessage network message.
+        //! \param data Network event data.
         bool HandleOSNE_GenericMessage(ProtocolUtilities::NetworkEventInboundData *data);
 
-        void DebugCreateTerrainVisData(const DecodedTerrainPatch &heightData, int patchSize);
-
+        //! Protocol module.
+        //! \todo Why is this here. It's not used for anything but some (random?) check in the ctor.
         boost::weak_ptr<ProtocolUtilities::ProtocolModuleInterface> protocolModule_;
 
-        RexLogicModule *rexlogicmodule_;
+        //! Owner module.
+        RexLogicModule *owner_;
 
         ScriptDialogHandlerPtr script_dialog_handler_; /// @todo: Move to RexLogic module
         bool ongoing_script_teleport_;
-
     };
 }
 
