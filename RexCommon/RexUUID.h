e44bb0c3c6738271e877b1486072c2412fac8efa
Stinkfist0 <ali.kamarainen@ludocraft.com>
2010-06-03 19:33:20 +0300

    Add few QString overloads for RexUUID + QDebug printing/serialization support.

--- RexCommon/RexUUID.h
+++ RexCommon/RexUUID.h
@@ -1,25 +1,29 @@
-// For conditions of distribution and use, see copyright notice in license.txt
-
 /**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
  *  @file   RexUUID.h
  *  @brief  RexUUID is a 16-byte identifier for resources in a virtual world.
 */
 
-#ifndef incl_RexUUID_h
-#define incl_RexUUID_h
+#ifndef incl_RexCommon_RexUUID_h
+#define incl_RexCommon_RexUUID_h
 
 #include "RexTypes.h"
 
+#include <QDebug>
+
+/// RexUUID is a 16-byte identifier for resources in a virtual world.
 class RexUUID
 {
 public:
-    /// Constructs an RexUUID from a string in form "1c1bbda2-304b-4cbf-ba3f-75324b044c73" or "1c1bbda2304b4cbfba3f75324b044c73".
+    /// Default constuctor. Constructs a null RexUUID.
+    RexUUID();
+
+    /// Constructor. Constructs an RexUUID from a string in form "1c1bbda2-304b-4cbf-ba3f-75324b044c73" or "1c1bbda2304b4cbfba3f75324b044c73".
     /// @param str String.
     explicit RexUUID(const char *str);
     explicit RexUUID(const std::string &str);
-
-    /// Constructs a null RexUUID.
-    RexUUID();
+    explicit RexUUID(const QString &str);
 
     /// Sets all 16 bytes of the ID to '00'.
     void SetNull();
@@ -37,6 +41,7 @@ public:
     /// @param str String.
     void FromString(const char *str);
     void FromString(const std::string &str) { FromString(str.c_str()); }
+    void FromString(const QString &str) { FromString(str.toStdString()); }
 
     /// Converts the RexUUID to a QString.
     std::string ToString() const;
@@ -48,19 +53,39 @@ public:
     /// @param str String.
     static bool IsValid(const char *str);
     static bool IsValid(const std::string &str) { return IsValid(str.c_str()); }
+    static bool IsValid(const QString &str) { return IsValid(str.toStdString()); }
 
+    /// Assign operator. Copies data from rhs to the new RexUUID.
     RexUUID &operator =(const RexUUID &rhs);
 
+    /// Equality operator
+    /// @return true if the RexUUID's are indentical, false otherwise.
     bool operator ==(const RexUUID &rhs) const;
 
+    /// Inequality operator
+    /// @return true if the RexUUID's don't match, false if they do.
     bool operator !=(const RexUUID &rhs) const;
 
+    /// Less than operator
+    /// @return true if the left-hand side is less than the right-hand side.
     bool operator <(const RexUUID &rhs) const;
 
+    /// Greater than operator
+    /// @return true if the left-hand side is greater than the right-hand side.
+    bool operator >(const RexUUID &rhs) const;
+
+    /// Serializes RexUUID to ostream
     friend std::ostream& operator << (std::ostream &out, const RexUUID &rhs)
     {
-        out << "RexUUID(" << rhs.ToString() << ")";
+        out << rhs.ToString();
         return out;
+    }
+
+    /// Serializes RexUUID to QDebug
+    friend QDebug &operator << (QDebug &dbg, const RexUUID &rhs)
+    {
+        dbg.nospace() << rhs.ToQString();
+        return dbg.space();
     }
 
     /// Size in bytes.
