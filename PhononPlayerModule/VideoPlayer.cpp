e4f6ad7099bfdc2f43dc66dc270d8e0dd85c6209
mattiku <matti.kuonanoja@evocativi.com>
2010-08-02 13:37:27 +0300

    Changed video playback to use source queue.

--- PhononPlayerModule/VideoPlayer.cpp
+++ PhononPlayerModule/VideoPlayer.cpp
@@ -18,7 +18,7 @@ namespace PlayerService
     {
         //layout_ = new QBoxLayout(QBoxLayout::LeftToRight, this);
 //        this->setLayout(new QBoxLayout(QBoxLayout::LeftToRight, this));
-      //  Play("http://inventaaustralia.zftp.com/videos/MPEGIO_MPEG2_176X144_Video1024Kbps.mpg");
+//        Play("http://inventaaustralia.zftp.com/videos/MPEGIO_MPEG2_176X144_Video1024Kbps.mpg");
         Play(url);
     }
 
@@ -26,10 +26,9 @@ namespace PlayerService
     {
         if (media_object_)
         {
-            if (media_object_->state() == Phonon::PlayingState)
-                media_object_->stop();
-
+            media_object_->disconnect(this);
             media_object_->clear();
+            delete media_object_;
         }
     }
 
@@ -39,20 +38,17 @@ namespace PlayerService
         video_widget_ = new Phonon::VideoWidget(this);
         Phonon::createPath(media_object_, video_widget_);
         
-       // QObject::connect(media_object_, SIGNAL(finished()), this, SLOT(Restart()));
-        QObject::connect(media_object_, SIGNAL(stateChanged(Phonon::State, Phonon::State)), this, SLOT(CheckState(Phonon::State)));
+        QObject::connect(media_object_, SIGNAL(stateChanged(Phonon::State, Phonon::State)), this, SLOT(CheckState(Phonon::State, Phonon::State)));
         QObject::connect(media_object_, SIGNAL(hasVideoChanged(bool)), this, SLOT(StartVideoPlayback(bool)));
+        QObject::connect(media_object_, SIGNAL(aboutToFinish()), this, SLOT(RestartVideoPlayback()));
     
         media_object_->setCurrentSource(url);
-                    media_object_->stop();
-
-//        StartVideoPlayback(true);
+        media_object_->stop();
         media_object_->play();
     }
 
     void VideoPlayer::StartVideoPlayback(bool has_video)
     {
-        qDebug() << "<----2 b";
         if (!has_video)
             return;
 
@@ -61,48 +57,27 @@ namespace PlayerService
         
         if (!video_widget_->parent())
         {
-                //this->layout()->addWidget(video_widget_);
+            //    this->layout()->addWidget(video_widget_);
                 //media_object_->setParent(this);
 //            layout_->addWidget(video_widget_);
 //            video_widget_->setParent(this);
             //video_widget_->layout()->setMargin(0);
             //this->layout()->addWidget(video_widget_);
         }
-        qDebug() << media_object_->state();
 
-            if (media_object_->isSeekable())
-                media_object_->seek(0);
+        if (media_object_->isSeekable() && media_object_->currentTime() != 0)
+            media_object_->seek(0);
 
-            media_object_->stop();
-
-       // media_object_->play();
-        qDebug() << "<----2 e";
+        //media_object_->stop();
+        //media_object_->play();
     }
        
-    void VideoPlayer::Restart()
+    void VideoPlayer::RestartVideoPlayback()
     {
-        if (!media_object_)
-            return;
-
-        if ( media_object_->state() == Phonon::PausedState )
-        {
-            //media_object_->pause();
-            if (media_object_->isSeekable())
-                media_object_->seek(0);
-            media_object_->play();
-            qDebug() << media_object_->state();
-            return;
-        }
-
-        if (media_object_->state() == Phonon::StoppedState ) // || media_object_->state() == Phonon::PausedState)
-        {
-            media_object_->play();
-            qDebug() << media_object_->state();
-            return;
-        }
+        media_object_->enqueue(media_object_->currentSource());
     }
 
-    void VideoPlayer::CheckState(Phonon::State new_state)
+    void VideoPlayer::CheckState(Phonon::State new_state, Phonon::State old_state)
     {
         if (new_state == Phonon::ErrorState)
         {
@@ -115,31 +90,6 @@ namespace PlayerService
 //                video_widget_->close();
             }
             return;
-        }
-//        if (media_object_->state() == Phonon::PlayingState)
-        {
-            //if (!video_widget_->parent())
-            //{
-            //    //this->layout()->addWidget(video_widget_);
-
-            //    //media_object_->setParent(this);
-            //    video_widget_->setParent(this);
-            //    //qDebug() << this->layout();
-            //    //qDebug() << video_widget_->layout();
-            //    //video_widget_->layout()->setMargin(0);
-            //    //layout_.addWidget(video_widget_);
-            //    //layout_.setMargin(0);
-            //}
-        }
-        qDebug() << new_state;
-        qDebug() << media_object_->totalTime();
-        qDebug() << media_object_->currentTime();
-        media_object_->seek(0);
-
-        if ( ( new_state == Phonon::StoppedState || new_state == Phonon::PausedState ) && !error_handled_)
-        //if ( ( new_state == Phonon::StoppedState ) && !error_handled_)
-        {
-            Restart();
         }
     }
 }
