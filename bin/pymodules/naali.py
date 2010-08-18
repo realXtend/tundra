b339cc47dea0a97864a22cd9dd706cbdbe47d381
Nathan Letwory <nathan@letworyinteractive.com>
2010-08-18 14:30:06 +0300

    Add  getter for camera entity to naali module

--- bin/pymodules/naali.py
+++ bin/pymodules/naali.py
@@ -1,12 +1,19 @@
 """namespace config, 'cause the c++ side doesn't do it too nicely"""
 
 from __main__ import _naali
-#from _naali import *
 
-renderer = _naali.GetRenderer()
+n = _naali
+
+# module methods
 runjs = _naali.RunJavascriptString
-inputcontext = _naali.GetInputContext()
-mediaplayerservice = _naali.GetMediaPlayerService()
 getCamera = _naali.GetCamera
+getCameraEntity = _naali.GetCameraEntity
+
 def getScene(name):
     return _naali.GetScene(name)
+
+# module variables
+renderer = _naali.GetRenderer()
+inputcontext = _naali.GetInputContext()
+mediaplayerservice = _naali.GetMediaPlayerService()
+
