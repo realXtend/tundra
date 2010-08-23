e53110b934801190959d1736246ab87bb9e37926
Toni Alatalo <toni@playsign.net>
2010-08-23 18:31:15 +0300

    ported javascript component handler support to the new system. removed old separate jscomponent handler, and moved the code to the main componenthandler. had to make a function that creates functors on the fly to deal with the prob that the internals don't currently tell when a newly added component is ready - when ComponentAdded is triggered, the data from current EC storage is not there yet.

--- bin/pymodules/apitest/componenthandler.py
+++ bin/pymodules/apitest/componenthandler.py
@@ -1,8 +1,9 @@
 import circuits
 import rexviewer as r
 import naali
+import urllib2 #for js_src downloading
 
-"""a register of component handlers, by type"""
+"""a registry of component handlers, by type"""
 handlertypes = {}
 def register(compname, handlertype):
     handlertypes[compname] = handlertype
@@ -14,8 +15,8 @@ import door
 register(door.COMPNAME, door.DoorHandler)
 
 class ComponenthandlerRegistry(circuits.BaseComponent):
-    #def __init__(self):
-    #    circuits.BaseComponent.__init__(self)
+    def __init__(self):
+        circuits.BaseComponent.__init__(self)
     #    self.handlerinstances = []
 
     @circuits.handler("on_sceneadded")
@@ -37,3 +38,55 @@ class ComponenthandlerRegistry(circuits.BaseComponent):
             if comp.Name in handlertypes:
                 handlertypes[comp.Name](entity, comp, changetype)
                 #self.handlerinstances.append(h)
+
+            #if the data was there already, could do this.
+            #but it's not - must now listen to onChanged and check instead
+            #jssrc = comp.GetAttribute("js_src")
+            #print "JS SRC:", jssrc
+            #if jssrc is not None:
+            #    self.apply_js(jssrc)
+            jscheck = make_jssrc_handler(entity, comp, changetype)
+            comp.connect("OnChanged()", jscheck)
+
+def make_jssrc_handler(entity, comp, changetype):
+    #def handle_js():
+    class JsHandler(): #need a functor so that can disconnect itself
+        def __call__(self):
+            jssrc = comp.GetAttribute("js_src")
+            print "JS SRC:", jssrc
+            if jssrc is not None:
+                apply_js(jssrc, comp)
+            comp.disconnect("OnChanged()", self)
+    return JsHandler()
+
+def apply_js(jssrc, comp):
+    jscode = loadjs(jssrc)
+
+    print jscode
+
+    ctx = {
+        #'entity'/'this': self.entity
+        'component': comp
+    }
+
+    ent = r.getEntity(comp.GetParentEntityId())
+    try:
+        ent.touchable
+    except AttributeError:
+        pass
+    else:
+        ctx['touchable'] = ent.touchable
+    try:
+        ent.placeable
+    except:
+        pass
+    else:
+        ctx['placeable'] = ent.placeable
+            
+    naali.runjs(jscode, ctx)
+    print "-- done with js"
+
+def loadjs(srcurl):
+    print "js source url:", srcurl
+    f = urllib2.urlopen(srcurl)
+    return f.read()
