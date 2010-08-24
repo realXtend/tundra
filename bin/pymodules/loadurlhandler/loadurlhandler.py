9c4b072ba046b48ffa816673e8381a21da7eb378
Jonne Nauha <jonne.nauha@evocativi.com>
2010-08-24 19:31:16 +0300

    MediaUrlHandler: dont create webview if texture is not found inworld, better qwebview delete code (stop networking before deleting), dont apply submeshes if they are same as have been applied before. etc.. hopefully fixes some crash issues on exit and performance issues on login to mediaurl heavy worlds (such as toy).

--- bin/pymodules/loadurlhandler/loadurlhandler.py
+++ bin/pymodules/loadurlhandler/loadurlhandler.py
@@ -1,6 +1,9 @@
 import rexviewer as r
-from circuits import Component
 import PythonQt
+
+from circuits import Component
+from PythonQt.QtGui import QWidget, QVBoxLayout
+from PythonQt.QtWebKit import QWebView
 
 instance = None
 
@@ -8,25 +11,37 @@ class LoadURLHandler(Component):
     def __init__(self):
         Component.__init__(self)
         self.proxywidget = None
+        self.container = None
         self.webview = None
         
         global instance
         instance = self
 
-    def loadurl(self, url):
+    def loadurl(self, url):       
+        if self.webview == None and self.container == None and self.proxywidget == None:
+            self.init_ui()
+
+        self.webview.load(url)
+        print "Loaded url:", str(url)
+        if not self.proxywidget.isVisible():
+            print "Showing webview proxy in scene"
+            self.proxywidget.show()
+            
+    def init_ui(self):
+        print "Initialising ui"
+        self.container = QWidget()
+        self.webview = QWebView(self.container)
+        self.container.setLayout(QVBoxLayout())
+        self.container.layout().addWidget(self.webview)
+        
+        print "Adding webview to scene"
         uism = r.getUiSceneManager()
-        uiprops = r.createUiWidgetProperty(2) #2 == scene(?) widget or so
+        uiprops = r.createUiWidgetProperty(2) # SceneWidget
         uiprops.widget_name_ = 'LoadURL'
-        
-        self.webview = PythonQt.QtWebKit.QWebView()
-        self.proxywidget = r.createUiProxyWidget(self.webview, uiprops)
-        
+        self.proxywidget = r.createUiProxyWidget(self.container, uiprops)
         if not uism.AddProxyWidget(self.proxywidget):
             r.logError('LoadURLHandler: Adding the ProxyWidget to the scene failed.')
             return
-            
-        self.proxywidget.show()
-        self.webview.load(url)
 
     def on_exit(self):
         r.logInfo('LoadURLHandler exiting..')
