2627a89e5da7c2a58c5ed8de439c72b469bd9ea4
Joosua <jonnevaisanen@ludocraft.com>
2010-08-27 13:16:51 +0300

    - Added a new version of EC_ParticleSystem that don't depend on Primitive object. EC_ParticleSystem can now be edited with ECEditor. - Fixed Entity GetComponents template method to use shared pointers (previous version didn't work).

--- RexLogicModule/EntityComponent/ComponentResourceHandler.cpp
+++ RexLogicModule/EntityComponent/ComponentResourceHandler.cpp
@@ -5,6 +5,7 @@
 #include "OgreMeshResource.h"
 
 #include "EntityComponent/EC_Mesh.h"
+#include "EC_ParticleSystem.h"
 
 namespace RexLogic
 {
@@ -29,7 +30,17 @@ void ComponentResourceHandler::HandleResourceEvent(event_id_t event_id, Foundati
         for(;iter != entities.end(); iter++)
         {
             boost::shared_ptr<EC_Mesh> mesh = (*iter)->GetComponent<EC_Mesh>();
-            mesh->HandleResourceEvent(event_id, data);
+            if(mesh)
+                mesh->HandleResourceEvent(event_id, data);
+        }
+
+        entities = scene->GetEntitiesWithComponent("EC_ParticleSystem");
+        iter = entities.begin();
+        for(;iter != entities.end(); iter++)
+        {
+            std::vector<boost::shared_ptr<EC_ParticleSystem> > partComps = (*iter)->GetComponents<EC_ParticleSystem>();
+            for(uint i = 0; i < partComps.size(); i++)
+                partComps[i]->HandleResourceEvent(event_id, data);
         }
     }
 }
