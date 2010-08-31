2f35c0b41234d6ae14e5447255c323619c9f9f0c
Stinkfist0 <ali.kamarainen@ludocraft.com>
2010-08-31 17:10:16 +0300

    *Made Framework a QObject which emits FrameProcessed(double frametime) signal after each frame is processed.

--- Foundation/Framework.h
+++ Foundation/Framework.h
@@ -48,13 +48,16 @@ namespace Foundation
 
         \ingroup Foundation_group
     */
-    class Framework
+    class Framework : public QObject
     {
+        Q_OBJECT
+
     public:
         typedef std::map<std::string, Scene::ScenePtr> SceneMap;
 
         //! constructor. Initializes the framework.
         Framework(int argc, char** argv);
+
         //! destructor
         ~Framework();
 
@@ -255,6 +258,12 @@ namespace Foundation
         {
             return GetServiceManager()->GetService<T>().lock().get();
         }
+
+    signals:
+        /** Emitted after one frame is processed.
+         *  @param frametime Elapsed time in seconds since the last frame.
+         */
+        void FrameProcessed(double frametime);
 
     private:
         //! Registers framework specific console commands
