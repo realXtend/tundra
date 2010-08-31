1cc60f53025f4ea51eaefa97aa0b126b8c1cc042
Joosua <jonnevaisanen@ludocraft.com>
2010-08-31 11:48:47 +0300

    Forgot to add some files.

---
+++ EntityComponents/EC_Sound/EC_Sound.h
@@ -0,0 +1,49 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
+#ifndef incl_EC_Sound_EC_Sound_h
+#define incl_EC_Sound_EC_Sound_h
+
+#include "ComponentInterface.h"
+#include "AttributeInterface.h"
+#include "ResourceInterface.h"
+#include "Declare_EC.h"
+
+namespace Foundation
+{
+    class ModuleInterface;
+}
+
+class EC_Sound : public Foundation::ComponentInterface
+{
+    DECLARE_EC(EC_Sound);
+    Q_OBJECT
+public:
+    ~EC_Sound();
+    virtual bool IsSerializable() const { return true; }
+
+    Foundation::Attribute<std::string>  soundId_;
+    Foundation::Attribute<Real>         soundInnerRadius_;
+    Foundation::Attribute<Real>         soundOuterRadius_;
+    Foundation::Attribute<Real>         soundGain_;
+    Foundation::Attribute<bool>         loopSound_;
+    Foundation::Attribute<bool>         triggerSound_;
+
+public slots:
+    void AttributeUpdated(Foundation::ComponentInterface *component, Foundation::AttributeInterface *attribute);
+    void PlaySound();
+    void StopSound();
+    //! Get each attribute values and update sound's parameters if it's in playing state.
+    void UpdateSoundSettings();
+
+private slots:
+    void UpdateSignals();
+
+private:
+    explicit EC_Sound(Foundation::ModuleInterface *module);
+    void FindPlaceable();
+
+    Foundation::ComponentInterfacePtr placeable_;
+    sound_id_t sound_id_;
+};
+
+#endif
