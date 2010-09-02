44ab8879f91aab21f5b244e19301a65546624664
Toni Alatalo <toni@playsign.net>
2010-09-02 14:11:11 +0300

    made importable

--- bin/pymodules/apitest/chessview/pychessview.py
+++ bin/pymodules/apitest/chessview/pychessview.py
@@ -1,59 +1,56 @@
+import gtk
 import __builtin__, gettext, gtk.glade
 gettext.install("pychess", unicode=1)
 t = gettext.translation("pychess", fallback=True)
 __builtin__.__dict__["ngettext"] = t.ungettext #evilness copy-paste from pychess startup script
 
-from pychess.System.Log import log
-import parsemove
-
 import pychess.Main
-p = pychess.Main.PyChess(None)
+from pychess.System.Log import log
 
-import gtk
-gtk.gdk.threads_init()
+import pychess.widgets.ionest as i #current hack to get access to GameModel
+import parsemove #the own thing here for parsing move data from pychess
 
-log.log("Started\n")
-print "Hello"
+def main():
+    p = pychess.Main.PyChess(None)
+    gtk.gdk.threads_init()
 
-#gtk.main()
-import pychess.widgets.ionest as i
-g = None
-prev_board = None
-prev_arBoard = None
+    log.log("Started from Naali\n")
+    print "PyChess in Naali"
 
-while 1:
-    #print ".",
-    gtk.main_iteration(block=False)
+    g = None
+    prev_board = None
+    prev_arBoard = None
+
+    while 1:
+        #print ".",
+        gtk.main_iteration(block=False)
     
-    if g is None and i.globalgamemodel is not None:
-        g = i.globalgamemodel
+        if g is None and i.globalgamemodel is not None:
+            g = i.globalgamemodel
 
-    if g is not None:
-        #print g.players, id(g.boards[-1]), g.boards[-1]
-        board = g.boards[-1]
+            if g is not None:
+                #print g.players, id(g.boards[-1]), g.boards[-1]
+                board = g.boards[-1]
 
-        if board is not prev_board:
-            #log.log("BOARD: %s" % str(board))
-            #log.log("ENPASS %s" % str(b.enpassant))
-            #log.log("HISTORY: %s" % b.history)
-            print "BOARD:", board
-            if len(board.board.history) > 0:
-                prevmove = board.board.history[-1][0]
-                parsemove.parsemove(board, prev_arBoard, prevmove)
+            if board is not prev_board:
+                print "BOARD:", board
+                if len(board.board.history) > 0:
+                    prevmove = board.board.history[-1][0]
+                    parsemove.parsemove(board, prev_arBoard, prevmove)
 
-            prev_board = board
-            prev_arBoard = board.board.arBoard[:]
+                prev_board = board
+                prev_arBoard = board.board.arBoard[:]
 
 """in parsemove.py now
 def removePiece(tcord, tpiece, opcolor):
-    log.log("REMOVE")
     print "REMOVE:", tcord, tpiece, opcolor
 
 def addPiece(tcord, piece, color):
-    log.log("ADD")
-    print "ADD:", tcord, tpiece, opcolor
+    print "ADD:", tcord, piece, opcolor
 
 def move(fcord, tcord, piece, color):
-    log.log("MOVE")
     print "REMOVE:", tcord, tpiece, opcolor
 """
+
+if __name__ == '__main__':
+    main()
