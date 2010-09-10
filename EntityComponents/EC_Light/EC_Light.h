9f7279a009c6efaf50d25d327af4ae41ab8c75cc
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2010-09-10 14:53:55 +0300

    Added a sample documentation plate for Entity components.

--- EntityComponents/EC_Light/EC_Light.h
+++ EntityComponents/EC_Light/EC_Light.h
@@ -17,7 +17,62 @@ namespace Ogre
     class Light;
 }
 
+/**
 
+<table style="margin: 20px;">
+<tr>
+<td style="width:500px; height: 100px; border: solid 1px black; background-color: #e0d0ff; vertical-align: top; padding: 5px;">
+<h2>Light</h2>
+Makes the entity a light source.
+
+Registered by RexLogic::RexLogicModule.
+
+<b>Attributes</b>:
+<ul>
+<li>Vector3df: direction.
+<div style="margin: 5px;">Specifies the direction vector the light is shining at.</div> 
+<li>enum: light type. 
+<div style="margin: 5px;">One of the values "Point", "Spot" or "Directional".</div> 
+<li>Color: diffuse color.
+<div style="margin: 5px;">Specifies the color the light casts.</div> 
+<li>Color: specular color.
+<div style="margin: 5px;">Specifies the color of the reflections the light casts.</div> 
+<li>bool: cast shadows.
+<div style="margin: 5px;">If true, this light casts dynamically calculated shadows on the scene.</div> 
+<li>float: light range.
+<div style="margin: 5px;">Specifies how far in world space units the light reaches.</div> 
+<li>float: constant attenuation.
+<div style="margin: 5px;">Specifies the constant term of the light attenuation equation.</div> 
+<li>float: linear attenuation.
+<div style="margin: 5px;">Specifies the linear term of the light attenuation equation.</div> 
+<li>float: quadratic attenuation.
+<div style="margin: 5px;">Specifies the quadratic term of the light attenuation equation.</div> 
+<li>float: light inner angle.
+<div style="margin: 5px;">Specifies inner umbra angle of the light. Only applicable for spotlights.</div> 
+<li>float: light outer angle.   
+<div style="margin: 5px;">Specifies outer penumbra angle of the light. Only applicable for spotlights.</div>
+</ul>
+
+<b>Exposes the following scriptable functions:</b>
+<ul>
+<li>...
+</ul>
+
+<b>Reacts on the following actions:</b>
+<ul>
+<li>"hide": Disables the light from affecting the scene.
+<li>"show": Enables the light in the scene.
+<li>"toggleVisibility": Toggles between the enabled and disabled states.
+</ul>
+</td>
+</tr>
+
+Does not emit any actions.
+
+<b>Depends on the component OgrePlaceable</b>. The position in the OgrePlaceable component specifies the position in the world space where this light is placed at. 
+</table>
+
+*/
 class EC_Light : public Foundation::ComponentInterface
 {
     DECLARE_EC(EC_Light);
