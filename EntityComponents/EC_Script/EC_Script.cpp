d24579e0cf9acfdad2028ee9358b483acf749f47
Stinkfist <stinkfist0@gmail.com>
2010-09-10 15:59:33 +0300

    *Add IScriptInstance class. *Add couple more forward declarations to ForwardDefines.h. *Declare EC_Script in PythonScriptModule.

--- EntityComponents/EC_Script/EC_Script.cpp
+++ EntityComponents/EC_Script/EC_Script.cpp
@@ -1,23 +1,38 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
 #include "StableHeaders.h"
 #include "EC_Script.h"
 #include "AttributeInterface.h"
-
-EC_Script::EC_Script(Foundation::ModuleInterface *module):
-    Foundation::ComponentInterface(module->GetFramework()),
-    scriptRef_(this, "Script ref")
-{
-    connect(this, SIGNAL(OnAttributeChanged(AttributeInterface*, AttributeChange::Type)), this, SLOT(HandleAttributeChanged(AttributeInterface*, AttributeChange::Type)));
-}
+#include "IScriptInstance.h"
 
 EC_Script::~EC_Script()
 {
-    
+}
+
+void EC_Script::SetScriptInstance(IScriptInstance *instance)
+{
+    // If we already have a script instance, unload and delete it.
+    if (scriptInstance_)
+    {
+        scriptInstance_->Unload();
+        SAFE_DELETE(scriptInstance_);
+    }
+
+    scriptInstance_ = instance;
+}
+
+EC_Script::EC_Script(Foundation::ModuleInterface *module):
+    Foundation::ComponentInterface(module->GetFramework()),
+    scriptRef(this, "Script ref"),
+    type(this, "Type"),
+    scriptInstance_(0)
+{
+    connect(this, SIGNAL(OnAttributeChanged(AttributeInterface*, AttributeChange::Type)),
+        SLOT(HandleAttributeChanged(AttributeInterface*, AttributeChange::Type)));
 }
 
 void EC_Script::HandleAttributeChanged(AttributeInterface* attribute, AttributeChange::Type change)
 {
-    if(attribute->GetNameString() == scriptRef_.GetNameString())
-    {
-        emit onScriptRefChanged(scriptRef_.Get());
-    }
+    if (attribute->GetNameString() == scriptRef.GetNameString())
+        emit ScriptRefChanged(scriptRef.Get());
 }
