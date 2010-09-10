ac8a11d34bba01f8d998d199297275c73d58baca
mattiku <matti.kuonanoja@evocativi.com>
2010-09-10 16:14:02 +0300

    Initial version of new voice conrol ui.

---
+++ UiModule/Inworld/View/VoiceController.h
@@ -0,0 +1,71 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
+#ifndef incl_UiModule_VoiceControler_h
+#define incl_UiModule_VoiceControler_h
+
+#include <QObject>
+#include <QWidget>
+#include "ui_VoiceControl.h"
+
+namespace Communications
+{
+    namespace InWorldVoice
+    {
+        class SessionInterface;
+        class ParticipantInterface;
+    }
+}
+
+namespace CommUI
+{
+    class VoiceController : public QObject
+    {
+        Q_OBJECT
+//        Q_PROPERTY(TransmissionMode transmissionMode READ GetTransmissionMode WRITE SetTransmissionMode)
+
+    public:
+        enum State { Disabled, Connecting, Connected, ConnectionLost };
+        enum TransmissionMode { Mute, ContinuousTransmission, PushToTalk, ToggleMode, VoiceActivity };
+
+        VoiceController(Communications::InWorldVoice::SessionInterface* voice_session);
+        virtual ~VoiceController();
+
+    public slots:
+        virtual void SetTransmissionMode(TransmissionMode mode);
+        virtual void SetPushToTalkOn();
+        virtual void SetPushToTalkOff();
+        //virtual void SetVoiceActivityOn();
+        //virtual void SetVoiceActivityOff();
+        //virtual TransmissionMode GetTransmissionMode();
+        //virtual void SetTransmissionMode(TransmissionMode mode);
+    signals:
+        void TransmittingAudioStarted();
+        void TransmittingAudioStopped();
+        void TransmissionModeChanged(TransmissionMode new_mode);
+
+    protected:
+        void SetTransmissionState();
+
+        TransmissionMode transmission_mode_;
+        bool transmitting_audio_;
+        bool push_to_talk_on_;
+        bool toggle_mode_on_;
+        int voice_activity_timeout_ms_;
+        Communications::InWorldVoice::SessionInterface* in_world_voice_session_;
+//        QTime last_voice_activity_time_;
+    };
+
+    class VoiceControllerWidget : public QObject, public VoiceController, private Ui::VoiceControl, public QWidget
+    {
+        Q_OBJECT
+    public:
+        VoiceControllerWidget(Communications::InWorldVoice::SessionInterface* voice_session);
+        virtual ~VoiceControllerWidget();
+    private slots:
+        void ApplyTransmissionModeSelection(int selection);
+        void OpenParticipantListWidget();
+    };
+
+} // CommUI
+
+#endif // incl_UiModule_VoiceControler_h
