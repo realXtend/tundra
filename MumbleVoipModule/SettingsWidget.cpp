4517742fcbb3ad6f3c238e0450c23103e6c5f6d9
mattiku <matti.kuonanoja@evocativi.com>
2010-09-16 10:13:00 +0300

    Added voice participant list to voice control widget.

--- MumbleVoipModule/SettingsWidget.cpp
+++ MumbleVoipModule/SettingsWidget.cpp
@@ -102,8 +102,8 @@ namespace MumbleVoip
         }
         else
         {
-            averageBandwidthInLabel->setText("");
-            averageBandwidthOutLabel->setText("");
+            averageBandwidthInLabel->setText("0.0 kB/s");
+            averageBandwidthOutLabel->setText("0.0 kB/s");
             serverInfoLabel->setText("");
         }
     }
