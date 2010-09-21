c94939b66123f8798a8126474dd49467805386ad
Erno Kuusela <erno@playsign.net>
2010-09-21 15:54:51 +0300

    debug prints

--- bin/pymodules/core/componentpropertyadder.py
+++ bin/pymodules/core/componentpropertyadder.py
@@ -1,6 +1,6 @@
 import circuits
 import naali
-
+import rexviewer as r
 #from PythonQt.QtCore import QVariant
 import PythonQt
 
@@ -44,14 +44,17 @@ class ComponentPropertyAdder(circuits.BaseComponent):
             ent.setProperty(propname, comp)
 
     def onComponentRemoved(self, ent, comp, changetype):
+        r.logInfo("XXX onComponentRemoved called")
         if comp.TypeName in compshorthand:
             propname = compshorthand[comp.TypeName]
         else:
             propname = comp.TypeName
-
+        r.logInfo("XXX propname " +str(propname))
+        r.logInfo("XXX dynamicpropertynames " + str(ent.dynamicPropertyNames()))
         if propname not in ent.dynamicPropertyNames():
             # qt docs: "A property can be removed from an instance by
             # passing the property name and an invalid QVariant value
             # to QObject::setProperty(). The default constructor for
             # QVariant constructs an invalid QVariant."
             ent.setProperty(propname, invalid_qvariant())
+            print "XXX deleted prop", propname
