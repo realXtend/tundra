c37b416c8caf60597e9f0c67e167f77d2aa1a792
Stinkfist <stinkfist0@gmail.com>
2010-09-23 11:58:06 +0300

    SceneInterract class stub.

---
+++ RexLogicModule/SceneInterract.cpp
@@ -0,0 +1,10 @@
+/**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
+ *  @file   SceneInterract.cpp
+ *  @brief  
+ */
+
+#include "StableHeaders.h"
+#include "SceneInterract.h"
+
---
+++ RexLogicModule/SceneInterract.h
@@ -0,0 +1,19 @@
+/**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
+ *  @file   SceneInterract.h
+ *  @brief  
+ */
+
+#include <QObject>
+
+#ifndef incl_RexLogicModule_SceneInterract_h
+#define incl_RexLogicModule_SceneInterract_h
+
+class SceneInterract : public QObject
+{
+    SceneInterract() {}
+    ~SceneInterract() {}
+};
+
+#endif
