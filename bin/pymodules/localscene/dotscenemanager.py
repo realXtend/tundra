bddf82aa5ce47e8c5e40ffd0a632d57a81c8e145
Nathan Letwory <nathan@letworyinteractive.com>
2010-09-23 10:18:40 +0300

    Use Qt datatypes for position, scale and orientation throughout localscene

--- bin/pymodules/localscene/dotscenemanager.py
+++ bin/pymodules/localscene/dotscenemanager.py
@@ -32,9 +32,9 @@ class DotSceneManager:
     def setOgreNodePosition(self, on, x, y ,z):
         e = on.naali_ent
         p = e.placeable
-        onx = on.position[0]
-        ony = on.position[1]
-        onz = on.position[2]
+        onx = on.position.x()
+        ony = on.position.y()
+        onz = on.position.z()
         if(self.flipZY):
             p.Position = Vec(x + onx, z + onz, y + ony)
         else:
@@ -44,9 +44,9 @@ class DotSceneManager:
     def setOgreNodeScale(self, on, x, y ,z):
         e = on.naali_ent
         p = e.placeable
-        onx = on.scale[0]
-        ony = on.scale[1]
-        onz = on.scale[2]
+        onx = on.scale.x()
+        ony = on.scale.y()
+        onz = on.scale.z()
         if(self.flipZY):
             p.Scale = Vec(x * onx, z * onz, y * ony)
         else:
