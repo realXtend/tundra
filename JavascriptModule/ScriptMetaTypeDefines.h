85d3655c024266dd968fcda01dc0275fb7663417
Joosua <jonnevaisanen@ludocraft.com>
2010-09-24 18:12:37 +0300

    Added a new example to javascript that use UiServiceInterface. Example will load widget from the ui  file and set it to ui scene.

--- JavascriptModule/ScriptMetaTypeDefines.h
+++ JavascriptModule/ScriptMetaTypeDefines.h
@@ -50,6 +50,4 @@ void ReqisterUiMetaTypes(QScriptEngine *engine);
 //! Will register all meta data types that are needed to use Naali Core API objects.
 void ReqisterCoreApiMetaTypes(QScriptEngine *engine);
 
-//QScriptValue LoadWidgetFromFile(QScriptContext *context, QScriptEngine *engine);
-
 #endif
