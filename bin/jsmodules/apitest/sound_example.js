fd5d04c8fe69677b2b046e460aa2882b32ff9473
Stinkfist <stinkfist0@gmail.com>
2010-09-27 17:39:28 +0300

    "sound" -> "audio" in sound_example.js

--- bin/jsmodules/apitest/sound_example.js
+++ bin/jsmodules/apitest/sound_example.js
@@ -1,16 +1,16 @@
-print("Loading sound example script.");
+print("Loading audio example script.");
 
-var sound_id = sound.PlaySound("b31a9a52-fb05-4648-87de-60c8697db3ca"); //Change sound asset id here
-var pitch = sound.GetPitch(sound_id)
+var sound_id = audio.PlaySound("b31a9a52-fb05-4648-87de-60c8697db3ca"); //Change sound asset id here
+var pitch = audio.GetPitch(sound_id)
 print("Sound's original pitch was " + pitch + " and setting it to " + pitch * 0.5);
-sound.SetPitch(sound_id, pitch * 0.5);
+audio.SetPitch(sound_id, pitch * 0.5);
 
 frame.DelayedExecute(0.1).Triggered.connect(fadeSound);
 
 function fadeSound(time)
 {
-    var gain = sound.GetGain(sound_id) * 0.9;
-    sound.SetGain(sound_id, gain);
+    var gain = audio.GetGain(sound_id) * 0.9;
+    audio.SetGain(sound_id, gain);
 
     // Check if sound is already near to zero and it's ok to stop the sound.
     if(gain >= 0.05)
@@ -20,6 +20,6 @@ function fadeSound(time)
     else
     {
         print("Stop sound");
-        sound.StopSound(sound_id);
+        audio.StopSound(sound_id);
     }
 }
