5e3feb1ba4d3cb472c5915172238ef46f31820a7
Nathan Letwory <nathan@letworyinteractive.com>
2010-09-29 18:21:43 +0300

    user avatar from naali using wrapper func

--- bin/pymodules/objectedit/objectedit.py
+++ bin/pymodules/objectedit/objectedit.py
@@ -467,7 +467,7 @@ class ObjectEdit(Component):
             
     def validId(self, id):
         if id != 0 and id > 50: #terrain seems to be 3 and scene objects always big numbers, so > 50 should be good, though randomly created local entities can get over 50...
-            if id != naali.worldlogic.GetUserAvatarEntityRaw().Id: #XXX add other avatar id's check
+            if id != naali.getUserAvatar().Id: #XXX add other avatar id's check
                 if not self.manipulator.compareIds(id):  #and id != self.selection_box.Id:
                     return True
         return False
