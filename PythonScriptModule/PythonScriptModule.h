53b6979428096e8e6728d2f91487c4ddd905ecdb
Toni Alatalo <toni@playsign.net>
2010-10-02 13:36:08 +0300

    A nice python console from PythonQt, does autocomplete with a qt pulldown. Opened now from command.py (by default currently), so press alt-. to see. Integrating with the rest of the GUI / the F1 console to be done later.

--- PythonScriptModule/PythonScriptModule.h
+++ PythonScriptModule/PythonScriptModule.h
@@ -6,6 +6,7 @@
 #include "Foundation.h"
 #include "IModule.h"
 #include "ModuleLoggingFunctions.h"
+#include "PythonQtScriptingConsole.h"
 
 #include <QObject>
 #include <QList>
@@ -74,6 +75,8 @@ namespace PythonScript
             @param filename Filename of the script.
         */
         void LoadScript(const QString &filename);
+
+        PythonQtScriptingConsole* CreateConsole();
 
     public:
         PythonScriptModule();
@@ -164,6 +167,9 @@ namespace PythonScript
         InputContextPtr input;
 
         QList<InputContextPtr> created_inputs_;
+
+        //PythonQtScriptingConsole *pythonqtconsole;
+
 
     private slots:
         /** Called when new component is added to the active scene.
