d739cbfcc4600c3e23339fbce30f72b4e0ffbff5
Toni Alatalo <toni@playsign.net>
2010-10-08 15:48:30 +0300

    animsync from develop -> NaaliCore. doesn't actually sync the pos now on either, but gives slider ok .. and slider updates when value is changed in ecedit.

--- bin/pymodules/apitest/animsync.py
+++ bin/pymodules/apitest/animsync.py
@@ -38,6 +38,7 @@ class AnimationSync(circuits.BaseComponent):
         circuits.BaseComponent.__init__(self)
         self.inworld_inited = False #a cheap hackish substitute for some initin
         self.initgui()
+        #todo: OnChanged() is deprecated
         comp.connect("OnChanged()", self.onChanged)
 
     def initgui(self):
@@ -45,7 +46,7 @@ class AnimationSync(circuits.BaseComponent):
         self.widget.connect('valueChanged(int)', self.sliderChanged)
 
         #naali proxywidget boilerplate
-        uism = naali.Ui()
+        uism = naali.ui
         self.proxywidget = r.createUiProxyWidget(self.widget)
         self.proxywidget.setWindowTitle(self.GUINAME)
         if not uism.AddWidgetToScene(self.proxywidget):
@@ -62,7 +63,8 @@ class AnimationSync(circuits.BaseComponent):
             now = time.time()
             if self.prev_sync + INTERVAL < now:
                 comp.SetAttribute("timepos", guival / 100)
-                comp.ComponentChanged("Local")
+                #ComponentChanged is no longer necessary to trigger network sync
+                #comp.ComponentChanged("Local")
                 self.prev_sync = now
 
     def onChanged(self):
@@ -71,15 +73,15 @@ class AnimationSync(circuits.BaseComponent):
         #copy-paste from door.py which also had a onClick handler
         if not self.inworld_inited:
             try:
-                ent = r.getEntity(self.comp.GetParentEntityId())
+                ent = self.comp.GetParentEntity()
             except ValueError:
                 return
 
             try:
                 t = ent.touchable
             except AttributeError:
-                print "no touchable in animsynced obj? it doesn't persist yet? adding..", ent.id
-                print ent.createComponent("EC_Touchable")
+                print "no touchable in animsynced obj? it doesn't persist yet? adding..", ent.Id
+                print ent.GetOrCreateComponentRaw("EC_Touchable")
                 t = ent.touchable
             else:
                 print "touchable pre-existed in animated character for animsync."
@@ -97,7 +99,7 @@ class AnimationSync(circuits.BaseComponent):
         if comp is not None:
             #print comp.GetAttribute()
             try:
-                ent = r.getEntity(comp.GetParentEntityId())
+                ent = comp.GetParentEntity()
             except ValueError:
                 print "No entity"
                 return
