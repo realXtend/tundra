0e6fb2a255106da8b6a16eb9f9b38f83fc94ccdf
Stinkfist <stinkfist0@gmail.com>
2010-10-12 14:44:58 +0300

    NaaliGraphicsView emits now DragEnterEvent(), DragMoveEvent() and DropEvent(). Hook up with these if you interested in the main window events.

--- Ui/NaaliGraphicsView.h
+++ Ui/NaaliGraphicsView.h
@@ -1,3 +1,5 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
 #ifndef incl_Core_NaaliGraphicsView_h
 #define incl_Core_NaaliGraphicsView_h
 
@@ -21,6 +23,21 @@ public:
 signals:
     void WindowResized(int newWidth, int newHeight);
 
+    /// Emitted when DragEnterEvent is received for the main window.
+    /** @param e Event.
+    */
+    void DragEnterEvent(QDragEnterEvent *e);
+
+    /// Emitted when DragMoveEvent is received for the main window.
+    /** @param e Event.
+    */
+    void DragMoveEvent(QDragMoveEvent *e);
+
+    /// Emitted when DropEvent is received for the main window.
+    /** @param e Event.
+    */
+    void DropEvent(QDropEvent *e);
+
 public slots:
     void Resize(int newWidth, int newHeight);
 
@@ -31,6 +48,9 @@ private:
     void drawBackground(QPainter *, const QRectF &);
     bool event(QEvent *event);
     void resizeEvent(QResizeEvent *e);
+    void dragEnterEvent(QDragEnterEvent *e);
+    void dragMoveEvent(QDragMoveEvent *e);
+    void dropEvent(QDropEvent *e);
 
 private slots:
     void HandleSceneChanged(const QList<QRectF> &rectangles);
