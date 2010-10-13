0e66b195e9b0f3d0ae71824ca2c4b74283820f24
Stinkfist <stinkfist0@gmail.com>
2010-10-13 18:16:47 +0300

    *Fixed issue that caused deserialize code to fail in EC_DynamicComponent (by Joosua) *Declare EC_DynamicComponent in JavascriptModule.

--- JavascriptModule/JavascriptModule.cpp
+++ JavascriptModule/JavascriptModule.cpp
@@ -13,6 +13,7 @@
 #include "JavascriptEngine.h"
 
 #include "EC_Script.h"
+#include "EC_DynamicComponent.h"
 #include "SceneManager.h"
 #include "InputContext.h"
 #include "Input.h"
@@ -45,6 +46,7 @@ JavascriptModule::~JavascriptModule()
 void JavascriptModule::Load()
 {
     DECLARE_MODULE_EC(EC_Script);
+    DECLARE_MODULE_EC(EC_DynamicComponent);
 }
 
 
