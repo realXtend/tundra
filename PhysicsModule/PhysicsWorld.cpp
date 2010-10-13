0eddc7a5694c2a0b527df5f365d32e6373e6f441
cadaver <lasse.oorni@ludocraft.com>
2010-10-13 13:19:36 +0300

    Server creates physics world for its scene (TundraLogic depends on PhysicsModule now) Gravity & physics FPS are configurable per physics world. Fixed PhysicsModule debug build.

--- PhysicsModule/PhysicsWorld.cpp
+++ PhysicsModule/PhysicsWorld.cpp
@@ -2,18 +2,24 @@
 
 #include "StableHeaders.h"
 #include "PhysicsWorld.h"
+#include "PhysicsUtils.h"
+#include "Profiler.h"
 
 #include "btBulletDynamicsCommon.h"
 
 namespace Physics
 {
 
+// Assume we generate at least 10 frames per second. If less, the physics will start to slow down.
+static const float cMinFps = 10.0f;
+
 PhysicsWorld::PhysicsWorld() :
     collisionConfiguration_(0),
     collisionDispatcher_(0),
     broadphase_(0),
     solver_(0),
-    world_(0)
+    world_(0),
+    physicsUpdatePeriod_(1.0f / 60.0f)
 {
     collisionConfiguration_ = new btDefaultCollisionConfiguration();
     collisionDispatcher_ = new btCollisionDispatcher(collisionConfiguration_);
@@ -40,9 +46,35 @@ PhysicsWorld::~PhysicsWorld()
     collisionConfiguration_ = 0;
 }
 
-void PhysicsWorld::Simulate(float deltaTime, int maxSubSteps, float internalTimeStep)
+void PhysicsWorld::SetPhysicsUpdatePeriod(float updatePeriod)
 {
-    world_->stepSimulation((float)deltaTime, maxSubSteps, internalTimeStep);
+    // Allow max.1000 fps
+    if (updatePeriod <= 0.001f)
+        updatePeriod = 0.001f;
+    physicsUpdatePeriod_ = updatePeriod;
+}
+
+void PhysicsWorld::SetGravity(const Vector3df& gravity)
+{
+    world_->setGravity(ToBtVector3(gravity));
+}
+
+Vector3df PhysicsWorld::GetGravity() const
+{
+    return ToVector3(world_->getGravity());
+}
+
+btDynamicsWorld* PhysicsWorld::GetWorld() const
+{
+    return world_;
+}
+
+void PhysicsWorld::Simulate(f64 frametime)
+{
+    PROFILE(PhysicsWorld_Simulate);
+    
+    int maxSubSteps = (int)((1.0f / physicsUpdatePeriod_) / cMinFps);
+    world_->stepSimulation((float)frametime, maxSubSteps, physicsUpdatePeriod_);
 }
 
 }
