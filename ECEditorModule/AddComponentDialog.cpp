6e717813158570dfa0da8decf22633181c912489
Jonne Vaisanen <jonnevaisanen@ludocraft.com>
2010-10-14 20:13:22 +0300

    Added synchronization option to add component dialog widget.

--- ECEditorModule/AddComponentDialog.cpp
+++ ECEditorModule/AddComponentDialog.cpp
@@ -27,6 +27,7 @@ namespace ECEditor
         component_name_label_(0),
         name_line_edit_(0),
         type_combo_box_(0),
+        synch_combo_box_(0),
         ok_button_(0),
         cancel_button_(0)
     {
@@ -64,6 +65,19 @@ namespace ECEditor
         return name_line_edit_->text();
     }
 
+    AttributeChange::Type AddComponentDialog::GetSynchronization() const
+    {
+        if(synch_combo_box_)
+        {
+            QString synchText = synch_combo_box_->currentText();
+            if(synchText == "Replicate")
+                return AttributeChange::Replicate;
+            else if(synchText == "Local")
+                return AttributeChange::LocalOnly;
+        }
+        return AttributeChange::Default;
+    }
+
     entity_id_t AddComponentDialog::GetEntityId() const
     {
         return entity_id_;
@@ -93,17 +107,23 @@ namespace ECEditor
         layout->setContentsMargins(5,5,5,5);
         layout->setSpacing(6);
 
-        component_type_label_ = new QLabel(tr("Component type:"));
-        component_name_label_ = new QLabel(tr("Component name:"));
+        component_type_label_ = new QLabel(tr("Component:"));
+        component_name_label_ = new QLabel(tr("Name:"));
+        component_synch_label_ = new QLabel(tr("Synchronization:"));
         name_line_edit_ = new QLineEdit();
         if(name_line_edit_)
             connect(name_line_edit_, SIGNAL(textChanged(const QString&)), this, SLOT(CheckComponentName(const QString&)));
         type_combo_box_ = new QComboBox();
+        synch_combo_box_ = new QComboBox();
+        synch_combo_box_->addItem(tr("Replicate"));
+        synch_combo_box_->addItem(tr("Local"));
 
-        layout->addWidget(component_type_label_);
-        layout->addWidget(type_combo_box_);
         layout->addWidget(component_name_label_);
         layout->addWidget(name_line_edit_);
+        layout->addWidget(component_type_label_);
+        layout->addWidget(type_combo_box_);
+        layout->addWidget(component_synch_label_);
+        layout->addWidget(synch_combo_box_);
 
         QSpacerItem *spacer = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Expanding);
         layout->insertSpacerItem(-1, spacer);
