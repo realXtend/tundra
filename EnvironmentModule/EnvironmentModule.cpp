d117e95edae5bab30632527abdef13537a81305a
Mikko Alaluusua <mikko.alaluusua@evocativi.com>
2010-10-18 15:51:58 +0300

    *Terrain texture weights editor can now be popped with console message TerrainTextureEditor (cherry picked from commit dad9db057e49b9c602d50ce3e95d56b84029fce0)

--- EnvironmentModule/EnvironmentModule.cpp
+++ EnvironmentModule/EnvironmentModule.cpp
@@ -15,6 +15,7 @@
 #include "EnvironmentEditor.h"
 #include "PostProcessWidget.h"
 
+
 #include "EC_WaterPlane.h"
 #include "EC_Fog.h"
 #include "EC_SkyPlane.h"
@@ -36,6 +37,7 @@
 
 #include "UiServiceInterface.h"
 #include "UiProxyWidget.h"
+#include "ConsoleCommandServiceInterface.h"
 
 #include "TerrainWeightEditor.h"
 
@@ -117,7 +119,9 @@ namespace Environment
 
         w_editor_ = new TerrainWeightEditor(this);
         w_editor_->Initialize();
-        
+        RegisterConsoleCommand(Console::CreateCommand("TerrainTextureEditor",
+            "Shows the terrain texture weight editor.",
+            Console::Bind(w_editor_, &TerrainWeightEditor::ShowWindow)));
     }
 
     void EnvironmentModule::Uninitialize()
