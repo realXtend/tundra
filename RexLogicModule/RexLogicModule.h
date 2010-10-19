56dcee8d3f1b57be7373864fbc2aec57e56ef681
Saso Markoski <atrium.animus@gmail.com>
2010-10-19 12:55:56 +0200

    Camera state notification widget

--- RexLogicModule/RexLogicModule.h
+++ RexLogicModule/RexLogicModule.h
@@ -78,6 +78,7 @@ namespace RexLogic
     class LoginHandler;
     class ObjectCameraController;
     class CameraControl;
+    class NotificationWidget;
 
     namespace InWorldChat { class Provider; }
 
@@ -86,6 +87,7 @@ namespace RexLogic
     typedef boost::shared_ptr<CameraControllable> CameraControllablePtr;
     typedef boost::shared_ptr<ObjectCameraController> ObjectCameraControllerPtr;
     typedef boost::shared_ptr<CameraControl> CameraControlPtr;
+    typedef boost::shared_ptr<NotificationWidget> NotificationWidgetPtr;
 
     //! Camera states handled by rex logic
     enum CameraState
@@ -358,6 +360,8 @@ namespace RexLogic
         //! Creates a new camera entity to the scene. Marked as private since this function has some side-effects, like adding the camera
         //! as the default current camera.
         void CreateOpenSimViewerCamera(Scene::ScenePtr scene);
+
+        NotificationWidgetPtr notification_widget_;
 
     private slots:
         /** Called when new component is added to the active scene.
