9008e23ed9d7e03e936587c03bc05c86bd627dd2
cmayhem <heikki.tormala@ludocraft.com>
2010-10-29 11:29:13 +0300

    OpenAssetImport: multiple nodes are now imported, but transformations are not yet working (all nodes appear in origo). Fixed few issues that resulted in empty meshes or problems with Ogre.

--- OpenAssetImport/OpenAssetImport.cpp
+++ OpenAssetImport/OpenAssetImport.cpp
@@ -86,185 +86,191 @@ namespace AssImp
         
         if (renderer)
         {
-            ImportNode(renderer, scene, rootNode, file, outMeshNames);
+            ImportNode(renderer, scene, rootNode, file, 0, outMeshNames);
         }
 	}
 
     void OpenAssetImport::ImportNode(const boost::shared_ptr<OgreRenderer::Renderer> &renderer, const aiScene *scene, 
-        const aiNode *node, const QString& file, std::vector<std::string> &outMeshNames)
+        const aiNode *node, const QString& file, int nodeIdx, std::vector<std::string> &outMeshNames)
     {
         try
         {
             boost::filesystem::path path(file.toStdString());
-            outMeshNames.push_back(path.filename());
-            std::string meshname = std::string("mesh___") + path.filename();
+            std::string meshname = path.filename() + boost::lexical_cast<std::string>(nodeIdx);
 
 
             aiMatrix4x4 transform = node->mTransformation;
-            Ogre::MeshPtr ogreMesh = Ogre::MeshManager::getSingleton().createManual(meshname, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-            ogreMesh->setAutoBuildEdgeLists(false);
-
-            Ogre::Vector3 vmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
-            Ogre::Vector3 vmax(std::numeric_limits<float>::min(), std::numeric_limits<float>::min(), std::numeric_limits<float>::min());
-
-            for (unsigned int i=0 ; i<node->mNumMeshes ; ++i)
+            if (node->mNumMeshes > 0)
             {
-                const aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
-                
-                SubMesh *ogreSubmesh = ogreMesh->createSubMesh();
-                
-                ogreSubmesh->useSharedVertices = false;
-                ogreSubmesh->vertexData = new Ogre::VertexData();
-                ogreSubmesh->vertexData->vertexCount = mesh->mNumVertices;
-                Ogre::VertexData *data = ogreSubmesh->vertexData;
-                
-                // Vertex declarations
-                size_t offset = 0;
-                Ogre::VertexDeclaration* decl = data->vertexDeclaration;
-                decl->addElement(0, offset, VET_FLOAT3, VES_POSITION);
-                offset += VertexElement::getTypeSize(VET_FLOAT3);
-                decl->addElement(0, offset, VET_FLOAT3, VES_NORMAL);
+                Ogre::MeshPtr ogreMesh = Ogre::MeshManager::getSingleton().createManual(std::string("mesh___") + meshname, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+                ogreMesh->setAutoBuildEdgeLists(false);
 
-                offset = 0;
-                for (int tn=0 ; tn<AI_MAX_NUMBER_OF_TEXTURECOORDS ; ++tn)
+                Ogre::Vector3 vmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+                Ogre::Vector3 vmax(std::numeric_limits<float>::min(), std::numeric_limits<float>::min(), std::numeric_limits<float>::min());
+
+                for (unsigned int i=0 ; i<node->mNumMeshes ; ++i)
                 {
-                    if (mesh->mTextureCoords[tn])
-                    {
-                        if (mesh->mNumUVComponents[tn] == 3)
-                        {
-                            decl->addElement(1, offset, Ogre::VET_FLOAT3, Ogre::VES_TEXTURE_COORDINATES, tn);
-                            offset += VertexElement::getTypeSize(VET_FLOAT3);
-                        } else
-                        {
-                            decl->addElement(1, offset, Ogre::VET_FLOAT2, Ogre::VES_TEXTURE_COORDINATES, tn);
-                            offset += VertexElement::getTypeSize(VET_FLOAT2);
-                        }
-                    }
-                }
-                if (mesh->HasTangentsAndBitangents())
-                {
-                    decl->addElement(1, offset, Ogre::VET_FLOAT3, Ogre::VES_TANGENT);
+                    const aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+                    
+                    SubMesh *ogreSubmesh = ogreMesh->createSubMesh();
+                    
+                    ogreSubmesh->useSharedVertices = false;
+                    ogreSubmesh->vertexData = new Ogre::VertexData();
+                    ogreSubmesh->vertexData->vertexCount = mesh->mNumVertices;
+                    Ogre::VertexData *data = ogreSubmesh->vertexData;
+                    
+                    // Vertex declarations
+                    size_t offset = 0;
+                    Ogre::VertexDeclaration* decl = data->vertexDeclaration;
+                    decl->addElement(0, offset, VET_FLOAT3, VES_POSITION);
                     offset += VertexElement::getTypeSize(VET_FLOAT3);
-                    decl->addElement(1, offset, Ogre::VET_FLOAT3, Ogre::VES_BINORMAL);
-                }
+                    decl->addElement(0, offset, VET_FLOAT3, VES_NORMAL);
 
-                // Write vertex data to buffer
-                Ogre::HardwareVertexBufferSharedPtr vbuf = Ogre::HardwareBufferManager::getSingleton().createVertexBuffer(
-                    decl->getVertexSize(0),                     // This value is the size of a vertex in memory
-                    data->vertexCount,                          // The number of vertices you'll put into this buffer
-                    Ogre::HardwareBuffer::HBU_DYNAMIC // Properties
-                    );
-                Ogre::Real *vbData = static_cast<Ogre::Real*>(vbuf->lock(Ogre::HardwareBuffer::HBL_NORMAL));
-                
-                offset = 0;
-                for (unsigned int n=0 ; n<data->vertexCount ; ++n)
-                {
-                    vbData[offset++] = mesh->mVertices[n].x;
-                    vbData[offset++] = mesh->mVertices[n].y;
-                    vbData[offset++] = mesh->mVertices[n].z;
-
-                    vbData[offset++] = mesh->mNormals[n].x;
-                    vbData[offset++] = mesh->mNormals[n].y;
-                    vbData[offset++] = mesh->mNormals[n].z;
-
-                    vmin.x = std::min(vmin.x, mesh->mVertices[n].x);
-                    vmin.y = std::min(vmin.y, mesh->mVertices[n].y);
-                    vmin.z = std::min(vmin.z, mesh->mVertices[n].z);
-
-                    vmax.x = std::max(vmax.x, mesh->mVertices[n].x);
-                    vmax.y = std::max(vmax.y, mesh->mVertices[n].y);
-                    vmax.z = std::max(vmax.z, mesh->mVertices[n].z);
-                }
-                vbuf->unlock();
-                data->vertexBufferBinding->setBinding(0, vbuf);
-                
-
-                vbuf = Ogre::HardwareBufferManager::getSingleton().createVertexBuffer(
-                    decl->getVertexSize(1),                     // This value is the size of a vertex in memory
-                    data->vertexCount,                          // The number of vertices you'll put into this buffer
-                    Ogre::HardwareBuffer::HBU_DYNAMIC // Properties
-                    );
-                vbData = static_cast<Ogre::Real*>(vbuf->lock(Ogre::HardwareBuffer::HBL_NORMAL));
-                
-                offset = 0;
-                for (unsigned int n=0 ; n<data->vertexCount ; ++n)
-                {
+                    offset = 0;
                     for (int tn=0 ; tn<AI_MAX_NUMBER_OF_TEXTURECOORDS ; ++tn)
                     {
                         if (mesh->mTextureCoords[tn])
                         {
                             if (mesh->mNumUVComponents[tn] == 3)
                             {
-                                vbData[offset++] = mesh->mTextureCoords[tn][n].x;
-                                vbData[offset++] = mesh->mTextureCoords[tn][n].y;
-                                vbData[offset++] = mesh->mTextureCoords[tn][n].z;
+                                decl->addElement(1, offset, Ogre::VET_FLOAT3, Ogre::VES_TEXTURE_COORDINATES, tn);
+                                offset += VertexElement::getTypeSize(VET_FLOAT3);
                             } else
                             {
-                                vbData[offset++] = mesh->mTextureCoords[tn][n].x;
-                                vbData[offset++] = mesh->mTextureCoords[tn][n].y;
+                                decl->addElement(1, offset, Ogre::VET_FLOAT2, Ogre::VES_TEXTURE_COORDINATES, tn);
+                                offset += VertexElement::getTypeSize(VET_FLOAT2);
                             }
                         }
                     }
-
                     if (mesh->HasTangentsAndBitangents())
                     {
-                        vbData[offset++] = mesh->mTangents[n].x;
-                        vbData[offset++] = mesh->mTangents[n].y;
-                        vbData[offset++] = mesh->mTangents[n].z;
-
-                        vbData[offset++] = mesh->mBitangents[n].x;
-                        vbData[offset++] = mesh->mBitangents[n].y;
-                        vbData[offset++] = mesh->mBitangents[n].z;
+                        decl->addElement(1, offset, Ogre::VET_FLOAT3, Ogre::VES_TANGENT);
+                        offset += VertexElement::getTypeSize(VET_FLOAT3);
+                        decl->addElement(1, offset, Ogre::VET_FLOAT3, Ogre::VES_BINORMAL);
                     }
-                }
-                vbuf->unlock();
-                data->vertexBufferBinding->setBinding(1, vbuf);
 
-                // indices
-                size_t numIndices = mesh->mNumFaces * 3;            // support only triangles, so 3 indices per face
-                
-                Ogre::HardwareIndexBuffer::IndexType idxType = Ogre::HardwareIndexBuffer::IT_16BIT;
-                if (numIndices > 65535)
-                    idxType = Ogre::HardwareIndexBuffer::IT_32BIT;
-
-                Ogre::HardwareIndexBufferSharedPtr ibuf = Ogre::HardwareBufferManager::getSingleton().createIndexBuffer(
-                    idxType,                                        // You can use several different value types here
-                    numIndices,                                     // The number of indices you'll put in that buffer
-                    Ogre::HardwareBuffer::HBU_DYNAMIC     // Properties
-                    );
-                
-                if (idxType == Ogre::HardwareIndexBuffer::IT_16BIT)
-                {
-                    u16 *idxData = static_cast<u16*>(ibuf->lock(Ogre::HardwareBuffer::HBL_NORMAL));
+                    // Write vertex data to buffer
+                    Ogre::HardwareVertexBufferSharedPtr vbuf = Ogre::HardwareBufferManager::getSingleton().createVertexBuffer(
+                        decl->getVertexSize(0),                     // This value is the size of a vertex in memory
+                        data->vertexCount,                          // The number of vertices you'll put into this buffer
+                        Ogre::HardwareBuffer::HBU_DYNAMIC // Properties
+                        );
+                    Ogre::Real *vbData = static_cast<Ogre::Real*>(vbuf->lock(Ogre::HardwareBuffer::HBL_NORMAL));
+                    
                     offset = 0;
-                    for (int n=0 ; n<mesh->mNumFaces ; ++n)
+                    for (unsigned int n=0 ; n<data->vertexCount ; ++n)
                     {
-                        idxData[offset++] = mesh->mFaces[n].mIndices[0];
-                        idxData[offset++] = mesh->mFaces[n].mIndices[1];
-                        idxData[offset++] = mesh->mFaces[n].mIndices[2];
-                    }
-                } else
-                {
-                    u32 *idxData = static_cast<u32*>(ibuf->lock(Ogre::HardwareBuffer::HBL_NORMAL));
-                    offset = 0;
-                    for (int n=0 ; n<mesh->mNumFaces ; ++n)
-                    {
-                        idxData[offset++] = mesh->mFaces[n].mIndices[0];
-                        idxData[offset++] = mesh->mFaces[n].mIndices[1];
-                        idxData[offset++] = mesh->mFaces[n].mIndices[2];
-                    }
-                }
-                ibuf->unlock();
+                        vbData[offset++] = mesh->mVertices[n].x;
+                        vbData[offset++] = mesh->mVertices[n].y;
+                        vbData[offset++] = mesh->mVertices[n].z;
 
-                ogreSubmesh->indexData->indexBuffer = ibuf;         // The pointer to the index buffer
-                ogreSubmesh->indexData->indexCount = numIndices;    // The number of indices we'll use
-                ogreSubmesh->indexData->indexStart = 0;
+                        vbData[offset++] = mesh->mNormals[n].x;
+                        vbData[offset++] = mesh->mNormals[n].y;
+                        vbData[offset++] = mesh->mNormals[n].z;
+
+                        vmin.x = std::min(vmin.x, mesh->mVertices[n].x);
+                        vmin.y = std::min(vmin.y, mesh->mVertices[n].y);
+                        vmin.z = std::min(vmin.z, mesh->mVertices[n].z);
+
+                        vmax.x = std::max(vmax.x, mesh->mVertices[n].x);
+                        vmax.y = std::max(vmax.y, mesh->mVertices[n].y);
+                        vmax.z = std::max(vmax.z, mesh->mVertices[n].z);
+                    }
+                    vbuf->unlock();
+                    data->vertexBufferBinding->setBinding(0, vbuf);
+                    
+
+                    vbuf = Ogre::HardwareBufferManager::getSingleton().createVertexBuffer(
+                        decl->getVertexSize(1),                     // This value is the size of a vertex in memory
+                        data->vertexCount,                          // The number of vertices you'll put into this buffer
+                        Ogre::HardwareBuffer::HBU_DYNAMIC // Properties
+                        );
+                    vbData = static_cast<Ogre::Real*>(vbuf->lock(Ogre::HardwareBuffer::HBL_NORMAL));
+                    
+                    offset = 0;
+                    for (unsigned int n=0 ; n<data->vertexCount ; ++n)
+                    {
+                        for (int tn=0 ; tn<AI_MAX_NUMBER_OF_TEXTURECOORDS ; ++tn)
+                        {
+                            if (mesh->mTextureCoords[tn])
+                            {
+                                if (mesh->mNumUVComponents[tn] == 3)
+                                {
+                                    vbData[offset++] = mesh->mTextureCoords[tn][n].x;
+                                    vbData[offset++] = mesh->mTextureCoords[tn][n].y;
+                                    vbData[offset++] = mesh->mTextureCoords[tn][n].z;
+                                } else
+                                {
+                                    vbData[offset++] = mesh->mTextureCoords[tn][n].x;
+                                    vbData[offset++] = mesh->mTextureCoords[tn][n].y;
+                                }
+                            }
+                        }
+
+                        if (mesh->HasTangentsAndBitangents())
+                        {
+                            vbData[offset++] = mesh->mTangents[n].x;
+                            vbData[offset++] = mesh->mTangents[n].y;
+                            vbData[offset++] = mesh->mTangents[n].z;
+
+                            vbData[offset++] = mesh->mBitangents[n].x;
+                            vbData[offset++] = mesh->mBitangents[n].y;
+                            vbData[offset++] = mesh->mBitangents[n].z;
+                        }
+                    }
+                    vbuf->unlock();
+                    data->vertexBufferBinding->setBinding(1, vbuf);
+
+                    // indices
+                    size_t numIndices = mesh->mNumFaces * 3;            // support only triangles, so 3 indices per face
+                    
+                    Ogre::HardwareIndexBuffer::IndexType idxType = Ogre::HardwareIndexBuffer::IT_16BIT;
+                    if (numIndices > 65535)
+                        idxType = Ogre::HardwareIndexBuffer::IT_32BIT;
+
+                    Ogre::HardwareIndexBufferSharedPtr ibuf = Ogre::HardwareBufferManager::getSingleton().createIndexBuffer(
+                        idxType,                                        // You can use several different value types here
+                        numIndices,                                     // The number of indices you'll put in that buffer
+                        Ogre::HardwareBuffer::HBU_DYNAMIC     // Properties
+                        );
+                    
+                    if (idxType == Ogre::HardwareIndexBuffer::IT_16BIT)
+                    {
+                        u16 *idxData = static_cast<u16*>(ibuf->lock(Ogre::HardwareBuffer::HBL_NORMAL));
+                        offset = 0;
+                        for (int n=0 ; n<mesh->mNumFaces ; ++n)
+                        {
+                            idxData[offset++] = mesh->mFaces[n].mIndices[0];
+                            idxData[offset++] = mesh->mFaces[n].mIndices[1];
+                            idxData[offset++] = mesh->mFaces[n].mIndices[2];
+                        }
+                    } else
+                    {
+                        u32 *idxData = static_cast<u32*>(ibuf->lock(Ogre::HardwareBuffer::HBL_NORMAL));
+                        offset = 0;
+                        for (int n=0 ; n<mesh->mNumFaces ; ++n)
+                        {
+                            idxData[offset++] = mesh->mFaces[n].mIndices[0];
+                            idxData[offset++] = mesh->mFaces[n].mIndices[1];
+                            idxData[offset++] = mesh->mFaces[n].mIndices[2];
+                        }
+                    }
+                    ibuf->unlock();
+
+                    ogreSubmesh->indexData->indexBuffer = ibuf;         // The pointer to the index buffer
+                    ogreSubmesh->indexData->indexCount = numIndices;    // The number of indices we'll use
+                    ogreSubmesh->indexData->indexStart = 0;
+                }
+
+                if (vmin.x <= vmax.x && vmin.y <= vmax.y && vmin.z <= vmax.z)
+                {
+                    ogreMesh->_setBounds(Ogre::AxisAlignedBox(vmin, vmax));
+                    Ogre::Real maxvertex = std::max(abs(vmax.x), std::max(abs(vmin.x), std::max(abs(vmax.y), std::max(abs(vmin.y), std::max(vmax.z, vmin.z)))));
+                    ogreMesh->_setBoundingSphereRadius(maxvertex / 2.f);
+                    ogreMesh->load();
+                    outMeshNames.push_back(meshname);
+                }
             }
-
-            ogreMesh->_setBounds(Ogre::AxisAlignedBox(vmin, vmax));
-            Ogre::Real maxvertex = std::max(abs(vmax.x), std::max(abs(vmin.x), std::max(abs(vmax.y), std::max(abs(vmin.y), std::max(vmax.z, vmin.z)))));
-            ogreMesh->_setBoundingSphereRadius(maxvertex / 2.f);
-            ogreMesh->load();
         } catch (Ogre::Exception &e)
         {
             // error
@@ -272,7 +278,7 @@ namespace AssImp
 
         // import children
         for (int i=0 ; i<node->mNumChildren ; ++i)
-            ImportNode(renderer, scene, node->mChildren[i], file, outMeshNames);
+            ImportNode(renderer, scene, node->mChildren[i], file, ++nodeIdx, outMeshNames);
     }
     
     void OpenAssetImport::AssImpLogStream::write(const char* message)
