9d82c0bf7c4ba1bd42f69d02bb91d6e984b1d717
Toni Alatalo <toni@playsign.net>
2010-11-01 13:14:27 +0200

    py webmodule loading actually works now. edit TRUSTEDBASEURL and put DC "pythonmodule" with filename of the naali py module.
    api is same as with naali modules loaded locally, except that these must register possible circuits components themselves now.
    
    made quickly now, could clean things like how the handler is registered & how the webmodules can register to the core circuitsmanager etc. to be cleaned.

--- bin/pymodules/apitest/webmoduleloader.py
+++ bin/pymodules/apitest/webmoduleloader.py
@@ -3,17 +3,22 @@ import circuits
 import imp
 import urllib
 
-TRUSTEDBASEURL = "http://localhost:8000/"
+TRUSTEDBASEURL = "http://localhost:82/"
+
+webmodules = {} #modulename : moduleobject, to avoid loading same several times
 
 def loadwebmodule(srcurl):
-    filepath = urllib.urlretrieve(srcurl)[0]
-    f = open(filepath)
-    mod = imp.load_module("webmod", f, 'webmod.py', ('.py', 'U', 1))
+    if srcurl not in webmodules:
+        url = TRUSTEDBASEURL + srcurl
+        filepath = urllib.urlretrieve(url)[0]
+        f = open(filepath)
+        mod = imp.load_module("webmod", f, 'webmod.py', ('.py', 'U', 1))
+        webmodules[srcurl] = mod
+    else:
+        mod = webmodules[srcurl]
     return mod
 
 class WebPythonmoduleLoader(circuits.BaseComponent):
-    ADDMENU = False
-
     def __init__(self, entity, comp, changetype):
         circuits.BaseComponent.__init__(self)
         comp.connect("OnChanged()", self.onChanged)
@@ -24,4 +29,4 @@ class WebPythonmoduleLoader(circuits.BaseComponent):
         print srcurl
         mod = loadwebmodule(srcurl)
 
-componenthandler.register("pythonmodule", WebPythonmoduleLoader)
+#componenthandler.register("pythonmodule", WebPythonmoduleLoader)
