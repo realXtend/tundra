b6d0776c8eb0d345f7b0382a10b869110f151ca3
mattiku <matti.kuonanoja@evocativi.com>
2010-11-01 15:47:18 +0200

    Changed EC_VoiceChannel to use component attributes.

--- MumbleVoipModule/EC_VoiceChannel.h
+++ MumbleVoipModule/EC_VoiceChannel.h
@@ -1,52 +1,60 @@
 // For conditions of distribution and use, see copyright notice in license.txt
 
-#ifndef incl_MumbleVoip_EC_VoiceAgent_h
-#define incl_MumbleVoip_EC_VoiceAgent_h
+#ifndef incl_MumbleVoip_EC_VoiceChannel_h
+#define incl_MumbleVoip_EC_VoiceChannel_h
 
 #include "IComponent.h"
 #include "Declare_EC.h"
 #include "IAttribute.h"
-//#include "ServerInfo.h"
-struct MumbleServerInfo
-{
-    QString address;
-    QString version;
-    QString user_name;
-    QString password;
-    QString channel;
-};
 
 /// Mumble based voice channel
 /**
-  Offer basic information for clients to join a mumble voice channel
-  (?) Creates
-  @todo Define properties
+  Defines a mumble channel to join.
+  @todo 'enabled' attribute
 */
 class EC_VoiceChannel : public IComponent
 {
     DECLARE_EC(EC_VoiceChannel)
     Q_OBJECT
-        
 public:
     virtual ~EC_VoiceChannel();
 
     /// IComponent override. This component is serializable.
     virtual bool IsSerializable() const { return true; }
 
-    virtual MumbleServerInfo GetServerInfo() { return server_info_; }
-    virtual void SetServerInfo(const MumbleServerInfo& server_info) { server_info_ = server_info; };
+    /// VOIP protocol used eg. 'mumble'
+    Q_PROPERTY(QString protocol READ getprotocol WRITE setprotocol);
+    DEFINE_QPROPERTY_ATTRIBUTE(QString, protocol);
 
-signals:
-    void ChannelExpired(QString name);
+    /// Mumbe server version eg. '1.2.2'
+    Q_PROPERTY(QString version READ getversion WRITE setversion);
+    DEFINE_QPROPERTY_ATTRIBUTE(QString, version);
+
+    /// Mumbe server address eg. 'http://myserver:8000'
+    Q_PROPERTY(QString serveraddress READ getserveraddress WRITE setserveraddress);
+    DEFINE_QPROPERTY_ATTRIBUTE(QString, serveraddress);
+
+    /// Mumbe server password eg. 'mypassword'
+    Q_PROPERTY(QString serverpassword READ getserverpassword WRITE setserverpassword);
+    DEFINE_QPROPERTY_ATTRIBUTE(QString, serverpassword);
+
+    /// Mumbe server username eg. 'John'
+    Q_PROPERTY(QString username READ getusername WRITE setusername);
+    DEFINE_QPROPERTY_ATTRIBUTE(QString, username);
+
+    /// Human readable name for the channel eg. 'My channel'
+    Q_PROPERTY(QString channelname READ getchannelname WRITE setchannelname);
+    DEFINE_QPROPERTY_ATTRIBUTE(QString, channelname);
+
+    /// Mumble channel id eg. 'voip/regions/chat/channel24442'
+    Q_PROPERTY(QString channelid READ getchannelid WRITE setchannelid);
+    DEFINE_QPROPERTY_ATTRIBUTE(QString, channelid);
 
 private:
     /// Constuctor.
     /** @param module Declaring module.
     */
     explicit EC_VoiceChannel(IModule *module);
-
-private:
-    MumbleServerInfo server_info_;
 };
 
-#endif // incl_MumbleVoip_EC_VoiceAgent_h
+#endif // incl_MumbleVoip_EC_VoiceChannel_h
