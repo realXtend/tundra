5863104f99bebb74334d4e1f36123bf224d6b1a8
cmayhem <heikki.tormala@ludocraft.com>
2010-11-11 17:58:32 +0200

    WaterPlane demo scene: tweaked buoyancy.

--- bin/scenes/WaterPlane/watervolume.js
+++ bin/scenes/WaterPlane/watervolume.js
@@ -4,15 +4,17 @@ print("Loading WaterPlane Volume Trigger script");
 var rigidbody = me.GetComponentRaw("EC_RigidBody");
 var volumetrigger = me.GetComponentRaw("EC_VolumeTrigger");
 var entitiesInside = new Array();
-
-if(rigidbody && volumetrigger)
-{	
-	ConnectSignals();
-}	
-else
+if (server.IsRunning())
 {
-	print("Missing needed Components, Check that you have RigidBody and VolumeTrigger");
-	me.ComponentAdded.connect(CheckComponent);
+	if(rigidbody && volumetrigger)
+	{	
+		ConnectSignals();
+	}	
+	else
+	{
+		print("Missing needed Components, Check that you have RigidBody and VolumeTrigger");
+		me.ComponentAdded.connect(CheckComponent);
+	}
 }
 
 //Checking if needed components are added after EC_Script to Entity
@@ -38,27 +40,23 @@ function ConnectSignals()
 	rigidbody.GetPhysicsWorld().Updated.connect(ServerUpdatePhysics);
 	
 	// Hook to volume trigger signals
-	//volumetrigger.EntityEnter.connect(EntityEnter);
-	//volumetrigger.EntityLeave.connect(EntityLeave);
+	volumetrigger.EntityEnter.connect(EntityEnter);
+	volumetrigger.EntityLeave.connect(EntityLeave);
 }
 
 function ServerUpdatePhysics(frametime)
 {
 	for (var i=0 ; i<volumetrigger.GetNumEntitiesInside() ; i++)
 	{
-		//print(volumetrigger.GetEntityInsidePercentByName(volumetrigger.GetEntityInside(i).GetName()));
-	
 		var entity = volumetrigger.GetEntityInside(i);
 		
 		var mag = volumetrigger.GetEntityInsidePercent(entity);
 		var rb = entity.GetComponentRaw("EC_RigidBody");
-		if (rb)
+		if (rb && mag > 0.1)
 		{
 			var impulseVec = new Vector3df();
-			impulseVec.z = mag * 0.7;
+			impulseVec.z = mag * 0.8;
 			rb.ApplyImpulse(impulseVec);
-			rb.angularDamping = 0.25;
-			rb.linearDamping = 0.25;
 		}
 	}
 }
@@ -66,8 +64,14 @@ function ServerUpdatePhysics(frametime)
 
 function EntityEnter (entity)
 {
+	var rb = entity.GetComponentRaw("EC_RigidBody");
+	rb.angularDamping = 0.25;
+	rb.linearDamping = 0.15;
 }
 
 function EntityLeave (entity)
 {
+	var rb = entity.GetComponentRaw("EC_RigidBody");
+	rb.angularDamping = 0;
+	rb.linearDamping = 0;
 }
