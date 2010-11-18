d3c214d55dc1b5338b748ae0a8951d00fe8dc9f3
mattiku <matti.kuonanoja@evocativi.com>
2010-11-18 16:37:02 +0200

    Changed version number 0.3.4-RC1

--- Foundation/Framework.cpp
+++ Foundation/Framework.cpp
@@ -126,7 +126,7 @@ namespace Foundation
 
             // Set config values we explicitly always want to override
             config_manager_->SetSetting(Framework::ConfigurationGroup(), std::string("version_major"), std::string("0"));
-            config_manager_->SetSetting(Framework::ConfigurationGroup(), std::string("version_minor"), std::string("3.3"));
+            config_manager_->SetSetting(Framework::ConfigurationGroup(), std::string("version_minor"), std::string("3.4-RC1"));
 
             CreateLoggingSystem(); // depends on config and platform
 
--- viewer-installer.nsi
+++ viewer-installer.nsi
@@ -1,4 +1,4 @@
-!define VERSION "0.3.3"
+!define VERSION "0.3.4-RC1"
 
 Name "Naali ${VERSION}"
 
@@ -8,7 +8,7 @@ Page instfiles
 InstallDir "$PROGRAMFILES\Naali ${VERSION}"
 VIProductVersion "${VERSION}"
 
-OutFile "Naali-0.3.3.exe"
+OutFile "Naali-0.3.4-RC1.exe"
 
 XPStyle on
 
