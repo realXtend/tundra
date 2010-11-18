6770573492884f46e3145fedc8fa6ce28aa8c9f7
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2010-11-18 17:43:04 +0200

    Fixed DayNight demo world sun animation script.

--- bin/scenes/DayNight/assets/daynight.js
+++ bin/scenes/DayNight/assets/daynight.js
@@ -1,25 +1,34 @@
 /// This script assumes that it is in same entity where EC_EnvironmentLight component exist.
-var environment_ = me.GetComponentRaw("EC_EnvironmentLight");
-environment_.currentTimeAttr = 0.0;
-environment_.fixedTimeAttr = true;
+
 var speed_ = 1.0 / 60.0;
 
-function Update(frametime) {
+var environment_ = null;
 
-    if (environment_ != null) {
-        // Day-night-cycle goes from 0-1.0
-        var time = environment_.currentTimeAttr;
-        time += speed_ * frametime;
-        if (time > 1.0) {
-            environment_.currentTimeAttr = 0.0;
-        }
-        else {
-            environment_.currentTimeAttr = time;
-        } 
+function FindEnvironmentLight()
+{
+  if (environment_ == null)
+  {
+    environment_ = me.GetComponentRaw("EC_EnvironmentLight");
+    if (environment_ != null)
+    {
+      environment_.currentTimeAttr = 0.0;
+      environment_.fixedTimeAttr = true;
     }
-    
-    
+  }
+}
 
+function Update(frametime)
+{
+  FindEnvironmentLight();
+  
+  if (environment_ != null)
+  {
+    // Day-night-cycle goes from 0-1.0
+    var time = environment_.currentTimeAttr;
+    time += speed_ * frametime;
+    time = time - Math.floor(time);
+    environment_.currentTimeAttr = time;
+  }
 }
 
 frame.Updated.connect(Update);
