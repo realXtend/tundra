9094d25886e0fc3f2f3e9d70a5ea7c91bcdef6fb
Nathan Letwory <nathan@letworyinteractive.com>
2010-11-25 22:57:57 +0200

    Quaternion API: equals, dotProduct, set(x,y,z,w), set(x,y,z), normalize, makeInverse, slerp, fromAngleAxis, toEuler, toAngleAxis, makeIdentity, rotationFromTo
    Quaternion API is now complete.

--- PythonScriptModule/QuaternionDecorator.h
+++ PythonScriptModule/QuaternionDecorator.h
@@ -24,14 +24,32 @@ namespace PythonScript
             float y(Quaternion* self);
             float z(Quaternion* self);
             float w(Quaternion* self);
+            
+            bool equals(Quaternion* self, const Quaternion& other, const float tolerance = ROUNDING_ERROR_32);
 
             void setx(Quaternion* self, float value);
             void sety(Quaternion* self, float value);
             void setz(Quaternion* self, float value);
             void setw(Quaternion* self, float value);
+            
+            float dotProduct(Quaternion* self, const Quaternion& other);
 
-            void toEuler(Quaternion* self, Vector3df& euler);
+            void set(Quaternion* self, float x, float y, float z, float w);
+            void set(Quaternion* self, float x, float y, float z);
             void set(Quaternion* self, const Vector3df& euler);
+            
+            void normalize(Quaternion* self);
+            void makeInverse(Quaternion* self);
+            void slerp(Quaternion* self, Quaternion q1, Quaternion q2, float interpolate);
+            
+            void fromAngleAxis(Quaternion* self, float angle, const Vector3df& axis);
+            
+            void toEuler(Quaternion* self, Vector3df& euler);
+            void toAngleAxis(Quaternion* self, float& angle, Vector3df& axis);
+            
+            void makeIdentity(Quaternion* self);
+            
+            void rotationFromTo(Quaternion* self, const Vector3df& from, const Vector3df& to);
     };
 }
 
