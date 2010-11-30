ec08711f938bc06fe452d836e0f9319785ada576
therauli <playsign@.(none)>
2010-11-30 21:52:17 +0200

    Some christmas cleaning

--- bin/pymodules/websocketserver/websocketserver.py
+++ bin/pymodules/websocketserver/websocketserver.py
@@ -14,7 +14,7 @@ import mathutils
 
 import async_eventlet_wsgiserver
 
-SPAWNPOS = 131.806, 48.9571, 28.7691 #at the end of pie ",".join(args)r in w.r.o:9000
+clients = set()
 
 class NaaliWebsocketServer(circuits.BaseComponent):
     instance = None
@@ -23,27 +23,20 @@ class NaaliWebsocketServer(circuits.BaseComponent):
         circuits.BaseComponent.__init__(self)
         self.sock = eventlet.listen(('0.0.0.0', 9999))
         self.server = async_eventlet_wsgiserver.server(self.sock, handle_clients)
+
         print "websocket server started."
 
         NaaliWebsocketServer.instance = self
-        self.clientavs = {}
-
-    def newclient(self, clientid, position, orientation):
-
-        #emit userconnected signal to js
-        naali._naali.server.UserConnected(1, 0)
+    
+    def newclient(self, clientid):
+        naali._naali.server.UserConnected(clientid, 0)
         
-        # ent = naali.createMeshEntity("Jack.mesh")
-        # ent.placeable.Position = Vec3(position[0], position[1], position[2])
-        # ent.placeable.Orientation = Quat(mathutils.euler_to_quat(orientation))
-        # print "New entity for web socket presence at", ent.placeable.Position
+    @circuits.handler("on_sceneadded")
+    def on_sceneadded(self, name):
+        self.scene = naali.getScene(name)
+        self.scene.connect("AttributeChanged(IComponent*, IAttribute*, AttributeChange::Type)", onComponentChanged)
 
-        # self.clientavs[clientid] = ent
-
-    def updateclient(self, clientid, position, orientation):
-        ent = self.clientavs[clientid]
-        ent.placeable.Position = Vec3(position[0], position[1], position[2])
-        ent.placeable.Orientation = Quat(mathutils.euler_to_quat(orientation))
+        self.scene.connect("EntityCreated(Scene::Entity*, AttributeChange::Type)",onNewEntity)
 
     @circuits.handler("update")
     def update(self, t):
@@ -55,11 +48,18 @@ class NaaliWebsocketServer(circuits.BaseComponent):
         # Need to figure something out what to do and how
         pass
 
-clients = set()
-       
 def sendAll(data):
     for client in clients:
         client.send(json.dumps(data))        
+
+def onComponentChanged(component, attribute, changeType):
+    #print component, attribute
+    #print component.GetParentEntity().Id
+    pass
+
+def onNewEntity(entity, changeType):
+    sendAll(['addEntity', {'id': entity.Id}])
+    print entity
 
 @websocket.WebSocketWSGI
 def handle_clients(ws):
@@ -67,8 +67,8 @@ def handle_clients(ws):
     myid = random.randrange(1,10000)
     clients.add(ws)
     
-    scene = naali.getScene("TundraServer")
-    
+    scene = NaaliWebsocketServer.instance.scene
+
     while True:
         # "main loop" for the server. When your done with the
         # connection break from the loop. It is important to remove
@@ -96,45 +96,20 @@ def handle_clients(ws):
         if function == 'CONNECTED':
             ws.send(json.dumps(['initGraffa', {}]))
 
-            x, y, z = SPAWNPOS[0], SPAWNPOS[1], SPAWNPOS[2]
-            start_position = (x, y, z)
-            start_orientation = (1.57, 0, 0)
-            NaaliWebsocketServer.instance.newclient(myid, start_position, start_orientation)
+            NaaliWebsocketServer.instance.newclient(myid)
 
-            # ws.send(json.dumps(['setId', {'id': myid}]))
-            # sendAll(['newAvatar', {'id': myid, 'position': start_position, 'orientation': start_orientation}])
+            ws.send(json.dumps(['setId', {'id': myid}]))
 
-            # ents = scene.GetEntitiesWithComponentRaw("EC_DynamicComponent")
-
-            # for ent in ents:
-            #     id = ent.Id
-            #     position = ent.placeable.Position.x(), ent.placeable.Position.y(), ent.placeable.Position.z()
-            #     orientation = mathutils.quat_to_euler(ent.placeable.Orientation)
-            #     sendAll(['addObject', {'id': id, 'position': position, 'orientation': orientation, 'xml': scene.GetEntityXml(ent).data()}])
-
-        elif function == 'Naps':
-            ws.send(json.dumps(['logMessage', {'message': 'Naps itelles!'}]))
-            
-        elif function == 'giev update':
-            pass
-        
         elif function == 'Action':
             action = params.get('action')
             args = params.get('params')
+            id = params.get('id')
+            av = scene.GetEntityByNameRaw("Avatar%s" % id)
 
-            av = scene.GetEntityByNameRaw("Avatar1")
-
-            av.Exec(1, action, args)
-
+            av.Exec(myid, action, args)
                 
-        elif function == 'setSize':
-            y_max = params['height']
-            x_max = params['width']
-
         elif function == 'reboot':
             break
 
-
     clients.remove(ws)
     print 'END', ws
-
