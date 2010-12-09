22d442ac222ff1edb0756d61a0a77680ee8240d6
Stinkfist <stinkfist0@gmail.com>
2010-12-09 17:55:57 +0200

    Show asset storages as top-level items in Assets window tree widget and add assets as children to them.

--- SceneStructureModule/AssetsWindow.cpp
+++ SceneStructureModule/AssetsWindow.cpp
@@ -9,10 +9,14 @@
 
 #include "StableHeaders.h"
 #include "AssetsWindow.h"
+#include "DebugOperatorNew.h"
 
 #include "Framework.h"
 #include "AssetAPI.h"
 #include "IAsset.h"
+#include "IAssetStorage.h"
+
+#include "MemoryLeakCheck.h"
 
 AssetsWindow::AssetsWindow(Foundation::Framework *fw) :
     framework(fw)
@@ -55,11 +59,42 @@ AssetsWindow::~AssetsWindow()
 
 void AssetsWindow::PopulateTreeWidget()
 {
+    foreach(AssetStoragePtr storage, framework->Asset()->GetAssetStorages())
+    {
+        QTreeWidgetItem *item = new QTreeWidgetItem;
+        item->setText(0, storage->Name());
+        treeWidget->addTopLevelItem(item);
+    }
+
+    // Create "No provider" for assets without storage.
+    QTreeWidgetItem *noProviderItem = new QTreeWidgetItem;
+    noProviderItem->setText(0, tr("No provider"));
+    treeWidget->addTopLevelItem(noProviderItem);
+
     std::pair<QString, AssetPtr> pair;
     foreach(pair, framework->Asset()->GetAllAssets())
     {
         QTreeWidgetItem *item = new QTreeWidgetItem;
         item->setText(0, pair.first);
-        treeWidget->addTopLevelItem(item);
+
+        AssetStoragePtr storage = pair.second->GetAssetStorage();
+        bool storageFound = false;
+        if (storage)
+            for (int i = 0; i < treeWidget->topLevelItemCount(); ++i)
+            {
+                QTreeWidgetItem *storageItem = treeWidget->topLevelItem(i);
+                if (storageItem->text(0) == storage->Name())
+                {
+                    storageItem->addChild(item);
+                    storageFound = true;
+                    break;
+                }
+            }
+
+        if (!storageFound)
+            noProviderItem->addChild(item);
     }
+
+    if (noProviderItem->childCount() == 0)
+        SAFE_DELETE(noProviderItem);
 }
