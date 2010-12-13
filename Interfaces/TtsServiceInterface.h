57ba29fe3417d3daef69497552c05bb8f30ce599
mattiku <matti.kuonanoja@evocativi.com>
2010-12-13 15:38:41 +0200

    Cleanup header file documentation.

--- Interfaces/TtsServiceInterface.h
+++ Interfaces/TtsServiceInterface.h
@@ -31,27 +31,6 @@
 
 namespace Tts
 {
-
-	//typedef std::string Voice;
- //   /// Available voice list. \note Every new language must be added here and in the Festival library folder. Also Festival.exe must be updated.
-	//struct AvailableVoice
-	//{
-	//	Voice ES1;
-	//	Voice ES2;
-	//	Voice EN1;
-	//	Voice EN2;
-	//	Voice EN3;
-	//	Voice EN4;
-	//	Voice EN5;
-	//	Voice EN6;
-	//	Voice CAT1;
-	//	Voice CAT2;
-	//	Voice FI;
-	//};
-
- //   ///Voice tag values
-	//static const AvailableVoice Voices = {"-ES1","-ES2","-EN1","-EN2","-EN3","-EN4","-EN5","-EN6","-CAT1","-CAT2","-FI"};
-
 	/// TTS Service Interface
 	/// Available methods are Text or File to Speech, WAV or PHO.
 	/// \todo Implement GetVoice GetVoice throw components
@@ -63,36 +42,37 @@ namespace Tts
         
 	    virtual ~TtsServiceInterface() {}
         /** Synthetizes text to speech
-		\param message Text to synthetize*/
+		\param message Text to synthetize
+        \param voice Voice database. @see GetAvailableVoices*/
 		virtual void Text2Speech(QString message, QString voice) = 0;
 		/** Synthetizes text to WAV
 		\param message Text to synthetize.
 		\param pathAndFileName The path and file name where the WAV is saved.
-		\param voice Voice database, must be one from AvailableVoices. */
+		\param voice Voice database. @see GetAvailableVoices*/
         virtual void Text2WAV(QString message, QString pathAndFileName, QString voice) = 0;
         /** Synthetizes text to PHO
 		\param message Text to synthetize.
 		\param pathAndFileName The path and file name where the phonetic information (PHO) is saved.
-		\param voice Voice database, must be one from AvailableVoices. */
+		\param voice Voice database. @see GetAvailableVoices*/
 		virtual void Text2PHO(QString message, QString pathAndFileName, QString voice) = 0;
 
         /** Synthetizes a text file to speech
 		\param pathAndFileName Input text file path, to synthetize.
-		\param voice Voice database, must be one from AvailableVoices. */
+		\param voice Voice database. @see GetAvailableVoices*/
 		virtual void File2Speech(QString pathAndFileName, QString voice) = 0;
         /** Synthetizes a text file to WAV
 		\param pathAndFileNameIn Input text file path, to synthetize.
         \param pathAndFileNameOut The path and file name where the WAV is saved.
-		\param voice Voice database, must be one from AvailableVoices. */
+		\param voice Voice database. @see GetAvailableVoices*/
         virtual void File2WAV(QString pathAndFileNameIn, QString pathAndFileNameOut, QString voice) = 0;
         /** Synthetizes a text file to PHO
 		\param pathAndFileNameIn Input text file path, to synthetize.
         \param pathAndFileNameOut The path and file name where the PHO is saved.
-		\param voice Voice database, must be one from AvailableVoices. */
+		\param voice Voice database. @see GetAvailableVoices*/
 		virtual void File2PHO(QString pathAndFileNameIn, QString pathAndFileNameOut, QString voice) = 0;
 
         /**
-         \return Available voice options. 
+         \return Available voice options.
          \see Text2Speech 
          */
         virtual QStringList GetAvailableVoices() const = 0;
