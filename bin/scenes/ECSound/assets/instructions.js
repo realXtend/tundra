94f04c0ea0faee686e89e50f917c3f21aa577445
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2010-12-21 16:12:58 +0200

    Improved ECSound sample scene.

---
+++ bin/scenes/ECSound/assets/instructions.js
@@ -0,0 +1,15 @@
+engine.ImportExtension("qt.core");
+engine.ImportExtension("qt.gui");
+
+var label = new QLabel();
+label.indent = 10;
+label.text = "This scene demonstrates how to use the Sound component. This component\nallows you to add spatial sound sources to the scene.\n\nThe freelook camera contains a SoundListener component, which acts as the \"ears\"\nfor receiving spatial audio.";
+label.resize(660,150);
+label.setStyleSheet("QLabel {background-color: transparent; font-size: 16px; }");
+
+var proxy = new UiProxyWidget(label);
+uiservice.AddProxyWidgetToScene(proxy);
+proxy.x = 100;
+proxy.y = 30;
+proxy.windowFlags = 0;
+proxy.visible = true;
