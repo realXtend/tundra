8491d597c60dc94323bc8e00eb225cc2fedec802
Toni Alatalo <toni@playsign.net>
2010-12-24 09:05:47 +0200

    oops. there is also this QProcess, which allows running any system command..

--- bin/scenes/RemoteJavascriptSandbox/systemaccess.js
+++ bin/scenes/RemoteJavascriptSandbox/systemaccess.js
@@ -5,3 +5,8 @@ print("Your home directory is at: " + home.path());
 print("And has these files:" + home.entryList());
 
 print("--luckily untrusted code from the net can not read this info.");
+
+p = new QProcess();
+p.start("ls /");
+out = p.readAll();
+//print(out);
