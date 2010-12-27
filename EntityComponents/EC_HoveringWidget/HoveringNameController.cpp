e0fff885bcf2eb89f620291b3ebe7a906b4309ab
Jonne Nauha <jonne.nauha@evocativi.com>
2010-12-27 14:08:15 +0200

    EC_HoveringWidget: modified to be ready for tundra usage. The ogre billboard dimesions were too small. Added slots to set height and font size. This ec is not on top of attribute system, only for local use.

--- EntityComponents/EC_HoveringWidget/HoveringNameController.cpp
+++ EntityComponents/EC_HoveringWidget/HoveringNameController.cpp
@@ -23,11 +23,27 @@ void HoveringNameController::SetText(const QString &text)
     textLabel->setText(text);
 }
 
+void HoveringNameController::SetTextHeight(int height)
+{
+    this->setFixedHeight(height);
+    middleFrame->setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0.204545 rgba(0, 0, 0, 200), "
+        "stop:1 rgba(75, 75, 75, 200)); border: 1px solid grey; border-radius:" + QString::number(height/4) + "px;");
+}
+
+void HoveringNameController::SetFontSize(int pt_size)
+{
+    textLabel->setStyleSheet("background-color: transparent; border: 0px; color: white;	font-size: " + QString::number(pt_size) + 
+        "pt; padding-left: " + QString::number(pt_size/3)+ "px; padding-right: " + QString::number(pt_size/3) + "px;");
+}
+
 void HoveringNameController::SetPixmap(QPixmap* pixmap)
 {
     pixmap_ = pixmap;
     if (pixmap_)
+    {
         pixmapLabel->setPixmap(*pixmap_);
+        pixmapLabel->resize(pixmap_->size());
+    }
 }
 
 QPixmap* HoveringNameController::GetPixmap()
