f627f4aa1e1339dbde4869dfeba9906e9dddf57e
Nathan Letwory <nathan@letworyinteractive.com>
2010-12-30 23:16:30 +0200

    Add EC_Selected.

---
+++ EntityComponents/EC_Selected/EC_Selected.h
@@ -0,0 +1,55 @@
+/**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
+ *  @file   EC_Selected.h
+ *  @brief  EC_Selected denotes its entity is selected
+ *  @author Nathan Letwory | http://www.letworyinteractive.com
+ */
+ 
+#ifndef incl_EC_Selected_EC_Selected_h
+#define incl_EC_Selected_EC_Selected_h
+
+#include "IComponent.h"
+#include "IAttribute.h"
+#include "Declare_EC.h"
+
+/// Selection of an entity
+/**
+<table class="header">
+<tr>
+<td>
+<h2>Selected</h2>
+Denote that entity is selected
+
+Registered by RexLogic::RexLogicModule.
+
+Doesn't have any attributes.
+
+Doesn't expose scriptable functions.
+
+Doesn't react on any actions.
+
+Does not emit any actions.
+
+</table>
+*/
+class EC_Selected : public IComponent
+{
+    Q_OBJECT
+    DECLARE_EC(EC_Selected);
+
+public:
+    /// Destructor.
+    ~EC_Selected();
+
+public slots:
+    /// IComponent override. Returns true.
+    virtual bool IsTemporary() const { return true; }
+
+private:
+    /// Constuctor.
+    /// @param module Owner module.
+    explicit EC_Selected(IModule *module);
+};
+
+#endif
