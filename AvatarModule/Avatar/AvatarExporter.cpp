834e55a661464bf9d59fedfa9b46781bb799157c
Stinkfist <stinkfist0@gmail.com>
2011-01-05 15:57:36 +0200

    Fix AvatarExporter.cpp(277) : warning C4702: unreachable code

--- AvatarModule/Avatar/AvatarExporter.cpp
+++ AvatarModule/Avatar/AvatarExporter.cpp
@@ -7,9 +7,9 @@
 #include "LLSDUtilities.h"
 #include "XmlRpcEpi.h"
 
-#include "Poco/MD5Engine.h"
-#include "Poco/SHA1Engine.h"
-#include "Poco/Base64Encoder.h"
+#include <Poco/MD5Engine.h>
+#include <Poco/SHA1Engine.h>
+#include <Poco/Base64Encoder.h>
 
 #include <QByteArray>
 #include <QUrl>
@@ -257,7 +257,7 @@ namespace Avatar
             {
                 sessionhash = call.GetReply<std::string>("sessionHash");
                 avatar_url = call.GetReply<std::string>("avatarStorageUrl");
-                return true;
+                //return true; return at the end of this func
             }
             else
             {
