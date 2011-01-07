3ceacdbec30061cf206a5fe535edac9ac5c1a410
therauli <playsign@.(none)>
2011-01-07 17:41:49 +0200

    Added decorator that I was supposed to add earlier

---
+++ PythonScriptModule/TransformDecorator.h
@@ -0,0 +1,33 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
+#ifndef incl_PythonScriptModule_TransformDecorator_h
+#define incl_PythonScriptModule_TransformDecorator_h
+
+#include <QObject>
+#include "Core.h"
+#include "Transform.h"
+
+namespace PythonScript
+{
+    class TransformDecorator : public QObject
+    {
+        Q_OBJECT
+
+		public slots:
+			Transform* new_Transform(const Vector3df &pos, const Vector3df &rot, const Vector3df &scale);
+			Transform* new_Transform();
+			void delete_Transform(Transform* self);
+
+			Vector3df position(const Transform* self);
+			Vector3df rotation(const Transform* self);
+			Vector3df scale(const Transform* self);
+
+			void SetPos(Transform* self, float x, float y, float z);
+			void SetRot(Transform* self, float x, float y, float z);
+			void SetScale(Transform* self, float x, float y, float z);
+  
+	};
+}
+
+#endif
+
