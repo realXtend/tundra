9d22cb756e026c79b09dc75a9bf654578f92038d
Jonne Nauha <jonne.nauha@evocativi.com>
2011-01-11 15:36:55 +0200

    Lots of obj manip code tweaks. Now localonly ents that wont sync to server. TAB to change manip. Added Vector3df setters to Transform decorator. Added CreateEntityLocalRaw() to scene manager. Added few funcs to naali.py, added params to createEntity(). Removed opensim spesific py modules from default.ini

--- PythonScriptModule/TransformDecorator.h
+++ PythonScriptModule/TransformDecorator.h
@@ -11,23 +11,26 @@ namespace PythonScript
 {
     class TransformDecorator : public QObject
     {
-        Q_OBJECT
+    Q_OBJECT
 
-		public slots:
-			Transform* new_Transform(const Vector3df &pos, const Vector3df &rot, const Vector3df &scale);
-			Transform* new_Transform();
-			void delete_Transform(Transform* self);
+    public slots:
+        Transform* new_Transform(const Vector3df &pos, const Vector3df &rot, const Vector3df &scale);
+        Transform* new_Transform();
+        void delete_Transform(Transform* self);
 
-			Vector3df position(const Transform* self);
-			Vector3df rotation(const Transform* self);
-			Vector3df scale(const Transform* self);
+        Vector3df position(const Transform* self);
+        Vector3df rotation(const Transform* self);
+        Vector3df scale(const Transform* self);
 
-			void SetPos(Transform* self, float x, float y, float z);
-			void SetRot(Transform* self, float x, float y, float z);
-			void SetScale(Transform* self, float x, float y, float z);
-  
+        void SetPos(Transform* self, float x, float y, float z);		
+        void SetPos(Transform* self, Vector3df pos);
+
+        void SetRot(Transform* self, float x, float y, float z);
+        void SetRot(Transform* self, Vector3df rot);
+
+        void SetScale(Transform* self, float x, float y, float z);
+        void SetScale(Transform* self, Vector3df scale);
 	};
 }
 
 #endif
-
