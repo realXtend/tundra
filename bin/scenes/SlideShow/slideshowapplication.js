7740a549ac26071781f6c0c3b049807f7fd3f890
therauli <playsign@.(none)>
2011-01-20 13:54:02 +0200

    Initial commit for slideshow thingy

---
+++ bin/scenes/SlideShow/slideshowapplication.js
@@ -0,0 +1,98 @@
+/*
+  SlideShow
+
+  TODO
+   * Get filesuffix better
+   * Accept only one file at the time 
+   * Open asset from url!
+
+*/
+
+engine.ImportExtension("qt.core");
+engine.ImportExtension("qt.gui");
+
+// connect to Drag events
+ui.GraphicsView().DragEnterEvent.connect(handleEnter);
+ui.GraphicsView().DragMoveEvent.connect(handleMove);
+ui.GraphicsView().DropEvent.connect(handleDrop);
+print ('foo')
+
+function checkSuffix(url) {
+    // FIXME
+    if (("" + url).split('.txt').length == 2) {
+	print("I accept!");
+
+	return true;
+    }
+    print("No sir, I don't like it");
+    return false;
+}
+
+function accept(event) {
+    
+    var mimedata = event.mimeData();
+
+    if (mimedata.hasUrls()) {
+	var urls = mimedata.urls();
+	for (u = 0; u < urls.length; u++ ) {
+	    if (checkSuffix(urls[u])) {
+		event.acceptProposedAction();		
+	    }
+	}
+    }
+}
+
+function handleEnter(event) {
+    print("Enter the event!");
+    accept(event);
+}
+
+function handleMove(event) {
+    print("I like to move it move it!");
+    accept(event);
+}
+
+function handleDrop(event) {
+    print ("DROP!");
+    accept(event);
+    
+    var mimedata = event.mimeData();
+    var urls = mimedata.urls();
+    for (u = 0; u < urls.length; u++ ) {
+	if (checkSuffix(urls[u])) {
+	    //FIXME!!!!!!1!
+	    print(urls[u]+"");
+	    var filename = ("" + urls[u]).split('///')[1];
+	    var file = new QFile(filename);
+	    file.open(QIODevice.ReadOnly);
+	    var streamer = new QTextStream(file);
+	    var content = [];
+	    while (true) {
+		var line = streamer.readLine();
+		if (!line) {
+		    break;
+		}
+		content.push(line)
+	    }
+    	    print(content);
+	    createCanvas(content);
+
+	    
+	}
+    }
+}
+
+function createCanvas(slides) {
+    var entity = scene.CreateEntityRaw(scene.NextFreeId(), ['EC_Placeable', 'EC_Mesh', 'EC_3DCanvasSource']);
+
+    mesh_ref = entity.mesh.meshRef;
+    mesh_ref.ref = 'local://screen.mesh';
+    entity.mesh.meshRef = mesh_ref;
+    
+    canvassource = entity.GetComponentRaw('EC_3DCanvasSource')
+    canvassource.show2d = false;
+    canvassource.source = slides[0];
+
+    
+
+}
