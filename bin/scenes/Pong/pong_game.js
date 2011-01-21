47e53cbb3daeef316993171d6d36e7373fdf3c12
Toni Alatalo <toni@playsign.net>
2011-01-21 23:24:03 +0200

    pong: disable physics movement attempts, use own vel vec to transform placeable. collision signal does give pos & nor etc correctly, had stupid own mistake earlier! dunno yet if should calc own new dirs from those, or try get somehow what bullet would do.

--- bin/scenes/Pong/pong_game.js
+++ bin/scenes/Pong/pong_game.js
@@ -14,18 +14,20 @@ ball.placeable.transform = t;
 
 var r = ball.rigidbody;
 //r.linearVelocity = new Vector3df(1.0, 0, 0);
-r.SetLinearVelocity(new Vector3df(1.0, 0, 0));
+//XXX TODO: doesn't work for some reason, test more and check internals
+//works from ecedit gui but not with this call.
+//r.SetLinearVelocity(new Vector3df(1.0, 0, 0));
 
-var MAXSPEED = 10.0;
+//var MAXSPEED = 10.0;
 var motion_x = 1.0;
 var motion_y = 0.0;
-var move_force = 0.1;
+var speed = 10.0;
 
 function handlecoll(other, pos, nor, dist, imp, newcoll) {
 //PhysicsCollision(Scene::Entity* otherEntity, const Vector3df& position, const Vector3df& normal, float distance, float impulse, bool newCollision);
+  print(pos.x + " : " + pos.y);
   //print(nor);
-  //print(nor.x() + " : " + nor.y());
-  //print(pos.x() + " : " + pos.y());
+  print(nor.x + " : " + nor.y);
   print(other.Id);
   //most ultimately lame HACK!
   if(other.Id == bat_b.Id)
@@ -42,33 +44,25 @@ function update(dt) {
   var placeable = ball.placeable;
   var rigidbody = ball.rigidbody;
 
-  var velvec = rigidbody.GetLinearVelocity();
+  /*var velvec = rigidbody.GetLinearVelocity();
   print(velvec.x);
   var curspeed = Math.abs(velvec.x); //Math.sqrt(rigidbody.GetLinearVelocity().x
   var curdir = new Vector3df(); //null; //velvec.Normalize()
-  if (velvec.x < 0) {
-    //curdir = new Vector3df(-1.0, 0, 0);
-    curdir.x = 1.0; //= new Vector3df(1.0, 0, 0);
-  }
-  else if (velvec.x == 0) {
-    curdir.x = 1.0;
-  }
-  else {
-    //curdir = new Vector3df(1.0, 0, 0);
-    curdir.x = -1.0; //= new Vector3df(1.0, 0, 0);
-  }
+  print(curspeed);*/
 
-  //print(curspeed);
-  if (curspeed < MAXSPEED) {
-    if ((motion_x != 0) || (motion_y != 0)) {
-      var mag = 1.0; // / Math.sqrt(motion_x * motion_y); // + curdir.x * curdir.y);
-      var impulseVec = new Vector3df();
+  //if (curspeed < MAXSPEED) {
+  if ((motion_x != 0) || (motion_y != 0)) {
+      //var mag = 1.0; // / Math.sqrt(motion_x * motion_y); // + curdir.x * curdir.y);
+      /*var impulseVec = new Vector3df();
       print(mag + " - " + curdir.x);
       impulseVec.x = mag * move_force * motion_x; //curdir.x;
       impulseVec.y = -mag * move_force * motion_y; //curdir.y;
       impulseVec = placeable.GetRelativeVector(impulseVec);
-      rigidbody.ApplyImpulse(impulseVec);
-    }
+      rigidbody.ApplyImpulse(impulseVec);*/
+
+      t = placeable.transform;
+      t.pos.x += speed * motion_x * dt;
+      placeable.transform = t;
   }
 }
 
