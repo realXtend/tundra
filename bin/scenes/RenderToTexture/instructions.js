56553c466789d0a1c833f79813c04fcf65d2d29d
Toni Alatalo <toni@playsign.net>
2011-02-05 06:20:29 +0200

    RttDemo finished: scene has 3 cameras, and buttons to switch the source cam for the display work. Video at http://www.youtube.com/watch?v=OmF1KV5P1tE

--- bin/scenes/RenderToTexture/instructions.js
+++ bin/scenes/RenderToTexture/instructions.js
@@ -4,7 +4,8 @@ engine.ImportExtension("qt.gui");
 var label = new QLabel();
 label.indent = 10;
 label.text = "This scene demonstrates render-to-texture support.\n"
-             + "You can use it to create external renders of any scene camera.\n\n"
+             + "You can use it to create external renders of any scene camera.\n"
+             + "Press the square buttons next to the big display to switch the source camera.\n\n"
              + "See the associated rttdisplay.js for details.";
 label.resize(660,100);
 label.setStyleSheet("QLabel {background-color: transparent; font-size: 16px; }");
