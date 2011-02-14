40c6fd4d026df71f7286bbbc0e9acd78b2f66559
antont <toni@playsign>
2011-02-14 10:37:17 +0200

    comment out call to ui_console_manager_->ToggleConsole() 'cause it's not defined after merge

--- SupportModules/ConsoleModule.cpp
+++ SupportModules/ConsoleModule.cpp
@@ -60,7 +60,7 @@ namespace Console
 
     void ConsoleModule::ToggleConsole()
     {
-        ui_console_manager_->ToggleConsole();
+        //ui_console_manager_->ToggleConsole();
     }
 
     void ConsoleModule::HandleKeyEvent(KeyEvent *keyEvent)
