b09479637905fc8171ed16df05d94dddf7d5fd7c
antont <toni@playsign>
2011-02-16 03:31:08 +0200

    memoryleak.js - something in js api leaks in demo scenes, tried to reproduce here in a controlled manner, but failed so far. did find out that free camera leaks! same things from it's update don't leak here :/ qtscript has ownership confs in newQObject which default to script engine not owning, but we are not using that even.

---
+++ bin/jsmodules/apitest/memoryleak.js
@@ -0,0 +1,52 @@
+var ent;
+var myscene;
+
+function init() {
+    myscene = framework.DefaultScene();
+    ent = myscene.CreateEntityRaw(myscene.NextFreeId(), ['EC_Placeable']);
+}
+
+//doesn't actually leak -- couldn't reproduce a leak here yet.
+//but free cam leaks! misobserved that here first, before removed it from startup.
+function no_leak(dt) {
+    if (!myscene) init();
+
+    ent = myscene.GetEntityRaw(ent.Id);
+    var x;
+    for(var i=0; i < 1000; i++) {
+	var t = ent.placeable.transform;
+	x = t.pos.x;
+    }
+    print(x);
+}
+/* (this was actually due to freecam)
+  PID USER      PR  NI  VIRT  RES  SHR S %CPU %MEM    TIME+  COMMAND            
+ 2803 antont    20   0  939m 161m  82m R   92  4.1   1:26.02 server             
+
+  PID USER      PR  NI  VIRT  RES  SHR S %CPU %MEM    TIME+  COMMAND            
+ 2803 antont    20   0  945m 168m  82m R   92  4.2   8:53.39 server            
+*/
+
+function createvec(dt) {
+    var v;
+    for(var i=0; i < 1000; i++) {
+	v = new Vector3df();
+	v.x = 1.2;
+    }
+    print(v.x);
+}
+
+function noop(dt) {
+}
+/* stays at:
+ PID USER      PR  NI  VIRT  RES  SHR S %CPU %MEM    TIME+  COMMAND            
+2892 antont    20   0  901m 143m  81m R   52  3.6   1:59.48 server             
+*/
+
+//run = noop;
+//run = no_leak;
+run = createvec;
+
+frame.Updated.connect(run);
+
+
