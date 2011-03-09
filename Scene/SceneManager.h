565301fa0691ded9e5a09bf82d5ab47ba4359617
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2011-03-09 14:26:25 +0200

    Replace the replaceOnConflict boolean in SceneManager with a useEntityIDsFromFile boolean, which specifies whether to use the id's from file, or whether to simply ignore them and generate new ones.
    The replaceOnConflict boolean is not that usable since you cannot ever make guarantees what id you got, so better to generate a set of clean ones. Modify GetNextFreeId() and GetNextFreeIdLocal() to always generate id's that are bigger than any id currently in the scene. These both work to hide a rather difficult-to-fix bug in SyncManager that it cannot guarantee proper replication if an entity with ID x is first deleted and then immediately re-created with the same ID.

--- Scene/SceneManager.h
+++ Scene/SceneManager.h
@@ -190,7 +190,7 @@ public slots:
 
         void RemoveEntityRaw(int entityid, AttributeChange::Type change = AttributeChange::Default) { RemoveEntity(entityid, change); }
 
-        void LoadSceneXMLRaw(const QString &filename, bool clearScene, bool replaceOnConflict, AttributeChange::Type change) { LoadSceneXML(filename.toStdString(), clearScene, replaceOnConflict, change); }
+        void LoadSceneXMLRaw(const QString &filename, bool clearScene, bool useEntityIDsFromFile, AttributeChange::Type change) { LoadSceneXML(filename.toStdString(), clearScene, useEntityIDsFromFile, change); }
 
         //! Is scene view enabled (i.e. rendering-related components actually create stuff).
         bool ViewEnabled() const { return viewEnabled_; }
