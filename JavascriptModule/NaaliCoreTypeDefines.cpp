f1d9bc32a730b7a898de352bf82aeb673e637a6e
Jonne Nauha <jonne.nauha@adminotech.com>
2011-03-11 00:30:56 +0200

    JavascriptModule: Added to/from function for QList<Scene::Entity*> and std::string so they can be used in scripts. Removed the commented out code of QColor, QVector3D and QQuaternion.

--- JavascriptModule/NaaliCoreTypeDefines.cpp
+++ JavascriptModule/NaaliCoreTypeDefines.cpp
@@ -10,6 +10,7 @@
 #include "Matrix4.h"
 #include "IAttribute.h"
 #include "AssetReference.h"
+#include "Entity.h"
 
 #include <QScriptEngine>
 #include <QColor>
@@ -39,25 +40,7 @@ void fromScriptValueColor(const QScriptValue &obj, Color &s)
     s.b = (float)obj.property("b").toNumber();
     s.a = (float)obj.property("a").toNumber();
 }
-/*
-QScriptValue toScriptValueQColor(QScriptEngine *engine, const QColor &s)
-{
-    QScriptValue obj = engine->newObject();
-    obj.setProperty("r", QScriptValue(engine, s.red()));
-    obj.setProperty("g", QScriptValue(engine, s.green()));
-    obj.setProperty("b", QScriptValue(engine, s.blue()));
-    obj.setProperty("a", QScriptValue(engine, s.alpha()));
-    return obj;
-}
 
-void fromScriptValueQColor(const QScriptValue &obj, QColor &s)
-{
-    s.setRed((float)obj.property("r").toNumber());
-    s.setGreen((float)obj.property("g").toNumber());
-    s.setBlue((float)obj.property("b").toNumber());
-    s.setAlpha((float)obj.property("a").toNumber());
-}
-*/
 QScriptValue Vector3df_prototype_normalize(QScriptContext *ctx, QScriptEngine *engine);
 QScriptValue Vector3df_prototype_getLength(QScriptContext *ctx, QScriptEngine *engine);
 QScriptValue Vector3df_prototype_mul(QScriptContext *ctx, QScriptEngine *engine);
@@ -114,23 +97,7 @@ QScriptValue Vector3df_prototype_mul(QScriptContext *ctx, QScriptEngine *engine)
 
     return toScriptValueVector3(engine, vec * scalar);
 }
-/*
-QScriptValue toScriptValueQVector3D(QScriptEngine *engine, const QVector3D &s)
-{
-    QScriptValue obj = engine->newObject();
-    obj.setProperty("x", QScriptValue(engine, s.x()));
-    obj.setProperty("y", QScriptValue(engine, s.y()));
-    obj.setProperty("z", QScriptValue(engine, s.z()));
-    return obj;
-}
 
-void fromScriptValueQVector3D(const QScriptValue &obj, QVector3D &s)
-{
-    s.setX((float)obj.property("x").toNumber());
-    s.setY((float)obj.property("y").toNumber());
-    s.setZ((float)obj.property("z").toNumber());
-}
-*/
 QScriptValue toScriptValueQuaternion(QScriptEngine *engine, const Quaternion &s)
 {
     QScriptValue obj = engine->newObject();
@@ -148,25 +115,7 @@ void fromScriptValueQuaternion(const QScriptValue &obj, Quaternion &s)
     s.z = (float)obj.property("z").toNumber();
     s.w = (float)obj.property("w").toNumber();
 }
-/*
-QScriptValue toScriptValueQQuaternion(QScriptEngine *engine, const QQuaternion &s)
-{
-    QScriptValue obj = engine->newObject();
-    obj.setProperty("x", QScriptValue(engine, s.x()));
-    obj.setProperty("y", QScriptValue(engine, s.y()));
-    obj.setProperty("z", QScriptValue(engine, s.z()));
-    obj.setProperty("w", QScriptValue(engine, s.scalar()));
-    return obj;
-}
 
-void fromScriptValueQQuaternion(const QScriptValue &obj, QQuaternion &s)
-{
-    s.setX((float)obj.property("x").toNumber());
-    s.setY((float)obj.property("y").toNumber());
-    s.setZ((float)obj.property("z").toNumber());
-    s.setScalar((float)obj.property("w").toNumber());
-}
-*/
 QScriptValue toScriptValueTransform(QScriptEngine *engine, const Transform &s)
 {
     QScriptValue obj = engine->newObject();
@@ -233,6 +182,45 @@ QScriptValue toScriptValueAssetReferenceList(QScriptEngine *engine, const AssetR
     return obj;
 }
 
+void fromScriptValueEntityList(const QScriptValue &obj, QList<Scene::Entity*> &ents)
+{
+    ents.clear();
+    QScriptValueIterator it(obj);
+    while (it.hasNext())
+    {
+        it.next();
+        QObject *qent = it.value().toQObject();
+        if (qent)
+        {
+            Scene::Entity *ent = qobject_cast<Scene::Entity*>(qent);
+            if (ent)
+                ents.append(ent);
+        }
+    }
+}
+
+QScriptValue toScriptValueEntityList(QScriptEngine *engine, const QList<Scene::Entity*> &ents)
+{
+    QScriptValue obj = engine->newArray(ents.size());
+    for(int i=0; i<ents.size(); ++i)
+    {
+        Scene::Entity *ent = ents.at(i);
+        if (ent)
+            obj.setProperty(i, engine->newQObject(ent));
+    }
+    return obj;
+}
+
+void fromScriptValueStdString(const QScriptValue &obj, std::string &s)
+{
+    s = obj.toString().toStdString();
+}
+
+QScriptValue toScriptValueStdString(QScriptEngine *engine, const std::string &s)
+{
+    return engine->newVariant(QString::fromStdString(s));
+}
+
 void fromScriptValueIAttribute(const QScriptValue &obj, IAttribute *&s)
 {
 }
@@ -289,20 +277,29 @@ void RegisterNaaliCoreMetaTypes()
 void ExposeNaaliCoreTypes(QScriptEngine *engine)
 {
     qScriptRegisterMetaType(engine, toScriptValueColor, fromScriptValueColor);
-    //qScriptRegisterMetaType(engine, toScriptValueQColor, fromScriptValueQColor);
     qScriptRegisterMetaType(engine, toScriptValueVector3, fromScriptValueVector3);
-    //qScriptRegisterMetaType(engine, toScriptValueQVector3D, fromScriptValueQVector3D);
     qScriptRegisterMetaType(engine, toScriptValueQuaternion, fromScriptValueQuaternion);
-    //qScriptRegisterMetaType(engine, toScriptValueQQuaternion, fromScriptValueQQuaternion);
     qScriptRegisterMetaType(engine, toScriptValueTransform, fromScriptValueTransform);
     qScriptRegisterMetaType(engine, toScriptValueAssetReference, fromScriptValueAssetReference);
     qScriptRegisterMetaType(engine, toScriptValueAssetReferenceList, fromScriptValueAssetReferenceList);
-
+    
     //qScriptRegisterMetaType<IAttribute*>(engine, toScriptValueIAttribute, fromScriptValueIAttribute);
     int id = qRegisterMetaType<IAttribute*>("IAttribute*");
     qScriptRegisterMetaType_helper(
         engine, id, reinterpret_cast<QScriptEngine::MarshalFunction>(toScriptValueIAttribute),
         reinterpret_cast<QScriptEngine::DemarshalFunction>(fromScriptValueIAttribute),
+        QScriptValue());
+
+    id = qRegisterMetaType< QList<Scene::Entity*> >("QList<Scene::Entity*>");
+    qScriptRegisterMetaType_helper(
+        engine, id, reinterpret_cast<QScriptEngine::MarshalFunction>(toScriptValueEntityList),
+        reinterpret_cast<QScriptEngine::DemarshalFunction>(fromScriptValueEntityList),
+        QScriptValue());
+ 
+    id = qRegisterMetaType<std::string>("std::string");
+    qScriptRegisterMetaType_helper(
+        engine, id, reinterpret_cast<QScriptEngine::MarshalFunction>(toScriptValueStdString),
+        reinterpret_cast<QScriptEngine::DemarshalFunction>(fromScriptValueStdString),
         QScriptValue());
 
     // Register constructors
