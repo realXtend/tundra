e59889804f1d5da501bdac02f44b8199bbfb9043
Erno Kuusela <erno@playsign.net>
2011-03-14 13:29:42 +0200

    can now run txml test files too (in addition to .js)

--- tools/js-viewer-server-test.bash
+++ tools/js-viewer-server-test.bash
@@ -3,16 +3,33 @@
 # load arg script to server and connect with viewer
 
 bindir=$(dirname $(readlink -f $0))/../bin
-jsfile=`readlink -f $1`
+testfile=`readlink -f $1`
+
+echo 'var d = frame.DelayedExecute(15.0); d.Triggered.connect(function (time) { print("exiting!"); framework.Exit(); });' > exitdelay.js
+echo 'var d = frame.DelayedExecute(0.5); d.Triggered.connect(function (time) { client.Login("localhost",2345,"foo"," ","tcp");});' > dologin.js
+
+case $testfile in 
+    *.js)
+	echo "Setting up args for a js file ($testfile)"
+	cat exitdelay.js $testfile > s.js
+	servercmd="./server --headless --run s.js"
+	viewercmd="./viewer --run v.js"
+	;;
+    *.txml)
+	echo "Setting up args for a txml file ($testfile)"
+	servercmd="./server --file $testfile --run exitdelay.js"
+	viewercmd="./viewer --storage `dirname $testfile` --run v.js"
+	;;
+esac
+
+
 cd $bindir
 ulimit -t 60 # cpu time limit
 ulimit -c unlimited # i'd like core dumps with that
-echo 'var d = frame.DelayedExecute(5.0); d.Triggered.connect(function (time) { print("exiting!"); framework.Exit(); });' > exitdelay.js
-echo 'var d = frame.DelayedExecute(0.5); d.Triggered.connect(function (time) { client.Login("localhost",2345,"foo"," ","tcp");});' > dologin.js
-cat $jsfile exitdelay.js > s.js
-cat dologin.js exitdelay.js > v.js
-xterm -e bash -c './server --headless --run s.js 2>&1 | tee s.out; echo $? > exitstatus.s' &
-xterm -e bash -c './viewer --run v.js 2>&1 | tee v.out; echo $? > exitstatus.v' 
+cat exitdelay.js dologin.js > v.js
+xterm -e bash -c "$servercmd"' 2>&1 | tee s.out; echo $? > exitstatus.s' &
+xterm -e bash -c "$viewercmd"' 2>&1 | tee v.out; echo $? > exitstatus.v' 
 wait
 grep . exitstatus.?
+grep Error: [sv].out
 test -f core && exit 1
