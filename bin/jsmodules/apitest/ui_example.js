2318d4911f3ce723420ac1a596050e7003ee807b
Erno Kuusela <erno@playsign.net>
2011-03-14 09:16:22 +0200

    fix backslashes in path

--- bin/jsmodules/apitest/ui_example.js
+++ bin/jsmodules/apitest/ui_example.js
@@ -1,7 +1,7 @@
 print("Loading ui example script.");
 
 // Widget that will get destoyed when the script instance is termited.
-var widget = uiservice.LoadFromFile(".\\jsmodules\\apitest\\time_counter\\time_counter.ui", false);
+var widget = uiservice.LoadFromFile("./jsmodules/apitest/time_counter/time_counter.ui", false);
 var proxy = new UiProxyWidget(widget);
 uiservice.AddWidgetToScene(proxy);
 proxy.x = 600;
@@ -10,9 +10,18 @@ proxy.visible = true;
 proxy.windowFlags = 0; //Qt::Widget
 
 // Same but the widget wont get destoyed when the script instance is termited.
-var widget2 = uiservice.LoadFromFile(".\\jsmodules\\apitest\\time_counter\\time_counter.ui", false);
+var widget2 = uiservice.LoadFromFile("./jsmodules/apitest/time_counter/time_counter.ui", false);
 var proxy2 = uiservice.AddWidgetToScene(widget2);
 proxy2.x = 600;
 proxy2.y = 50;
 proxy2.visible = true;
 proxy2.windowFlags = 3; //Qt::Dialog
+
+function exit(time)
+{
+    print(time + " seconds has passed, exit.");
+    framework.Exit()
+}
+
+var delayed2 = frame.DelayedExecute(3.0);
+delayed2.Triggered.connect(exit);
