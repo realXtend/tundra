ebdc5b861df79091a1e83199f7352fabee3b73ab
Stinkfist <stinkfist0@gmail.com>
2011-04-05 19:13:21 +0300

    Fix EC_Avatar and EC_WaterPlane classes' doxygen documentation (detailed description) to show up properly.

--- AvatarModule/EntityComponent/EC_Avatar.h
+++ AvatarModule/EntityComponent/EC_Avatar.h
@@ -10,6 +10,9 @@
 #include "AssetFwd.h"
 
 
+class AvatarDescAsset;
+typedef boost::shared_ptr<AvatarDescAsset> AvatarDescAssetPtr;
+
 //! Avatar component.
 /**
 <table class="header">
@@ -41,15 +44,11 @@ Does not emit any actions.
 <b>Depends on the components Mesh, Placeable, AnimationController and AvatarAppearance</b>.
 </table>
 */
-
-class AvatarDescAsset;
-typedef boost::shared_ptr<AvatarDescAsset> AvatarDescAssetPtr;
-
 class AV_MODULE_API EC_Avatar : public IComponent
 {
     Q_OBJECT
-    
     DECLARE_EC(EC_Avatar);
+
 public:
     //! Asset id for the avatar appearance file that will be used to generate the visible avatar. Asset request is handled by the component.
     Q_PROPERTY(QString appearanceId READ getappearanceId WRITE setappearanceId);
@@ -57,15 +56,16 @@ public:
 
     //! Set component as serializable.
     virtual bool IsSerializable() const { return true; }
-        
+
     //! Destructor
     virtual ~EC_Avatar();
-    
+
 private slots:
     //! Called when some of the attributes has been changed.
     void AttributeUpdated(IAttribute *attribute);
     
     void OnAvatarAppearanceLoaded(AssetPtr asset);
+
 private:
     //! constructor
     /*! \param module avatar module
