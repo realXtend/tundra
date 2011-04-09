5ca21d2e3ccdbf89feb58d693a6e5b9510dd030f
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2011-04-09 21:46:47 +0300

    Remove duplicate code typo in previous commit.

--- JavascriptModule/ScriptMetaTypeDefines.cpp
+++ JavascriptModule/ScriptMetaTypeDefines.cpp
@@ -240,7 +240,6 @@ void ExposeCoreApiMetaTypes(QScriptEngine *engine)
     qScriptRegisterQObjectMetaType<AssetCache*>(engine);
 
     qRegisterMetaType<AssetMap>("AssetMap");
-    qRegisterMetaType<AssetMap>("AssetMap");
     qScriptRegisterMetaType<AssetMap>(engine, qScriptValueFromAssetMap, qScriptValueToAssetMap);
 
     // Ui metatypes.
