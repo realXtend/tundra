198e5fd3290093633e8c1ddb1b3d047a6e43d5e2
Jonne Nauha <jonne.nauha@adminotech.com>
2011-04-12 00:56:02 +0300

    Removed CommunicationsModule. Was a punch of dead code as of now.

--- CommunicationsModule/CommunicationsModuleFwd.h
+++
@@ -1,7 +0,0 @@
-// For conditions of distribution and use, see copyright notice in license.txt
-
-#ifndef incl_CommunicationsModuleFwd_h
-#define incl_CommunicationsModuleFwd_h
-
-
-#endif // incl_CommunicationsModuleFwd_h
