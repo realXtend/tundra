f93cfe3b1862110e882081f3288b66cf02220bce
Lasse Öörni <lasse.oorni@ludocraft.com>
2011-04-12 14:21:13 +0300

    Fix AttributeChanged signal connect in EC_ProximityTrigger.

--- EntityComponents/EC_ProximityTrigger/EC_ProximityTrigger.cpp
+++ EntityComponents/EC_ProximityTrigger/EC_ProximityTrigger.cpp
@@ -23,7 +23,7 @@ EC_ProximityTrigger::EC_ProximityTrigger(IModule *module) :
     period(this, "Period", 0.0f)
 {
     SetUpdateMode();
-    connect(this, SIGNAL(OnAttributeChanged(IAttribute*, AttributeChange::Type)), SLOT(OnAttributeUpdated(IAttribute*)));
+    connect(this, SIGNAL(AttributeChanged(IAttribute*, AttributeChange::Type)), SLOT(OnAttributeUpdated(IAttribute*)));
 }
 
 EC_ProximityTrigger::~EC_ProximityTrigger()
