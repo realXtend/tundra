9f9b1e6bd264529b5d91338158ffaf09f6cd5ca9
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2011-04-13 17:35:27 +0300

    Add debugging for Ogre ParticleSystem.

--- DebugStatsModule/TimeProfilerWindow.cpp
+++ DebugStatsModule/TimeProfilerWindow.cpp
@@ -2186,7 +2186,10 @@ void AddOgreTUState(QTreeWidgetItem *parent, Ogre::TextureUnitState *node, int i
 
 void AddOgrePass(QTreeWidgetItem *parent, Ogre::Pass *node, int idx)
 {
-    QString str = QString("Pass ") + QString::number(idx) + ": " + node->getName().c_str();
+    QString str = QString("Pass ") + QString::number(idx) + ": \"" + node->getName().c_str() + "\"";
+
+    if (node->getNumTextureUnitStates() == 0)
+        str += ", contains 0 TextureUnitStates";
 
     QTreeWidgetItem *item = AddNewItem(parent, str);
 
@@ -2253,6 +2256,15 @@ void AddOgreVertexElement(QTreeWidgetItem *parent, const Ogre::VertexElement *no
     /*QTreeWidgetItem *nodeItem = */AddNewItem(parent, str);
 }
 
+void AddOgreMaterial(QTreeWidgetItem *parent, Ogre::MaterialPtr material)
+{
+    if (material.get())
+    {
+        for(int i = 0; i < material->getNumTechniques(); ++i)
+            AddOgreTechnique(parent, material->getTechnique(i), i);
+    }
+}
+
 void AddOgreSubEntity(QTreeWidgetItem *parent, Ogre::SubEntity *node, int idx)
 {
     QString str = QString("SubEntity ") + QString::number(idx);
@@ -2263,12 +2275,8 @@ void AddOgreSubEntity(QTreeWidgetItem *parent, Ogre::SubEntity *node, int idx)
         str += "(not loaded)";
 
     QTreeWidgetItem *item = AddNewItem(parent, str);
-
     if (material.get())
-    {
-        for(int i = 0; i < material->getNumTechniques(); ++i)
-            AddOgreTechnique(item, material->getTechnique(i), i);
-    }
+        AddOgreMaterial(item, material);
 
     Ogre::SubMesh *submesh = node->getSubMesh();
     if (submesh && submesh->vertexData && !submesh->useSharedVertices && submesh->vertexData->vertexDeclaration)
@@ -2299,7 +2307,24 @@ void AddOgreMovableObject(QTreeWidgetItem *parent, Ogre::MovableObject *node)
             str += ", (null MeshPtr)";
     }
 
+    Ogre::ParticleSystem *ps = dynamic_cast<Ogre::ParticleSystem*>(node);
+    if (ps)
+    {
+        str += QString(", material: ") + ps->getMaterialName().c_str();
+
+        Ogre::MaterialPtr material = Ogre::MaterialManager::getSingleton().getByName(ps->getMaterialName());
+        if (!material.get())
+            str += " (not loaded)";
+    }
+
     QTreeWidgetItem *nodeItem = AddNewItem(parent, str);
+
+    if (ps)
+    {
+        Ogre::MaterialPtr material = Ogre::MaterialManager::getSingleton().getByName(ps->getMaterialName());
+        if (material.get())
+            AddOgreMaterial(nodeItem, material);
+    }
 
     if (e)
     {
