3b21d953c772d90a41ccee352ac875ccc82cd41a
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2011-04-16 15:33:18 +0300

    Revert "LoginScreen.js: Use ConfigAPI to remember last succesfull logins server, username and protocol." Deleted AutoServerStore.js.
    This reverts commit 826f173c55541045089875c50874dba563c1aab1.
    
    Conflicts:
    
    	bin/jsmodules/startup/LoginScreen.js

--- bin/jsmodules/startup/LoginScreen.js
+++ bin/jsmodules/startup/LoginScreen.js
@@ -12,17 +12,20 @@ var exitButton;
 var tcpButton;
 var udpButton;
 
-var configServer = null;
-var configUsername = null;
-var configProtocol = null;
+// Only show login window if we are using a viewer.
+if (!server.IsAboutToStart())
+    SetupLoginScreen();
 
 function SetupLoginScreen() {
     widget = new QWidget();
     widget.setLayout(new QVBoxLayout());
 
+    widget.setWindowState(Qt.WindowFullScreen);
     var child = ui.LoadFromFile("./data/ui/LoginWidget.ui", false);
     child.setParent(widget);
-    widget.setVisible(false);
+    ui.AddWidgetToScene(widget);
+    widget.setVisible(true);
+
     widget.layout().addWidget(child, 0, 0);
 
     loginButton = findChild(widget, "pushButton_Connect");
@@ -41,52 +44,11 @@ function SetupLoginScreen() {
     logoLabel.pixmap = new QPixmap("./data/ui/images/realxtend_logo.png");
 
     client.Connected.connect(HideLoginScreen);
-    client.Connected.connect(WriteConfigFromUi);
     client.Disconnected.connect(ShowLoginScreen);
-
-    ReadConfigToUi();
-    return widget;
-}
-
-function ReadConfigToUi() {
-    // Make double sure with "" default value and null 
-    // checks that we can in any case insert var to ui
-    configServer = framework.Config().Get(configFile, configSection, "login_server", "");
-    if (configServer == null)
-        configServer = "";
-    configUsername = framework.Config().Get(configFile, configSection, "login_username", "");
-    if (configUsername == null)
-        configUsername = "";
-    configProtocol = framework.Config().Get(configFile, configSection, "login_protocol", "");
-    if (configProtocol == null)
-        configProtocol = "";
-
-    serverAddressLineEdit.text = configServer;
-    usernameLineEdit.text = configUsername;
-    if (configProtocol == "tcp")
-        tcpButton.checked = true;
-    else if (configProtocol == "udp")
-        udpButton.checked = true;
-}
-
-function WriteConfigFromUi() {
-    // Downside of this is that user may do something to the UI elements while loggin in.
-    // In Tundra its so fast that doubt that will happen. Can be fixed to read in to configX values in LoginPresse()
-    framework.Config().Set(configFile, configSection, "login_server", TrimField(serverAddressLineEdit.text));
-    framework.Config().Set(configFile, configSection, "login_username", TrimField(usernameLineEdit.text));
-    framework.Config().Set(configFile, configSection, "login_protocol", GetProtocol());
 }
 
 function TrimField(txt) {
     return txt.replace(/^\s+|\s+$/g,"");
-}
-
-function GetProtocol() {
-    if (tcpButton.checked)
-        return "tcp";
-    else if (udpButton.checked)
-        return "udp";
-    return "";
 }
 
 function LoginPressed() {
@@ -94,10 +56,12 @@ function LoginPressed() {
 
     var username = TrimField(usernameLineEdit.text);
     var password = TrimField(passwordLineEdit.text);
-    var protocol = GetProtocol();
-    if (protocol == "")
-        return;
-    
+    var protocol = "";
+    if (tcpButton.checked)
+        protocol = "tcp";
+    else if (udpButton.checked)
+        protocol = "udp";
+
     var port = 2345;
     var strings = TrimField(serverAddressLineEdit.text).split(':');
     if (strings.length > 1)
