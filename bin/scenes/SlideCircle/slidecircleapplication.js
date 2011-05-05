dc36cb46470af2153004fa0e128fc87b2ab09a11
therauli <therauli@playsus.(none)>
2011-05-05 14:54:32 +0300

    Camera goes to in front of the screen

--- bin/scenes/SlideCircle/slidecircleapplication.js
+++ bin/scenes/SlideCircle/slidecircleapplication.js
@@ -6,7 +6,78 @@
 engine.ImportExtension("qt.core");
 engine.ImportExtension("qt.gui");
 
+function vprint(v) {
+    print(v.x + ", " + v.y + ", " + v.z);
+}
+
+function crossp(v1, v2) {
+    var result = new Vector3df();
+
+    result.x = v1.y * v2.z - v1.z * v2.y;
+    result.y = v1.z * v2.x - v1.x * v2.z;
+    result.z = v1.x * v2.y - v1.y * v2.x;
+
+    return result;
+}
+
+function vadd(v1, v2) {
+    var result = new Vector3df();
+
+    result.x = v1.x + v2.x;
+    result.y = v1.y + v2.y;
+    result.z = v1.z + v2.z;
+
+    return result;
+}
+
+function smul(v1, s) {
+    var result = new Vector3df();
+    
+    result.x = v1.x * s;
+    result.y = v1.y * s;
+    result.z = v1.z * s;
+
+    return result;
+}
+
+function conjg(quat, v) {
+    var qvec = new Vector3df();
+    qvec.x = quat.x();
+    qvec.y = quat.y();
+    qvec.z = quat.z();
+
+    var uv = crossp(qvec, v);
+    var uuv = crossp(qvec, uv);
+    uv = smul(uv, 2.0 * quat.scalar());
+    uuv = smul(uuv, 2.0);
+
+    return vadd(vadd(v, uv), uuv);
+}
+
 function viewScreen(screen) {
+    var placeable = screen.placeable;
+    var q = placeable.orientation;
+
+    var temp_position = new Vector3df();
+    temp_position.x = placeable.position.x();
+    temp_position.y = placeable.position.y();
+    temp_position.z = placeable.position.z();
+
+    var un = new Vector3df();
+
+    un.y = 1;
+
+    var v = conjg(q, un);
+    var worldpos = vadd(temp_position, smul(v, 10));
+
+    var p = camera.placeable.position;
+
+    p.setX(worldpos.x);
+    p.setY(worldpos.y);
+    p.setZ(worldpos.z);
+
+    camera.placeable.position = p;
+
     camera.placeable.LookAt(screen.placeable.position);
 }
 
