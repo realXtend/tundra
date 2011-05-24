c5d2dda0b67165f9fe094e303524d8994f1492f6
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2011-05-24 12:06:19 +0300

    Removed unnecessary Entity dtor code that caused 'Entity::setProperty: name is not a property' error messages.

--- Scene/Entity.cpp
+++ Scene/Entity.cpp
@@ -39,15 +39,8 @@ Entity::~Entity()
 {
     // If components still alive, they become free-floating
     for(size_t i=0 ; i<components_.size() ; ++i)
-    {
         components_[i]->SetParentEntity(0);
-        // Remove component dynamic properties in case scripts still try to access them
-        QString componentTypeName = components_[i]->TypeName();
-        componentTypeName.replace(0, 3, "");
-        componentTypeName = componentTypeName.toLower();
-        setProperty(componentTypeName.toStdString().c_str(), QVariant());
-    }
-    
+   
     components_.clear();
     qDeleteAll(actions_);
 }
