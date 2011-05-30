80434f56c01c5e72e5dfed0015c597536a446ba4
Stinkfist <stinkfist0@gmail.com>
2011-05-30 20:21:59 +0300

    Fix erroneous Q_ENUMS declaration.

--- Scene/EntityAction.h
+++ Scene/EntityAction.h
@@ -3,10 +3,6 @@
  *
  *  @file   EntityAction.h
  *  @brief  Represent an executable command on an Entity.
- *
- *          Components (or other instances) can register to these actions by using Entity::ConnectAction().
- *          Actions allow more complicated in-world logic to be built in slightly more data-driven fashion.
- *          Actions cannot be created directly, they're created by Entity::RegisterAction().
  */
 
 #ifndef incl_Scene_EntityAction_h
@@ -21,12 +17,11 @@ class Entity;
 /// Represent an executable command on an Entity.
 /** Components (or other instances) can register to these actions by using Entity::ConnectAction().
     Actions allow more complicated in-world logic to be built in slightly more data-driven fashion.
-    Actions cannot be created directly, they're created by Entity::RegisterAction().
-*/
+    Actions cannot be created directly, they're created by Entity::RegisterAction(). */
 class EntityAction : public QObject
 {
     Q_OBJECT
-    Q_ENUMS(ExecutionType)
+    Q_ENUMS(ExecType)
 
     friend class Entity;
 
@@ -35,14 +30,13 @@ public:
     ~EntityAction() {}
 
     /// Returns name of the action.
-    QString Name() const { return name_; }
+    QString Name() const { return name; }
 
     /// Execution type of the action, i.e. where the actions is executed.
     /** As combinations we get local+server, local+peers(all clients but not server),
         server+peers (everyone but me), local+server+peers (everyone).
         Not all of these sound immediately sensible even, but we know we need to be able to do different things at different times.
-        Use the ExecTypeField type to store logical OR combinations of execution types.
-    */
+        Use the ExecTypeField type to store logical OR combinations of execution types. */
     enum ExecType
     {
         Invalid = 0, ///< Invalid.
@@ -59,27 +53,22 @@ signals:
     /** @param param1 1st parameter for the action, if applicable.
         @param param2 2nd parameter for the action, if applicable.
         @param param3 3rd parameter for the action, if applicable.
-        @param params Rest of the parameters, if applicable.
-    */
+        @param params Rest of the parameters, if applicable. */
     void Triggered(QString param1, QString param2, QString param3, QStringList params);
 
 private:
     /// Constructor.
-    /** @param name Name of the action.
-    */
-    explicit EntityAction(const QString &name) : name_(name) {}
+    /** @param name Name of the action. */
+    explicit EntityAction(const QString &name);
 
     /// Triggers this action i.e. emits the Triggered signal.
-    /** @param param1 1st parameter for the action, if applicable.
-        @param param2 2nd parameter for the action, if applicable.
-        @param param3 3rd parameter for the action, if applicable.
-        @param params Rest of the parameters, if applicable.
-    */
-    void Trigger(const QString &param1 = QString(), const QString &param2 = QString(),
-        const QString &param3 = QString(), const QStringList &params = QStringList());
+    /** @param p1 1st parameter for the action, if applicable.
+        @param p1 2nd parameter for the action, if applicable.
+        @param p1 3rd parameter for the action, if applicable.
+        @param p1 Rest of the parameters, if applicable. */
+    void Trigger(const QString &p1 = "", const QString &p2 = "", const QString &p3 = "", const QStringList &params = QStringList());
 
-    /// Name of the action.
-    QString name_;
+    QString name; ///< Name of the action.
 };
 
 #endif
