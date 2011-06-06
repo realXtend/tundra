ad4378a9cfe007b94b33317b028266e513ba414d
Carlos Jimenez <carlosjimenez@enne.es>
2011-06-06 13:16:29 +0200

    UiWidget files added to solution

---
+++ Ui/UiWidget.cpp
@@ -0,0 +1,21 @@
+/**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
+ *  @file   UiProxyWidget.cpp
+ *  @brief  Represents Naali UI widget created by embedding QWidget to the same
+ *          canvas as the in-world 3D scene.
+ */
+
+#include "StableHeaders.h"
+#include "DebugOperatorNew.h"
+#include "UiWidget.h"
+#include "MemoryLeakCheck.h"
+
+UiWidget::UiWidget(QWidget *widget, Qt::WindowFlags flags):
+QWidget(widget, flags)
+{
+}
+
+UiWidget::~UiWidget(void)
+{
+}
---
+++ Ui/UiWidget.h
@@ -0,0 +1,22 @@
+/**
+ *  For conditions of distribution and use, see copyright notice in license.txt
+ *
+ *  @file   UiProxyWidget.h
+ *  @brief  Represents Naali UI widget created by embedding QWidget to the same
+ *          canvas as the in-world 3D scene.
+ */
+
+#ifndef incl_Ui_UiWidget_h
+#define incl_Ui_UiWidget_h
+
+#include <QWidget.h>
+
+class UiWidget :  public QWidget
+{
+     Q_OBJECT
+
+public:
+    UiWidget(QWidget *widget, Qt::WindowFlags flags);
+    ~UiWidget(void);
+};
+#endif
