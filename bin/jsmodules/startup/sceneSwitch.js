d4b6fb17c07250178910035e2a4650910bca34f0
Jukka Vatjus-Anttila <jukka.vatjus-anttila@cie.fi>
2011-06-09 11:11:15 +0300

    Removed debug information prints.

--- bin/jsmodules/startup/sceneSwitch.js
+++ bin/jsmodules/startup/sceneSwitch.js
@@ -6,32 +6,20 @@ function SceneChanged(scene)
 {
 	print("Default scene changed!");
 	if(scene==null)
-	{
-		print("Scene was null!");	
 		return;
-	}
 
 	var	cameraEntity = scene.GetEntityByNameRaw("AvatarCamera");
 	
 	if(cameraEntity==null)
-	{
-		print("Camera entity was null! Getting FreeLookCamera.");
 		cameraEntity = scene.GetEntityByNameRaw("FreeLookCamera");
-	}
 
 	if(cameraEntity==null)
-	{
-		print("CameraEntity null... returning!");
 		return;
-	}
 
 	camera = cameraEntity.GetComponentRaw("EC_OgreCamera");
 	
 	if(camera==null)
-	{
-		print("EC_OgreCamera null!");
 		return;
-	}
 
 	print("Setting camera active!");
 	camera.SetActive();
