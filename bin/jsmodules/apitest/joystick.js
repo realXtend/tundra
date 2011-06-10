6343d02387c07d33c36351998f4f13b785261d63
antont <toni@playsign>
2011-06-10 10:11:54 +0300

    javascript joystick input usage test: gets the device as registered by pyppet.py currently (uses sdl with ctypes). tested with a dual-stick 6 axis usb gamepad

---
+++ bin/jsmodules/apitest/joystick.js
@@ -0,0 +1,22 @@
+//reading joystick data, as sent by pyppet.py currently
+//for docs on the api see http://www.realxtend.org/doxygen/device_api_page.html
+
+//NOTE: might be cool if this worked also beforehand - that if the reading part is inited later, it'd start working
+//now doing a trick to get around load order probs when testing with --run
+var js0;
+function init() {
+    if (!js0) {
+	js0 = devices.GetDevice("joystick0"); //the naming technique in pyppet.py
+	if (js0) {
+	    print("found joystick 0.");
+	    js0.PositionEvent.connect(joymove)
+	}
+    }
+    //else disconnect this init thing from update
+}
+frame.Updated.connect(init);
+
+function joymove(axis, val) {
+    print("joystick move: " + axis + ": " + val.x + ", " + val.y); //the sender abuses vec3, doesn't have z actually
+}
+
