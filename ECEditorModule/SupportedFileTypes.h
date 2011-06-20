86108e476a7d1635b37b1bee98b679d5140913fe
Stinkfist <stinkfist0@gmail.com>
2011-06-20 20:23:35 +0300

    Fix typo.

--- ECEditorModule/SupportedFileTypes.h
+++ ECEditorModule/SupportedFileTypes.h
@@ -2,7 +2,7 @@
  *  For conditions of distribution and use, see copyright notice in license.txt
  *
  *  @file   SupportedFileTypes.h
- *  @brief  Defines for file extensions supported by Tundra.
+ *  @brief  Defines file extensions supported by Tundra.
  */
 
 #pragma once
@@ -14,8 +14,8 @@ const QString cOgreMeshFileExtension(".mesh");      ///< OGRE mesh file extensio
 const QString cOgreSceneFileExtension(".scene");    ///< Tundra binary file extension.
 
 // File filter definitions for supported files.
-const QString cOgreSceneFileFilter(QApplication::translate("SceneTreeWidget", "OGRE scene (*.scene)")); ///< OGRE .scene
-const QString cOgreMeshFileFilter(QApplication::translate("SceneTreeWidget", "OGRE mesh (*.mesh)"));    ///< OGRE .mesh
+const QString cOgreSceneFileFilter(QApplication::translate("SupportedFileTypes", "OGRE scene (*.scene)")); ///< OGRE .scene
+const QString cOgreMeshFileFilter(QApplication::translate("SupportedFileTypes", "OGRE mesh (*.mesh)"));    ///< OGRE .mesh
 
 #ifdef ASSIMP_ENABLED
 /// All supported mesh formats if Open Asset Import (http://assimp.sourceforge.net/) is used.
@@ -24,8 +24,8 @@ const QString cMeshFileFilter(QApplication::translate("SceneTreeWidget", "Mesh (
     "*.irr *.off *.ter *.mdl *.hmp *.ms3d *.lwo *.lws *.lxo *.csm *.ply *.cob *.scn)"));
 #endif
 
-const QString cTundraXmlFileFilter(QApplication::translate("SceneTreeWidget", "Tundra scene XML(*.txml)")); ///< Tundra XML file filter.
-const QString cTundraBinaryFileFilter(QApplication::translate("SceneTreeWidget", "Tundra Binary Format (*.tbin)")); ///< Tundra binary file filter.
+const QString cTundraXmlFileFilter(QApplication::translate("SupportedFileTypes", "Tundra scene XML(*.txml)")); ///< Tundra XML file filter.
+const QString cTundraBinaryFileFilter(QApplication::translate("SupportedFileTypes", "Tundra Binary Format (*.tbin)")); ///< Tundra binary file filter.
 
 #ifdef ASSIMP_ENABLED
 /// All supported file formats if Open Asset Import (http://assimp.sourceforge.net/) is used.
@@ -33,8 +33,8 @@ const QString cAllSupportedTypesFileFilter(QApplication::translate("SceneTreeWid
     "*.tbin *.xml *.dae *.bvh *.3ds *.ase *.obj *.ply *.dxf *.nff *.smd *.vta *.mdl *.md2 *.md3 *.mdc *.md5mesh *.x *.q3o *.q3s *.raw "
     "*.ac *.stl *.irrmesh *.irr *.off *.ter *.mdl *.hmp *.ms3d *.lwo *.lws *.lxo *.csm *.ply *.cob *.scn)"));
 #else
-const QString cAllSupportedTypesFileFilter(QApplication::translate("SceneTreeWidget", "All supported types (*.scene *.mesh *.txml *.tbin)"));
+const QString cAllSupportedTypesFileFilter(QApplication::translate("SupportedFileTypes", "All supported types (*.scene *.mesh *.txml *.tbin)"));
 #endif
 
-const QString cAllTypesFileFilter(QApplication::translate("SceneTreeWidget", "All types (*.*)"));   ///< All types file filter.
+const QString cAllTypesFileFilter(QApplication::translate("SupportedFileTypes", "All types (*.*)"));   ///< All types file filter.
 
