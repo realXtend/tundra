8559dcd6fdf7c4eeaacf033444f710a483c9ebcc
antont <toni@playsign>
2011-07-12 18:02:44 +0300

    merged to tundra2: bare bones skeleton for first permission hook. a js test for it.

--- Scene/Scene.cpp
+++ Scene/Scene.cpp
@@ -357,6 +357,13 @@ void Scene::EmitActionTriggered(Entity *entity, const QString &action, const QSt
     emit ActionTriggered(entity, action, params, type);
 }
 
+//before-the-fact counterparts for the modification signals above, for permission checks
+bool SceneManager::AllowModifyEntity(Scene::Entity *entity)
+{
+    emit AboutToModifyEntity(entity);
+    return true;
+}
+
 QVariantList Scene::GetEntityIdsWithComponent(const QString &type_name) const
 {
     QVariantList ret;
