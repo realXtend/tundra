085077e36a7146032af20396d60a489b0655f084
Jonne Nauha <jonne.nauha@adminotech.com>
2011-07-19 04:35:38 +0300

    Automated check in win_cmake2008_vs2008.bat if depencencies have been fetched as a git submodule, if not it will instruct how to do this. If deps are found set TUNDRA_DEP_PATH automatically to it and run cmake.

--- win_cmake_vs2008.bat
+++ win_cmake_vs2008.bat
@@ -1,11 +1,25 @@
-@rem Add -DUPDATE_LANGUAGE_TRANSLATIONS:BOOL=TRUE to cmake parameters to generate translations
 @echo off
 
-::SET TUNDRA_DEP_PATH=<path_to_vc9_build_deps>
+:check_vs2008_deps
+if exist "deps\vs2008\README" (
+    goto :set_env_variables
+) else (
+    echo ERROR: Could not find 'deps\vs2008\README', assuming git submodule has not been fetched yet.
+    echo ERROR: Please run win_update_deps_vs2008.bat to fetch the dependencies!
+    goto :end
+)
+
+:set_env_variables
+SET TUNDRA_DEP_PATH=%CD%\deps\vs2008\build_deps
 SET QTDIR=%TUNDRA_DEP_PATH%\Qt
 SET QMAKESPEC=%QTDIR%\mkspecs\win32-msvc2008
+goto :run_cmake
 
-cmake.exe -G "Visual Studio 9 2008"
+:run_cmake
+cmake.exe -G "Visual Studio 9 2008" 
+::cmake.exe -G "Visual Studio 9 2008" -DUPDATE_LANGUAGE_TRANSLATIONS:BOOL=TRUE
+goto :end
 
+:end
 echo.
 pause
