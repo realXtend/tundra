04d7b011f474deca182cae7b97dc2a84affcc449
Jonne Nauha <jonne.nauha@evocativi.com>
2011-07-19 20:35:31 +0300

    Fix gitignore so it wont hide source files under src folder. Add due to this a missing dllmain.cpp. Fix one signal connection in EC_WebView.

---
+++ src/Application/SceneWidgetComponents/dllmain.cpp
@@ -0,0 +1,20 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
+#include "Framework.h"
+#include "SceneAPI.h"
+#include "IComponentFactory.h"
+
+#include "EC_WidgetCanvas.h"
+#include "EC_WebView.h"
+
+extern "C"
+{
+
+DLLEXPORT void TundraPluginMain(Framework *fw)
+{
+    Framework::SetInstance(fw); // Inside this DLL, remember the pointer to the global framework object.
+    fw->Scene()->RegisterComponentFactory(ComponentFactoryPtr(new GenericComponentFactory<EC_WidgetCanvas>));
+    fw->Scene()->RegisterComponentFactory(ComponentFactoryPtr(new GenericComponentFactory<EC_WebView>));
+}
+
+} // ~extern "C"
