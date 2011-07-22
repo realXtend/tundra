07e7dc1c89dbc16c9472589cea45a39c811a9278
Erno Kuusela <erno@playsign.net>
2011-07-22 11:59:38 +0300

    fix ArgumentType.h templates related gcc linker error

--- src/Core/Framework/ArgumentType.h
+++ src/Core/Framework/ArgumentType.h
@@ -158,51 +158,51 @@ private:
 };
 
 // QString
-template<> QWidget *ArgumentType<QString>::CreateEditor(QWidget *parent)
+template<> inline QWidget *ArgumentType<QString>::CreateEditor(QWidget *parent)
 {
     editor = new QLineEdit(parent);
     return editor;
 }
 
-template<> void ArgumentType<QString>::UpdateValueFromEditor()
+template<> inline void ArgumentType<QString>::UpdateValueFromEditor()
 {
     QLineEdit *e = dynamic_cast<QLineEdit *>(editor);
     if (e)
         value = e->text();
 }
 
-template<> void ArgumentType<QString>::UpdateValueToEditor()
+template<> inline void ArgumentType<QString>::UpdateValueToEditor()
 {
     QLineEdit *e = dynamic_cast<QLineEdit *>(editor);
     if (e)
         e->setText(value);
 }
 
-template<> QString ArgumentType<QString>::ToString() const
+template<> inline QString ArgumentType<QString>::ToString() const
 {
     return value;
 }
 
-template<> void ArgumentType<QString>::FromString(const QString &str)
+template<> inline void ArgumentType<QString>::FromString(const QString &str)
 {
     value = str;
 }
 
 // QStringList
-template<> QWidget *ArgumentType<QStringList>::CreateEditor(QWidget *parent)
+template<> inline QWidget *ArgumentType<QStringList>::CreateEditor(QWidget *parent)
 {
     editor = new QTextEdit(parent);
     return editor;
 }
 
-template<> void ArgumentType<QStringList>::UpdateValueFromEditor()
+template<> inline void ArgumentType<QStringList>::UpdateValueFromEditor()
 {
     QTextEdit *e = dynamic_cast<QTextEdit *>(editor);
     if (e)
         value = e->toPlainText().split("\n", QString::SkipEmptyParts);
 }
 
-template<> void ArgumentType<QStringList>::UpdateValueToEditor()
+template<> inline void ArgumentType<QStringList>::UpdateValueToEditor()
 {
     QTextEdit *e = dynamic_cast<QTextEdit *>(editor);
     if (e)
@@ -214,7 +214,7 @@ template<> void ArgumentType<QStringList>::UpdateValueToEditor()
     }
 }
 
-template<> QString ArgumentType<QStringList>::ToString() const
+template<> inline QString ArgumentType<QStringList>::ToString() const
 {
     QString str;
     foreach(QString item, value)
@@ -223,206 +223,206 @@ template<> QString ArgumentType<QStringList>::ToString() const
     return str;
 }
 
-template<> void ArgumentType<QStringList>::FromString(const QString &str)
+template<> inline void ArgumentType<QStringList>::FromString(const QString &str)
 {
     value = str.split(",");
 }
 
 // std::string
-template<> QWidget *ArgumentType<std::string>::CreateEditor(QWidget *parent)
+template<> inline QWidget *ArgumentType<std::string>::CreateEditor(QWidget *parent)
 {
     editor = new QLineEdit(parent);
     return editor;
 }
 
-template<> void ArgumentType<std::string>::UpdateValueFromEditor()
+template<> inline void ArgumentType<std::string>::UpdateValueFromEditor()
 {
     QLineEdit *e = dynamic_cast<QLineEdit *>(editor);
     if (e)
         value = e->text().toStdString();
 }
 
-template<> void ArgumentType<std::string>::UpdateValueToEditor()
+template<> inline void ArgumentType<std::string>::UpdateValueToEditor()
 {
     QLineEdit *e = dynamic_cast<QLineEdit *>(editor);
     if (e)
         e->setText(value.c_str());
 }
 
-template<> QString ArgumentType<std::string>::ToString() const
+template<> inline QString ArgumentType<std::string>::ToString() const
 {
     return QString(value.c_str());
 }
 
-template<> void ArgumentType<std::string>::FromString(const QString &str)
+template<> inline void ArgumentType<std::string>::FromString(const QString &str)
 {
     value = str.toStdString();
 }
 
-template<> QVariant ArgumentType<std::string>::ToQVariant() const
+template<> inline QVariant ArgumentType<std::string>::ToQVariant() const
 {
     return QVariant(value.c_str());
 }
 
-template<> void ArgumentType<std::string>::FromQVariant(const QVariant &var)
+template<> inline void ArgumentType<std::string>::FromQVariant(const QVariant &var)
 {
     value = var.toString().toStdString();
 }
 
 // Boolean
-template<> QWidget *ArgumentType<bool>::CreateEditor(QWidget *parent)
+template<> inline QWidget *ArgumentType<bool>::CreateEditor(QWidget *parent)
 {
     editor = new QCheckBox(parent);
     return editor;
 }
 
-template<> void ArgumentType<bool>::UpdateValueFromEditor()
+template<> inline void ArgumentType<bool>::UpdateValueFromEditor()
 {
     QCheckBox *e = dynamic_cast<QCheckBox *>(editor);
     if (e)
         value = e->isChecked();
 }
 
-template<> void ArgumentType<bool>::UpdateValueToEditor()
+template<> inline void ArgumentType<bool>::UpdateValueToEditor()
 {
     QCheckBox *e = dynamic_cast<QCheckBox *>(editor);
     if (e)
         e->setChecked(value);
 }
 
-template<> QString ArgumentType<bool>::ToString() const
+template<> inline QString ArgumentType<bool>::ToString() const
 {
     return QString::number((int)value);
 }
 
-template<> void ArgumentType<bool>::FromString(const QString &str)
+template<> inline void ArgumentType<bool>::FromString(const QString &str)
 {
     value = ParseBool(str);
 }
 
 // Unsigned integer.
-template<> QWidget *ArgumentType<unsigned int>::CreateEditor(QWidget *parent)
+template<> inline QWidget *ArgumentType<unsigned int>::CreateEditor(QWidget *parent)
 {
     editor = new QSpinBox(parent);
     return editor;
 }
 
-template<> void ArgumentType<unsigned int>::UpdateValueFromEditor()
+template<> inline void ArgumentType<unsigned int>::UpdateValueFromEditor()
 {
     QSpinBox *e = dynamic_cast<QSpinBox *>(editor);
     if (e)
         value = e->value();
 }
 
-template<> void ArgumentType<unsigned int>::UpdateValueToEditor()
+template<> inline void ArgumentType<unsigned int>::UpdateValueToEditor()
 {
     QSpinBox *e = dynamic_cast<QSpinBox *>(editor);
     if (e)
         e->setValue(value);
 }
 
-template<> QString ArgumentType<unsigned int>::ToString() const
+template<> inline QString ArgumentType<unsigned int>::ToString() const
 {
     return QString::number((unsigned int)value);
 }
 
-template<> void ArgumentType<unsigned int>::FromString(const QString &str)
+template<> inline void ArgumentType<unsigned int>::FromString(const QString &str)
 {
     value = str.toInt();
 }
 
 // Integer
-template<> QWidget *ArgumentType<int>::CreateEditor(QWidget *parent)
+template<> inline QWidget *ArgumentType<int>::CreateEditor(QWidget *parent)
 {
     editor = new QSpinBox(parent);
     static_cast<QSpinBox *>(editor)->setRange(-std::numeric_limits<int>::max(), std::numeric_limits<int>::max());
     return editor;
 }
 
-template<> void ArgumentType<int>::UpdateValueFromEditor()
+template<> inline void ArgumentType<int>::UpdateValueFromEditor()
 {
     QSpinBox *e = dynamic_cast<QSpinBox *>(editor);
     if (e)
         e->setValue(value);
 }
 
-template<> void ArgumentType<int>::UpdateValueToEditor()
+template<> inline void ArgumentType<int>::UpdateValueToEditor()
 {
     QSpinBox *e = dynamic_cast<QSpinBox *>(editor);
     if (e)
         value = e->value();
 }
 
-template<> QString ArgumentType<int>::ToString() const
+template<> inline QString ArgumentType<int>::ToString() const
 {
     return QString::number((int)value);
 }
 
-template<> void ArgumentType<int>::FromString(const QString &str)
+template<> inline void ArgumentType<int>::FromString(const QString &str)
 {
     value = str.toInt();
 }
 
 // Float
-template<> QWidget *ArgumentType<float>::CreateEditor(QWidget *parent)
+template<> inline QWidget *ArgumentType<float>::CreateEditor(QWidget *parent)
 {
     editor = new QDoubleSpinBox(parent);
     static_cast<QDoubleSpinBox *>(editor)->setRange(-std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
     return editor;
 }
 
-template<> void ArgumentType<float>::UpdateValueFromEditor()
+template<> inline void ArgumentType<float>::UpdateValueFromEditor()
 {
     QDoubleSpinBox *e = dynamic_cast<QDoubleSpinBox *>(editor);
     if (e)
         value = e->value();
 }
 
-template<> void ArgumentType<float>::UpdateValueToEditor()
+template<> inline void ArgumentType<float>::UpdateValueToEditor()
 {
     QDoubleSpinBox *e = dynamic_cast<QDoubleSpinBox *>(editor);
     if (e)
         e->setValue(value);
 }
 
-template<> QString ArgumentType<float>::ToString() const
+template<> inline QString ArgumentType<float>::ToString() const
 {
     return QString::number((float)value);
 }
 
-template<> void ArgumentType<float>::FromString(const QString &str)
+template<> inline void ArgumentType<float>::FromString(const QString &str)
 {
     value = str.toFloat();
 }
 
 // Double
-template<> QWidget *ArgumentType<double>::CreateEditor(QWidget *parent)
+template<> inline QWidget *ArgumentType<double>::CreateEditor(QWidget *parent)
 {
     editor = new QDoubleSpinBox(parent);
     static_cast<QDoubleSpinBox *>(editor)->setRange(-std::numeric_limits<double>::max(), std::numeric_limits<double>::max());
     return editor;
 }
 
-template<> void ArgumentType<double>::UpdateValueFromEditor()
+template<> inline void ArgumentType<double>::UpdateValueFromEditor()
 {
     QDoubleSpinBox *e = dynamic_cast<QDoubleSpinBox *>(editor);
     if (e)
         value = e->value();
 }
 
-template<> void ArgumentType<double>::UpdateValueToEditor()
+template<> inline void ArgumentType<double>::UpdateValueToEditor()
 {
     QDoubleSpinBox *e = dynamic_cast<QDoubleSpinBox *>(editor);
     if (e)
         e->setValue(value);
 }
 
-template<> QString ArgumentType<double>::ToString() const
+template<> inline QString ArgumentType<double>::ToString() const
 {
     return QString::number((double)value);
 }
 
-template<> void ArgumentType<double>::FromString(const QString &str)
+template<> inline void ArgumentType<double>::FromString(const QString &str)
 {
     value = str.toDouble();
 }
