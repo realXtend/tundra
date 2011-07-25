3dc7f43166d58d1462283bb177d5aba90ad2fd5a
Jonne Nauha <jonne.nauha@adminotech.com>
2011-07-25 03:07:03 +0300

    Remove dead dep configure macros from cmake/ConfigurePackages.cmake.

--- cmake/ConfigurePackages.cmake
+++ cmake/ConfigurePackages.cmake
@@ -42,21 +42,6 @@ macro (configure_boost)
     sagase_configure_report (BOOST)
 endmacro (configure_boost)
 
-macro (configure_poco)
-    sagase_configure_package (POCO 
-        NAMES Poco PoCo poco
-        COMPONENTS Poco PocoFoundation PocoNet PocoUtil PocoXML
-        PREFIXES ${ENV_TUNDRA_DEP_PATH})
-
-    # POCO autolinks on MSVC
-    if (MSVC)
-        set (POCO_LIBRARIES "")
-        set (POCO_DEBUG_LIBRARIES "")
-    endif ()
-
-    sagase_configure_report (POCO)
-endmacro (configure_poco)
-
 macro (configure_qt4)
     sagase_configure_package (QT4 
         NAMES Qt4 4.6.1
@@ -119,8 +104,7 @@ macro (configure_python)
     endif ()
     
     # FindPythonLibs.cmake prefers the system-wide Python, which does not
-    # include debug libraries, so we force to NAALI_DEP_PATH.
-
+    # include debug libraries, so we force to TUNDRA_DEP_PATH.
     if (MSVC)
         set (PYTHON_LIBRARY_DIRS ${ENV_TUNDRA_DEP_PATH}/Python/lib)
         set (PYTHON_INCLUDE_DIRS ${ENV_TUNDRA_DEP_PATH}/Python/include)
@@ -231,129 +215,6 @@ macro (configure_qtpropertybrowser)
     sagase_configure_report (QT_PROPERTY_BROWSER)
 endmacro (configure_qtpropertybrowser)
 
-macro (configure_xmlrpc)
-    if (APPLE)
-        sagase_configure_package (XMLRPC 
-            NAMES xmlrpc xmlrpcepi xmlrpc-epi
-            COMPONENTS xmlrpc xmlrpcepi xmlrpc-epi xmlrpcepid iconv
-            PREFIXES ${ENV_TUNDRA_DEP_PATH}
-                ${ENV_TUNDRA_DEP_PATH}/xmlrpc-epi/src
-                ${ENV_TUNDRA_DEP_PATH}/xmlrpc-epi/Debug
-                ${ENV_TUNDRA_DEP_PATH}/xmlrpc-epi/Release)
-    else()
-        sagase_configure_package (XMLRPC 
-            NAMES xmlrpc xmlrpcepi xmlrpc-epi
-            COMPONENTS xmlrpc xmlrpcepi xmlrpc-epi xmlrpcepid
-            PREFIXES ${ENV_TUNDRA_DEP_PATH}
-                ${ENV_TUNDRA_DEP_PATH}/xmlrpc-epi/src
-                ${ENV_TUNDRA_DEP_PATH}/xmlrpc-epi/Debug
-        ${ENV_TUNDRA_DEP_PATH}/xmlrpc-epi/Release)
-    endif()
-   
-    if (MSVC)
-        set (XMLRPC_LIBRARIES xmlrpcepi)
-        set (XMLRPC_DEBUG_LIBRARIES xmlrpcepid)
-    endif()
-    
-    sagase_configure_report (XMLRPC)
-endmacro (configure_xmlrpc)
-
-macro (configure_curl)
-    sagase_configure_package (CURL 
-        NAMES Curl curl libcurl
-        COMPONENTS curl libcurl_imp libcurld_imp
-        PREFIXES ${ENV_TUNDRA_DEP_PATH}
-        ${ENV_TUNDRA_DEP_PATH}/libcurl/lib/DLL-Debug
-        ${ENV_TUNDRA_DEP_PATH}/libcurl/lib/DLL-Release)
-    
-    if (MSVC)
-        set (CURL_LIBRARIES libcurl_imp)
-        set (CURL_DEBUG_LIBRARIES libcurld_imp)
-    endif()
-
-    if (APPLE)
-        set (CURL_LIBRARIES curl)
-    endif()
-    
-    sagase_configure_report (CURL)
-endmacro (configure_curl)
-
-macro (configure_openjpeg)
-    sagase_configure_package (OPENJPEG
-        NAMES OpenJpeg OpenJPEG openjpeg
-        COMPONENTS OpenJpeg OpenJPEG openjpeg
-        PREFIXES ${ENV_TUNDRA_DEP_PATH}
-        ${ENV_TUNDRA_DEP_PATH}/OpenJpeg/libopenjpeg
-        ${ENV_TUNDRA_DEP_PATH}/OpenJpeg/Debug
-        ${ENV_TUNDRA_DEP_PATH}/OpenJpeg/Release)
-    
-    sagase_configure_report (OPENJPEG)
-endmacro (configure_openjpeg)
-
-macro (configure_telepathy_qt4)
-    sagase_configure_package (TELEPATHY_QT4 
-        NAMES QtTelepathy Telepathy-QT4 telepathy-qt4 TelepathyQt4 telepathy-1.0
-        COMPONENTS QtTelepathyCore QtTelepathyCommon QtTelepathyClient telepathy-qt4 telepathy-qt4-farsight connection # connection added here to help sagase to find include folder with connection.h file
-        PREFIXES ${ENV_TUNDRA_DEP_PATH} )
-    sagase_configure_report (TELEPATHY_QT4)
-endmacro (configure_telepathy_qt4)
-
-macro (configure_gstreamer)
-    sagase_configure_package (GSTREAMER
-        NAMES gstreamer gst gstreamer-0.10
-        COMPONENTS gstreamer gstfarsight gstinterfaces gst # gst added to help sagese to find include folder with gst.h file
-        PREFIXES ${ENV_TUNDRA_DEP_PATH})
-    sagase_configure_report (GSTREAMER)
-endmacro (configure_gstreamer)
-
-macro (configure_dbus)
-    sagase_configure_package (DBUS
-        NAMES dbus dbus-1
-        COMPONENTS dbus-1 dbus # dbus added to help sagese to find include folder with dbus.h file
-        PREFIXES ${ENV_TUNDRA_DEP_PATH})
-    sagase_configure_report (DBUS)
-endmacro (configure_dbus)
-
-macro (configure_glib)
-    sagase_configure_package (GLIB
-        NAMES glib-2.0 glib Glib
-        COMPONENTS glib-2.0 gobject-2.0 glib # glib added to help sagese to find include folder with glib.h file
-        PREFIXES ${ENV_TUNDRA_DEP_PATH})
-    sagase_configure_report (GLIB)
-endmacro (configure_glib)
-
-macro (configure_telepathy_glib)
-    sagase_configure_package (TELEPATHY_GLIB
-        NAMES telepathy-glib Telepathy-Glib
-        COMPONENTS telepathy-glib connection # connection added to help sagese to find include folder with connection.h file
-        PREFIXES ${ENV_TUNDRA_DEP_PATH})
-    sagase_configure_report (TELEPATHY_GLIB)
-endmacro (configure_telepathy_glib)
-
-macro (configure_telepathy_farsight)
-    sagase_configure_package (TELEPATHY_FARSIGHT 
-        NAMES telepathy-farsight Telepathy-Farsight
-        COMPONENTS telepathy-farsight stream # stream added to help sagese to find include folder with stream.h file
-        PREFIXES ${ENV_TUNDRA_DEP_PATH})
-    sagase_configure_report (TELEPATHY_FARSIGHT)
-endmacro (configure_telepathy_farsight)
-
-macro (configure_farsight2)
-    sagase_configure_package (FARSIGHT2
-        NAMES farsight2 farsight2-0.10
-        COMPONENTS gstfarsight fs-interfaces # fs-interfaces added to help sagese to find include folder with fs-interfaces.h file
-        PREFIXES ${ENV_TUNDRA_DEP_PATH}/farsight2)
-    sagase_configure_report (FARSIGHT2)
-endmacro (configure_farsight2)
-
-macro (configure_dbus_glib)
-    sagase_configure_package (DBUS_GLIB
-        NAMES dbus-glib dbus
-        COMPONENTS dbus-glib
-        PREFIXES ${ENV_TUNDRA_DEP_PATH}/dbus-glib)
-    sagase_configure_report (DBUS_GLIB)
-endmacro (configure_dbus_glib)
-
 macro (configure_openal)
     sagase_configure_package(OPENAL
         NAMES OpenAL openal
@@ -433,10 +294,7 @@ macro (configure_openssl)
     # remove 'NOTFOUND' entry which makes to linking impossible    
     if (MSVC)
         list(REMOVE_ITEM OPENSSL_LIBRARIES debug optimized SSL_EAY_RELEASE-NOTFOUND LIB_EAY_RELEASE-NOTFOUND SSL_EAY_DEBUG-NOTFOUND LIB_EAY_DEBUG-NOTFOUND NOTFOUND)
-    message(----------)
-    message(${OPENSSL_LIBRARIES})
-    message(----------)
-    endif ()        
+    endif ()
     sagase_configure_report (OPENSSL)
 endmacro (configure_openssl)
 
@@ -450,7 +308,6 @@ macro (configure_protobuf)
           set (PROTOBUF_INCLUDE_DIRS ${ENV_TUNDRA_DEP_PATH}/protobuf/include)
     endif ()
     sagase_configure_report (PROTOBUF)
-    
 endmacro (configure_protobuf)
 
 macro (configure_celt)
@@ -521,6 +378,11 @@ macro(link_package_bullet)
 endmacro()
 
 macro(use_package_assimp)
+    # todo: fix this hardcoded thing to a more robust one
+    # make the include and lib consistent with normal projects 'assimp_release_Win32'
+    # magic subfolders are not good...
+    message("** Configuring ASSIMP")
+    message(STATUS "todo: fix use_package_assimp() from hardcoded one to more robust using sagase.")
     if (WIN32)
         if ("$ENV{ASSIMP_DIR}" STREQUAL "")
            set(ASSIMP_DIR ${ENV_TUNDRA_DEP_PATH}/assimp)
