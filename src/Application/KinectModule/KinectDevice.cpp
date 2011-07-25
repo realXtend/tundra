2b8d5791fe111cd6fd9028c34f1c65e4234a6934
Jonne Nauha <jonne.nauha@adminotech.com>
2011-07-25 04:19:48 +0300

    KinectModule: Make build and load in tundra2. No longer DeviceAPI IDevice but a dynamic object in framework, KinectDevice.h has detailed instructions how to get and use from c++/javascritp/python.

--- src/Application/KinectModule/KinectDevice.cpp
+++ src/Application/KinectModule/KinectDevice.cpp
@@ -3,37 +3,14 @@
 #include "StableHeaders.h"
 #include "KinectDevice.h"
 
-KinectDevice::KinectDevice(const QString &name) :
-    IDevice(name),
+KinectDevice::KinectDevice(QObject *parent) :
+    QObject(parent),
     isTrackingSkeletons_(false)
 {
 }
 
 KinectDevice::~KinectDevice()
 {
-}
-
-bool KinectDevice::InternalInitialize()
-{
-    /// For now nothing to initialize
-    return true;
-}
-
-bool KinectDevice::InternalStart()
-{
-    /// For now there is not start or stop for the kinect device
-    return true;
-}
-
-bool KinectDevice::InternalStop()
-{
-    /// For now there is not start or stop for the kinect device
-    return true;
-}
-
-void KinectDevice::InternalUpdate(float frametime)
-{
-    /// Nothing to poll for Kinect, there is a threading monitoring kinect events in KinectModule
 }
 
 void KinectDevice::DoVideoUpdate(const QImage &image)
--- src/Application/KinectModule/KinectDevice.h
+++ src/Application/KinectModule/KinectDevice.h
@@ -1,9 +1,7 @@
 // For conditions of distribution and use, see copyright notice in license.txt
 
-#ifndef incl_KinectModule_KinectDevice_h
-#define incl_KinectModule_KinectDevice_h
+#pragma once
 
-#include "IDevice.h"
 #include "KinectAPI.h"
 
 #include <QObject>
@@ -11,20 +9,49 @@
 #include <QImage>
 #include <QVariantMap>
 
-/*!
-    KinectDevice is the way to get data from a Microsoft Kinect device.
+/*! KinectDevice is the way to get data from a Microsoft Kinect device.
     The KinectModule reads all available data from the device and emits the Tundra/Qt style data with this device.
 
-    You can acquire the Kinect device via DevicesAPI with name "kinect" and connecting to the signals specified in this class:
+    You can acquire the Kinect device via Frameworks dynamic object with name "kinect" and connecting to the signals specified in this class:
     VideoUpdate, DepthUpdate, SkeletonUpdate and TrackingSkeleton.
 
     \code
-    IDevice *kinect = framework_->Devices()->GetDevice("kinect");
-    if (kinect)
-        connect(kinect, SIGNAL(SkeletonUpdate(const QVariantMap)), SLOT(HandleSkeletonData(const QVariantMap)));
+    C++:
+        // In C++ framework properties are a bit inconvenient, but available non the less.
+        QVariant kinectVariant = GetFramework()->property("kinect");
+        if (kinectVariant.isNull())
+        {
+            LogWarning("Kinect module not present, oops!");
+            return;
+        }
+        if (kinectVariant.canConvert<QObject*>())
+        {
+            QObject *kinectPtr = kinectVariant.value<QObject*>();
+            if (kinectPtr)
+            {
+                QObject::connect(kinectPtr, SIGNAL(VideoUpdate(const QImage)), this, SLOT(OnKinectVideo(const QImage)));
+                QObject::connect(kinectPtr, SIGNAL(SkeletonUpdate(const QVariantMap)), this, SLOT(OnKinectSkeleton(const QVariantMap)));
+                QObject::connect(kinectPtr, SIGNAL(TrackingSkeleton(bool)), this, SLOT(OnKinectTrackingSkeleton(bool)));
+            }
+        }
+
+    JavaScript:
+        // All framework dynamic objects are automatically visible in JavaScript.
+        if (kinect == null) {
+            print("Kinect module not present, oops!");
+            return;
+        }
+        var isTrackingRightNow = kinect.IsTrackingSkeletons();
+        kinect.VideoUpdate.connect(onKinectVideo);
+        kinect.SkeletonUpdate.connect(onKinectSkeleton);
+        kinect.TrackingSkeleton.connect(onKinectTrackingSkeleton);
+
+    Python:
+        The KinectDevice class needs to be registered to PythonQt so the QObject is knows and can be used.
+        This is not done yet so python access is not working yet.
     \endcode
  */
-class KINECT_VOIP_MODULE_API KinectDevice : public IDevice
+class KINECT_VOIP_MODULE_API KinectDevice : public QObject
 {
 
 Q_OBJECT
@@ -32,7 +59,7 @@ Q_OBJECT
 friend class KinectModule;
 
 public:
-    KinectDevice(const QString &name);
+    KinectDevice(QObject *parent);
     ~KinectDevice();
 
 public slots:
@@ -71,21 +98,7 @@ protected:
     /// This function gets called by KinectModule when its outside the Kinect processing thread, do not call this from 3rd party code!
     void SetSkeletonTracking(bool tracking);
 
-    /// IDevice override.
-    virtual bool InternalInitialize();
-
-    /// IDevice override.
-    virtual bool InternalStart();
-
-    /// IDevice override.
-    virtual bool InternalStop();
-
-    /// IDevice override.
-    void InternalUpdate(float frametime);
-
 private:
     bool isTrackingSkeletons_;
 
 };
-
-#endif
