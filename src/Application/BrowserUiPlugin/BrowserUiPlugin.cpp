f6f2bf03d4f0908cd3db7acf30243d7b1d2822c5
Jonne Nauha <jonne.nauha@evocativi.com>
2011-07-28 22:53:47 +0300

    New BrowserUiPlugin: Replaces the few browser related signals that were hackisly in UiAPI in tundra 1.x. Implements CookieJar that was before hackisly in AssetCache source files. This module has the data it copies to install directory under the sources, if you are developing and want to test this you need to manually copy 'jsmodules' to /bin from /src/Application/BrowserUiPlugin. Also you need to add the cpp and js plugins to a start config file.

---
+++ src/Application/BrowserUiPlugin/BrowserUiPlugin.cpp
@@ -0,0 +1,59 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
+#include "Framework.h"
+#include "CoreDefines.h"
+#include "LoggingFunctions.h"
+
+#include "BrowserUiPlugin.h"
+#include "CookieJar.h"
+
+#include "JavascriptModule.h"
+#include "ScriptMetaTypeDefines.h"
+#include <QScriptEngine>
+
+Q_DECLARE_METATYPE(CookieJar*);
+
+BrowserUiPlugin::BrowserUiPlugin(Framework *framework) :
+    QObject(0),
+    framework_(framework)
+{
+    JavascriptModule *jsModule = framework_->GetModule<JavascriptModule>();
+    if (jsModule)
+        connect(jsModule, SIGNAL(ScriptEngineCreated(QScriptEngine*)), SLOT(OnScriptEngineCreated(QScriptEngine*)));
+    else
+        LogWarning("BrowserUiPlugin: Could not get JavascriptModule to connect to the engine created signal!");
+}
+
+BrowserUiPlugin::~BrowserUiPlugin()
+{
+}
+
+void BrowserUiPlugin::OnScriptEngineCreated(QScriptEngine *engine)
+{
+    qScriptRegisterQObjectMetaType<CookieJar*>(engine);
+}
+
+void BrowserUiPlugin::AddAction(QAction *action, const QString &group)
+{
+    emit ActionAddRequest(action, group);
+}
+
+void BrowserUiPlugin::OpenUrl(const QUrl &url, bool activateNewTab)
+{
+    emit OpenUrlRequest(url, activateNewTab);
+}
+
+CookieJar *BrowserUiPlugin::CreateCookieJar(const QString &cookieDiskFile)
+{
+    return new CookieJar(this, cookieDiskFile);
+}
+
+extern "C"
+{
+    DLLEXPORT void TundraPluginMain(Framework *fw)
+    {
+        Framework::SetInstance(fw); // Inside this DLL, remember the pointer to the global framework object.
+        BrowserUiPlugin *plugin = new BrowserUiPlugin(fw);
+        fw->RegisterDynamicObject("browserplugin", plugin);
+    }
+}
---
+++ src/Application/BrowserUiPlugin/BrowserUiPlugin.h
@@ -0,0 +1,58 @@
+// For conditions of distribution and use, see copyright notice in license.txt
+
+#include "FrameworkFwd.h"
+
+#include <QObject>
+#include <QString>
+#include <QAction>
+#include <QUrl>
+
+class CookieJar;
+class QScriptEngine;
+
+class BrowserUiPlugin : public QObject
+{
+
+Q_OBJECT
+
+public:
+    /// Constructor.
+    BrowserUiPlugin(Framework *framework);
+
+    /// Deconstructor
+    ~BrowserUiPlugin();
+
+public slots:
+    /// Adds a action to the browser ui.
+    /// @param action Action to be added to the browser ui.
+    /// @param group Additional group name, optional.
+    void AddAction(QAction *action, const QString &group = QString());
+
+    /// Opens url in the browser ui.
+    /// @param url Url.
+    /// @param activateNewTab If the new tab where the url is opened should be activated or not. Optional, defaults to true.
+    void OpenUrl(const QUrl &url, bool activateNewTab = true);
+
+    /// Creates a new cookie jar that implements disk writing and reading. Can be used with any QNetworkAccessManager with setCookieJar() function.
+    /// \note AssetCache will be the CookieJars parent and it will destroyed by it, don't take ownerwhip of the returned CookieJar.
+    /// \param QString File path to the file the jar will read/write cookies to/from.
+    CookieJar *CreateCookieJar(const QString &cookieDiskFile);
+
+private slots:
+    void OnScriptEngineCreated(QScriptEngine *engine);
+
+signals:
+    /// Request to add a action. Connect to this if you are implementing a browser ui.
+    /// @param action Action that wants to be added to browser ui.
+    /// @param group Additional group name, can be empty a string.
+    void ActionAddRequest(QAction *action, QString group);
+
+    /// Request to open a url. Connect to this if you are implementing a browser ui.
+    /// @param url Url.
+    void OpenUrlRequest(const QUrl &url, bool activateNewTab);
+
+private:
+    /// Framework ptr.
+    Framework *framework_;
+
+};
