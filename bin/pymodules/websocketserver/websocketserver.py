e4b03079f460920c958a6113b2a78c52e94b66bd
therauli <therauli@playsus.(none)>
2011-08-03 12:03:17 +0300

    WebSocket: Cleanup, EC_Avatar and Chat added

--- bin/pymodules/websocketserver/websocketserver.py
+++ bin/pymodules/websocketserver/websocketserver.py
@@ -25,6 +25,9 @@ class NaaliWebsocketServer(circuits.BaseComponent):
         self.server = async_eventlet_wsgiserver.server(self.sock, handle_clients)
         print "websocket server started."
 
+        naali.server.connect("UserConnected(int, UserConnection*)", self.onUserConnected)
+        naali.server.connect("UserDisconnected(int, UserConnection*)", self.onUserDisconnected)
+
         NaaliWebsocketServer.instance = self
     
     def newclient(self, connectionid):
@@ -34,6 +37,22 @@ class NaaliWebsocketServer(circuits.BaseComponent):
         # Return the id of the connection
         return id
 
+    def onUserConnected(self, connid, user):
+        # If user is none we are probably dealing with websocket connection
+        if user is None:
+            self.note("New user connection from websocket: %d" % connid)
+        else:
+            self.note("New user connected: %s" % user.GetProperty("username"))
+
+    def onUserDisconnected(self, connid, user):
+        if user is None:
+            self.note("Used disconnected from websocket: %d" % connid)
+        else:
+            self.note("User %s disconnected." % user.GetProperty("username"))
+
+    def note(self, data):
+        print 'ws', data
+
     def removeclient(self, connectionid):
         naali.server.UserDisconnected(connectionid, 0)
 
@@ -42,13 +61,13 @@ class NaaliWebsocketServer(circuits.BaseComponent):
         '''Connects to various signal when scene is added'''
         self.scene = naali.getScene(name)
 
-        self.scene.connect("AttributeChanged(IComponent*, IAttribute*, AttributeChange::Type)", onAttributeChanged)
+        self.scene.connect("AttributeChanged(IComponent*, IAttribute*, AttributeChange::Type)", self.onAttributeChanged)
 
-        self.scene.connect("EntityCreated(Scene::Entity*, AttributeChange::Type)", onNewEntity)
+        self.scene.connect("EntityCreated(Scene::Entity*, AttributeChange::Type)", self.onNewEntity)
 
-        self.scene.connect("ComponentAdded(Scene::Entity*, IComponent*, AttributeChange::Type)", onComponentAdded)
+        self.scene.connect("ComponentAdded(Scene::Entity*, IComponent*, AttributeChange::Type)", self.onComponentAdded)
 
-        self.scene.connect("EntityRemoved(Scene::Entity*, AttributeChange::Type)", onEntityRemoved)
+        self.scene.connect("EntityRemoved(Scene::Entity*, AttributeChange::Type)", self.onEntityRemoved)
 
     @circuits.handler("update")
     def update(self, t):
@@ -60,57 +79,25 @@ class NaaliWebsocketServer(circuits.BaseComponent):
         # Need to figure something out what to do and how
         pass
 
-def sendAll(data):
-    for client in clients:
-        client.send(json.dumps(data))        
+    def onAttributeChanged(self, component, attribute, changeType):
+        #FIXME Only syncs hard coded ec_placeable
+        #Maybe get attribute or something
 
-def onAttributeChanged(component, attribute, changeType):
-    #FIXME Only syncs hard coded ec_placeable
-    #Maybe get attribute or something
-    
-    #FIXME Find a better way to get component name
-    component_name = str(component).split()[0]
+        #FIXME Find a better way to get component name
+        component_name = str(component).split()[0]
 
-    #Let's only sync EC_Placeable
-    if component_name != "EC_Placeable":
-        return
+        #Let's only sync EC_Placeable
+        if component_name != "EC_Placeable":
+            return
 
-    entity = component.GetParentEntity()
-    
-    # Don't sync local stuff
-    if entity.IsLocal():
-        return
+        entity = component.GetParentEntity()
 
-    ent_id = entity.id
+        # Don't sync local stuff
+        if entity.IsLocal():
+            return
 
-    data = component.GetAttributeQVariant('Transform')
-    transform = list()
+        ent_id = entity.id
 
-    transform.extend([data.position().x(), data.position().y(), data.position().z()])
-    transform.extend([data.rotation().x(), data.rotation().y(), data.rotation().z()])
-    transform.extend([data.scale().x(), data.scale().y(), data.scale().z()])
-
-    sendAll(['setAttr', {'id': ent_id, 
-                         'component': component_name,
-                         'Transform': transform}])
-
-def onNewEntity(entity, changeType):
-    sendAll(['addEntity', {'id': entity.id}])
-    print entity
-
-def onComponentAdded(entity, component, changeType):
-    #FIXME Find a better way to get component name
-    component_name = str(component).split()[0]
-
-    # Just sync EC_Placeable and EC_Mesh since they are currently the
-    # only ones that are used in the client
-    if component_name not in ["EC_Placeable", "EC_Mesh"]:
-        return
-
-    if component_name == "EC_Mesh":
-
-        sendAll(['addComponent', {'id': entity.id, 'component': component_name, 'url': 'ankka.dae'}])
-    else:
         data = component.GetAttributeQVariant('Transform')
         transform = list()
 
@@ -118,19 +105,62 @@ def onComponentAdded(entity, component, changeType):
         transform.extend([data.rotation().x(), data.rotation().y(), data.rotation().z()])
         transform.extend([data.scale().x(), data.scale().y(), data.scale().z()])
 
-        sendAll(['addComponent', {'id': entity.id, 
+        sendAll(['setAttr', {'id': ent_id, 
                              'component': component_name,
                              'Transform': transform}])
 
-    print entity.id, component
+    def onNewEntity(self, entity, changeType):
+        sendAll(['addEntity', {'id': entity.id}])
 
-def onEntityRemoved(entity, changeType):
-    print "Removing", entity
-    sendAll(['removeEntity', {'id': entity.id}])
+        if entity.name == "ChatApplication":
+            self.chatapp = entity
+            print "WebSocket: Found ChatApp!"
+            self.init_chatapp_handlers()
+
+
+    def onComponentAdded(self, entity, component, changeType):
+        #FIXME Find a better way to get component name
+        component_name = str(component).split()[0]
+
+        # Just sync EC_Placeable and EC_Avatar since they are currently the
+        # only ones that are used in the client
+        if component_name not in ["EC_Placeable", "EC_Avatar"]:
+            return
+        #FIXME
+        if component_name == "EC_Avatar":
+            sendAll(['addComponent', {'id': entity.id, 'component': "EC_Avatar"}])
+        elif component_name == "EC_Mesh":
+            sendAll(['addComponent', {'id': entity.id, 'component': "EC_Mesh", 'url': 'FIXME'}])
+        else:
+            data = component.GetAttributeQVariant('Transform')
+            transform = list()
+
+            transform.extend([data.position().x(), data.position().y(), data.position().z()])
+            transform.extend([data.rotation().x(), data.rotation().y(), data.rotation().z()])
+            transform.extend([data.scale().x(), data.scale().y(), data.scale().z()])
+
+            sendAll(['addComponent', {'id': entity.id, 
+                                 'component': component_name,
+                                 'Transform': transform}])
+
+
+    def onEntityRemoved(self, entity, changeType):
+        sendAll(['removeEntity', {'id': entity.id}])
+
+    def init_chatapp_handlers(self):
+        csm = self.chatapp.Action("ClientSendMessage")
+        csm.connect("Triggered(QString, QString, QString, QStringList)", self.onClientSendMessage)
+
+    def onClientSendMessage(self, sender, msg):
+        sendAll(['chatMessage', {'sender': sender, 'msg': msg}])
+
+
+def sendAll(data):
+    for client in clients:
+        client.send(json.dumps(data))        
 
 @websocket.WebSocketWSGI
 def handle_clients(ws):
-    print 'START', ws
     clients.add(ws)
     
     # Don't do this! Figure out a way to fake a kNet connection or
@@ -150,8 +180,6 @@ def handle_clients(ws):
             #if there is an error we simply quit by exiting the
             #handler. Eventlet should close the socket etc.
             break
-
-        print msg
 
         if msg is None:
             # if there is no message the client will Quit
@@ -182,6 +210,15 @@ def handle_clients(ws):
             av = scene.GetEntityByNameRaw("Avatar%s" % connections[id])
 
             av.Exec(1, action, args)
+
+        elif function == 'chatMessage':
+            sender = params.get('sender')
+            message = params.get('msg')
+            chatApp = scene.GetEntityByNameRaw("ChatApplication")
+            if chatApp is None:
+                continue
+            chatApp.Exec(2, "ClientSendMessage", sender, message)
+            
                 
         elif function == 'reboot':
             break
@@ -190,4 +227,3 @@ def handle_clients(ws):
     NaaliWebsocketServer.instance.removeclient(connectionid)
             
     clients.remove(ws)
-    print 'END', ws
