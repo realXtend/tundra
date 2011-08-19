b1b7adc00ff049010d9ddcbabfaa91b97994e5e4
Stinkfist <stinkfist0@gmail.com>
2011-08-19 12:32:52 +0300

    Replace usage of boost::filesystem::path with QFileInfo.

--- TundraProtocolModule/TundraLogicModule.cpp
+++ TundraProtocolModule/TundraLogicModule.cpp
@@ -479,13 +479,11 @@ void TundraLogicModule::ImportScene(QString filename, bool clearScene, bool repl
         LogError("Empty filename given!");
         return;
     }
-    
-    ///\todo Lacks unicode support. -jj.
-    boost::filesystem::path path(filename.toStdString());
-    std::string dirname = path.branch_path().string();
-    
+
+    QString path = QDir::toNativeSeparators(QFileInfo(filename).dir().path());
+
     SceneImporter importer(scene);
-    QList<Entity *> entities = importer.Import(filename.toStdString(), dirname, Transform(),
+    QList<Entity *> entities = importer.Import(filename.toStdString(), path.toStdString(), Transform(),
         "local://", AttributeChange::Default, clearScene, replace);
 
     LogInfo("Imported " + QString::number(entities.size()) + " entities.");
@@ -506,12 +504,10 @@ void TundraLogicModule::ImportMesh(QString filename, float tx, float ty, float t
         return;
     }
 
-    ///\todo Lacks unicode support. -jj.
-    boost::filesystem::path path(filename.toStdString());
-    std::string dirname = path.branch_path().string();
-    
+    QString path = QDir::toNativeSeparators(QFileInfo(filename).dir().path());
+
     SceneImporter importer(scene);
-    EntityPtr entity = importer.ImportMesh(filename.toStdString(), dirname, Transform(float3(tx,ty,tz),
+    EntityPtr entity = importer.ImportMesh(filename.toStdString(), path.toStdString(), Transform(float3(tx,ty,tz),
         float3(rx,ry,rz), float3(sx,sy,sz)), std::string(), "local://", AttributeChange::Default, inspect);
 }
 
