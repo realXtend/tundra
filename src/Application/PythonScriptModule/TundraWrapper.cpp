54331263cc97993e1eae045495f0f27ec162f56b
antont <toni@playsign>
2011-08-25 02:28:52 +0300

    fix intendation from Ilikia merge

--- src/Application/PythonScriptModule/TundraWrapper.cpp
+++ src/Application/PythonScriptModule/TundraWrapper.cpp
@@ -66,4 +66,95 @@ namespace PythonScript
     {
         obj->type = assetType;
     }
+  
+    float TundraDecorator::x(float3* self)
+    {
+        return self->x;
+    }
+    
+    float TundraDecorator::y(float3* self)
+    {
+        return self->y;
+    }
+    
+    float TundraDecorator::z(float3* self)
+    {
+        return self->z;
+    }
+
+    //registering float3 meta type
+    int id = qRegisterMetaType<float3>("float3");
+
+    void TundraDecorator::setx(float3* self, float value)
+    {
+        self->x = value;
+    }
+    
+    void TundraDecorator::sety(float3* self, float value)
+    {
+        self->y = value;
+    }
+    
+    void TundraDecorator::setz(float3* self, float value)
+    {
+        self->z = value;
+    }
+
+    float3 TundraDecorator::neg(float3* self)
+    {   
+        return self->Neg();      
+    }
+
+    float3 TundraDecorator::position(const Transform* self)
+    {
+        return self->pos;
+    }
+
+    float3 TundraDecorator::rotation(const Transform* self)
+    {
+        return self->rot;
+    }
+
+    float3 TundraDecorator::scale(const Transform* self)
+    {
+        return self->scale;
+    }
+	
+    float3 TundraDecorator::QuatToEulerZYX(Quat* self) const
+    {
+        return self->ToEulerZYX();
+    }
+	
+    void TundraDecorator::set(float3* self, float x, float y, float z)
+    {
+        self->Set(x, y, z);
+    }
+  
+    void TundraDecorator::SetPos(Transform* self, float x, float y, float z) 
+    {
+        self->SetPos(x, y, z);
+    }
+
+    void TundraDecorator::SetRot(Transform* self, float x, float y, float z) 
+    {
+        self->SetRotation(x, y, z);
+    }
+
+    void TundraDecorator::SetScale(Transform* self, float x, float y, float z) 
+    {
+        self->SetScale(x, y, z);
+    }
+
+    //Quats
+    int q_id = qRegisterMetaType<Quat>("Quat");
+
+    Quat TundraDecorator::FromEulerZYX(Quat* self, float z, float y, float x) const
+    {
+        return self->FromEulerZYX(z, y ,x);
+    }
+    
+    void TundraDecorator::SetFromAxisAngle(Quat* self, const float3 &rotationAxis, const float rotationAngleRadians)
+    {
+        return self->SetFromAxisAngle(rotationAxis, rotationAngleRadians);
+    }
 }
--- src/Application/PythonScriptModule/TundraWrapper.h
+++ src/Application/PythonScriptModule/TundraWrapper.h
@@ -49,5 +49,36 @@ namespace PythonScript
         void setRef(AssetReference *obj, const QString &assetRef);
         QString type(const AssetReference *obj);
         void setType(AssetReference *obj, const QString &assetType);
-    };
+		
+        //getters for float3 x, y and z coordinates
+        float x(float3* self);
+        float y(float3* self);
+        float z(float3* self);
+
+        //setters for float3 x, y and z coordinates
+        void setx(float3* self, float value);
+        void sety(float3* self, float value);
+        void setz(float3* self, float value);
+
+        //procedure to negate the float3 object
+        float3 neg(float3* self);
+
+        //getters for Transform pos,rot and scale float3 objects
+        float3 position(const Transform* self);
+        float3 rotation(const Transform* self);
+        float3 scale(const Transform* self);
+
+        //setters for Transform objects
+        void SetPos(Transform* self, float x, float y, float z);
+        void SetRot(Transform* self, float x, float y, float z);
+        void SetScale(Transform* self, float x, float y, float z);
+
+        float3 QuatToEulerZYX(Quat* self) const;
+
+        //a setter for float3 objects: cannot assign back to function call
+        void set(float3* self, float x, float y, float z);
+        //setters for Quat
+        void SetFromAxisAngle(Quat* self, const float3 &rotationAxis, const float rotationAngleRadians);
+        Quat FromEulerZYX(Quat* self, float z, float y, float x) const;
+     };
 }
