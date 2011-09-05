c3aeae9d16de74e56730711979c6ad67e18f5482
Jukka Jylänki <jukka.jylanki@ludocraft.com>
2011-09-05 17:55:43 +0300

    Added missing file.

---
+++ src/Core/Framework/QScriptEngineHelpers.h
@@ -0,0 +1,26 @@
+#include <QScriptEngine>
+#include <QMetaType>
+
+// The following functions help register a custom QObject-derived class to a QScriptEngine.
+// See http://lists.trolltech.com/qt-interest/2007-12/thread00158-0.html .
+template <typename Tp>
+QScriptValue qScriptValueFromQObject(QScriptEngine *engine, Tp const &qobject)
+{
+    return engine->newQObject(qobject);
+}
+
+template <typename Tp>
+void qScriptValueToQObject(const QScriptValue &value, Tp &qobject)
+{   
+    qobject = qobject_cast<Tp>(value.toQObject());
+}
+
+template <typename Tp>
+int qScriptRegisterQObjectMetaType(QScriptEngine *engine, const QScriptValue &prototype = QScriptValue()
+#ifndef qdoc
+    , Tp * = 0
+#endif
+    )
+{
+    return qScriptRegisterMetaType<Tp>(engine, qScriptValueFromQObject, qScriptValueToQObject, prototype);
+}
