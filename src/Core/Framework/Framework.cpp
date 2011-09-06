85bf1ab4b5ddd1433d7eee39930fd7b13c0f821f
Stinkfist <stinkfist0@gmail.com>
2011-09-06 14:42:45 +0300

    Add command line parameter --connect help description.

--- src/Core/Framework/Framework.cpp
+++ src/Core/Framework/Framework.cpp
@@ -133,6 +133,7 @@ Framework::Framework(int argc, char** argv) :
     cmdLineDescs.commands["--file"] = "Load scene on startup. Accepts absolute and relative paths, local:// and http:// are accepted and fetched via the AssetAPI."; // TundraLogicModule & AssetModule
     cmdLineDescs.commands["--storage"] = "Adds the given directory as a local storage directory on startup"; // AssetModule
     cmdLineDescs.commands["--config"] = "Specifies the startup configration file to use"; // Framework
+    cmdLineDescs.commands["--connect"] = "Connects to a Tundra server automatically. Syntax: '--connect serverIp;port;protocol;name;password'. Password is optional.";
     cmdLineDescs.commands["--login"] = "Automatically login to server using provided data. Url syntax: {tundra|http|https}://host[:port]/?username=x[&password=y&avatarurl=z&protocol={udp|tcp}]. Minimum information needed to try a connection in the url are host and username";
     cmdLineDescs.commands["--netrate"] = "Specifies the number of network updates per second. Default: 30."; // TundraLogicModule
     cmdLineDescs.commands["--clear-asset-cache"] = "At the start of Tundra, remove all data and metadata files from asset cache.";
