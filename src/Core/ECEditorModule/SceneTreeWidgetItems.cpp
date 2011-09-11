2c97ffafaed54a563d6b481f621f974eb9f992cb
antont <toni@playsign>
2011-09-12 01:59:19 +0300

    build fix: remove the wrongly typed return variable for ParseAssetRef in SceneTreeWidgetItems.cpp. does msvc let these pass, or is lc branch in not-compiling state for msvc too?

--- src/Core/ECEditorModule/SceneTreeWidgetItems.cpp
+++ src/Core/ECEditorModule/SceneTreeWidgetItems.cpp
@@ -220,7 +220,7 @@ AssetItem::AssetItem(const AssetPtr &asset, QTreeWidgetItem *parent) :
     assetPtr(asset)
 {
     QString outPathFileName;
-    AssetAPI::AssetRefType refType = AssetAPI::ParseAssetRef(asset->Name(), 0, 0, 0, 0, &outPathFileName);
+    AssetAPI::ParseAssetRef(asset->Name(), 0, 0, 0, 0, &outPathFileName);
     setText(0, outPathFileName);
     MarkUnloaded(!asset->IsLoaded());
 }
