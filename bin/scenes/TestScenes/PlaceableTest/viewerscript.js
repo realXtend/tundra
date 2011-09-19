ab12f46a36640d8c3fc0f586a866904980882b1f
Mikko Lehtosaari <Lehtiz@gmail.com>
2011-09-19 13:23:42 +0300

    added Placeabletest

---
+++ bin/scenes/TestScenes/PlaceableTest/viewerscript.js
@@ -0,0 +1,18 @@
+// http://www.realxtend.org/doxygen/_tundra_script_reference.html
+
+var randomnumber=Math.floor(Math.random()*100);
+
+function AutoConnect(){
+    var isserver = server.IsRunning() || server.IsAboutToStart();
+    if(isserver==false){
+        client.Login("localhost",2345,"Guest"+randomnumber.toString()," ","udp");
+        //engine.IncludeFile("/scenes/PlaceableTest/createmovekilltest.js");
+        engine.IncludeFile("./../../../rextest2/PlaceableTest/createmovekilltest.js");
+    }
+    if(isserver==true){
+        print("server");
+        print("This script should be run as a parameter for the client.");
+    }
+}
+
+frame.DelayedExecute(1).Triggered.connect(this,AutoConnect);
