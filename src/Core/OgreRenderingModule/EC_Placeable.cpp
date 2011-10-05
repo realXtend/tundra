f21cb9848daf59195acce29972ae61febf79aa91
antont <toni@playsign>
2011-10-05 17:30:43 +0300

    the notorious placeable.LookAt ported from tundra1. to be considered - idea has been to have in the math lib, but is not implemented there now, so tested whether the old way of using ogre works. well, now we can test. seemed to work in first test for an obj to track cam.

--- src/Core/OgreRenderingModule/EC_Placeable.cpp
+++ src/Core/OgreRenderingModule/EC_Placeable.cpp
@@ -778,6 +778,16 @@ void EC_Placeable::SetScale(const float3 &scale)
     transform.Set(newtrans, AttributeChange::Default);
 }
 
+void EC_Placeable::LookAt(const float3 &v)
+{
+    // Don't rely on the stability of the lookat (since it uses previous orientation), 
+    // so start in identity transform
+    sceneNode_->setOrientation(Ogre::Quaternion::IDENTITY);
+    sceneNode_->lookAt(Ogre::Vector3(v.x, v.y, v.z), Ogre::Node::TS_WORLD);
+    const Ogre::Quaternion& ogrequat = sceneNode_->getOrientation();
+    SetOrientation(Quat(ogrequat.x, ogrequat.y, ogrequat.z, ogrequat.w));
+}
+
 void EC_Placeable::SetOrientationAndScale(const float3x3 &tm)
 {
     assume(tm.IsOrthogonal());
