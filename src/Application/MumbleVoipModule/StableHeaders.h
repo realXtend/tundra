06f985081b20776fe14eb961b1a16e2f1db16704
Jonne Nauha <jonne.nauha@evocativi.com>
2011-12-20 09:42:38 +0200

    Mumble: Fix some warnings from redefine and linker.

--- src/Application/MumbleVoipModule/StableHeaders.h
+++ src/Application/MumbleVoipModule/StableHeaders.h
@@ -7,6 +7,9 @@
 // If we don't do this, we get "boost/asio/detail/socket_types.hpp(27) : fatal error C1189: #error :
 // WinSock.h has already been included" for many files (User.cpp, Connection.cpp, etc.).
 #ifdef WIN32
+#if defined(_WINSOCKAPI_)
+#undef _WINSOCKAPI_
+#endif
 #include <Winsock2.h>
 #endif
 
