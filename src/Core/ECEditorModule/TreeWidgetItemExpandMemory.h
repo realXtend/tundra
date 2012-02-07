72763d4ad34ffcf06ac3fa8f4601a41a8aa7f326
Stinkfist <stinkfist0@gmail.com>
2012-02-07 17:08:50 +0200

    TreeWidgetItemExpandMemory: as multiple instances of this class can exist, use the groupName as identifier when storing and reading the state to and from config.

--- src/Core/ECEditorModule/TreeWidgetItemExpandMemory.h
+++ src/Core/ECEditorModule/TreeWidgetItemExpandMemory.h
@@ -1,11 +1,12 @@
 /**
- *  For conditions of distribution and use, see copyright notice in LICENSE
- *
- *  @file   TreeWidgetItemExpandMemory.h
- *  @brief  Utility class for keeping track of expanded items at a tree widget.
- */
+    For conditions of distribution and use, see copyright notice in LICENSE
+
+    @file   TreeWidgetItemExpandMemory.h
+    @brief  Utility class for keeping track of expanded items at a tree widget. */
 
 #pragma once
+
+#include "ECEditorModuleApi.h"
 
 #include <QObject>
 #include <QSet>
@@ -24,38 +25,34 @@ class QTreeWidgetItem;
             mem, SLOT(HandleItemExpanded(QTreeWidgetItem *)), Qt::UniqueConnection);
         connect(treeWidget, SIGNAL(itemCollapsed(QTreeWidgetItem *)),
             mem, SLOT(HandleItemCollapsed(QTreeWidgetItem *)), Qt::UniqueConnection);
-    @endcode
-*/
-class TreeWidgetItemExpandMemory : public QObject
+    @endcode */
+class ECEDITOR_MODULE_API TreeWidgetItemExpandMemory : public QObject
 {
     Q_OBJECT
 
 public:
     /// Constructs the object and loads information about expanded items from config file.
     /** @param group Group name identifier.
-        @param fw Framework.
-    */
+        @param fw Framework. */
     TreeWidgetItemExpandMemory(const char *group, Framework *fw);
 
     /// Destroys the object and saves information about currently expanded items to config file.
     ~TreeWidgetItemExpandMemory();
 
-    /// Expands @c item in the @c treeWidget if expand memory contains idenfitier for the @c item or
-    /// collapses the item if it's identifier is not found in the item info set.
+    /// Expands or collapses @c item in @c treeWidget depending if its identifier is found (expand) or not (collpse) from the expand memory.
     /** @param treeWidet Tree widget containting the @c item.
-        @param item Tree widget item.
-    */
+        @param item Tree widget item. */
     void ExpandItem(QTreeWidget *treeWidget, const QTreeWidgetItem *item) const;
 
     /// Return identifier for the @c item.
     /** The text is full "path" of item and all its predecessors, separated with dots,
         e.g. "TopLevelItemName.SecondLevelItemName.ItemName".
         @note Currently uses text from item column 0 only.
-        @param item Tree widget item.
-    */
-    QString GetIndentifierText(const QTreeWidgetItem *item) const;
+        @param item Tree widget item. */
+    QString IndentifierForItem(const QTreeWidgetItem *item) const;
 
     /// Loads information about expanded items from config file.
+    /** Clears possible already existing state information. */
     void Load();
 
     /// Saves information about currently expanded items to config file.
@@ -63,20 +60,18 @@ public:
 
 public slots:
     /// Creates identifier text for @c item and adds it to set of indentifier texts.
-    /** @param item Expanded tree widget item.
-    */
+    /** @param item Expanded tree widget item. */
     void HandleItemExpanded(QTreeWidgetItem *item);
 
     /// Creates identifier text for @c item and removes it from set of indentifier texts.
-    /** @param item Collapsed tree widget item.
-    */
+    /** @param item Collapsed tree widget item. */
     void HandleItemCollapsed(QTreeWidgetItem *item);
 
 private:
     /// Returns information about expanded items as one string suitable for saving to config file.
-    std::string ToString() const;
+    QString ToString() const;
 
     QSet<QString> items; ///< Set of item identifier texts.
     Framework *framework; ///< Framework.
-    std::string groupName; ///< Setting group name.
+    QString groupName; ///< Setting group name.
 };
