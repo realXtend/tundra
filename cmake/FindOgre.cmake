5db1ec00eecfeef8536d60cec330a4ec0e8bb06c
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2012-02-17 13:39:59 +0200

    Do not use sagase on Mac OS X to find Ogre.

--- cmake/FindOgre.cmake
+++ cmake/FindOgre.cmake
@@ -3,7 +3,7 @@
 # UNIX/MAC: Uses sagase/find_library in linux as it works there mighty well when its installed on the system.
 # WINDOWS: Uses some more custom logic on windows to find things correct either from Tunda deps, Ogre SDK or Ogre source repo clone.
 
-if (NOT WIN32)
+if (NOT WIN32 AND NOT APPLE)
 # TODO Remove the use of Sagase for Linux and Mac Ogre lookup.
 # TODO: Remove configure_ogre and replace it with a use_package_ogre() and link_package_ogre()
 macro(configure_ogre)
@@ -45,7 +45,12 @@ macro(configure_ogre)
     endif()
 
     if ("${OGRE_DIR}" STREQUAL "")
-        set(OGRE_DIR ${ENV_TUNDRA_DEP_PATH}/Ogre)
+        if (IS_DIRECTORY ${ENV_TUNDRA_DEP_PATH}/Ogre)
+            set(OGRE_DIR ${ENV_TUNDRA_DEP_PATH}/Ogre)
+        elseif (APPLE)
+            find_library(OGRE_LIBRARY Ogre)
+            set(OGRE_DIR ${OGRE_LIBRARY})
+        endif()
     endif()
 
     # The desired Ogre path is set in OGRE_DIR. The Ogre source tree comes in two flavors:
@@ -54,17 +59,25 @@ macro(configure_ogre)
     # We want to support both so that one can do active development on the Ogre Hg repository, without
     # having to always do the intermediate SDK installation/deployment step.
     
-    if (IS_DIRECTORY ${OGRE_DIR}/OgreMain) # Ogre path points to #1 above.    
+    if (APPLE AND IS_DIRECTORY ${OGRE_DIR}/Headers)
+        include_directories(${OGRE_DIR}/Headers)
+        link_directories(${OGRE_DIR})
+        
+    elseif (IS_DIRECTORY ${OGRE_DIR}/OgreMain) # Ogre path points to #1 above.    
         include_directories(${OGRE_DIR}/include)
         include_directories(${OGRE_DIR}/OgreMain/include)
-        include_directories(${OGRE_DIR}/RenderSystems/Direct3D9/include)
+	if (WIN32)
+       	    include_directories(${OGRE_DIR}/RenderSystems/Direct3D9/include)
+        endif()
         link_directories(${OGRE_DIR}/lib)
         message(STATUS "Using Ogre from Mercurial trunk directory " ${OGRE_DIR})
     elseif (IS_DIRECTORY ${OGRE_DIR}/include/OGRE) # If include/OGRE exists, then OGRE_DIR points to the SDK (#2 above)    
         include_directories(${OGRE_DIR}/include/OGRE)
-        include_directories(${OGRE_DIR}/include/OGRE/RenderSystems/Direct3D9)        
         link_directories(${OGRE_DIR}/lib)
-        link_directories(${OGRE_DIR}/lib/$(OutDir)/opt)
+        if (WIN32)
+            include_directories(${OGRE_DIR}/include/OGRE/RenderSystems/Direct3D9)        
+            link_directories(${OGRE_DIR}/lib/$(OutDir)/opt)
+        endif()
         message(STATUS "Using Ogre from SDK directory " ${OGRE_DIR})
     else()
         message(FATAL_ERROR "When looking for Ogre, the path ${OGRE_DIR} does not point to a valid Ogre directory!")
@@ -74,11 +87,13 @@ endmacro()
 endif()
 
 macro(link_ogre)
-    if (NOT WIN32)
+    if (WIN32)
+        target_link_libraries(${TARGET_NAME} debug OgreMain_d debug RenderSystem_Direct3D9_d)
+        target_link_libraries(${TARGET_NAME} optimized OgreMain optimized RenderSystem_Direct3D9)
+    elseif (APPLE)
+        target_link_libraries(${TARGET_NAME} ${OGRE_LIBRARY})
+    else()
         use_package(OGRE)
         link_package(OGRE)
-    else()
-        target_link_libraries(${TARGET_NAME} debug OgreMain_d debug RenderSystem_Direct3D9_d)
-        target_link_libraries(${TARGET_NAME} optimized OgreMain optimized RenderSystem_Direct3D9)        
     endif()
 endmacro()
