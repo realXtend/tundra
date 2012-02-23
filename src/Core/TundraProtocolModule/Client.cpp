4659f4d4e24861e59c84a3329f37e50212733eb1
Jukka Jylänki <jukka.jylanki@ludocraft.com>
2012-02-23 12:03:00 +0200

    Use SocketOverUDP as default when connecting using --login.

--- src/Core/TundraProtocolModule/Client.cpp
+++ src/Core/TundraProtocolModule/Client.cpp
@@ -106,13 +106,16 @@ void Client::Login(const QString& address, unsigned short port, const QString& u
     SetLoginProperty("password", password);
 
     QString p = protocol.trimmed().toLower();
-    kNet::SocketTransportLayer transportLayer = kNet::InvalidTransportLayer;
+    kNet::SocketTransportLayer transportLayer = kNet::SocketOverUDP;
     if (p == "tcp")
         transportLayer = kNet::SocketOverTCP;
     else if (p == "udp")
         transportLayer = kNet::SocketOverUDP;
     else
-        ::LogInfo("Client::Login: Unrecognized protocol: " + p);
+    {
+        ::LogError("Client::Login: Unrecognized protocol: " + p);
+        return;
+    }
     Login(address, port, transportLayer);
 }
 
