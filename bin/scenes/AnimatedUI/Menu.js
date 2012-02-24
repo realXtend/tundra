cae5ce480565a472e669d3d2964bc1d410cf810a
Jukka Jylänki <jukka.jylanki@ludocraft.com>
2012-02-24 15:20:43 +0200

    Suppress bogus errors if running AnimatedUI in headless server.

--- bin/scenes/AnimatedUI/Menu.js
+++ bin/scenes/AnimatedUI/Menu.js
@@ -1,64 +1,67 @@
 // !ref: Menu.ui
 // !ref: image.png
 
-engine.ImportExtension("qt.core");
-engine.ImportExtension("qt.gui");
-
-var menu = asset.GetAsset("Menu.ui").Instantiate(true, 0);
-menu.move(50, 50);
-menu.show();
-
-findChild(menu, "noanim").pressed.connect(NoAnimPressed);
-function NoAnimPressed()
+if (!framework.IsHeadless())
 {
-   scene.GetEntityByName("Animation").script.scriptRef = new AssetReference("");
-}
+    engine.ImportExtension("qt.core");
+    engine.ImportExtension("qt.gui");
 
-findChild(menu, "anim1").pressed.connect(Anim1Pressed);
-function Anim1Pressed()
-{
-   scene.GetEntityByName("Animation").script.scriptRef = new AssetReference("anim1.js");
-}
+    var menu = asset.GetAsset("Menu.ui").Instantiate(true, 0);
+    menu.move(50, 50);
+    menu.show();
 
-findChild(menu, "anim2").pressed.connect(Anim2Pressed);
-function Anim2Pressed()
-{
-   scene.GetEntityByName("Animation").script.scriptRef = new AssetReference("anim2.js");
-}
+    findChild(menu, "noanim").pressed.connect(NoAnimPressed);
+    function NoAnimPressed()
+    {
+       scene.GetEntityByName("Animation").script.scriptRef = new AssetReference("");
+    }
 
-findChild(menu, "anim3").pressed.connect(Anim3Pressed);
-function Anim3Pressed()
-{
-   scene.GetEntityByName("Animation").script.scriptRef = new AssetReference("anim3.js");
-}
+    findChild(menu, "anim1").pressed.connect(Anim1Pressed);
+    function Anim1Pressed()
+    {
+       scene.GetEntityByName("Animation").script.scriptRef = new AssetReference("anim1.js");
+    }
 
-findChild(menu, "anim4").pressed.connect(Anim4Pressed);
-function Anim4Pressed()
-{
-   scene.GetEntityByName("Animation").script.scriptRef = new AssetReference("anim4.js");
-}
+    findChild(menu, "anim2").pressed.connect(Anim2Pressed);
+    function Anim2Pressed()
+    {
+       scene.GetEntityByName("Animation").script.scriptRef = new AssetReference("anim2.js");
+    }
 
-function OnScriptDestroyed()
-{
-  menu.deleteLater();
-  
-//  renderer.DeleteUiPlane("MyPlane");
-}
+    findChild(menu, "anim3").pressed.connect(Anim3Pressed);
+    function Anim3Pressed()
+    {
+       scene.GetEntityByName("Animation").script.scriptRef = new AssetReference("anim3.js");
+    }
 
-frame.Updated.connect(OnFrameUpdate);
-function OnFrameUpdate()
-{
-   menu.raise();
-   var proxy = menu.graphicsProxyWidget();
-//   proxy.setZValue(1000);
-}
+    findChild(menu, "anim4").pressed.connect(Anim4Pressed);
+    function Anim4Pressed()
+    {
+       scene.GetEntityByName("Animation").script.scriptRef = new AssetReference("anim4.js");
+    }
 
-var uiplane = renderer.CreateUiPlane("MyPlane");
-uiplane.SetTexture(asset.GetAsset("image.png"));
-uiplane.Show();
-uiplane.SetAlpha(1);
-uiplane.SetZOrder(250);
-uiplane.SetX(0, true, 0);
-uiplane.SetY(0, true, 0);
-uiplane.SetWidth(288, true);
-uiplane.SetHeight(288, true);
+    function OnScriptDestroyed()
+    {
+      menu.deleteLater();
+      
+    //  renderer.DeleteUiPlane("MyPlane");
+    }
+
+    frame.Updated.connect(OnFrameUpdate);
+    function OnFrameUpdate()
+    {
+       menu.raise();
+       var proxy = menu.graphicsProxyWidget();
+    //   proxy.setZValue(1000);
+    }
+
+    var uiplane = renderer.CreateUiPlane("MyPlane");
+    uiplane.SetTexture(asset.GetAsset("image.png"));
+    uiplane.Show();
+    uiplane.SetAlpha(1);
+    uiplane.SetZOrder(250);
+    uiplane.SetX(0, true, 0);
+    uiplane.SetY(0, true, 0);
+    uiplane.SetWidth(288, true);
+    uiplane.SetHeight(288, true);
+}
