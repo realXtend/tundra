8fac4d7a630c818117e07ec0cad499635d9dff02
Jukka Jylänki <jukka.jylanki@ludocraft.com>
2012-02-28 20:50:35 +0200

    Updated version number to 2.3.0.

--- src/Core/Framework/Application.cpp
+++ src/Core/Framework/Application.cpp
@@ -61,7 +61,7 @@
 /// @note Modify these values when you are making a custom Tundra build. Also the version needs to be changed here on releases.
 const char *Application::organizationName = "realXtend";
 const char *Application::applicationName = "Tundra";
-const char *Application::version = "2.2.0";
+const char *Application::version = "2.3.0";
 
 Application::Application(Framework *owner, int &argc, char **argv) :
     QApplication(argc, argv),
