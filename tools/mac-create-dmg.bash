6b2159b9ae7732839721d3a64915257c103e63cc
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2012-03-20 18:41:20 +0200

    Adjust Mac installer icons, and work around OSX installer hidden file display bug.

--- tools/mac-create-dmg.bash
+++ tools/mac-create-dmg.bash
@@ -32,14 +32,21 @@ open
 set current view of container window to icon view
 set toolbar visible of container window to false
 set statusbar visible of container window to false
-set the bounds of container window to {400, 100, 885, 430}
+set the bounds of container window to {400, 100, 995, 450}
 set theViewOptions to the icon view options of container window
 set arrangement of theViewOptions to not arranged
 set icon size of theViewOptions to 72
 set background picture of theViewOptions to file ".background:'${backgroundPictureName}'"
 make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
-set position of item "'${applicationName}'" of container window to {100, 100}
-set position of item "Applications" of container window to {375, 100}
+set position of item "'${applicationName}'" of container window to {124, 190}
+set position of item "Applications" of container window to {480, 190}
+
+set position of item ".background" of container window to {10000, 0}
+set position of item ".DS_Store" of container window to {10000, 0}
+set position of item ".fseventsd" of container window to {10000, 0}
+set position of item ".Trashes" of container window to {10000, 0}
+close
+open
 update without registering applications
 delay 5
 end tell
