ede98d7b3f7858c02d0f8b1da57e7415e095cf55
Stinkfist <stinkfist0@gmail.com>
2012-03-21 17:17:14 +0200

    Append my QtXml-QtScript code snippet done in the past to the existing test/example code.

--- bin/jsmodules/apitest/qtxml.js
+++ bin/jsmodules/apitest/qtxml.js
@@ -1,5 +1,70 @@
+// For conditions of distribution and use, see copyright notice in LICENSE
+
 engine.ImportExtension("qt.core");
 engine.ImportExtension("qt.xml");
+
+QtXmlTest();
+
+// Loads the plugin.xml from Tundra/bin twice, first by using both plain QFile, and the second time using QFile and QXmlInputSource.
+function QtXmlTest()
+{
+    var file = new QFile("plugins.xml");
+    var errorMsg;
+    var errorLine;
+    var errorColumn;
+    var domDocFromFile = new QDomDocument();
+ 
+    // The following line should work to my knowledge, but for some (non-apparent) reason it doens't:
+    // var fileOk = domDocFromFile["setContent(QIODevice,bool,QString,int,int)"](file, true, errorMsg, errorLine, errorColumn);
+    // I'm suspecting it has something to do with the fact that QDomDocument doesn't inherit QObject (I have encountered some mysterious
+    // behavior with e.g. QTreeWidgetItem also), and the fact that the actual C++ function implementation uses default argument values:
+    // bool setContent(QIODevice *dev, bool namespaceProcessing, QString * errorMsg = 0, int * errorLine = 0, int * errorColumn = 0);
+    // My guess is that that passing C-type pointers (or references for that matter) which are used for output is not simply
+    // supported/understood by QtScript(Generator).
+    // Probably QtScriptGenerator doesn't generate function "signatures" for all cases (the function at hand would yield four different possible signatures).
+    // Hence the function call must be done with none of the "optional" arguments provided.
+    // So, the following works without problems (let's try "bool setContent(QIODevice * dev)"):
+    var fileOk = domDocFromFile.setContent(file);
+    if (fileOk)
+    {
+        // Should work ok!
+        // Simply dump the whole file contents to stdout.
+        print("File contents:");
+        print(domDocFromFile.toString());
+        print("");
+    }
+ 
+    file.close(); // Remember to close the file handle.
+ 
+    // So, the setContent function seems to be working by omitting the default arguments from the signatures
+    // (http://developer.qt.nokia.com/doc/qt-4.7/qdomdocument.html) alltogether, and the following signatures/overloads should work:
+    // bool setContent(const QByteArray & data, bool namespaceProcessing)
+    // bool setContent(const QString & text, bool namespaceProcessing)
+    // bool setContent(QIODevice * dev, bool namespaceProcessing)
+    // bool setContent(QXmlInputSource * source, bool namespaceProcessing)
+    // bool setContent(const QString & text)
+    // bool setContent(const QByteArray & buffer)
+    // bool setContent(QIODevice * dev) - which we usesd previously
+    // bool setContent(QXmlInputSource * source, QXmlReader * reader)
+ 
+    var file2 = new QFile("plugins.xml");
+    var source = new QXmlInputSource(file2);
+    var domDocFromInputSource = new QDomDocument();
+ 
+    // Utilize the Qt XML/DOM API and print the names of the C++ plugins defined int the XML file individually.
+    // Let's try "bool setContent(QXmlInputSource * source, bool namespaceProcessing)".
+    var sourceOk = domDocFromInputSource.setContent(source, true);
+    if (sourceOk)
+    {
+        // Should work ok!
+        var pluginElems = domDocFromInputSource.elementsByTagName("plugin");
+        print(pluginElems.length() + " C++ plugins found. The plugins are:");
+        for(var i = 0; i < pluginElems.length(); ++i)
+            print("* " + pluginElems.item(i).toElement().attribute("path"));
+    }
+ 
+    file2.close();
+}
 
 print("< JS qt.xml example:");
 
