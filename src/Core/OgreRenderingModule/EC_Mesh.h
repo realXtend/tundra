8e990bea9209eb9006734bb24667b0d19cedff8f
Stinkfist <stinkfist0@gmail.com>
2012-03-24 15:42:34 +0200

    Expose Ogre asset type classes TextureAsset, OgreMeshAsset, OgreMaterialAsset, OgreSkeletonAsset, OgreParticleAsset and their shared_ptr typedefs to QtScript. Closes #433, https://github.com/realXtend/naali/issues/433

--- src/Core/OgreRenderingModule/EC_Mesh.h
+++ src/Core/OgreRenderingModule/EC_Mesh.h
@@ -404,11 +404,11 @@ public slots:
     /// Returns the local space axis-aligned bounding box of this object.
     AABB LocalAABB() const;
 
-    boost::shared_ptr<OgreMeshAsset> MeshAsset() const;
+    OgreMeshAssetPtr MeshAsset() const;
 
-    boost::shared_ptr<OgreMaterialAsset> MaterialAsset(int materialIndex) const;
+    OgreMaterialAssetPtr MaterialAsset(int materialIndex) const;
 
-    boost::shared_ptr<OgreSkeletonAsset> SkeletonAsset() const;
+    OgreSkeletonAssetPtr SkeletonAsset() const;
 
 public:
     /// Raycast into an Ogre mesh entity using a world-space ray. Returns true if a hit happens, in which case the fields (which are not null) are filled appropriately
