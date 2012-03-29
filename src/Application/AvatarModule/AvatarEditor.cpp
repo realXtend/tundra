15c20515c056ba7214649ccc32479ba22e1de5bd
Eino Keskitalo <eino.keskitalo@ee.oulu.fi>
2012-03-29 12:49:50 +0300

    Replace loading avatar file from disk with loading avatar asset from asset storage.
    "Create new avatar" opens up an AssetsWindow, where you can pick a new avatar to the editor (and for your avatar entity). Canceling the window restores the previous avatar that you had prior to opening the window.
    
    Attachment loading is still done from disk, should change that too.

--- src/Application/AvatarModule/AvatarEditor.cpp
+++ src/Application/AvatarModule/AvatarEditor.cpp
@@ -10,7 +10,13 @@
 #include "SceneAPI.h"
 #include "Scene.h"
 #include "QtUtils.h"
+#include "UiAPI.h"
+#include "UiMainWindow.h"
+#include "AssetFwd.h"
+#include "IAssetTransfer.h"
+#include "AssetReference.h"
 #include "../../Core/ECEditorModule/SupportedFileTypes.h"
+#include "../../Core/ECEditorModule/AssetsWindow.h"
 
 #include "Entity.h"
 
@@ -39,7 +45,7 @@ AvatarEditor::AvatarEditor(Framework *fw, QWidget *parent) :
 
     // Connect signals
     connect(but_save, SIGNAL(clicked()), SLOT(SaveAvatar()));
-    connect(but_load, SIGNAL(clicked()), SLOT(LoadAvatar()));
+    connect(but_load, SIGNAL(clicked()), SLOT(OpenAvatarAsset()));
     connect(but_revert, SIGNAL(clicked()), SLOT(RevertAvatar()));
     connect(but_attachment, SIGNAL(clicked()), this, SLOT(AddAttachment()));
 
@@ -430,35 +436,73 @@ void AvatarEditor::changeEvent(QEvent* e)
         QWidget::changeEvent(e);
 }
 
-void AvatarEditor::LoadAvatar()
+void AvatarEditor::OpenAvatarAsset()
 {
-    if (fileDialog)
-        fileDialog->close();
-    fileDialog = QtUtils::OpenFileDialogNonModal(cAvatarFileFilter, tr("Choose avatar file"), "", 0, this, SLOT(OpenFileDialogClosed(int)), false);
+    previousAvatar_ = avatarAsset_;
+
+    AssetsWindow *assetsWindow = new AssetsWindow("Avatar", framework, framework->Ui()->MainWindow());
+    connect(assetsWindow, SIGNAL(AssetPicked(AssetPtr)), SLOT(HandleAssetPicked(AssetPtr)));
+    connect(assetsWindow, SIGNAL(PickCanceled()), SLOT(RestoreOriginalValue()));
+    assetsWindow->setAttribute(Qt::WA_DeleteOnClose);
+    assetsWindow->setWindowFlags(Qt::Tool);
+    assetsWindow->show();
 }
 
-void AvatarEditor::OpenFileDialogClosed(int result)
+void AvatarEditor::HandleAssetPicked(AssetPtr avatarAsset)
 {
-    QFileDialog * dialog = dynamic_cast<QFileDialog *>(sender());
-    assert(dialog);
-    if (!dialog)
-        return;
+    if (!avatarAsset->IsLoaded())
+    {
+        AssetTransferPtr transfer = framework->Asset()->RequestAsset(avatarAsset->Name(), avatarAsset->Type(), true);
+        connect(transfer.get(), SIGNAL(Succeeded(AssetPtr)), SLOT(OnAssetTransferSucceeded(AssetPtr)));
+        connect(transfer.get(), SIGNAL(Failed(IAssetTransfer *, QString)), SLOT(OnAssetTransferFailed(IAssetTransfer *, QString)));
+    }
+    else
+        OnAssetTransferSucceeded(avatarAsset);
+}
 
-    if (result != QDialog::Accepted)
-        return;
+void AvatarEditor::OnAssetTransferSucceeded(AssetPtr asset)
+{
+    AvatarDescAssetPtr avatarAsset = boost::dynamic_pointer_cast<AvatarDescAsset>(asset);
 
-    if (dialog->selectedFiles().isEmpty())
-        return;
+    if (avatarAsset)
+        SetAsset(avatarAsset);
+    else
+        LogError("AvatarEditor::OnAssetTransferSucceeded: not an avatar asset");
+}
 
-    Entity* entity;
-    EC_Avatar* avatar;
-    AvatarDescAsset* desc;
-    if (!GetAvatarDesc(entity, avatar, desc))
-        return;
+void AvatarEditor::OnAssetTransferFailed(IAssetTransfer *transfer, QString reason)
+{
+    LogError("AvatarEditor::OnAssetTransferFailed: " + reason);
+}
 
-    // Since the dialog is set to not allow multiple file selection, assume
-    // that there is only one file selected.
-    desc->LoadFromFile(dialog->selectedFiles().first());
+void AvatarEditor::SetAsset(AvatarDescAssetPtr desc)
+{
+    // Disconnect from old avatar asset change signals
+    AvatarDescAsset* oldDesc = avatarAsset_.lock().get();
+    if (oldDesc)
+        disconnect(oldDesc, SIGNAL(AppearanceChanged()), this, SLOT(RebuildEditView()));
+
+    avatarAsset_.reset();
+
+    AvatarDescAsset* newDesc = desc.get();
+    if (newDesc)
+        connect(newDesc, SIGNAL(AppearanceChanged()), this, SLOT(RebuildEditView()));
+
+    avatarAsset_ = desc;
+
+    avatarEntity_.lock().get()->GetComponent<EC_Avatar>().get()->appearanceRef.Set(AssetReference(desc->Name()), AttributeChange::Default);
+
+    RebuildEditView();
+}
+
+void AvatarEditor::RestoreOriginalValue()
+{
+    if (previousAvatar_.lock())
+    {
+        SetAsset(previousAvatar_.lock());
+    }
+    else
+        LogError("AvatarEditor::RestoreOriginalValue: No previous avatar found.");
 }
 
 void AvatarEditor::RevertAvatar()
