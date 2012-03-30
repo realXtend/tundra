e095c8863f2cb0a80f2691a78f6b616f82245830
Jonne Nauha <jonne.nauha@adminotech.com>
2012-03-30 10:10:24 +0300

    VlcPlugin: Handle VLC 1.x and 2.x different plugin loading mechanism correctly. For 1.x you can define --plugin-path, which for we need to locate the 'vlcplugins' folder. This code was left to 2.3.1 which made vlc break because plugins are located in /plugins/vlcplugins. Remove throwing for 1.x, report a warning and carry on without --plugin-path. NOTE: I left 1.x support here but I would hope no one uses 1.x series due to the licence, use VLC >=2.0.0.

--- src/Application/VlcPlugin/VlcMediaPlayer.h
+++ src/Application/VlcPlugin/VlcMediaPlayer.h
@@ -14,16 +14,6 @@
 #include <QSlider>
 #include <QUrl>
 
-#ifdef Q_WS_MAC
-class VlcPluginsException : public std::exception
-{
-    virtual const char* what() const throw()
-    {
-        return "Fatal error, could not find vlc plugins path!";
-    }
-};
-#endif
-
 class VlcMediaPlayer : public QWidget
 {
     Q_OBJECT
