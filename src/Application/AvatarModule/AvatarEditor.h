76a8ff0b270e0142b3b2240dbff578ad957042db
Eino Keskitalo <eino.keskitalo@ee.oulu.fi>
2012-04-10 14:52:55 +0300

    Clean up an unused method from AvatarEditor.h once more.

--- src/Application/AvatarModule/AvatarEditor.h
+++ src/Application/AvatarModule/AvatarEditor.h
@@ -94,10 +94,6 @@ private:
     bool reverting_;
 
 private slots:
-    /// Called by open file dialog when it's closed.
-    /** @param result Result of dialog closure. Open is 1, Cancel is 0. */
-    void OpenFileDialogClosed(int result);
-
     /// Called by open attachment file dialog when it's closed.
     /** @param result Result of dialog closure. Open is 1, Cancel is 0. */
     void OpenAttachmentDialogClosed(int result);
