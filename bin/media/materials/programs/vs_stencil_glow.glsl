90356184cd3277b86a41f5e0dae87150040e40f2
Cvetan Stefanovski <cvetan.stefanovski@itumtech.com>
2012-05-10 17:21:03 +0200

    EC_StencilGlow: Material files and Cg / GLSL scripts. Note: vertex shader is disabled from the material (StencilGlow.material)

---
+++ bin/media/materials/programs/vs_stencil_glow.glsl
@@ -0,0 +1,10 @@
+uniform float size_value;
+uniform float time;
+
+void main(void)
+{
+   vec3 normal = gl_NormalMatrix * gl_Normal;
+   vec3 Pos = gl_Vertex.xyz;
+   Pos += 0.3 * normal;
+   gl_Position = gl_ModelViewProjectionMatrix * vec4(Pos,1.0);
+}
