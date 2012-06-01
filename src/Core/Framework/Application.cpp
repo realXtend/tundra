e28271a78ddac1d837ab7348ae16bcababd06b33
Jonne Nauha <jonne.nauha@adminotech.com>
2012-06-01 13:50:38 +0300

    Adminotech release 2.3.3.1 versioning. Disabled UpdateModule from default build.

--- src/Core/Framework/Application.cpp
+++ src/Core/Framework/Application.cpp
@@ -61,7 +61,7 @@
 /// @note Modify these values when you are making a custom Tundra build. Also the version needs to be changed here on releases.
 const char *Application::organizationName = "Adminotech";
 const char *Application::applicationName = "Tundra";
-const char *Application::version = "2.3.3";
+const char *Application::version = "2.3.3.1";
 
 Application::Application(Framework *owner, int &argc, char **argv) :
     QApplication(argc, argv),
