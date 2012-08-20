e68a3813879742f21c6af965b1f9991f04c3381e
A-K <A-K@gmail.com>
2012-08-20 11:12:40 +0300

    Comments added.

--- src/Application/OpenAssetImport/OpenAssetImport.h
+++ src/Application/OpenAssetImport/OpenAssetImport.h
@@ -46,17 +46,25 @@ class OpenAssetImport : public QObject
 public:
     OpenAssetImport(AssetAPI *assetApi);
     ~OpenAssetImport();
+    /// Converts collada files to ogre meshes, also parses and genarates ogre materials.
     void Convert(const u8 *data_, size_t numBytes, const QString &fileName, const QString &diskSource, Ogre::MeshPtr mesh);
 
 signals:
+    /// This signal is emitted when the ogre mesh is created and generated materials are ready.
     void ConversionDone(bool success);
 
 private:
+    /// Sets texture unit to material.
     void SetTexture(QString &texFile);
+    /// Returns if all textures are loaded.
     bool PendingTextures();
+    /// Parses texture path.
     QString GetPathToTexture(const QString &meshFileName, const QString &meshFileDiskSource, QString &texturePath);
+    /// Loads texture files from disk or requests them from http asset server.
     void LoadTextureFile(QString &filename);
+    /// Creates vertex data to submeshes.
     bool CreateVertexData(const Ogre::String& name, const aiNode* pNode, const aiMesh *mesh, Ogre::SubMesh* submesh, Ogre::AxisAlignedBox& mAAB);
+    /// Generates the ogre materials.
     Ogre::MaterialPtr CreateMaterial(Ogre::String& matName, const aiMaterial* mat, const QString &meshFileDiskSource, const QString &meshFileName);
     Ogre::MaterialPtr CreateMaterialByScript(int index, const aiMaterial* mat);
     void GrabNodeNamesFromNode(const aiScene* mScene,  const aiNode* pNode);
@@ -83,6 +91,7 @@ private:
 
     AssetAPI *assetAPI;
     bool meshCreated;
+    ///Map that holds the corresponding texture name and the ogre material pointer whre the texture belongs.
     TextureMaterialPointerMap texMatMap;
 
     BoneMapType boneMap;
