723cae442bba9345b1e431e99d49caaf00c808c8
Kari Vatjus-Anttila <karivatj@paju.oulu.fi>
2012-08-24 09:29:35 +0300

    Added doxygen comments, removed debug messages

--- src/Core/TundraProtocolModule/IMProperties.cpp
+++ src/Core/TundraProtocolModule/IMProperties.cpp
@@ -1,9 +1,5 @@
-/**
- *  Copyright (c) 2011 CIE / University of Oulu, All Rights Reserved
- *  For conditions of distribution and use, see copyright notice in license.txt
- *
- */
- 
+// For conditions of distribution and use, see copyright notice in LICENSE
+
 #include "IMProperties.h"
 #include "LoggingFunctions.h"
 
@@ -24,34 +20,21 @@ IMProperties::~IMProperties()
 
 }
 
-bool IMProperties::GetEuclideanMode()             { return euclidean_mode_; }
-bool IMProperties::GetRelevanceMode()             { return relevance_mode_; }
-bool IMProperties::GetRaycastMode()               { return raycast_mode_;   }
-double IMProperties::GetRaycastRange()            { return raycast_range_;  }
-double IMProperties::GetMaxRange()                { return max_range_;      }
-double IMProperties::GetCriticalRange()           { return critical_range_; }
-u32 IMProperties::GetUpdateInterval()             { return update_interval_;}
+bool IMProperties::isEuclideanMode()              { return euclidean_mode_; }
+bool IMProperties::isRelevanceMode()              { return relevance_mode_; }
+bool IMProperties::isRaycastMode()                { return raycast_mode_;   }
+double IMProperties::getRaycastRange()            { return raycast_range_;  }
+double IMProperties::getMaxRange()                { return max_range_;      }
+double IMProperties::getCriticalRange()           { return critical_range_; }
+u32 IMProperties::getUpdateInterval()             { return update_interval_;}
 
-void IMProperties::SetEuclideanMode(bool mode)    { euclidean_mode_ = mode; }
-void IMProperties::SetRelevanceMode(bool mode)    { relevance_mode_ = mode; }
-void IMProperties::SetRaycastMode(bool mode)      { raycast_mode_ = mode;   }
-void IMProperties::SetMaximumRange(double r)      { max_range_ = r;         }
-void IMProperties::SetCriticalRange(double r)     { critical_range_ = r;    }
-void IMProperties::SetUpdateInterval(u32 i)       { update_interval_ = i;   }
-void IMProperties::SetRaycastRange(double r)      { raycast_range_ = r;     }
+void IMProperties::setEuclideanMode(bool mode)    { euclidean_mode_ = mode; }
+void IMProperties::setRelevanceMode(bool mode)    { relevance_mode_ = mode; }
+void IMProperties::setRaycastMode(bool mode)      { raycast_mode_ = mode;   }
+void IMProperties::setMaximumRange(double r)      { max_range_ = r;         }
+void IMProperties::setCriticalRange(double r)     { critical_range_ = r;    }
+void IMProperties::setUpdateInterval(u32 i)       { update_interval_ = i;   }
+void IMProperties::setRaycastRange(double r)      { raycast_range_ = r;     }
 
 bool IMProperties::isEnabled()                    { return enabled_;        }
 void IMProperties::setEnabled(bool mode)          { enabled_ = mode;        }
-void IMProperties::printConfiguration()
-{/*
-    LogError("IM Configuration");
-    LogError("----------------");
-    LogError("Enabled: " + QString:: (enabled_ == true ? "true" : "false"));
-    LogError("Euclidean Mode: " + (euclidean_mode_ == true ? "true" : "false"));
-    LogError("");
-    LogError("");
-    LogError("");
-    LogError("");
-    LogError("");
- */
-}
--- src/Core/TundraProtocolModule/IMProperties.h
+++ src/Core/TundraProtocolModule/IMProperties.h
@@ -8,11 +8,15 @@
 
 #include "Types.h"
 
+/// The IMProperties class
+/** A class that stores the configuration
+    of the Interest Manager. */
 class IMProperties
 {
 
 private:
 
+    /// Variables that store the IM settings.
     bool enabled_;
     bool euclidean_mode_;
     bool relevance_mode_;
@@ -28,24 +32,67 @@ public:
 
     ~IMProperties();
 
-    bool GetEuclideanMode();
-    bool GetRelevanceMode();
-    bool GetRaycastMode();
-    double GetMaxRange();
-    double GetCriticalRange();
-    u32 GetUpdateInterval();
-    double GetRaycastRange();
+    /// Returns a boolean to describe if the Euclidean distance filter is enabled or not.
+    /** @return boolean - status of the euclidean distance filter. */
+    bool isEuclideanMode();
 
-    void SetEuclideanMode(bool mode);
-    void SetRelevanceMode(bool mode);
-    void SetRaycastMode(bool mode);
-    void SetMaximumRange(double r);
-    void SetCriticalRange(double r);
-    void SetUpdateInterval(u32 i);
-    void SetRaycastRange(double r);
+    /// Returns a boolean to describe if the Relevance filter is enabled or not.
+    /** @return boolean - status of the relevance filter. */
+    bool isRelevanceMode();
 
+    /// Returns a boolean to describe if the Ray Visibility filter is enabled or not.
+    /** @return boolean - status of the ray visibility filter. */
+    bool isRaycastMode();
+
+    /// Returns a boolean to describe if the Interest Manager Scheme is enabled or not.
+    /** @return boolean - status of the IM scheme. */
     bool isEnabled();
-    void setEnabled(bool mode);
 
-    void printConfiguration();
+    /// Returns the maximum cutoff range.
+    /** @return double - the cutoff range for the filters. */
+    double getMaxRange();
+
+    /// Returns the radius of the critical area.
+    /** @return double - the critical range for the filters. */
+    double getCriticalRange();
+
+    /// Returns the raycasting range.
+    /** @return double - the cutoff range for the ray visibility filter*/
+    double getRaycastRange();
+
+    /// Returns the maximum update interval of the relevance filter.
+    /** @return u32 - time telling the maximum update interval in nanoseconds*/
+    u32 getUpdateInterval();
+
+    /// Enables or disables the euclidean distance filter
+    /** @param bool mode that tells the new status for the filter. */
+    void setEuclideanMode(bool mode);
+
+    /// Enables or disables the relevance filter
+    /** @param bool mode that tells the new status for the filter. */
+    void setRelevanceMode(bool mode);
+
+    /// Enables or disables the ray visibility filter
+    /** @param bool mode that tells the new status for the filter. */
+    void setRaycastMode(bool mode);
+
+    /// Sets the cutoff range.
+    /** @param bool mode that tells the new status for the filter. */
+    void setMaximumRange(double r);
+
+    /// Sets the critical range.
+    /** @param bool mode that tells the new status for the filter. */
+    void setCriticalRange(double r);
+
+    /// Sets the update interval of the relevance filter.
+    /** @param bool mode that tells the new status for the filter. */
+    void setUpdateInterval(u32 i);
+
+    /// Sets the raycasting cutoff range.
+    /** @param bool mode that tells the new status for the filter. */
+    void setRaycastRange(double r);
+
+    /// Enables or disables the IM scheme.
+    /** @param bool mode that tells the new status for the filter. */
+    void setEnabled(bool mode);
 };
