2d756699f846eb8ad8289727ed73e3455cc04e1f
Peter C <th3flyboy@gmail.com>
2012-08-31 18:23:26 -0400

    Updated VLC in the Windows build script to the newest version (2.0.3).

--- tools/windows-build-deps.cmd
+++ tools/windows-build-deps.cmd
@@ -760,21 +760,21 @@ IF NOT EXIST "%DEPS%\celt\lib\libcelt.lib" (
 )
 
 :: VLC
-IF NOT EXIST "%DEPS%\vlc-2.0.1-win32.zip". (
+IF NOT EXIST "%DEPS%\vlc-2.0.3-win32.zip". (
   CD "%DEPS%"
   rmdir /S /Q "%DEPS%\vlc"
-  cecho {0D}Downloading VLC 2.0.1{# #}{\n}
-  wget http://sourceforge.net/projects/vlc/files/2.0.1/win32/vlc-2.0.1-win32.zip/download
-  IF NOT EXIST "%DEPS%\vlc-2.0.1-win32.zip". GOTO :ERROR
+  cecho {0D}Downloading VLC 2.0.3{# #}{\n}
+  wget http://sourceforge.net/projects/vlc/files/2.0.3/win32/vlc-2.0.3-win32.zip/download
+  IF NOT EXIST "%DEPS%\vlc-2.0.3-win32.zip". GOTO :ERROR
 ) ELSE (
-   cecho {0D}VLC 2.0.1 already downloaded. Skipping.{# #}{\n}
+   cecho {0D}VLC 2.0.3 already downloaded. Skipping.{# #}{\n}
 )
 
 IF NOT EXIST "%DEPS%\vlc". (
    CD "%DEPS%"
    mkdir vlc
-   cecho {0D}Extracting VLC 2.0.1 package to "%DEPS%\vlc\vlc-2.0.1"{# #}{\n}
-   7za x -y -ovlc vlc-2.0.1-win32.zip
+   cecho {0D}Extracting VLC 2.0.3 package to "%DEPS%\vlc\vlc-2.0.3"{# #}{\n}
+   7za x -y -ovlc vlc-2.0.3-win32.zip
    cd vlc
    IF NOT %ERRORLEVEL%==0 GOTO :ERROR
    mkdir lib
@@ -782,13 +782,13 @@ IF NOT EXIST "%DEPS%\vlc". (
    mkdir bin\plugins\vlcplugins
    IF NOT %ERRORLEVEL%==0 GOTO :ERROR
    :: Copy from extraced location to our subfolders
-   cecho {0D}Copying needed VLC 2.0.1 files to \bin \lib and \include{# #}{\n}
-   copy /Y vlc-2.0.1\*.dll bin\
-   xcopy /E /I /C /H /R /Y vlc-2.0.1\plugins\*.* bin\plugins\vlcplugins
-   xcopy /E /I /C /H /R /Y vlc-2.0.1\sdk\include\*.* include
-   copy /Y vlc-2.0.1\sdk\lib\*.lib lib\
+   cecho {0D}Copying needed VLC 2.0.3 files to \bin \lib and \include{# #}{\n}
+   copy /Y vlc-2.0.3\*.dll bin\
+   xcopy /E /I /C /H /R /Y vlc-2.0.3\plugins\*.* bin\plugins\vlcplugins
+   xcopy /E /I /C /H /R /Y vlc-2.0.3\sdk\include\*.* include
+   copy /Y vlc-2.0.3\sdk\lib\*.lib lib\
    :: Remove extracted folder, not needed anymore
-   rmdir /S /Q vlc-2.0.1
+   rmdir /S /Q vlc-2.0.3
    IF NOT %ERRORLEVEL%==0 GOTO :ERROR
    :: Force deployment and clean vlc plugins cache file
    del /Q "%TUNDRA_BIN%\libvlc.dll"
@@ -796,15 +796,15 @@ IF NOT EXIST "%DEPS%\vlc". (
    del /Q "%TUNDRA_BIN%\plugins\plugins*.dat"
    IF NOT %ERRORLEVEL%==0 GOTO :ERROR
 ) ELSE (
-   cecho {0D}VLC 2.0.1 already extracted. Skipping.{# #}{\n}
+   cecho {0D}VLC 2.0.3 already extracted. Skipping.{# #}{\n}
 )
 
 IF NOT EXIST "%TUNDRA_BIN%\libvlc.dll". (
-   cecho {0D}Deploying VLC 2.0.1 DLLs to Tundra bin\{# #}{\n}
+   cecho {0D}Deploying VLC 2.0.3 DLLs to Tundra bin\{# #}{\n}
    xcopy /E /I /C /H /R /Y "%DEPS%\vlc\bin\*.*" "%TUNDRA_BIN%"
    IF NOT %ERRORLEVEL%==0 GOTO :ERROR
 ) ELSE (
-   cecho {0D}VLC 2.0.1 already deployed. Skipping.{# #}{\n}
+   cecho {0D}VLC 2.0.3 already deployed. Skipping.{# #}{\n}
 )
 
 echo.
