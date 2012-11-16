2ee637f68e8b789c0f79c6a2a92f15b45334a491
Lasse Öörni <lasse.oorni@ludocraft.com>
2012-11-16 17:02:13 +0200

    Added initial Tundra bash build script.

---
+++ android/BuildTundra.bash
@@ -0,0 +1,45 @@
+set -e # Quit the script immediately if any of the commands fails.
+
+echo "ANDROID_NDK is at '$ANDROID_NDK'"
+if [ -z "$ANDROID_NDK" ]; then
+	echo "ERROR: Please export environment variable ANDROID_NDK to point to where you installed the Android NDK (r8)! Note that NDK r8b or newer are not (yet) supported!"
+	exit 1
+fi
+
+echo "Necessitas-Qt is at '$QTDIR'"
+if [ -z "$QTDIR" ]; then
+	echo "ERROR: Please export environment variable QTDIR to point to where you cloned and built Necessitas-Qt!"
+	exit 1
+fi
+
+tundra_android_native_api_level=9 # This is the minimum API level we can possibly support, we require NativeActivity and AAssetManager.
+echo "Targeting Android Native API level $tundra_android_native_api_level"
+export TUNDRA_ANDROID_ABI=x86 # Possible options 'armeabi', 'armeabi-v7a', 'x86'
+echo "Targeting Android ABI '$TUNDRA_ANDROID_ABI'"
+
+tundra=$(dirname $(readlink -f $0))/..
+echo "Tundra root directory is '$tundra'"
+deps=$tundra/deps-android
+echo "Tundra deps root directory is '$deps'"
+tundra_android=$tundra/android
+
+cmake_build_type=Release
+
+export BOOST_ROOT=$deps/boost
+export TUNDRA_DEP_PATH=$deps
+export KNET_DIR=$deps/kNet
+export BULLET_DIR=$deps/bullet
+export OGRE_HOME=$deps/ogre
+
+# Add Necessitas QT bin directory for moc
+export PATH=$PATH:$QTDIR/bin
+
+echo "Preparing Tundra build.."
+cd $tundra
+cmake -DCMAKE_TOOLCHAIN_FILE=$tundra_android/android.toolchain.cmake -DBOOST_ROOT=$deps/boost -DANDROID=1 -DANDROID_NATIVE_API_LEVEL=9 -DANDROID_ABI=$TUNDRA_ANDROID_ABI -DCMAKE_BUILD_TYPE=$cmake_build_type
+
+echo "Building Tundra.."
+make
+
+
+
