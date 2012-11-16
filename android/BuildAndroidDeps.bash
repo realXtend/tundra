b9001e1e4712c5516e90a8b1b9a387326f300098
Jukka Jylanki <jujjyl@gmail.com>
2012-11-16 13:13:22 +0200

    Added kNet to android build script.

--- android/BuildAndroidDeps.bash
+++ android/BuildAndroidDeps.bash
@@ -27,10 +27,26 @@ if [ ! -d bullet ]; then
 	echo "Checking out Bullet.."
 	svn checkout http://bullet.googlecode.com/svn/tags/bullet-2.78 bullet
 	echo "Running cmake for Bullet.."
-	cmake -DBUILD_DEMOS=OFF -DBUILD_{NVIDIA,AMD,MINICL}_OPENCL_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DCMAKE_TOOLCAHIN_FILE=$tundra_android/android.toolchain.cmake -DCMAKE_BUILD_TYPE=$cmake_build_type .
+	cd bullet
+	cmake -G "Unix Makefiles" -DBUILD_DEMOS=OFF -DBUILD_{NVIDIA,AMD,MINICL}_OPENCL_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DCMAKE_TOOLCHAIN_FILE=$tundra_android/android.toolchain.cmake -DCMAKE_BUILD_TYPE=$cmake_build_type .
 	echo "Building Bullet.."
 	make
+	cd $deps
 else
 	echo "Bullet already checked out. Skipping.."
+fi
+
+if [ ! -d kNet ]; then
+	echo "Cloning kNet.."
+	git clone https://github.com/juj/kNet
+	cd kNet
+	echo "Switching to master branch, stable doesn't yet work on Android. (TODO: Remove this after merge)"
+	git checkout master
+	echo "Running cmake for kNet.."
+	cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=$tundra_android/android.toolchain.cmake -DBOODT_ROOT=$deps/boost -DCMAKE_BUILD_TYPE=$cmake_build_type .
+	echo "Building kNet.."
+	make
+else
+	echo "kNet already cloned. Skipping.."
 fi
 
