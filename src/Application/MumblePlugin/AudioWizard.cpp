00622142af428c80dae165fb00eab8b2d16b2464
Jonne Nauha <jonne.nauha@adminotech.com>
2012-12-18 19:06:47 +0200

    MumblePlugin: Add automated voice level detection widget. Running this will help you setup VAD values to approximately the correct positions, manual tweaking can be done after it. Ask if the detection should be ran if it has not been ran on this machine (OS user profile) before.

--- src/Application/MumblePlugin/AudioWizard.cpp
+++ src/Application/MumblePlugin/AudioWizard.cpp
@@ -2,15 +2,25 @@
 
 #include "AudioWizard.h"
 #include "Framework.h"
-#include "AudioAPI.h"
-#include "mumble/AudioStats.h"
+#include "CoreDefines.h"
 #include "LoggingFunctions.h"
+
+#include "UiAPI.h"
+#include "UiMainWindow.h"
+#include "AudioAPI.h"
+#include "ConfigAPI.h"
+
+#include "mumble/AudioStats.h"
+
+#include <QMessageBox>
 
 namespace MumbleAudio
 {
     AudioWizard::AudioWizard(Framework *framework, AudioSettings settings) : 
+        framework_(framework),
         peakTicks(0),
-        peakMax(0)
+        peakMax(0),
+        detectorWidget_(0)
     {
         originalSettings = settings;
         currentSettings = settings;
@@ -20,6 +30,7 @@ namespace MumbleAudio
 
         setupUi(this);
         setAttribute(Qt::WA_DeleteOnClose, true);
+        setWindowIcon(framework_->Ui()->MainWindow()->windowIcon());
         
         // Audio bar init
         audioBar = new Mumble::AudioBar(this);
@@ -120,13 +131,14 @@ namespace MumbleAudio
         connect(sliderSpeech, SIGNAL(valueChanged(int)), SLOT(OnMaxVADChanged(int)));
 
         connect(sliderSuppression, SIGNAL(sliderReleased()), SLOT(OnSliderReleased()));
-        connect(sliderAmplification, SIGNAL(sliderReleased()), SLOT(OnSliderReleased()));        
+        connect(sliderAmplification, SIGNAL(sliderReleased()), SLOT(OnSliderReleased()));
         connect(sliderSilence, SIGNAL(sliderReleased()), SLOT(OnSliderReleased()));
         connect(sliderSpeech, SIGNAL(sliderReleased()), SLOT(OnSliderReleased()));
 
         connect(buttonOK, SIGNAL(clicked()), SLOT(OnOKPressed()));
         connect(buttonCancel, SIGNAL(clicked()), SLOT(OnCancelPressed()));
         connect(buttonApply, SIGNAL(clicked()), SLOT(OnApplyPressed()));
+        connect(detectLevelsButton, SIGNAL(clicked()), SLOT(StartAudioLevelDetection()));
 
         connect(comboBoxInputDevice, SIGNAL(currentIndexChanged(const QString&)), SLOT(OnInputDeviceChanged(const QString&)));
         connect(comboBoxTransmitMode, SIGNAL(currentIndexChanged(const QString&)), SLOT(OnTransmitModeChanged(const QString&)));
@@ -148,10 +160,24 @@ namespace MumbleAudio
         show();
 
         resize(1,1);
-    }   
+        
+        // Check if automatic voice level detection has been suggested.
+        // Whatever the answer is don't bug the user again.
+        ConfigData data("mumbleplugin", "wizard");
+        bool levelsDetected = (framework_->Config()->HasValue(data, "leveldetectionran") ? framework_->Config()->Get(data, "leveldetectionran", false).toBool() : false);
+        if (!levelsDetected)
+        {
+            framework_->Config()->Set(data, "leveldetectionran", true);
+            QMessageBox::StandardButton answer = QMessageBox::question(this, "Audio Level Detection", "Would you like to run the automatic audio level detection?<br><br>Running it will improve your speech detection. You can also run it later with the <b>Start Audio Level Detection</b> button.", QMessageBox::Yes|QMessageBox::No);
+            if (answer == QMessageBox::Yes)
+                StartAudioLevelDetection();
+        }
+    }
 
     AudioWizard::~AudioWizard()
     {
+        if (detectorWidget_)
+            SAFE_DELETE(detectorWidget_);
     }
 
     void AudioWizard::SetLevels(float level, bool isSpeech)
@@ -171,12 +197,46 @@ namespace MumbleAudio
             audioBar->iValue = iLevel;
             audioBar->update();
         }
+        
+        if (detectorWidget_)
+        {
+            // Convert to [0,96] db scale
+            detectorWidget_->AddLevel((96.0f + level));
+
+            // Close if completed
+            if (detectorWidget_->state == AudioLevelWidget::Completed)
+            {
+                detectorWidget_->close();
+                SAFE_DELETE(detectorWidget_);
+            }
+        }
 
         const QPixmap *currentPixmap = labelIconVAD->pixmap();
         if (isSpeech && currentPixmap != &activePixmap)
             labelIconVAD->setPixmap(activePixmap);
         else if (!isSpeech && currentPixmap != &inactivePixmap)
             labelIconVAD->setPixmap(inactivePixmap);
+    }
+    
+    void AudioWizard::StartAudioLevelDetection()
+    {
+        // Enable VAD mode if not active.
+        if (currentSettings.transmitMode != TransmitVoiceActivity)
+            OnTransmitModeChanged("Voice Activity");
+
+        if (!detectorWidget_)
+        {
+            detectorWidget_ = new AudioLevelWidget(this);
+            detectorWidget_->setWindowModality(Qt::WindowModal);
+            detectorWidget_->show();
+
+            connect(detectorWidget_, SIGNAL(AudioLevelsCalculated(float, float)), SLOT(OnAudioLevelsDetected(float, float)));
+        }
+        else
+        {
+            detectorWidget_->activateWindow();
+            detectorWidget_->raise();
+        }
     }
 
     void AudioWizard::OnQualityChanged()
@@ -194,11 +254,11 @@ namespace MumbleAudio
     void AudioWizard::OnInputDeviceChanged(const QString &deviceName)
     {
         currentSettings.recordingDevice = deviceName;
-        
+
         // Automatically apply this option when changed.
         OnApplyPressed();
     }
-    
+
     void AudioWizard::OnTransmitModeChanged(const QString &mode)
     {
         if (mode == "Voice Activity")
@@ -212,7 +272,7 @@ namespace MumbleAudio
         sliderSpeech->setEnabled(vadEnabled);
         labelValueSpeech->setEnabled(vadEnabled);
         audioBar->setEnabled(vadEnabled);
-        
+
         peakTicks = 0;
         peakMax = 0;
         audioBar->iPeak = -1;
@@ -269,7 +329,7 @@ namespace MumbleAudio
 
         buttonApply->setEnabled(true);
     }
-       
+
     void AudioWizard::OnInnerRangeChanged(int value)
     {
         currentSettings.innerRange = value;
@@ -308,7 +368,7 @@ namespace MumbleAudio
         bool visible = !groupBoxQuality->isVisible();
         groupBoxQuality->setVisible(visible);
         groupBoxProcessing->setVisible(visible);
-        
+
         QString text = buttonAdvanced->text();
         buttonAdvanced->setText(!visible ? text.replace("Hide", "Show") : text.replace("Show", "Hide"));
         resize(visible ? QSize(999,999) : QSize(1,1));
@@ -331,11 +391,140 @@ namespace MumbleAudio
         emit SettingsChanged(currentSettings, true);
         buttonApply->setDisabled(true);
     }
-    
+
+    void AudioWizard::OnAudioLevelsDetected(float silenceLevel, float voiceLevel)
+    {
+        // Sanity check
+        if (silenceLevel > voiceLevel)
+        {
+            QMessageBox::warning(this, "Audio level detection failed", 
+                "Measured silence level seems to be louder than the speaking volume. Please try again.");
+            return;
+        }
+
+        // Balance diff to me maximum 30db
+        // or raise the silence a bit from the measured value.
+        float diff = voiceLevel - silenceLevel;
+        if (diff > 30.0f)
+            silenceLevel = voiceLevel - 30.0f;
+        else if (diff > 7.5f)
+            silenceLevel += 5.0f;
+        if (silenceLevel < 5.0f)
+            silenceLevel = 5.0f;
+            
+        // Lower the speech trigger volume a bit from the measured one.
+        voiceLevel -= 3.0f;
+        
+        // The incoming values are on the range of [0,96]. Scale to [0,1] for VAD values.
+        currentSettings.VADmin = (silenceLevel / 96.0f);
+        currentSettings.VADmax = (voiceLevel / 96.0f);
+
+        // Apply current VAD
+        int silenceValue = static_cast<int>(currentSettings.VADmin * 32767.0f + 0.5f);
+        sliderSilence->setValue(silenceValue);
+        OnMinVADChanged(silenceValue);
+
+        int speechValue = static_cast<int>(currentSettings.VADmax * 32767.0f + 0.5f);
+        sliderSpeech->setValue(speechValue);
+        OnMaxVADChanged(speechValue);
+        
+        OnApplyPressed();
+    }
+
     void AudioWizard::OnSliderReleased()
     {
         // Automatically apply when a slider is released. We don't want to spam 
         // settings changed signal when the slider is still being moved.
         OnApplyPressed();
+    }
+
+    /// AudioLevelWidget
+    
+    AudioLevelWidget::AudioLevelWidget(QWidget *parent) : 
+        QDialog(parent, Qt::Tool),
+        state(Starting),
+        silenceLevelCombined(0.0f),
+        voiceLevelCombined(0.0f),
+        stepSecondsLeft_(5),
+        firstSample_(true)
+    {
+        connect(&tickTimer_, SIGNAL(timeout()), SLOT(OnSecondPassed()));
+        
+        setupUi(this);
+        labelInfo->setText("Starting detection: <span style='color:black;'>Please be quiet in</span>");
+        labelTimer->setText(QString("%1 seconds").arg(stepSecondsLeft_));
+        
+        tickTimer_.start(1000);
+    }
+
+    AudioLevelWidget::~AudioLevelWidget()
+    {
+    }
+    
+    void AudioLevelWidget::closeEvent(QCloseEvent *e)
+    {
+        state = Completed;
+        if (tickTimer_.isActive())
+            tickTimer_.stop();
+    }
+    
+    void AudioLevelWidget::AddLevel(float level)
+    {
+        if (state == Silence)
+        {
+            silenceLevelCombined += level;           
+            if (!firstSample_)
+                silenceLevelCombined = (silenceLevelCombined / 2.0f);
+            else
+                firstSample_ = false;
+        }
+        else if (state == Voice)
+        {
+            // Ignore levels that are below the before measured silence.
+            if (level > silenceLevelCombined)
+            {
+                // Ignore levels that are under 75% of the current speech level.
+                if (level > (voiceLevelCombined / 1.25f))
+                {
+                    voiceLevelCombined += level;           
+                    if (!firstSample_)
+                        voiceLevelCombined = (voiceLevelCombined / 2.0f);
+                    else
+                        firstSample_ = false;
+                }
+            }
+        }
+    }
+    
+    void AudioLevelWidget::OnSecondPassed()
+    {
+        stepSecondsLeft_--;
+        if (stepSecondsLeft_ > 0)
+            labelTimer->setText(QString("%1 seconds").arg(stepSecondsLeft_));
+        else
+        {
+            if (state == Starting)
+            {
+                state = Silence;
+                firstSample_ = true;
+                stepSecondsLeft_ = 5;
+                labelInfo->setText("Please be quiet for");
+                labelTimer->setText(QString("%1 seconds").arg(stepSecondsLeft_));
+            }
+            else if (state == Silence)
+            {
+                state = Voice;
+                firstSample_ = true;
+                stepSecondsLeft_ = 8;
+                labelInfo->setText("Repeat the following sentence<br><span style='color:black;'>The quick brown fox jumps over the lazy dog</span>");
+                labelTimer->setText(QString("%1 seconds").arg(stepSecondsLeft_));
+            }
+            else if (state == Voice)
+            {
+                state = Completed;
+                tickTimer_.stop();
+                emit AudioLevelsCalculated(silenceLevelCombined, voiceLevelCombined);
+            }
+        }
     }
 }
--- src/Application/MumblePlugin/AudioWizard.h
+++ src/Application/MumblePlugin/AudioWizard.h
@@ -7,7 +7,11 @@
 #include "MumbleDefines.h"
 
 #include "ui_AudioWizard.h"
+#include "ui_AudioLevelDetectorWidget.h"
+
 #include <QPixmap>
+#include <QTimer>
+#include <QDialog>
 
 /// @cond PRIVATE
 namespace Mumble
@@ -17,20 +21,63 @@ namespace Mumble
 
 namespace MumbleAudio
 {
+    class AudioLevelWidget : public QDialog, public Ui::AudioLevelDetectorWidget
+    {
+    
+    Q_OBJECT
+    
+    public:
+        AudioLevelWidget(QWidget *parent = 0);
+        ~AudioLevelWidget();
+        
+        void AddLevel(float level);
+        
+        enum DetectionState
+        {
+            None = 0,
+            Starting,
+            Silence,
+            Voice,
+            Completed
+        };
+        
+        DetectionState state;
+        float voiceLevelCombined;
+        float silenceLevelCombined;
+
+    protected:
+        void closeEvent(QCloseEvent *e);
+
+    signals:
+        void AudioLevelsCalculated(float silenceLevel, float voiceLevel);
+        
+    private slots:
+        void OnSecondPassed();
+        
+    private:
+        bool firstSample_;
+        uint stepSecondsLeft_;
+        QTimer tickTimer_;
+    };
+    
     class AudioWizard : public QWidget, private Ui::AudioWizardWidget
-    {    
+    {
     Q_OBJECT
 
     public:
         AudioWizard(Framework *framework, AudioSettings settings);
         ~AudioWizard();
 
+    public slots:
+        void StartAudioLevelDetection();
         void SetLevels(float level, bool isSpeech);
 
     signals:
         void SettingsChanged(MumbleAudio::AudioSettings settings, bool saveConfig);
 
     private slots:
+        void OnAudioLevelsDetected(float silenceLevel, float voiceLevel);
+        
         void OnQualityChanged();
 
         void OnInputDeviceChanged(const QString &deviceName);
@@ -38,10 +85,10 @@ namespace MumbleAudio
 
         void OnSuppressChanged(int value);
         void OnAmplificationChanged(int value);
-        
+
         void OnMinVADChanged(int value);
         void OnMaxVADChanged(int value);
-        
+
         void OnInnerRangeChanged(int value);
         void OnOuterRangeChanged(int value);
 
@@ -56,10 +103,13 @@ namespace MumbleAudio
         void OnSliderReleased();
 
     private:
+        Framework *framework_;
+        
         AudioSettings currentSettings;
         AudioSettings originalSettings;
 
-        Mumble::AudioBar *audioBar;
+        Mumble::AudioBar *audioBar;       
+        AudioLevelWidget *detectorWidget_;
 
         int peakTicks;
         int peakMax;
