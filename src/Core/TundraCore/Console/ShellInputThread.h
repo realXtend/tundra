0f13c74fb1c6e3319af94fa917a42d302de2c09d
Stinkfist <stinkfist0@gmail.com>
2013-01-18 16:45:23 +0200

    Replace usage of boost::thread with QThread in ShellInputThread. #595

--- src/Core/TundraCore/Console/ShellInputThread.h
+++ src/Core/TundraCore/Console/ShellInputThread.h
@@ -4,28 +4,29 @@
 
 #include <vector>
 #include <string>
-#include <boost/thread.hpp>
-#include <boost/thread/mutex.hpp>
+
+#include <QThread>
+#include <QMutex>
 
 /// Retrieves input from the native console/shell.
 /** @cond PRIVATE */
-class ShellInputThread
+class ShellInputThread : public QThread
 {
+    Q_OBJECT
+
 public:
     /// Starts the thread.
     ShellInputThread();
     /// Stops the thread.
     ~ShellInputThread();
-
-    /// Boost thread entry point.
-    void ThreadMain();
-
     /// Gets a new line of input from the input thread if it exists, or an empty string otherwise.
     std::string GetLine();
 
 private:
-    boost::thread inputThread;
-    boost::mutex inputQueueLock;
+    /// QThread override
+    void run();
+
+    QMutex inputQueueLock;
     std::vector<std::string> inputQueue;
 };
 /** @endcond */
