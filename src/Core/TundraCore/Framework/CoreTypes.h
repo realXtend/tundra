fe6ecd781f8bcf02a130aa5476e94f94dd113ae9
Stinkfist <stinkfist0@gmail.com>
2013-01-29 00:51:54 +0200

    Take TUNDRA_NO_BOOST into consideration in CoreTypes.h when defining the typedefs for fixed-width types.

--- src/Core/TundraCore/Framework/CoreTypes.h
+++ src/Core/TundraCore/Framework/CoreTypes.h
@@ -4,8 +4,6 @@
 
 // types
 
-#include <boost/shared_ptr.hpp>
-
 #include <vector>
 #include <list>
 
@@ -13,16 +11,12 @@
 #include <QStringList>
 
 #if defined(unix) || defined(__APPLE__)
-
 #include <cmath>
 #include <limits>
-/**
- * Gnu GCC have C99-standard macros as an extension but in some system there does not exist them so we define them by ourself. 
- */
+// Gnu GCC has C99-standard macros as an extension but in some system there does not exist them so we define them by ourself.
 template <class T> inline bool _finite(T f) { return f != std::numeric_limits<T>::infinity(); }
 template <class T> inline bool _isnan(T f) { return f != f; }
-
-#endif 
+#endif
 
 // If we have C99, take the types from there.
 #if (__STDC_VERSION__ >= 199901L) || (_MSC_VER >= 1600)
@@ -39,7 +33,8 @@ typedef int16_t s16; ///< 2 bytes: -32768 - 32767.
 typedef int32_t s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
 typedef int64_t s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
 
-#else
+// Otherwise, if we have Boost and its usage is not disabled, we can also pull the types from there.
+#elif !defined(TUNDRA_NO_BOOST)
 
 #include <boost/cstdint.hpp>
 
@@ -52,6 +47,29 @@ typedef boost::int8_t s8; ///< a single byte: -128 - 127.
 typedef boost::int16_t s16; ///< 2 bytes: -32768 - 32767.
 typedef boost::int32_t s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
 typedef boost::int64_t s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
+
+#else // No Boost or unknown if we have C99. Have to guess the following are correct.
+
+#include <limits.h>
+
+#pragma warning "Not using boost and C99 not defined. Guessing the built-ins for fixed-width types!"
+
+typedef unsigned char u8; ///< a single byte: 0-255.
+typedef unsigned short u16; ///< 2 bytes: 0 - 65535.
+typedef unsigned long long u64; ///< 8 bytes: 18,446,744,073,709,551,615 ~1.8e19.
+
+typedef signed char s8; ///< a single byte: -128 - 127.
+typedef signed short s16; ///< 2 bytes: -32768 - 32767.
+
+#if ULONG_MAX == 0xffffffff
+typedef unsigned long u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
+typedef long s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
+#elif UINT_MAX == 0xffffffff
+typedef unsigned int u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
+typedef int s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
+#endif
+
+typedef signed long long s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
 
 #endif
 
