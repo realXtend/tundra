4e2ce45add0a32fb4df4aeaa945b6682a8bd575b
Stinkfist <stinkfist0@gmail.com>
2013-01-28 16:35:48 +0200

    Add pauses to VS CMake BATs.

--- CMakeVS2008.bat
+++ CMakeVS2008.bat
@@ -3,3 +3,4 @@ del /Q tundra.sln
 cd tools
 call windows-run-cmake.cmd "Visual Studio 9 2008"
 cd ..
+pause
--- CMakeVS2010.bat
+++ CMakeVS2010.bat
@@ -3,3 +3,4 @@ IF EXIST tundra.sln (del /Q tundra.sln)
 cd tools
 call windows-run-cmake "Visual Studio 10"
 cd ..
+pause
