f46a9366132b64f0d3a387b5eda6844497454cda
Stinkfist <stinkfist0@gmail.com>
2013-03-21 18:08:36 +0200

    Move invocation of QTimer::singleShot that reads the Client/Server command-line arguments from TundraLogicModule ctor to TundraLogicModule::Initialize in order to prevent (somewhat non-deterministic) apparently startup timing related issues that might cause ReadStartupParams to be executed too early while the Client/Server internals are not yet valid.

--- src/Core/TundraProtocolModule/TundraLogicModule.cpp
+++ src/Core/TundraProtocolModule/TundraLogicModule.cpp
@@ -71,8 +71,6 @@ TundraLogicModule::TundraLogicModule() :
     IModule("TundraLogic"),
     kristalliModule_(0)
 {
-    // Read startup params when application event loop starts.
-    QTimer::singleShot(0, this, SLOT(ReadStartupParameters()));
 }
 
 TundraLogicModule::~TundraLogicModule()
@@ -158,6 +156,9 @@ void TundraLogicModule::Initialize()
     kristalliModule_ = framework_->GetModule<KristalliProtocolModule>();
     if (!kristalliModule_)
         throw Exception("Fatal: could not get KristalliProtocolModule");
+
+    // Read startup params when application event loop starts.
+    QTimer::singleShot(0, this, SLOT(ReadStartupParameters()));
 }
 
 void TundraLogicModule::Uninitialize()
