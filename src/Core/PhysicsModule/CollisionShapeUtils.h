9b0873f954a832bd61f51c045b0fd119d76b34e7
Stinkfist <stinkfist0@gmail.com>
2013-04-16 18:15:39 +0300

    DLL export some useful functionality of PhysicsModule.

--- src/Core/PhysicsModule/CollisionShapeUtils.h
+++ src/Core/PhysicsModule/CollisionShapeUtils.h
@@ -7,16 +7,11 @@
 #include "PhysicsModuleFwd.h"
 #include "Math/float3.h"
 
-namespace Ogre
-{
-    class Mesh;
-};
+namespace Ogre { class Mesh; };
 
 namespace Physics
 {
-    void GenerateTriangleMesh(Ogre::Mesh* mesh, btTriangleMesh* ptr);
-    void GetTrianglesFromMesh(Ogre::Mesh* mesh, std::vector<float3>& dest);
-    void GenerateConvexHullSet(Ogre::Mesh* mesh, ConvexHullSet* ptr);
+    void PHYSICS_MODULE_API GenerateTriangleMesh(Ogre::Mesh* mesh, btTriangleMesh* ptr);
+    void PHYSICS_MODULE_API GetTrianglesFromMesh(Ogre::Mesh* mesh, std::vector<float3>& dest);
+    void PHYSICS_MODULE_API GenerateConvexHullSet(Ogre::Mesh* mesh, ConvexHullSet* ptr);
 }
-
-
