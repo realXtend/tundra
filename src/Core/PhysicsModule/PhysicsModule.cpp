98a90ca4d10a518e1c1eb85cfc30ebaacd375cde
Jonne Nauha <jonne.nauha@adminotech.com>
2013-04-28 20:42:06 +0300

    PhysicsModule: Disable EC_PhysicsConstraint from the source code. Seems like the file is not included in the build so this does not build atm. Cvetan: Dont enable this back in main repo before its completed.

--- src/Core/PhysicsModule/PhysicsModule.cpp
+++ src/Core/PhysicsModule/PhysicsModule.cpp
@@ -10,7 +10,7 @@
 #include "EC_RigidBody.h"
 #include "EC_VolumeTrigger.h"
 #include "EC_PhysicsMotor.h"
-#include "EC_PhysicsConstraint.h"
+//#include "EC_PhysicsConstraint.h"
 #include "OgreRenderingModule.h"
 #include "EC_Mesh.h"
 #include "EC_Placeable.h"
@@ -68,7 +68,7 @@ void PhysicsModule::Load()
     framework_->Scene()->RegisterComponentFactory(ComponentFactoryPtr(new GenericComponentFactory<EC_RigidBody>));
     framework_->Scene()->RegisterComponentFactory(ComponentFactoryPtr(new GenericComponentFactory<EC_VolumeTrigger>));
     framework_->Scene()->RegisterComponentFactory(ComponentFactoryPtr(new GenericComponentFactory<EC_PhysicsMotor>));
-    framework_->Scene()->RegisterComponentFactory(ComponentFactoryPtr(new GenericComponentFactory<EC_PhysicsConstraint>));
+    //framework_->Scene()->RegisterComponentFactory(ComponentFactoryPtr(new GenericComponentFactory<EC_PhysicsConstraint>));
 }
 
 void PhysicsModule::Initialize()
