26c1c38ec6232a17259db3985a7d1363167db9c5
Stinkfist <stinkfist0@gmail.com>
2013-05-10 11:00:02 +0300

    Tundra & TundraConsole: use kNet's BottomMemoryAllocator on to catch potential 64-bit memory allocation bugs early on. Also some CMakeLists.txt cleanup.

--- src/Core/TundraConsole/main.cpp
+++ src/Core/TundraConsole/main.cpp
@@ -4,6 +4,11 @@
 
 int TUNDRACORE_API run(int argc, char **argv);
 
+#ifdef _WIN64 // Use kNet's BottomMemoryAllocator on to catch potential 64-bit memory allocation bugs early on.
+#include <kNet/64BitAllocDebugger.h>
+BottomMemoryAllocator bma;
+#endif
+
 int main(int argc, char **argv)
 {
     return run(argc, argv);
