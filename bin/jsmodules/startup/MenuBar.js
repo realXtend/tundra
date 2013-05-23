d1adb3b042fd88b444714917d19e9141f1eeb4ba
Jonne Nauha <jonne.nauha@adminotech.com>
2013-05-23 17:39:07 +0300

    MenuBar script: Fix profiler menu item to renamed show profiler funtion.

--- bin/jsmodules/startup/MenuBar.js
+++ bin/jsmodules/startup/MenuBar.js
@@ -218,7 +218,7 @@ if (!framework.IsHeadless())
     }
 
     function OpenProfilerWindow() {
-        framework.GetModuleByName("DebugStats").ShowProfilingWindow();
+        framework.GetModuleByName("DebugStats").ShowProfilerWindow();
     }
 
     function OpenTerrainEditor() {
