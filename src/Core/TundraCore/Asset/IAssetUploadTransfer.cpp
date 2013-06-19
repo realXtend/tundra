8e26e5aec6148b813c5233632d51f82e533b5c9a
Jonne Nauha <jonne.nauha@adminotech.com>
2013-06-20 01:30:48 +0300

    Implement handling 302 and 303 HTTP GET responses, fixes #685. Implement IAssetUploadTransfer to store respose headers and make a slot to query them from script, other than HTTP uploads wont be needing this so maybe it could be time to make a HttpAssetUploadTransfer (implementing the interface) class and move HTTP related things there. Cleaned up documentation and code in IAssetUploadTransfer and HttpAssetProvider::OnHttpTransferFinished.

--- src/Core/TundraCore/Asset/IAssetUploadTransfer.cpp
+++ src/Core/TundraCore/Asset/IAssetUploadTransfer.cpp
@@ -7,6 +7,10 @@
 
 #include "MemoryLeakCheck.h"
 
+IAssetUploadTransfer::~IAssetUploadTransfer()
+{
+}
+
 void IAssetUploadTransfer::EmitTransferCompleted()
 {
     emit Completed(this);
@@ -15,4 +19,38 @@ void IAssetUploadTransfer::EmitTransferCompleted()
 void IAssetUploadTransfer::EmitTransferFailed()
 {
     emit Failed(this);
+}
+
+QString IAssetUploadTransfer::AssetRef()
+{
+    shared_ptr<IAssetStorage> storage = destinationStorage.lock();
+    if (!storage)
+        return "";
+    return storage->GetFullAssetURL(destinationName);
+}
+
+QByteArray IAssetUploadTransfer::RawData() const
+{
+    if (assetData.size() == 0)
+        return QByteArray();
+    else
+        return QByteArray((const char*)&assetData[0], (int)assetData.size());
+}
+
+QString IAssetUploadTransfer::ReplyHeader(const QString &header) const
+{
+    foreach(const QString &name, replyHeaders.keys())
+        if (name.compare(header, Qt::CaseInsensitive) == 0)
+            return replyHeaders[name];
+    return "";
+}
+
+QString IAssetUploadTransfer::SourceFilename() const
+{
+    return sourceFilename;
+}
+
+QString IAssetUploadTransfer::DestinationName() const
+{
+    return destinationName;
 }
