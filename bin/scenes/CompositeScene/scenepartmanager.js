be6db95d733d51d7ad2667d978a69637aff72a7d
antont <toni@playsign>
2013-06-30 00:03:52 +0300

    CompositeScene - finish the basics: parents the ents of the scenepart to the part placeholder. made the parts and the master scene so that are sane and look somewhat clear .. with two different simply city block like things (parts a and b are same otherwise but colours switched).

--- bin/scenes/CompositeScene/scenepartmanager.js
+++ bin/scenes/CompositeScene/scenepartmanager.js
@@ -5,25 +5,39 @@
 
 print("[ScenePartManager] Loading script...");
 
+var part2ents = []; //a map of part.name -> ents in that part
+
 function print(s) {
     console.LogInfo(s);
 }
 
-var parts = scene.EntitiesWithComponent("EC_DynamicComponent", "ScenePart");
-//print(parts);
+function loadAll() {
+    var parts = scene.EntitiesWithComponent("EC_DynamicComponent", "ScenePart");
+    //print(parts);
 
-var partfile;
-for (var i=0; i<parts.length; i++) {
-    //print(i + ":" + parts[i]);
 
-    var placeholder = parts[i];
-    partfile = placeholder.dynamiccomponent.GetAttribute("sceneref");
-    loadPart(placeholder, partfile);
+    var partfile;
+    for (var i=0; i<parts.length; i++) {
+        //print(i + ":" + parts[i]);
+
+        var placeholder = parts[i];
+        partfile = placeholder.dynamiccomponent.GetAttribute("sceneref");
+        loadPart(placeholder, partfile);
+    }
 }
 
 function loadPart(placeholder, partfile) {
     print(placeholder + ":" + partfile);
-    scene.LoadSceneXML("scenes/CompositeScene/" + partfile, false, false, 2); //, changetype);
+    var ents = scene.LoadSceneXML("scenes/CompositeScene/" + partfile, false, false, 2); //, changetype);
+    var ent;
+    for (var i=0; i<ents.length; i++) {
+        ent = ents[i];
+        if (ent.placeable) {
+            ent.placeable.SetParent(placeholder, false);
+            ent.SetTemporary(true);
+        }
+    }
+    part2ents[placeholder.name] = ents;
 }
 
-//var vol = me.GetOrCreateComponent("EC_VolumeTrigger");
+loadAll();
