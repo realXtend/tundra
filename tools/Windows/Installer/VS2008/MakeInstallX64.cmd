932528f90663a864e9c2f6e815a82c5864776738
Stinkfist <stinkfist0@gmail.com>
2013-07-12 14:42:17 +0300

    Make passing of arguments to RunCMake.cmd really work. MakeInstall.cmd tweaking.

--- tools/Windows/Installer/VS2008/MakeInstallX64.cmd
+++ tools/Windows/Installer/VS2008/MakeInstallX64.cmd
@@ -1,3 +1,3 @@
 @rem Builds and deploys Tundra x64 using the INSTALL project, so that you can run the NSIS install script.
 @cd ..
-@call MakeInstall.cmd "Visual Studio 9 2008 Win64"
+@call MakeInstall.cmd "Visual Studio 9 2008 Win64" -DINSTALL_BINARIES_ONLY:BOOL=ON
