35e25f70c16614a173fd97006a833bec8248a8be
Stinkfist0 <stinkfist0@gmail.com>
2013-08-22 19:07:59 +0300

    Win.h: define NOGDI and WIN32_LEAN_AND_MEAN before including windows.h in order to reduce number of unuseful inclusions in windows.h.

--- src/Core/TundraCore/Framework/Win.h
+++ src/Core/TundraCore/Framework/Win.h
@@ -28,6 +28,12 @@
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
+#ifndef NOGDI
+#define NOGDI
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 
 #undef Polygon
