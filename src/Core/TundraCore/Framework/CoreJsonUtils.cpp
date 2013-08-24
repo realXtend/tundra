c26fa56470137f895bd18b4978e2f628f526d2c1
Jonne Nauha <jonne.nauha@adminotech.com>
2013-08-24 23:49:28 +0300

    CoreJsonUtils: Add Serializing QVariant and QObject* to QByteArray JSON data, also direct serialization of QVariant to file. Exposed ToObject and ToVariant from QJson that neatly converts QObject properties to a QVariantMap and back.

--- src/Core/TundraCore/Framework/CoreJsonUtils.cpp
+++ src/Core/TundraCore/Framework/CoreJsonUtils.cpp
@@ -5,7 +5,9 @@
 #include "CoreStringUtils.h"
 #include "LoggingFunctions.h"
 
-#include "QJson/Parser"
+#include <QJson/Parser>
+#include <QJson/Serializer>
+#include <QJson/QObjectHelper>
 
 #include <QFile>
 
@@ -52,5 +54,52 @@ namespace TundraJson
     QVariant Value(const QVariantMap &data, const QString &key, const QVariant &defaultValue)
     {
         return data.value(key, defaultValue);
+    }
+
+    bool SerializeToFile(const QString &filePath, const QVariant &variant, IndentMode indentMode)
+    {
+        bool ok = false;
+        QByteArray data = Serialize(variant, indentMode, &ok);
+        if (!ok)
+            return false;
+
+        QFile file(filePath);
+        if (file.open(QFile::WriteOnly))
+        {
+            file.write(data);
+            file.close();
+            return true;
+        }
+        return false;
+    }
+
+    QByteArray Serialize(const QVariant &variant, IndentMode indentMode, bool *ok)
+    {
+        QJson::Serializer serializer;
+        serializer.setIndentMode(static_cast<QJson::IndentMode>(indentMode));
+        QByteArray data = serializer.serialize(variant, ok);
+        if (ok != 0 && *ok == false)
+            LogError(QString("TundraJson::Serialize: %1").arg(serializer.errorMessage()));
+        return data;
+    }
+
+    QByteArray Serialize(const QObject *object, const QStringList &ignoredPropertyNames, IndentMode indentMode, bool *ok)
+    {
+        if (object)
+            return Serialize(ToVariant(object, ignoredPropertyNames), indentMode, ok);
+        return QByteArray();
+    }
+
+    void ToObject(const QVariantMap &variant, QObject *target)
+    {
+        if (target)
+            QJson::QObjectHelper::qvariant2qobject(variant, target);
+    }
+
+    QVariantMap ToVariant(const QObject *object, const QStringList &ignoredPropertyNames)
+    {
+        if (object)
+            return QJson::QObjectHelper::qobject2qvariant(object, ignoredPropertyNames);
+        return QVariantMap();
     }
 }
--- src/Core/TundraCore/Framework/CoreJsonUtils.h
+++ src/Core/TundraCore/Framework/CoreJsonUtils.h
@@ -4,12 +4,65 @@
 
 #include "TundraCoreApi.h"
 
+#include <QObject>
 #include <QString>
 #include <QByteArray>
 #include <QVariant>
 
 namespace TundraJson
 {
+    /// Indentation mode for serialization.
+    /**
+    IndentNone
+    @code
+    { "foo" : 0, "foo1" : 1, "foo2" : [ { "foo3" : 3, "foo4" : 4 } ] }
+    @endcode
+
+    IndentCompact
+    @code
+    {"foo":0,"foo1":1,"foo2":[{"foo3":3,"foo4":4}]}
+    @endcode
+
+    IndentMinimum
+    @code
+    { "foo" : 0, "foo1" : 1, "foo2" : [
+      { "foo3" : 3, "foo4" : 4 }
+    ] }
+    @endcode
+
+    IndentMedium
+    @code
+    {
+     "foo" : 0, "foo1" : 1, "foo2" : [
+      {
+       "foo3" : 3, "foo4" : 4
+      }
+     ]
+    }
+    @endcode
+
+    IndentFull
+    @code
+    {
+     "foo" : 0,
+     "foo1" : 1,
+     "foo2" : [
+      {
+       "foo3" : 3,
+       "foo4" : 4
+      }
+     ]
+    }
+    @endcode */
+    enum IndentMode
+    {
+        IndentNone,
+        IndentCompact,
+        IndentMinimum,
+        IndentMedium,
+        IndentFull
+    };
+
     /// Parse JSON data from a file.
     /** This also function provides ignoring comment lines which can be useful in config files.
         If you are expecting that the file content has comment lines starting with '//' and/or '#' set removeCommentLines to true.
@@ -45,4 +98,38 @@ namespace TundraJson
         @param Default value that will be returned if key cannot be found.
         @return Value as a QVariant. Use its functions to convert to the correct type eg. toInt(), toBool(), toList() and toMap(). */
     QVariant TUNDRACORE_API Value(const QVariantMap &data, const QString &key, const QVariant &defaultValue = QVariant());
+
+    /// Serializes QVariant to JSON and store it to file.
+    /** @param File to store the serialization results.
+        @param QVariant to serialize.
+        @param Indentation mode.
+        @param Boolean that will be set to true if serialization succeeded, otherwise false.
+        @return False if file could not be written to or serialization failed, otherwise true. */
+    bool TUNDRACORE_API SerializeToFile(const QString &filePath, const QVariant &variant, IndentMode indentMode = IndentCompact);
+
+    /// Serializes QVariant to JSON.
+    /** @param QVariant to serialize.
+        @param Indentation mode.
+        @param Boolean that will be set to true if serialization succeeded, otherwise false.
+        @return JSON string data. If .isEmpty() the input was empty, if .isNull() there was a parsing error. */
+    QByteArray TUNDRACORE_API Serialize(const QVariant &variant, IndentMode indentMode = IndentCompact, bool *ok = 0);
+
+    /// Serializes QObject to JSON.
+    /** @param QObject to serialize.
+        @param QObject property names that should be ignored in serialization. eg. "objectName".
+        @param Indentation mode.
+        @param Boolean that will be set to true if serialization succeeded, otherwise false.
+        @return JSON string data. If .isEmpty() the input was empty, if .isNull() there was a parsing error. */
+    QByteArray TUNDRACORE_API Serialize(const QObject *object, const QStringList &ignoredPropertyNames, IndentMode indentMode = IndentCompact, bool *ok = 0);
+
+    /// Converts QVariantMap data to QObject properties.
+    /** @param QVariant input data.
+        @param Target QObject ptr. */
+    void TUNDRACORE_API ToObject(const QVariantMap &variant, QObject *target);
+
+    /// Converts QObject properties to QVariantMap.
+    /** @param QObject to convert.
+        @param QObject property names that should be ignored in serialization. eg. "objectName".
+        @return Data map. */
+    QVariantMap TUNDRACORE_API ToVariant(const QObject *object, const QStringList &ignoredPropertyNames);
 }
