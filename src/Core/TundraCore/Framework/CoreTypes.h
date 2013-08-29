96f39eeb8f105ffeb25b2daef1aa3684e3b5b391
Stinkfist <stinkfist0@gmail.com>
2013-08-29 17:06:46 +0300

    CoreTypes.h: Use kNet/Types.h for fixed-width types instead of duplicating the code.

--- src/Core/TundraCore/Framework/CoreTypes.h
+++ src/Core/TundraCore/Framework/CoreTypes.h
@@ -18,60 +18,8 @@ template <class T> inline bool _finite(const T &f) { return f != std::numeric_li
 template <class T> inline bool _isnan(const T &f) { return f != f; }
 #endif
 
-// If we have C99, take the types from there.
-#if (__STDC_VERSION__ >= 199901L) || (_MSC_VER >= 1600)
-
-#include <cstdint>
-
-typedef uint8_t u8; ///< a single byte: 0-255.
-typedef uint16_t u16; ///< 2 bytes: 0 - 65535.
-typedef uint32_t u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
-typedef uint64_t u64; ///< 8 bytes: 18,446,744,073,709,551,615 ~1.8e19.
-
-typedef int8_t s8; ///< a single byte: -128 - 127.
-typedef int16_t s16; ///< 2 bytes: -32768 - 32767.
-typedef int32_t s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
-typedef int64_t s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
-
-// Otherwise, if we have Boost and its usage is not disabled, we can also pull the types from there.
-#elif !defined(TUNDRA_NO_BOOST)
-
-#include <boost/cstdint.hpp>
-
-typedef boost::uint8_t u8; ///< a single byte: 0-255.
-typedef boost::uint16_t u16; ///< 2 bytes: 0 - 65535.
-typedef boost::uint32_t u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
-typedef boost::uint64_t u64; ///< 8 bytes: 18,446,744,073,709,551,615 ~1.8e19.
-
-typedef boost::int8_t s8; ///< a single byte: -128 - 127.
-typedef boost::int16_t s16; ///< 2 bytes: -32768 - 32767.
-typedef boost::int32_t s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
-typedef boost::int64_t s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
-
-#else // No Boost or unknown if we have C99. Have to guess the following are correct.
-
-#include <limits.h>
-
-//#pragma warning "Not using Boost and C99 not defined. Guessing the built-ins for fixed-width types!"
-
-typedef unsigned char u8; ///< a single byte: 0-255.
-typedef unsigned short u16; ///< 2 bytes: 0 - 65535.
-typedef unsigned long long u64; ///< 8 bytes: 18,446,744,073,709,551,615 ~1.8e19.
-
-typedef signed char s8; ///< a single byte: -128 - 127.
-typedef signed short s16; ///< 2 bytes: -32768 - 32767.
-
-#if ULONG_MAX == 0xffffffff
-typedef unsigned long u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
-typedef long s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
-#elif UINT_MAX == 0xffffffff
-typedef unsigned int u32; ///< 4 bytes: 0 - 4,294,967,295 ~ 4000 million or 4e9.
-typedef int s32; ///< 4 bytes signed: max 2,147,483,647 ~ 2000 million or 2e9.
-#endif
-
-typedef signed long long s64; ///< 8 bytes signed. 9,223,372,036,854,775,807 ~ 9e18.
-
-#endif
+// Use kNet/Types.h for fixed-width types instead of duplicating the code here.
+#include <kNet/Types.h>
 
 // Floating-point types
 typedef float f32;
