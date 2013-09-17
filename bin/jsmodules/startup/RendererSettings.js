95acb65844e6554006d0c351717e8b8af06765a9
Stinkfist0 <stinkfist0@gmail.com>
2013-09-18 00:59:50 +0300

    Rename UI files in bin/data/ui consistently (upper camel case) cont'd.

--- bin/jsmodules/startup/RendererSettings.js
+++ bin/jsmodules/startup/RendererSettings.js
@@ -72,7 +72,7 @@ if (!framework.IsHeadless())
         settingsWidget = new QWidget(ui.MainWindow());
         settingsWidget.setWindowFlags(Qt.Tool);
         settingsWidget.setLayout(new QVBoxLayout());
-        var child = ui.LoadFromFile(application.installationDirectory + "data/ui/_RendererSettings.ui", false);
+        var child = ui.LoadFromFile(application.installationDirectory + "data/ui/RendererSettings.ui", false);
         child.setParent(settingsWidget);
         settingsWidget.layout().addWidget(child, 0, 0);
 
--- bin/jsmodules/startup/SoundSettings.js
+++ bin/jsmodules/startup/SoundSettings.js
@@ -60,7 +60,7 @@ if (!framework.IsHeadless())
         settingsWidget = new QWidget(ui.MainWindow());
         settingsWidget.setWindowFlags(Qt.Tool);
         settingsWidget.setLayout(new QVBoxLayout());
-        var child = ui.LoadFromFile(application.installationDirectory + "data/ui/_SoundSettings.ui", false);
+        var child = ui.LoadFromFile(application.installationDirectory + "data/ui/SoundSettings.ui", false);
         child.setParent(settingsWidget);
         settingsWidget.layout().addWidget(child, 0, 0);
 
